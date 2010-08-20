bdcaf40b198a33b235dfee02a43b490bb2c56759
Beans0063 <Beans0063@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2010-08-20 00:45:53 +0000

    fix db
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@19 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- dialer-dameon/config/post-daemonize/database_setup.rb
+++ dialer-dameon/config/post-daemonize/database_setup.rb
@@ -3,12 +3,13 @@
 DaemonKit.logger.info "Setting up ActiveRecord Connection" 
 if DaemonKit.env=="development"
   database_yaml = IO.read('/Volumes/MacHD/Users/blevine/dev/impact_dialing/config/database.yml')
+  databases = YAML::load(database_yaml)
   ActiveRecord::Base.establish_connection(databases["development"])
 else
   database_yaml = IO.read('/var/www/html/trunk/config/database.yml')
+  databases = YAML::load(database_yaml)
   ActiveRecord::Base.establish_connection(databases["production"])
 end
-databases = YAML::load(database_yaml)
 
 #memcached
 CACHE = MemCache.new 'localhost:11211'
