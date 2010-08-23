28970a6e818ada05dd8b9ac50468361243d41eb2
Beans0063 <Beans0063@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2010-08-23 01:07:58 +0000

    memcahced fix
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@40 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- dialer-dameon/config/post-daemonize/database_setup.rb
+++ dialer-dameon/config/post-daemonize/database_setup.rb
@@ -12,4 +12,27 @@ else
 end
 
 #memcached
-CACHE = MemCache.new 'localhost:11211'
+#CACHE = MemCache.new 'localhost:11211'
+
+memcache_options = {
+  :c_threshold => 10000,
+  :compression => true,
+  :debug => false,
+  :namespace => 'some_ns',
+  :readonly => false,
+  :urlencode => false
+}
+ 
+CACHE = MemCache.new memcache_options
+CACHE.servers = '127.0.0.1:11211'
+begin
+   PhusionPassenger.on_event(:starting_worker_process) do |forked|
+     if forked
+       # We're in smart spawning mode, so...
+       # Close duplicated memcached connections - they will open themselves
+       CACHE.reset
+     end
+   end
+# In case you're not running under Passenger (i.e. devmode with mongrel)
+rescue NameError => error
+end
