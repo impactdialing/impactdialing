03ea47ea77581cf6729d768caaecf3d85ef14e0c
srushti <srushti@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-08 04:09:30 +0000

    broke something in the campaigns_controller while trying to refactor (this is what i end up breaking when i don't write specs)
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@310 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class CampaignsController < AdminController
     def index
-      @entities = type.by_updated.paginate(:per_page => Campaign.per_page, :page => params[:page])
+      @campaigns = Campaign.by_updated.paginate(:per_page => Campaign.per_page, :page => params[:page])
     end
 
     def restore
