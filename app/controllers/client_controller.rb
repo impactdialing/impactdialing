8d5615eeec9edac9bede8fb9f8eb9e98c9a4cb8e
michaelrkn <michaelrkn@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-08 17:25:40 +0000

    updated text for caller id and funding account
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@311 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -612,7 +612,7 @@ Do you want to buy a widget?"
 
     #    @campaign.check_valid_caller_id_and_save
     #    flash.now[:error]="Your Campaign Caller ID is not verified."  if !@campaign.caller_id.blank? && !@campaign.caller_id_verified
-    flash.now[:error]="You must enter and verify a Caller ID before you can make calls."  if @campaign.caller_id.blank?
+    flash.now[:error]="When Impact Dialing makes a call, it needs a phone number to use for the Caller ID. Enter the phone number you want to use for your Caller ID and click Verify. To prevent abuse, Impact Dialing will call that number and ask you to enter a validation code that will appear on your screen. Until you do this, you can't make calls with this campaign."  if @campaign.caller_id.blank?
     @isAdmin = @user.admin
     @show_voter_buttons = @user.show_voter_buttons
 #    render :layout=>"campaign_view"
