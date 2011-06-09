dd80f8f9420c5a0fab937d618e8a3dcae6ae2eb4
kaimiddleton <kaimiddleton@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-09 02:42:50 +0000

    Added a test to assert that non-logged-in users are redirected to the login page
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@315 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- spec/controllers/client_controller_spec.rb
+++ spec/controllers/client_controller_spec.rb
@@ -3,51 +3,61 @@ require "spec_helper"
 describe ClientController do
   include ActionController::TestProcess
 
-  before :each do
-    login_as Factory(:user)
+  describe "when not logged in" do
+    it "redirects to the login page" do
+      get "/client"
+      response.should redirect_to "/client/login"
+    end
   end
 
-  describe 'new campaign' do
-    it "creates a new campaign & redirects to campaign view" do
-      lambda {
-        get :campaign_new
-      }.should change(Campaign, :count).by(1)
-      response.should redirect_to "/client/campaign_view/#{Campaign.last.id}"
+
+  describe "when logged in" do
+    before :each do
+      login_as Factory(:user)
     end
 
-    describe "upload voters list" do
-      let(:csv_file_upload) { {"datafile" => fixture_file_upload("files/voters_list.csv")} }
-      before :each do
-        @campaign = Factory(:campaign, :user_id => session[:user])
-      end
-
-      it "needs an uploaded file" do
-        post :voter_upload, :id => @campaign.id
-        flash[:error].should == "You must select a file to upload"
-      end
-
-      it "needs a list name" do
-        post :voter_upload, :id => @campaign.id,
-          :upload            => csv_file_upload
-        flash[:error].should include "Name can't be blank"
-      end
-
-      it "creates a voter list entry" do
+    describe 'new campaign' do
+      it "creates a new campaign & redirects to campaign view" do
         lambda {
-          Campaign.should_receive(:find_by_id_and_user_id).
-          with(@campaign.id.to_s, session[:user]).
-          and_return(@campaign)
+          get :campaign_new
+        }.should change(Campaign, :count).by(1)
+        response.should redirect_to "/client/campaign_view/#{Campaign.last.id}"
+      end
 
-          @campaign.should_receive(:voter_upload).with(
-            csv_file_upload, session[:user], ",", anything()
-          )
-          post :voter_upload,
-            :id        => @campaign.id,
-            :upload    => csv_file_upload,
-            :list_name => "foobar"
-          flash[:error].should be_blank
-          response.code.should == "200"
-        }.should change(VoterList, :count)
+      describe "upload voters list" do
+        let(:csv_file_upload) { {"datafile" => fixture_file_upload("files/voters_list.csv")} }
+        before :each do
+          @campaign = Factory(:campaign, :user_id => session[:user])
+        end
+
+        it "needs an uploaded file" do
+          post :voter_upload, :id => @campaign.id
+          flash[:error].should == "You must select a file to upload"
+        end
+
+        it "needs a list name" do
+          post :voter_upload, :id => @campaign.id,
+               :upload => csv_file_upload
+          flash[:error].should include "Name can't be blank"
+        end
+
+        it "creates a voter list entry" do
+          lambda {
+            Campaign.should_receive(:find_by_id_and_user_id).
+                with(@campaign.id.to_s, session[:user]).
+                and_return(@campaign)
+
+            @campaign.should_receive(:voter_upload).with(
+                csv_file_upload, session[:user], ",", anything()
+            )
+            post :voter_upload,
+                 :id => @campaign.id,
+                 :upload => csv_file_upload,
+                 :list_name => "foobar"
+            flash[:error].should be_blank
+            response.code.should == "200"
+          }.should change(VoterList, :count)
+        end
       end
     end
   end
