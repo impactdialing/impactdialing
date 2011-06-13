bee99a8ccfef6a9859f0049c53f137e8fdb9d427
srushti <srushti@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-13 08:36:47 +0000

    added a staging environment and a capistrano task
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@349 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,7 @@
 require "bundler/capistrano"
 
-server_name = "ec2-174-129-172-31.compute-1.amazonaws.com"
 repository = "https://impact-dialing.svn.beanstalkapp.com/webapp/trunk"
 set :application, "impactdialing"
-set :server_name, server_name
 set :user, "rails"
 set :scm, :subversion
 set :scm_username, "srushti"
@@ -15,10 +13,6 @@ set :use_sudo, false
 set :deploy_via, :export
 set :deploy_to, "/var/www/rails/#{application}"
 set :chmod755, "app config db lib public vendor script script/* public/ disp*"
-set :rails_env, 'production'
-role :web, 'stagingimpactdialing.com'
-role :app, 'stagingimpactdialing.com'
-role :db, 'stagingimpactdialing.com'
 
 namespace :deploy do
   task :restart, :roles => :app do
@@ -30,4 +24,17 @@ namespace :deploy do
   task :link_configuration, :roles => :app do
     run "ln -s #{deploy_to}/shared/config/database.yml #{current_path}/config/database.yml"
   end
+end
+
+task :production do
+  set :server_name, "domU-12-31-39-09-65-F3.compute-1.internal"
+  set :rails_env, 'production'
+end
+
+task :staging do
+  set :server_name, "ec2-174-129-172-31.compute-1.amazonaws.com"
+  set :rails_env, 'production'
+  role :web, 'staging.impactdialing.com'
+  role :app, 'staging.impactdialing.com'
+  role :db, 'staging.impactdialing.com'
 end
---
+++ config/environments/staging.rb
@@ -0,0 +1,69 @@
+TWILIO_ACCOUNT="AC422d17e57a30598f8120ee67feae29cd"
+TWILIO_AUTH="897298ab9f34357f651895a7011e1631"
+APP_NUMBER="4157020991"
+APP_URL="http://admin.impactdialing.com"
+TEST_CALLER_NUMBER="8583679749"
+TEST_VOTER_NUMBER="4154486970"
+
+# Settings specified here will take precedence over those in config/environment.rb
+
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
+
+# See everything in the log (default is :info)
+config.log_level = :debug
+
+config.after_initialize do
+#  ActiveMerchant::Billing::Base.mode = :test
+  ActiveMerchant::Billing::LinkpointGateway.pem_file  = File.read(RAILS_ROOT + '/1359891.pem')
+  ::BILLING_GW = gateway = ActiveMerchant::Billing::LinkpointGateway.new(
+     :login => "1359891"  
+   )
+end
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host = "http://assets.example.com"
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+# Enable threaded mode
+# config.threadsafe!
+
+
+# memcache_options = {
+#   :c_threshold => 10000,
+#   :compression => true,
+#   :debug => false,
+#   :namespace => 'some_ns',
+#   :readonly => false,
+#   :urlencode => false
+# }
+#  
+# CACHE = MemCache.new memcache_options
+# #CACHE.servers = '127.0.0.1:11211'
+# CACHE.servers = 'domU-12-31-39-10-89-26.compute-1.internal:11211'
+# 
+# begin
+#    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+#      if forked
+#        # We're in smart spawning mode, so...
+#        # Close duplicated memcached connections - they will open themselves
+#        CACHE.reset
+#      end
+#    end
+# # In case you're not running under Passenger (i.e. devmode with mongrel)
+# rescue NameError => error
+# end
