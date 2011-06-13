7db7c8123aa6ae27b456516a9aab3fd05445e738
srushti <srushti@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-13 08:09:11 +0000

    moved the capistrano stuff to the new vanilla ubuntu box
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@343 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 require "bundler/capistrano"
 
-server_name = "ec2-50-19-57-159.compute-1.amazonaws.com"
+server_name = "ec2-174-129-172-31.compute-1.amazonaws.com"
 repository = "https://impact-dialing.svn.beanstalkapp.com/webapp/trunk"
 set :application, "impactdialing"
 set :server_name, server_name
-set :user, "root"
+set :user, "rails"
 set :scm, :subversion
 set :scm_username, "srushti"
 set :scm_auth_cache, true
@@ -16,7 +16,18 @@ set :deploy_via, :export
 set :deploy_to, "/var/www/rails/#{application}"
 set :chmod755, "app config db lib public vendor script script/* public/ disp*"
 set :rails_env, 'production'
-role :web, 'staging.impactdialing.com'
-role :app, 'staging.impactdialing.com'
-role :db, 'staging.impactdialing.com'
+role :web, 'stagingimpactdialing.com'
+role :app, 'stagingimpactdialing.com'
+role :db, 'stagingimpactdialing.com'
 
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+
+  after('deploy:symlink', 'deploy:link_configuration')
+
+  task :link_configuration, :roles => :app do
+    run "ln -s #{deploy_to}/shared/config/database.yml #{current_path}/config/database.yml"
+  end
+end
