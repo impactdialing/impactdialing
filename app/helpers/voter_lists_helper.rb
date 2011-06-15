4e942fe57f6b03d987deb55b831bd89f9d2479ac
jasim <jasim@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-15 19:11:25 +0000

    automatically map csv fields with ID to DWID
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@380 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- app/helpers/voter_lists_helper.rb
+++ app/helpers/voter_lists_helper.rb
@@ -1,8 +1,10 @@
 module VoterListsHelper
   def matching_system_header_for(csv_header)
     normalized_csv_header = csv_header.upcase.gsub(' ', '')
-    VoterList::VOTER_DATA_COLUMNS.find do |system_header|
+    match = VoterList::VOTER_DATA_COLUMNS.find do |system_header|
       system_header.upcase.gsub(' ', '') == normalized_csv_header
     end
+    match ||= ('DWID' if normalized_csv_header.include? 'ID')
+    match
   end
 end
