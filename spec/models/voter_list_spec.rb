3bdccf63a336c524660ef1279c1ee005c65fe309
jasim <jasim@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-16 15:53:45 +0000

    voter list spec : grouped all import tests together
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@383 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- spec/models/voter_list_spec.rb
+++ spec/models/voter_list_spec.rb
@@ -26,82 +26,84 @@ describe VoterList do
     let(:campaign) { Factory(:campaign, :user => user) }
     let(:voter_list) { Factory(:voter_list, :campaign => campaign, :user_id => user.id) }
 
-    before :each do
-      Voter.destroy_all
-      @result = voter_list.import_leads(
-          MAPPINGS,
-          csv_file_upload,
-          ",")
-    end
+    describe "import from csv" do
+      before :each do
+        Voter.destroy_all
+        @result = voter_list.import_leads(
+            USER_MAPPINGS,
+            csv_file_upload,
+            ",")
+      end
 
-    it "should be successful" do
-      @result.should == {
-          :successCount => 2,
-          :failedCount  => 0
-      }
-    end
+      it "should be successful" do
+        @result.should == {
+            :successCount => 2,
+            :failedCount  => 0
+        }
+      end
 
-    it "should parse it and save to the voters list table" do
-      Voter.count.should == 1
+      it "should parse it and save to the voters list table" do
+        Voter.count.should == 1
 
-      voter = Voter.first
-      voter.campaign_id.should == campaign.id
-      voter.user_id.should == user.id
-      voter.voter_list_id.should == voter_list.id
+        voter = Voter.first
+        voter.campaign_id.should == campaign.id
+        voter.user_id.should == user.id
+        voter.voter_list_id.should == voter_list.id
 
-      # check some values from the csv fixture
-      voter.Phone.should == "1234567895"
-      voter.FirstName.should == "Foo"
-      voter.LastName.should == "Bar"
-      voter.Email.should == "foo@bar.com"
-      voter.MiddleName.should be_blank
-      voter.Suffix.should be_blank
-    end
+        # check some values from the csv fixture
+        voter.Phone.should == "1234567895"
+        voter.FirstName.should == "Foo"
+        voter.LastName.should == "Bar"
+        voter.Email.should == "foo@bar.com"
+        voter.MiddleName.should be_blank
+        voter.Suffix.should be_blank
+      end
 
-    it "should add a family member when two voters in the same voters list have same phone number" do
-      Family.count.should == 1
-      Voter.first.num_family.should == 2
+      it "should add a family member when two voters in the same voters list have same phone number" do
+        Family.count.should == 1
+        Voter.first.num_family.should == 2
 
-      family_member = Family.first
-      family_member.campaign_id.should == campaign.id
-      family_member.user_id.should == user.id
-      family_member.voter_list_id.should == voter_list.id
+        family_member = Family.first
+        family_member.campaign_id.should == campaign.id
+        family_member.user_id.should == user.id
+        family_member.voter_list_id.should == voter_list.id
 
-      # check some values from the csv fixture
-      family_member.Phone.should == "1234567895"
-      family_member.FirstName.should == "Chocolate"
-      family_member.LastName.should == "Bar"
-      family_member.Email.should == "choco@bar.com"
-      family_member.MiddleName.should be_blank
-      family_member.Suffix.should be_blank
-    end
-    it "should ignore the same phone is repeated in another voters list for the same campaign" do
-      another_voter_list = Factory(:voter_list, :campaign => campaign, :user_id => user.id)
-      another_voter_list.import_leads(
-          MAPPINGS,
-          csv_file_upload,
-          ",").should ==
-          {
-              :successCount => 0,
-              :failedCount  => 2
-          }
-    end
-    it "should add even if the same phone is repeated in a different campaign" do
-      another_voter_list = Factory(:voter_list,
-                                   :campaign => Factory(:campaign, :user => user),
-                                   :user_id => user.id)
-      another_voter_list.import_leads(
-          MAPPINGS,
-          csv_file_upload,
-          ",").should ==
-          {
-              :successCount => 2,
-              :failedCount  => 0
-          }
-    end
+        # check some values from the csv fixture
+        family_member.Phone.should == "1234567895"
+        family_member.FirstName.should == "Chocolate"
+        family_member.LastName.should == "Bar"
+        family_member.Email.should == "choco@bar.com"
+        family_member.MiddleName.should be_blank
+        family_member.Suffix.should be_blank
+      end
+      it "should ignore the same phone is repeated in another voters list for the same campaign" do
+        another_voter_list = Factory(:voter_list, :campaign => campaign, :user_id => user.id)
+        another_voter_list.import_leads(
+            USER_MAPPINGS,
+            csv_file_upload,
+            ",").should ==
+            {
+                :successCount => 0,
+                :failedCount  => 2
+            }
+      end
+      it "should add even if the same phone is repeated in a different campaign" do
+        another_voter_list = Factory(:voter_list,
+                                     :campaign => Factory(:campaign, :user => user),
+                                     :user_id  => user.id)
+        another_voter_list.import_leads(
+            USER_MAPPINGS,
+            csv_file_upload,
+            ",").should ==
+            {
+                :successCount => 2,
+                :failedCount  => 0
+            }
+      end
 
-    it "should update only DWID as the CustomId if both DWID and VAN ID are present" do
-      Voter.first.CustomID.should == "987"
+      it "should update only DWID as the CustomId if both DWID and VAN ID are present" do
+        Voter.first.CustomID.should == "987"
+      end
     end
   end
 end
