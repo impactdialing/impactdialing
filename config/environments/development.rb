7eaf2739e0344d71a5682a24101eac997c617f0f
srushti <srushti@2f5aa9f8-122e-46c4-b90d-7e846bb7426a>
2011-06-21 06:39:38 +0000

    made sure mails don't get sent during development
    git-svn-id: https://impact-dialing.svn.beanstalkapp.com/webapp/trunk@399 2f5aa9f8-122e-46c4-b90d-7e846bb7426a

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,9 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = false
+config.action_mailer.delivery_method = :test
 
 config.after_initialize do
   ActiveMerchant::Billing::Base.mode = :test
