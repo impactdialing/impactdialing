79dc6fa0ae3133d8298aad2906348b18d7825031
Jasim A Basheer <jasim@c42.in>
2011-06-26 14:08:43 +0530

    named scope: on_call for caller sessions

---
+++ spec/models/caller_session_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe CallerSession do
+  it "lists active calls" do
+    call1 = Factory(:caller_session, :on_call=> true)
+    call2 = Factory(:caller_session, :on_call=> false)
+    call3 = Factory(:caller_session, :on_call=> true)
+    CallerSession.on_call.all.should =~ [call1, call3]
+  end
+end
