75411b91324e4e4afb7fecc25f3256e581d8644a
Jasim A Basheer <jasim@c42.in>
2011-06-26 07:52:22 +0530

    voters list import handles invalid csv files gracefully

--- spec/controllers/voter_lists_controller_spec.rb
+++ spec/controllers/voter_lists_controller_spec.rb
@@ -10,7 +10,7 @@ describe VoterListsController do
   end
 
   describe "voters list" do
-    let(:csv_file_upload) { {"datafile" => fixture_file_upload("files/voters_list.csv")} }
+    let(:csv_file_upload) { {"datafile" => fixture_file_upload("files/valid_voters_list.csv")} }
     before :each do
       @campaign = Factory(:campaign, :user_id => session[:user])
     end
@@ -21,69 +21,83 @@ describe VoterListsController do
     end
 
     describe "create voter list" do
-      before :each do
-        session[:voters_list_upload] = nil
-        post :create,
-             :campaign_id => @campaign.id,
-             :upload      => csv_file_upload
-      end
-      it "sets the session to the new voter list entry" do
-        session[:voters_list_upload].should_not be_empty
-      end
-      it "saves the uploaded csv" do
-        File.should exist("#{Rails.root}/tmp/#{session[:voters_list_upload]['filename']}")
-      end
-
-      it "renders the mappings screen" do
-        flash[:error].should be_blank
-        response.code.should == "200"
-        response.should render_template("column_mapping")
+      def import(parameters={})
+        defaults = {
+            :separator               => ",",
+            :voter_list_name         => "voter list name",
+            :json_csv_column_headers => ["Phone", "LAST"].to_json,
+            :campaign_id             => @campaign.id,
+            :csv_to_system_map       => {
+                "Phone" => "Phone",
+                "LAST"  =>"LastName"
+            }
+        }
+        post :import, defaults.merge(parameters)
       end
 
-      describe "import" do
-        def import(parameters={})
-          defaults = {
-              :separator               => ",",
-              :voter_list_name         => "voter list name",
-              :json_csv_column_headers => ["Phone", "LAST"].to_json,
-              :campaign_id             => @campaign.id,
-              :csv_to_system_map       => {
-                  "Phone" => "Phone",
-                  "LAST"  =>"LastName"
-              }
-          }
-          post :import, defaults.merge(parameters)
+      describe "valid csv file" do
+        before :each do
+          session[:voters_list_upload] = nil
+          post :create,
+               :campaign_id => @campaign.id,
+               :upload      => csv_file_upload
+        end
+        it "sets the session to the new voter list entry" do
+          session[:voters_list_upload].should_not be_empty
+        end
+        it "saves the uploaded csv" do
+          File.should exist("#{Rails.root}/tmp/#{session[:voters_list_upload]['filename']}")
         end
 
-        describe "requirements" do
-          it "needs a list name" do
-            import :voter_list_name => ""
-            response.flash.now[:error].first.should include "Name can't be blank"
-            response.should render_template "column_mapping"
-          end
-
-          it "should not save a list if the user already has a list with the same name" do
-            Factory(:voter_list, :user_id => @current_user.id, :campaign_id => @campaign.id, :name => "abcd")
-            import :voter_list_name   => "abcd"
-            response.flash.now[:error].first.should include "Name for this voter list is already taken"
-            response.should render_template "column_mapping"
-          end
+        it "renders the mappings screen" do
+          flash[:error].should be_blank
+          response.code.should == "200"
+          response.should render_template("column_mapping")
         end
 
-        describe "after import" do
-          it "saves all the voters in the csv according to the mappings" do
-            Voter.delete_all
-            import
-            Voter.count.should == 1
-            Voter.first.Phone.should == "1234567895"
-            Voter.first.LastName.should == "Bar"
+        describe "import" do
+          describe "requirements" do
+            it "needs a list name" do
+              import :voter_list_name => ""
+              response.flash.now[:error].first.should include "Name can't be blank"
+              response.should render_template "column_mapping"
+            end
+
+            it "should not save a list if the user already has a list with the same name" do
+              Factory(:voter_list, :user_id => @current_user.id, :campaign_id => @campaign.id, :name => "abcd")
+              import :voter_list_name => "abcd"
+              response.flash.now[:error].first.should include "Name for this voter list is already taken"
+              response.should render_template "column_mapping"
+            end
           end
-          it "removes the temporary file from disk" do
-            temp_filename = "#{Rails.root}/tmp/#{session[:voters_list_upload]['filename']}"
-            import
-            File.should_not exist(temp_filename)
-            session[:voters_list_upload].should be_blank
+
+          describe "after import" do
+            it "saves all the voters in the csv according to the mappings" do
+              Voter.delete_all
+              import
+              Voter.count.should == 1
+              Voter.first.Phone.should == "1234567895"
+              Voter.first.LastName.should == "Bar"
+            end
+            it "removes the temporary file from disk" do
+              temp_filename = "#{Rails.root}/tmp/#{session[:voters_list_upload]['filename']}"
+              import
+              File.should_not exist(temp_filename)
+              session[:voters_list_upload].should be_blank
+            end
           end
+        end
+      end
+
+      describe "malformed csv file" do
+        it "should flash an error" do
+          session[:voters_list_upload] = nil
+          post :create,
+               :campaign_id => @campaign.id,
+               :upload      => {"datafile" => fixture_file_upload("files/invalid_voters_list.csv")}
+          import
+          flash[:error].join.should include "Invalid CSV file"
+          response.code.should == "302"
         end
       end
     end
