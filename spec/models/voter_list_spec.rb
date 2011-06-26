a324f845bacb92c59313fdcb691db65e1a04a195
Jasim A Basheer <jasim@c42.in>
2011-06-26 08:29:38 +0530

    fixed build

--- spec/models/voter_list_spec.rb
+++ spec/models/voter_list_spec.rb
@@ -6,9 +6,9 @@ describe VoterList do
   describe "upload voters list" do
     let(:csv_file_upload) {
       fixture_path  = ActionController::TestCase.fixture_path
-      source_file   = "#{fixture_path}files/voters_list.csv"
+      source_file   = "#{fixture_path}files/valid_voters_list.csv"
       temp_dir      = "#{fixture_path}test_tmp"
-      temp_filename = "#{temp_dir}/voters_list.csv"
+      temp_filename = "#{temp_dir}/valid_voters_list.csv"
       FileUtils.cp source_file, temp_filename
       temp_filename
     }
