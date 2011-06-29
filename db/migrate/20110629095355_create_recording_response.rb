7662ccc714924aaf87f64e81bcf5108fe662add7
Aninda Kundu <spacefugitive@gmail.com>
2011-06-29 16:35:07 +0530

    added nested responses for recording

---
+++ db/migrate/20110629095355_create_recording_response.rb
@@ -0,0 +1,13 @@
+class CreateRecordingResponse < ActiveRecord::Migration
+  def self.up
+    create_table :recording_responses do |t|
+      t.integer :recording_id
+      t.string :response
+      t.integer :keypad
+    end
+  end
+
+  def self.down
+    drop_table :recording_responses
+  end
+end
