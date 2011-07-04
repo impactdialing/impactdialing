55ad899f171b42be49ca575bc8991ebeb5c63d4c
Srushti Ambekallu <srushti@c42.in>
2011-07-04 10:42:48 +0530

    created new actions for fallback & status callback

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,15 @@
 class TwilioController < ApplicationController
   def callback
-    puts 11111, params, 22222
+    render :xml => Twilio::Verb.play(URI.escape('http://impactdialingapp.s3.amazonaws.com/development/uploads/1/6MontyPython.wav'))
+  end
+
+  def report_error
+    puts "Error", params
+    render :text => ''
+  end
+
+  def call_ended
+    puts "Call Ended", params
+    render :text => ''
   end
 end
--- lib/dialer.rb
+++ lib/dialer.rb
@@ -7,8 +7,10 @@ module Dialer
     def dial
       Twilio.default_options[:ssl_ca_file] = File.join(RAILS_ROOT, 'cacert.pem')
       Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
-      callback_url = "http://4bbe.localtunnel.com/twilio_callback"
-      puts Twilio::Call.make('+14155130242', '+14155130242', "#{callback_url}?url=true", 'Timeout' => '20', 'FallbackUrl' => "#{callback_url}?fallback=1", 'StatusCallback' => "#{callback_url}?status_callback=1")
+      callback_url = "http://3wuy.localtunnel.com/twilio_callback"
+      fallback_url = "http://3wuy.localtunnel.com/twilio_report_error"
+      callended_url = "http://3wuy.localtunnel.com/twilio_call_ended"
+      puts Twilio::Call.make(nil, '+14155130242', callback_url, 'Timeout' => '20', 'FallbackUrl' => fallback_url, 'StatusCallback' => callended_url)
     end
   end
 end
