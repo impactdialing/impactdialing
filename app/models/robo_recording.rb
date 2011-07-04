3359a9db2787fc5c83069544dc0153dbb065e4a3
Aninda Kundu <spacefugitive@gmail.com>
2011-07-04 13:09:17 +0530

    added mime type for s3 storage.

--- app/models/robo_recording.rb
+++ app/models/robo_recording.rb
@@ -6,6 +6,12 @@ class RoboRecording < ActiveRecord::Base
   belongs_to :script
   has_many :recording_responses
   accepts_nested_attributes_for :recording_responses, :allow_destroy => true
+  before_post_process :set_content_type
 
   validates_presence_of :name, :message => "Please name your recording."
+
+  def set_content_type
+    self.file.instance_write(:content_type,MIME::Types.type_for(self.file_file_name).to_s)
+  end
+
 end
