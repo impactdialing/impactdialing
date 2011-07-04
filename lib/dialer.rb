b994e79b803bd428bd224ddfa45671e2f44f20a1
Aninda Kundu <spacefugitive@gmail.com>
2011-07-04 13:09:44 +0530

    called and played a song.

--- lib/dialer.rb
+++ lib/dialer.rb
@@ -7,10 +7,10 @@ module Dialer
     def dial
       Twilio.default_options[:ssl_ca_file] = File.join(RAILS_ROOT, 'cacert.pem')
       Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
-      callback_url = "http://3wuy.localtunnel.com/twilio_callback"
-      fallback_url = "http://3wuy.localtunnel.com/twilio_report_error"
-      callended_url = "http://3wuy.localtunnel.com/twilio_call_ended"
-      puts Twilio::Call.make(nil, '+14155130242', callback_url, 'Timeout' => '20', 'FallbackUrl' => fallback_url, 'StatusCallback' => callended_url)
+      callback_url = "http://3vhu.localtunnel.com/twilio_callback"
+      fallback_url = "http://3vhu.localtunnel.com/twilio_report_error"
+      callended_url = "http://3vhu.localtunnel.com/twilio_call_ended"
+      puts Twilio::Call.make('+14155130242', '+14155130242', callback_url, 'Timeout' => '20', 'FallbackUrl' => fallback_url, 'StatusCallback' => callended_url)
     end
   end
 end
