1f98b6c84ccb4e98f6863660960080e1404b34ea
Aninda Kundu <spacefugitive@gmail.com>
2011-07-05 17:20:59 +0530

    Can play multiple robo recordings and record responses.

--- app/controllers/call_attempts_controller.rb
+++ app/controllers/call_attempts_controller.rb
@@ -1,5 +1,24 @@
 class CallAttemptsController < ApplicationController
   def create
-    puts "=-======================================= I got a create! #{params.inspect}\n\n\n"
+    robo_recording = RoboRecording.find(params[:robo_recording_id])
+    recording_response = robo_recording.response_for(params[:Digits])
+    call_attempt = CallAttempt.find(params[:id])
+    xml =
+        if recording_response
+          call_attempt.call_responses.create!(:response => params[:Digits], :recording_response => recording_response)
+          next_robo_recording = robo_recording.next
+          if next_robo_recording
+            next_robo_recording.twilio_xml(call_attempt)
+          else
+            Twilio::Verb.new { |v| v.hangup }.response
+          end
+        else
+          invalid_response = Twilio::Verb.new do |v|
+            v.say "You pressed something invalid. Hanging up."
+            v.hangup
+          end
+          invalid_response.response
+        end
+    render :xml => xml
   end
 end
