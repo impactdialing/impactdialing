490084872aaeb225d8fd8194133e97befe0c32c9
Srushti Ambekallu <srushti@c42.in>
2011-07-07 11:47:12 +0530

    added the ability to pass options (like :class) to link_to_add_fields & link_to_remove_fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -78,16 +78,16 @@ module ApplicationHelper
     end
   end
 
-  def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  def link_to_remove_fields(name, f, opts = {})
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", opts)
   end
 
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, opts = {})
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
+    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"), opts)
   end
 
   def button_tag(value, opts = {})
