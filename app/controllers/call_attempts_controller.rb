ca4fb31f777e2e53fa5314724fd6903abccb3abf
Aninda Kundu <spacefugitive@gmail.com>
2011-07-13 14:49:19 +0530

    Digits and removed unwanted funtion

--- app/controllers/call_attempts_controller.rb
+++ app/controllers/call_attempts_controller.rb
@@ -2,15 +2,11 @@ class CallAttemptsController < ApplicationController
   def create
     call_attempt = CallAttempt.find(params[:id])
     robo_recording = RoboRecording.find(params[:robo_recording_id])
-    call_response = CallResponse.log_response(call_attempt, robo_recording, params[:Digit])
+    call_response = CallResponse.log_response(call_attempt, robo_recording, params[:Digits])
     xml = call_attempt.next_recording(robo_recording, call_response)
 
     logger.info "[dialer] DTMF input received. call_attempt: #{params[:id]} keypad: #{params[:Digits]} Response: #{xml}"
     render :xml => xml
   end
 
-  private
-  def hangup
-
-  end
 end
