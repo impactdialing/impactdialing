115c10638f3c9c97301e898e55a9de23ed2d53d0
Michael Kaiser-Nyman <michael@impactdialing.com>
2011-07-13 10:39:47 -0700

    updated a couple error messages

--- app/models/recording_response.rb
+++ app/models/recording_response.rb
@@ -1,6 +1,6 @@
 class RecordingResponse < ActiveRecord::Base
   belongs_to :robo_recording
-  validates_presence_of :response, :message => "Please specify a response"
-  validates_presence_of :keypad, :message => "Please specify a keypad value"
-  validates_uniqueness_of :keypad, :scope => :robo_recording_id, :message=>"Recording must have unique keypad response."
+  validates_presence_of :response, :message => "Each response must have a label."
+  validates_presence_of :keypad, :message => "Each response must have a keypad number."
+  validates_uniqueness_of :keypad, :scope => :robo_recording_id, :message=>"For each recording, you can only use each keypad number once."
 end
