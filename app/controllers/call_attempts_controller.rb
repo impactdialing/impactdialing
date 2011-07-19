6d01dce3ee6bea0beae178519ca95ae4a9b5f8d6
Aninda Kundu <spacefugitive@gmail.com>
2011-07-19 15:21:35 +0530

    removed TWILIO_LOG messages from the controllers

--- app/controllers/call_attempts_controller.rb
+++ app/controllers/call_attempts_controller.rb
@@ -5,8 +5,8 @@ class CallAttemptsController < ApplicationController
     call_response = CallResponse.log_response(call_attempt, robo_recording, params[:Digits])
     xml = call_attempt.next_recording(robo_recording, call_response)
 
-    TWILIO_LOG.info "#{call_attempt.voter.Phone} : DTMF input received : #{ params[:Digits] } for recording : #{robo_recording.name}"
-    TWILIO_LOG.info "Responding with : #{xml}"
+    #TWILIO_LOG.info "#{call_attempt.voter.Phone} : DTMF input received : #{ params[:Digits] } for recording : #{robo_recording.name}"
+    #TWILIO_LOG.info "Responding with : #{xml}"
 
     logger.info "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     logger.info "[dialer] DTMF input received. call_attempt: #{params[:id]} keypad: #{params[:Digits]} Response: #{xml}"
