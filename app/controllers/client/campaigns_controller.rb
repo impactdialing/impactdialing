64aa13f768c4f7d880d930b00fe5393c453a083e
Srushti Ambekallu <srushti@c42.in>
2011-07-20 17:41:22 +0530

    fixed the layout on deleted entities

--- app/controllers/client/campaigns_controller.rb
+++ app/controllers/client/campaigns_controller.rb
@@ -1,4 +1,9 @@
 module Client
   class CampaignsController < ::CampaignsController
+    layout 'client'
+
+    def deleted
+      render 'campaigns/deleted'
+    end
   end
 end
--- app/controllers/client/scripts_controller.rb
+++ app/controllers/client/scripts_controller.rb
@@ -1,4 +1,9 @@
 module Client
   class ScriptsController < ::ScriptsController
+    layout 'client'
+
+    def deleted
+      render 'scripts/deleted'
+    end
   end
 end
