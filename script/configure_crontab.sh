1409f9c1519db2dd67e823641ffa13609bd5eab7
Srushti Ambekallu <srushti@c42.in>
2011-07-25 16:00:39 +0530

    fixed the mktemp command

--- script/configure_crontab.sh
+++ script/configure_crontab.sh
@@ -1,4 +1,4 @@
-TMPFILE=$(mktemp -t cron)
+TMPFILE=$(mktemp -t cron.XXX)
 echo "*/10 * * * * cd $2/current && /usr/local/bin/bundle exec rake -f $2/current/Rakefile update_twilio_stats RAILS_ENV=$1"
 crontab -r
 crontab $TMPFILE 
