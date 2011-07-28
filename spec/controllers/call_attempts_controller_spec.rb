67c2d21c28db20658da66c6a20188275d476da12
Aninda Kundu <spacefugitive@gmail.com>
2011-07-28 19:04:34 +0530

    schedule callbacks [WIP]

---
+++ spec/controllers/call_attempts_controller_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe CallAttemptsController do
+  it "updates a call attempt" do
+    call_attempt = Factory(:call_attempt, :scheduled_date => nil)
+    scheduled_date = 2.days.from_now
+    put :update, :id => call_attempt.id, :call_attempt => { :scheduled_date => scheduled_date }
+    call_attempt.reload.scheduled_date.to_s.should == scheduled_date.to_s
+    response.status.should == '200 OK'
+  end
+end
