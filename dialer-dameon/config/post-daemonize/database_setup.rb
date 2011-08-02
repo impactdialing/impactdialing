d881e46b904a4880282247c88fd3949f32d62221
Srushti Ambekallu <srushti@c42.in>
2011-08-02 17:47:54 +0530

    changed the paths in the daemon to relative paths and consequently managed to remove hard-coded absolute paths

--- dialer-dameon/config/post-daemonize/database_setup.rb
+++ dialer-dameon/config/post-daemonize/database_setup.rb
@@ -1,19 +1,13 @@
-#db_yaml = DaemonKit::Config.load('database') 
+#db_yaml = DaemonKit::Config.load('database')
 #ActiveRecord::Base.establish_connection(db_yaml.to_h)
-DaemonKit.logger.info "Setting up ActiveRecord Connection" 
-if DaemonKit.env=="development"
-  database_yaml = IO.read('/Volumes/MacHD/Users/blevine/dev/impact_dialing/config/database.yml')
-  databases = YAML::load(database_yaml)
-  ActiveRecord::Base.establish_connection(databases["development"])
-else
-  database_yaml = IO.read('/opt/bitnami/apache2/trunk/config/database.yml')
-  databases = YAML::load(database_yaml)
-  ActiveRecord::Base.establish_connection(databases["production"])
-end
+DaemonKit.logger.info "Setting up ActiveRecord Connection"
+database_yaml = IO.read(File.join(File.dirname(__FILE__), '../../../config/database.yml'))
+databases = YAML::load(database_yaml)
+ActiveRecord::Base.establish_connection(databases[DaemonKit.env])
 
 #memcached
 #CACHE = MemCache.new 'localhost:11211'
-# 
+#
 # memcache_options = {
 #   :c_threshold => 10000,
 #   :compression => true,
@@ -22,12 +16,12 @@ end
 #   :readonly => false,
 #   :urlencode => false
 # }
-#  
+#
 # CACHE = MemCache.new memcache_options
 # if DaemonKit.env=="development"
 #   CACHE.servers = '127.0.0.1:11211'
 # else
-#   
+#
 #   CACHE.servers = 'domU-12-31-39-10-89-26.compute-1.internal:11211'
 #   begin
 #      PhusionPassenger.on_event(:starting_worker_process) do |forked|
