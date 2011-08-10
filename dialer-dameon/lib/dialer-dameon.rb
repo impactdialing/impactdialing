0a9ff74c8b540ea98a6952fed002cf3624acf528
Aninda Kundu <spacefugitive@gmail.com>
2011-08-10 19:24:52 +0530

    rewriting a bit of the dialer

--- dialer-dameon/lib/dialer-dameon.rb
+++ dialer-dameon/lib/dialer-dameon.rb
@@ -37,59 +37,7 @@ class Dialer
     APP_URL
   end
   def self.startcall(voter, campaign)
-    require "hpricot"
-    require "open-uri"
-    voter.status="Call in progress"
-    voter.save
-    t = TwilioLib.new(TWILIO_ACCOUNT, TWILIO_AUTH)
-#    a=t.call("POST", "Calls", {'IfMachine'=>"Hangup", 'Caller' => APP_NUMBER, 'Called' => voter.Phone, 'Url'=>"#{APP_URL}/callin/voterFindSession?campaign=#{campaign.id}&voter=#{voter.id}"})
-    if !campaign.caller_id.blank? && campaign.caller_id_verified
-      caller_num=campaign.caller_id
-    else
-      caller_num=APP_NUMBER
-    end
-    #DaemonKit.logger.info "APP_URL: #{APP_URL}"
-    c = CallAttempt.new
-    c.dialer_mode=campaign.predective_type
-    c.voter_id=voter.id
-    c.campaign_id=campaign.id
-    c.status="Call ready to dial"
-    c.save
-#    DaemonKit.logger.info caller_num
-    if campaign.predective_type.index("robo,")
-      a=t.call("POST", "Calls", {'Timeout'=>"15", 'IfMachine'=>'Hangup', 'Caller' => caller_num, 'Called' => voter.Phone, 'Url'=>"#{APP_URL}/robo/#{campaign.predective_type.split(",").last}&campaign=#{campaign.id}&voter=#{voter.id}&attempt=#{c.id}"})
-    elsif campaign.predective_type=="preview"
-      a=t.call("POST", "Calls", {'Timeout'=>"20", 'Caller' => caller_num, 'Called' => voter.Phone, 'Url'=>"#{APP_URL}/callin/voterFindSession?campaign=#{campaign.id}&voter=#{voter.id}&attempt=#{c.id}"})
-    elsif campaign.use_answering
-      if campaign.use_recordings
-        a=t.call("POST", "Calls", {'Timeout'=>campaign.answer_detection_timeout, 'Caller' => caller_num, 'Called' => voter.Phone, 'Url'=>"#{APP_URL}/callin/voterFindSession?campaign=#{campaign.id}&voter=#{voter.id}&attempt=#{c.id}", 'IfMachine'=>'Continue'})
-      else
-        a=t.call("POST", "Calls", {'Timeout'=>campaign.answer_detection_timeout, 'Caller' => caller_num, 'Called' => voter.Phone, 'Url'=>"#{APP_URL}/callin/voterFindSession?campaign=#{campaign.id}&voter=#{voter.id}&attempt=#{c.id}", 'IfMachine'=>'Hangup'})
-      end
-    else
-      a=t.call("POST", "Calls", {'Timeout'=>"15", 'Caller' => caller_num, 'Called' => voter.Phone, 'Url'=>"#{APP_URL}/callin/voterFindSession?campaign=#{campaign.id}&voter=#{voter.id}&attempt=#{c.id}"})
-    end
-    require 'rubygems'
-    require 'hpricot'
-    DaemonKit.logger.info a
-    @doc = Hpricot::XML(a)
-    puts @doc if DaemonKit.env=="development"
-    c.sid=(@doc/"Sid").inner_html
-    c.status="Call in progress"
-    c.save
-    v = Voter.find(voter.id)
-    v.last_call_attempt_id=c.id
-    v.last_call_attempt_time=Time.now
-    v.save
-
-    # avail_campaign_hash = cache_get("avail_campaign_hash") {{}}
-    # if !avail_campaign_hash.has_key?(campaign.id)
-    #   avail_campaign_hash[campaign.id] = {"callers"=>[],"calls"=>[c]}
-    # else
-    #   avail_campaign_hash[campaign.id]["calls"] << c
-    # end
-    # cache_set("avail_campaign_hash") {avail_campaign_hash}
-
+    voter.dial_predictive #booya
   end
 end
 
