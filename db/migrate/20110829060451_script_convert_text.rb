6c82f181a0315c5d5034348802e747363cfebc17
Beans0063 <beans@Beanserver.net>
2011-08-28 23:07:07 -0700

    don't default result_set digits to nil to save space so the row can be saved

---
+++ db/migrate/20110829060451_script_convert_text.rb
@@ -0,0 +1,19 @@
+class ScriptConvertText < ActiveRecord::Migration
+  def self.up
+    change_column :scripts, :result_set_11, :text
+    change_column :scripts, :result_set_12, :text
+    change_column :scripts, :result_set_13, :text
+    change_column :scripts, :result_set_14, :text
+    change_column :scripts, :result_set_15, :text
+    change_column :scripts, :result_set_16, :text
+  end
+
+  def self.down
+    change_column :scripts, :result_set_11, :string
+    change_column :scripts, :result_set_12, :string
+    change_column :scripts, :result_set_13, :string
+    change_column :scripts, :result_set_14, :string
+    change_column :scripts, :result_set_15, :string
+    change_column :scripts, :result_set_16, :string
+  end
+end
