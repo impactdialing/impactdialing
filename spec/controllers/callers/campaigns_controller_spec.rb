e5559ca01db3c3ad6b69479b045a70331a13f3f5
Aninda Kundu <spacefugitive@gmail.com>
2011-09-13 16:12:16 +0530

    Caller UI [WIP}

--- spec/controllers/callers/campaigns_controller_spec.rb
+++ spec/controllers/callers/campaigns_controller_spec.rb
@@ -10,20 +10,20 @@ describe Callers::CampaignsController do
   end
 
   it "lists all active campaigns with a web ui" do
-    caller.campaigns << Factory(:campaign, :active => false)
-    caller.campaigns << Factory(:campaign, :active => false, :use_web_ui => true)
-    caller.campaigns << Factory(:campaign, :active => true, :use_web_ui => false)
+    user.campaigns << Factory(:campaign, :active => false)
+    user.campaigns << Factory(:campaign, :active => false, :use_web_ui => true)
+    user.campaigns << Factory(:campaign, :active => true, :use_web_ui => false)
     campaign1 = Factory(:campaign, :active => true, :use_web_ui => true)
-    caller.campaigns << campaign1
+    user.campaigns << campaign1
     caller.save
     get :index
     assigns(:campaigns).should == [campaign1]
   end
 
   it "finds a callers campaign" do
-    caller.campaigns << Factory(:campaign, :active => false)
+    user.campaigns << Factory(:campaign, :active => false)
     campaign1 = Factory(:campaign, :active => true, :use_web_ui => true)
-    caller.campaigns << campaign1
+    user.campaigns << campaign1
     caller.save
     get :show, :id => campaign1.id
     assigns(:campaign).should == campaign1
