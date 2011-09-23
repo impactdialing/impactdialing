c3c66bf2d3026101e6fc239ec7c8fc4a5fd41146
Srushti Ambekallu <srushti@c42.in>
2011-09-23 17:32:37 +0530

    fixed the subject & body of the admin invite emails

---
+++ app/models/white_labeling.rb
@@ -0,0 +1,16 @@
+module WhiteLabeling
+  ['title', 'full_title', 'phone', 'email', ].each do |value|
+    define_method("white_labeled_#{value}") do |domain|
+      I18n.t("white_labeling.#{correct_domain(domain)}.#{value}")
+    end
+  end
+
+  def correct_domain(domain)
+    d = domain.downcase.gsub(/\..+$/, '')
+    if I18n.t("white_labeling.#{d}", :default => '').blank?
+      'impactdialing'
+    else
+      d
+    end
+  end
+end
