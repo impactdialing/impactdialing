b1ef0d55e5bd416651f57421bc3ff35cd76a427f
Aninda Kundu <aninda@c42.in>
2011-09-28 15:01:07 +0530

    fixed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,6 @@ end
 task :staging do
   set :server_name, "ec2-174-129-172-31.compute-1.amazonaws.com"
   set :rails_env, 'staging'
-  set :branch, "client_rewrite"
   role :web, 'staging.impactdialing.com'
   role :app, 'staging.impactdialing.com'
   role :db, 'staging.impactdialing.com', :primary => true
