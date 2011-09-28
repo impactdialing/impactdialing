42d52a0bee17258528a72613a1c0513e20058829
Aninda Kundu <aninda@c42.in>
2011-09-28 15:40:23 +0530

    fixing bugs on billing account and account relationship with billing account

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,10 +4,14 @@ class Account < ActiveRecord::Base
   has_many :all_campaigns, :class_name => 'Campaign'
   has_many :recordings
   has_many :custom_voter_fields
-  has_many :billing_accounts
+  has_one :billing_account
   has_many :scripts
   has_many :callers
   has_many :voter_lists
   has_many :voters
   has_many :families
+
+  def new_billing_account
+    BillingAccount.create(:account => self)
+  end
 end
