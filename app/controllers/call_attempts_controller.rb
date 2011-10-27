06676132b59db0855669dc8fee73c5308de6fb33
Aninda Kundu <aninda@c42.in>
2011-10-27 09:07:33 +0530

    pushes new voter after voter response.

--- app/controllers/call_attempts_controller.rb
+++ app/controllers/call_attempts_controller.rb
@@ -56,6 +56,7 @@ class CallAttemptsController < ApplicationController
       voters_response = PossibleResponse.find(answer["value"])
       @voter.answers.create(:possible_response => voters_response, :question => voters_response.question)
     end
+    Pusher[@call_attempt.caller_session.session_key].trigger("voter_push", Voter.to_be_dialed.first.info)
     render :nothing => true
   end
 end
