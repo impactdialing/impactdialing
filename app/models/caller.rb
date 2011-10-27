b45eb916d088933ffcc3e609314733e10ed4a804
Nikhil Fernandes <nikhil@activesphere.com>
2011-10-27 17:04:26 +0530

    Fixing persisting callers for campaigns

--- app/models/caller.rb
+++ app/models/caller.rb
@@ -3,7 +3,7 @@ class Caller < ActiveRecord::Base
   include Deletable
   validates_presence_of :name, :on => :create, :message => "can't be blank"
   validates_format_of :email, :allow_blank => true, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
-  has_many :caller_campaigns, :foreign_key => :caller_id
+  has_many :caller_campaigns
   has_many :campaigns, :through => :caller_campaigns
   belongs_to :account
   has_many :caller_sessions
