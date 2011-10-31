b04edfb6c9492b1bffab2616c288c7cbfc2fa62e
Sreehari <sree0514@gmail.com>
2011-10-31 16:44:23 +0530

    update method script_controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ActiveRecord::Base
   validates_presence_of :text
   belongs_to :script
   has_many :possible_responses
-  accepts_nested_attributes_for :possible_responses
+  accepts_nested_attributes_for :possible_responses, :allow_destroy => true
 end
