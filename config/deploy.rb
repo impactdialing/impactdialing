b8460a3ebb9294faf2cb90c86c46c1925abaf9b8
Srushti Ambekallu <srushti@c42.in>
2011-11-02 15:22:59 +0530

    deploying predictive to the staging server

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ end
 task :staging do
   set :server_name, "ec2-174-129-172-31.compute-1.amazonaws.com"
   set :rails_env, 'staging'
-  set :branch, "temp_production"
+  set :branch, "predictive"
   role :web, 'ec2-174-129-172-31.compute-1.amazonaws.com'
   role :app, 'ec2-174-129-172-31.compute-1.amazonaws.com'
   role :db, 'ec2-174-129-172-31.compute-1.amazonaws.com', :primary => true
