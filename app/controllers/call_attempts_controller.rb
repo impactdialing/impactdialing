88abd31ebb2d56a0fedd47950467100a0ea5f929
Aninda Kundu <spacefugitive@gmail.com>
2011-11-08 17:24:33 +0530

    Push dialer type for voter push

--- app/controllers/call_attempts_controller.rb
+++ app/controllers/call_attempts_controller.rb
@@ -72,7 +72,7 @@ class CallAttemptsController < ApplicationController
       @voter.answers.create(:possible_response => voters_response, :question => voters_response.question)
     end
     voter = @call_attempt.campaign.all_voters.to_be_dialed.first
-    Pusher[@call_attempt.caller_session.session_key].trigger("voter_push", voter ? voter.info : {})
+    @call_attempt.caller_session.publish("voter_push", voter ? voter.info : {})
     @call_attempt.caller_session.update_attribute(:voter_in_progress, nil)
     render :nothing => true
   end
