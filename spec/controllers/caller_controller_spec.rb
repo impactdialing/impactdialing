3dbf5f54b2d6e1bf24e0866dac6c846e194ba095
Aninda Kundu <spacefugitive@gmail.com>
2011-11-09 14:41:48 +0530

    commit for converting pause attempt from fixnum to integer

--- spec/controllers/caller_controller_spec.rb
+++ spec/controllers/caller_controller_spec.rb
@@ -151,7 +151,7 @@ describe CallerController do
 
     it "pauses a callers session while an attempt is in progress" do
       session = Factory(:caller_session, :caller => caller, :campaign => Factory(:campaign), :available_for_call => false, :on_call => true, :voter_in_progress => Factory(:voter), :session_key => 'some_key')
-      post :pause, :id => caller.id, :session_id => session.id
+      post :pause, :id => caller.id, :session_id => session.id, :attempt => nil
       response.body.should == session.pause_for_results
     end
 
