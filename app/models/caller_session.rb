dbb907e2c512023df264ca047453f2193404cb6f
Aninda Kundu <spacefugitive@gmail.com>
2011-11-09 15:39:07 +0530

    commenting answering machine detection

--- app/models/caller_session.rb
+++ app/models/caller_session.rb
@@ -43,8 +43,8 @@ class CallerSession < ActiveRecord::Base
     response = Twilio::Call.make(self.campaign.caller_id, voter.Phone, connect_call_attempt_url(attempt, :host => Settings.host, :port => Settings.port),
                                  {
                                      'StatusCallback' => end_call_attempt_url(attempt, :host => Settings.host, :port => Settings.port),
-                                     'IfMachine' => self.campaign.use_recordings? ? 'Continue' : 'Hangup',
-                                     'Timeout' => campaign.answer_detection_timeout || "20"
+                                     #'IfMachine' => self.campaign.use_recordings? ? 'Continue' : 'Hangup',
+                                     #'Timeout' => campaign.answer_detection_timeout || "20"
                                  }
     )
     self.publish('calling_voter', voter.info)
