2467ba717615f9c7faea034233e9154ac0170024
Aninda Kundu <spacefugitive@gmail.com>
2011-11-15 17:20:56 +0530

    answered, answered within scope

--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -30,6 +30,22 @@ describe Voter do
     Voter.active.should == [active_voter]
   end
 
+  it "returns voters that have responded" do
+    Factory(:voter)
+    3.times {Factory(:voter, :result_date => Time.now)}
+    Voter.answered.size.should == 3
+  end
+
+  it "returns voters that have responded within a date range" do
+    Factory(:voter)
+    v1 =Factory(:voter, :result_date => Time.now)
+    v2 = Factory(:voter, :result_date => 1.day.ago)
+    v3 = Factory(:voter, :result_date => 2.days.ago)
+    Voter.answered_within(2.days.ago, 0.days.ago).should == [v1,v2,v3]
+    Voter.answered_within(2.days.ago, 1.days.ago).should == [v2,v3]
+    Voter.answered_within(1.days.ago, 1.days.ago).should == [v2]
+  end
+
   it "conferences with a caller" do
     voter = Factory(:voter)
     caller = Factory(:caller_session)
