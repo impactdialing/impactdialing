9abfdabe643e9fe69d612304253f117eb1eb1034
Aninda Kundu <aninda@c42.in>
2011-11-16 22:30:47 +0530

    another fix for report download

--- app/controllers/client/reports_controller.rb
+++ app/controllers/client/reports_controller.rb
@@ -57,7 +57,7 @@ module Client
         @campaign.all_voters.answered_within(@from_date, @to_date).each do |v|
           custom_fields, answers, voter_details = [], [], [v.CustomID, v.LastName, v.FirstName, v.MiddleName, v.Suffix, v.Phone, v.last_call_attempt.caller.name, v.status, v.last_call_attempt.call_start, v.last_call_attempt.call_end, v.call_attempts.size]
           @campaign.account.custom_voter_fields.each { |cf| custom_fields << v.custom_voter_field_values.for_field(cf).first.try(:value) }
-          @campaign.questions.each { |q| answers << v.answers.for(q).first.try(:possible_response).try(:value) }
+          @campaign.script.questions.each { |q| answers << v.answers.for(q).first.try(:possible_response).try(:value) }
           csv << [voter_details, custom_fields, answers].flatten
         end
       end
