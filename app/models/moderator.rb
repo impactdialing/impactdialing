a31c9ebcd60825197eb402e05fc132bb5a9592a2
Sreehari <sree0514@gmail.com>
2011-11-19 18:02:52 +0530

    monitoring conference

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -3,16 +3,16 @@ class Moderator < ActiveRecord::Base
   
   def switch_monitor_mode(session)
     Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
-    if params[:to_type] == "eavesdrop"
-      Twilio.Conference.mute_participant(session.session_key, call_sid).response
+    if params[:type] == "breakin"
+      Twilio.Conference.mute_participant(session.session_key, call_sid)
     else
-      Twilio.Conference.unmute_participant(session.session_key, call_sid).response
+      Twilio.Conference.unmute_participant(session.session_key, call_sid)
     end
   end
   
-  def stop_monitor(session)
+  def stop_monitoring(session)
     Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
-    Twilio.Conference.kick_participant(session.session_key, call_sid).response
+    Twilio.Conference.kick_participant(session.session_key, call_sid)
   end
   
 end
