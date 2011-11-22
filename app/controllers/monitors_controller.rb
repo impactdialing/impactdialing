6147eac9072bff80e271b931c6ad1bbdc368df6d
Srushti Ambekallu <srushti@c42.in>
2011-11-22 14:41:37 +0530

    switched jquery, jquery-ui, pusher & twilio js to https sources

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -1,13 +1,13 @@
 class MonitorsController < ClientController
   layout 'client'
-  
+
   def index
     @campaigns = account.campaigns.with_running_caller_sessions
     twilio_capability = Twilio::Util::Capability.new(TWILIO_ACCOUNT, TWILIO_AUTH)
     twilio_capability.allow_client_outgoing(MONITOR_TWILIO_APP_SID)
     @token = twilio_capability.generate
   end
-  
+
   def start
     session = CallerSession.find(params[:session_id])
     puts session.voter_in_progress.call_attempts.last.status
@@ -17,18 +17,18 @@ class MonitorsController < ClientController
     mute_type = params[:type]=="breakin" ? false : true
     render xml:  session.join_conference(mute_type, params[:CallSid])
   end
-  
+
   def switch_mode
     type = params[:type]
     session = CallerSession.find(params[:session_id])
     session.moderator.switch_monitor_mode(session, type)
-    render text: "Your currently "+ type + " on "+ session.caller.name
+    render text: "You're currently "+ type + " on "+ session.caller.name
   end
-  
+
   def stop
     session = CallerSession.find(params[:session_id])
     session.moderator.stop_monitoring(session)
     render nothing: true
   end
-  
+
 end
