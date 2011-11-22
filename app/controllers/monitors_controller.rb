4b429895df30aa613fa1c919d84722dc78502428
Sreehari <sree0514@gmail.com>
2011-11-22 18:32:53 +0530

    monitor pusher event when no voter on call

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -2,6 +2,7 @@ class MonitorsController < ClientController
   layout 'client'
   
   def index
+    puts @user.id.to_s
     @campaigns = account.campaigns.with_running_caller_sessions
     twilio_capability = Twilio::Util::Capability.new(TWILIO_ACCOUNT, TWILIO_AUTH)
     twilio_capability.allow_client_outgoing(MONITOR_TWILIO_APP_SID)
@@ -10,9 +11,8 @@ class MonitorsController < ClientController
   
   def start
     session = CallerSession.find(params[:session_id])
-    puts session.voter_in_progress.call_attempts.last.status
     unless session.voter_in_progress.call_attempts.last.status == "Call in progress"
-      session.publish('no_voter_on_call', {})
+      Pusher[params[:monitor_session]].trigger('no_voter_on_call',{})
     end
     mute_type = params[:type]=="breakin" ? false : true
     render xml:  session.join_conference(mute_type, params[:CallSid])
