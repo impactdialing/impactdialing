569984a4bd6b1e02b10fdb2fb268a03f6db5f570
Srushti Ambekallu <srushti@c42.in>
2011-11-30 14:50:49 +0530

    fixed the spec since setting :created_at doesn't work while creating the object itself

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,6 @@
 require "spec_helper"
 
 describe Answer do
-  
   it "should returns all the answers within the specified period" do
     answer1 = Factory(:answer, :voter => Factory(:voter), :possible_response => Factory(:possible_response), :question => Factory(:question, :script => Factory(:script)), :created_at => 2.days.ago)
     answer2 = Factory(:answer, :voter => Factory(:voter), :possible_response => Factory(:possible_response), :question => Factory(:question, :script => Factory(:script)), :created_at => 1.days.ago)
@@ -11,5 +10,4 @@ describe Answer do
     Answer.within(Time.now + 2.day, Time.now + 3.day).should == []
     Answer.within(Time.now, Time.now).should == [answer3, answer4]
   end
-
 end
