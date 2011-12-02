32dd15b6f769a85b7b742334f6c65bfa1229ad33
Nikhil Fernandes <nikhil@activesphere.com>
2011-12-02 13:19:48 +0530

    Fixing errors for restarting dialer on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,8 +43,8 @@ namespace :deploy do
   end
   
   task :restart_dialer do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec script/predictive_dialer_control.rb stop &"
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec script/predictive_dialer_control.rb run &"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec script/predictive_dialer_control.rb stop"
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec script/predictive_dialer_control.rb run"
   end
 end
 
