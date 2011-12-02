38ce937cbba75d6e6654ebe59817d84a5a121d35
Nikhil Fernandes <nikhil@activesphere.com>
2011-12-02 13:54:55 +0530

    Killing dialer process by pid

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ namespace :deploy do
   end
   
   task :restart_dialer do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec script/predictive_dialer_control.rb stop"
+    run "ps -ef | grep 'predictive_dialer' | grep -v grep | awk '{print $2}' | xargs kill || echo 'no process with name predictive_dialer found'"
     run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec script/predictive_dialer_control.rb start"
   end
 end
