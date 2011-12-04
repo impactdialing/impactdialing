7f06b8b086753c5c8018f426d78180efa952079f
Nikhil Fernandes <nikhil@activesphere.com>
2011-12-04 19:10:33 +0530

    Skip voter fix error

--- app/models/caller_session.rb
+++ app/models/caller_session.rb
@@ -82,7 +82,7 @@ class CallerSession < ActiveRecord::Base
       end
     end.response
     update_attributes(:on_call => true, :available_for_call => true, :attempt_in_progress => nil)
-    publish('caller_connected_dialer', {}) if campaign.predictive_type != Campaign::Type::PREVIEW || campaign.predictive_type != Campaign::Type::PROGRESSIVE
+    publish('caller_connected_dialer', {}) if campaign.predictive_type != Campaign::Type::PREVIEW && campaign.predictive_type != Campaign::Type::PROGRESSIVE
     response
   end
   
