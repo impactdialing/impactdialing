0154b0115a0d09dcd25c03223144a0aa58809311
Nikhil Fernandes <nikhil@activesphere.com>
2011-12-05 16:10:02 +0530

    Redirecting cron logs

--- script/configure_crontab.sh
+++ script/configure_crontab.sh
@@ -1,5 +1,5 @@
 TMPFILE=$(mktemp -t cron.XXX)
-echo "*/10 * * * * cd $2/current && /usr/local/bin/bundle exec rake -f $2/current/Rakefile update_twilio_stats RAILS_ENV=$1" >> $TMPFILE
+echo "*/10 * * * * cd $2/current && /usr/local/bin/bundle exec rake -f $2/current/Rakefile update_twilio_stats RAILS_ENV=$1 >> $2/current/cron_jobs.log" >> $TMPFILE
 crontab -r
 crontab $TMPFILE 
 crontab -l
