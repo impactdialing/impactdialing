58d6d423d3fb74dc2154d25aea26d7acf3212602
Nikhil Fernandes <nikhil@activesphere.com>
2011-12-12 14:53:31 +0530

    Pushing new voter when call fails

--- public/javascripts/callers/caller_pusher.js
+++ public/javascripts/callers/caller_pusher.js
@@ -88,14 +88,6 @@ function call_voter() {
     })
 }
 
-function ready_to_call(dialer) {
-    if (dialer && dialer.toLowerCase() == "preview") {
-        $("#stop_calling").show();
-        $("#skip_voter").show();
-        $("#call_voter").show();
-    }
-
-}
 
 
 function schedule_for_later() {
@@ -121,7 +113,7 @@ function send_voter_response() {
     });
     $("#voter_responses").trigger("submit");
 	$("#voter_responses").unbind("submit");
-	
+
 }
 
 function send_voter_response_and_disconnect() {
@@ -202,6 +194,19 @@ function expand_scheduler() {
     $("#callback_info").show();
 }
 
+function ready_for_calls(data){
+	if (data.dialer && data.dialer.toLowerCase() == "progressive") {
+	  $("#stop_calling").show();
+	  call_voter();
+    }
+    if (data.dialer && data.dialer.toLowerCase() == "preview") {
+        $("#stop_calling").show();
+        $("#skip_voter").show();
+        $("#call_voter").show();
+    }	
+
+}
+
 function subscribe(session_key) {
     channel = pusher.subscribe(session_key);
     console.log(channel)
@@ -217,14 +222,16 @@ function subscribe(session_key) {
         if (!$.isEmptyObject(data.fields)) {
             set_message("Status: Ready for calls.");
             set_voter(data);
-		    if (data.dialer && data.dialer.toLowerCase() == "progressive") {
-			  call_voter();
-		    }
-
+	    	ready_for_calls(data)
         } else {
             set_message("Status: There are no more numbers to call in this campaign.");
         }
     });
+
+	channel.bind('conference_started' , function(data){
+	    ready_for_calls(data)
+	});
+
 
     channel.bind('caller_connected_dialer', function(data) {
 		hide_all_actions();
@@ -240,10 +247,6 @@ function subscribe(session_key) {
     channel.bind('voter_push', function(data) {        
 		set_message("Status: Ready for calls.");
 		set_voter(data);
-	    if (!$.isEmptyObject(data.fields) && data.dialer && data.dialer.toLowerCase() == "progressive") {
-		  $("#stop_calling").show();
-		  call_voter();
-	    }
         $("#start_calling").hide();
     });
 
@@ -311,7 +314,6 @@ function subscribe(session_key) {
             bind_voter(data);
             hide_response_panel();
             hide_all_actions();
-            ready_to_call(data.dialer);
 
         } else {
 			clear_voter();
