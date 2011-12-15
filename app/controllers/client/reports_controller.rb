9b09e960e753cdbf3eed850eae7d9d15f1cdb163
Sreehari <sreehari@Sreeharis-MacBook-Pro.local>
2011-12-15 17:54:30 +0530

    able to download all voters or only answered voters in the specified range

--- app/controllers/client/reports_controller.rb
+++ app/controllers/client/reports_controller.rb
@@ -68,17 +68,21 @@ module Client
         selected_voter_fields = params[:voter_fields]
         selected_custom_voter_fields = params[:custom_voter_fields]
         csv << [selected_voter_fields ? selected_voter_fields : [], selected_custom_voter_fields ? selected_custom_voter_fields : [], "Caller", "Status", "Call start", "Call end", "Attempts", "Recording", @campaign.script.questions.collect { |q| q.text }, @campaign.script.notes.collect { |note| note.note }].flatten
-        
-        @campaign.all_voters.answered_within(@from_date, @to_date).each do |v|
+        voters = params[:download_all_voters] ? @campaign.all_voters : @campaign.all_voters.answered_within(@from_date, @to_date)
+        voters.try(:each) do |v|
           last_call_attempt = v.last_call_attempt
+          
           notes, voter_custom_fields, answers, call_details = [], [], [], [last_call_attempt ? last_call_attempt.caller.try(:email) : '', v.status, last_call_attempt ? last_call_attempt.call_start : '', last_call_attempt ? last_call_attempt.call_end : '', v.call_attempts.size, last_call_attempt ? last_call_attempt.report_recording_url : ''].flatten
           voter_fields = selected_voter_fields ? [selected_voter_fields.try(:collect){|f| v.send(f)}].flatten : []
           custom_voter_field_objects = @campaign.account.custom_voter_fields.try(:select){|cf| selected_custom_voter_fields.try(:include?, cf.name)}
           custom_voter_field_objects.each { |cf| voter_custom_fields << v.custom_voter_field_values.for_field(cf).first.try(:value) }
-          
-          @campaign.script.questions.each { |q| answers << v.answers.for(q).first.try(:possible_response).try(:value) }
-          @campaign.script.notes.each { |note| notes << v.note_responses.for(note).last.try(:response) }
-          csv << [voter_fields, voter_custom_fields, call_details, answers, notes].flatten
+          if last_call_attempt
+            @campaign.script.questions.each { |q| answers << v.answers.for(q).first.try(:possible_response).try(:value) }
+            @campaign.script.notes.each { |note| notes << v.note_responses.for(note).last.try(:response) }
+            csv << [voter_fields, voter_custom_fields, call_details, answers, notes].flatten
+          else
+            csv << [voter_fields, voter_custom_fields, nil ,"Not Dialed"].flatten
+          end
         end
       end
       report
