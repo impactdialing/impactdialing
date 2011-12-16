d689a91fbf6c6c519d6cf2f55026a8cc10e0458b
Sreehari <sreehari@Sreeharis-MacBook-Pro.local>
2011-12-16 15:45:58 +0530

    monitor bugs fix index page error, session expired when switching caller

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -1,5 +1,5 @@
 class MonitorsController < ClientController
-  skip_before_filter :start,:stop
+  skip_before_filter :check_login, :only => [:start,:stop,:switch_mode, :deactivate_session]
   layout 'client'
   
   def index
