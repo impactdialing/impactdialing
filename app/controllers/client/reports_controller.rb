56fcab8a644e4f2acffa989350888a9c73883a21
Sreehari <sreehari@Sreeharis-MacBook-Pro.local>
2011-12-23 15:43:48 +0530

    download report start and end time should be in the campaign time zone

--- app/controllers/client/reports_controller.rb
+++ app/controllers/client/reports_controller.rb
@@ -72,7 +72,7 @@ module Client
         voters.try(:each) do |v|
           last_call_attempt = v.last_call_attempt
           
-          notes, voter_custom_fields, answers, call_details = [], [], [], [last_call_attempt ? last_call_attempt.caller.try(:email) : '', v.status, last_call_attempt ? last_call_attempt.call_start : '', last_call_attempt ? last_call_attempt.call_end : '', v.call_attempts.size, last_call_attempt ? last_call_attempt.report_recording_url : ''].flatten
+          notes, voter_custom_fields, answers, call_details = [], [], [], [last_call_attempt ? last_call_attempt.caller.try(:email) : '', v.status, last_call_attempt ? last_call_attempt.call_start.try(:in_time_zone, @campaign.time_zone) : '', last_call_attempt ? last_call_attempt.call_end.try(:in_time_zone, @campaign.time_zone) : '', v.call_attempts.size, last_call_attempt ? last_call_attempt.report_recording_url : ''].flatten
           voter_fields = selected_voter_fields ? [selected_voter_fields.try(:collect){|f| v.send(f)}].flatten : []
           custom_voter_field_objects = @campaign.account.custom_voter_fields.try(:select){|cf| selected_custom_voter_fields.try(:include?, cf.name)}
           custom_voter_field_objects.each { |cf| voter_custom_fields << v.custom_voter_field_values.for_field(cf).first.try(:value) }
