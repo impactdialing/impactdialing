540099c9bb910575fddcb3bb27fcdfd675ee507f
Nikhil Fernandes <nikhil@activesphere.com>
2011-12-27 19:16:23 +0530

    Adding response from phones only

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -156,7 +156,7 @@ class CallerController < ApplicationController
   
   def phones_only
     caller_session = CallerSession.find(params[:session_id])
-    caller_session.ask_caller_to_choose_voter    
+    render :xml => caller_session.ask_caller_to_choose_voter    
   end
   
   def ping
