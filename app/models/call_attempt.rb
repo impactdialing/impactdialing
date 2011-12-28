4d96bf90d8bcefc13c9242af5f4a49b7d993f004
Nikhil Fernandes <nikhil@activesphere.com>
2011-12-28 13:31:47 +0530

    Adding host and port to url

--- app/models/call_attempt.rb
+++ app/models/call_attempt.rb
@@ -139,7 +139,7 @@ class CallAttempt < ActiveRecord::Base
       else
         Rails.logger.debug("failed called being redirected")
         Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
-        Twilio::Call.redirect(caller_session.sid, phones_only_caller_index_path(session_id: caller_session.id))        
+        Twilio::Call.redirect(caller_session.sid, phones_only_caller_index_url(:host => Settings.host, :port => Settings.port, session_id: caller_session.id))        
         Rails.logger.debug("failed called after redirected --- #{caller_session.sid}")
       end  
     else
