3f2294b71ebbeb2f4706f388acd29eb0a741dff6
Sreehari <sreehari@Sreeharis-MacBook-Pro.local>
2012-01-01 23:53:15 +0530

    bug - answer report taking answers from all campaigns

--- spec/models/possible_response_spec.rb
+++ spec/models/possible_response_spec.rb
@@ -4,9 +4,10 @@ describe PossibleResponse do
   
   it "returns the calculated percentage value for possible response" do
     now = Time.now
+    campaign = Factory(:campaign)
     possible_response = Factory(:possible_response)
-    answer = Factory(:answer, :voter => Factory(:voter),:possible_response => possible_response, :question => Factory(:question, :script => Factory(:script)), :created_at => now)
-    possible_response.stats(now, now, 25).should == {answer: "no_response", number: 1, percentage:  4}
+    answer = Factory(:answer, :voter => Factory(:voter, :campaign => campaign),:possible_response => possible_response, :question => Factory(:question, :script => Factory(:script)), :created_at => now)
+    possible_response.stats(now, now, 25, campaign).should == {answer: "no_response", number: 1, percentage:  4}
   end
   
 end
