48990579d6212a47ff33270f7fc4a8487bddae42
Beans0063 <beans@Beanserver.net>
2012-01-01 21:21:50 -0800

    heroku staging fix

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,6 +1,8 @@
 class Settings < Settingslogic
   if Rails.env == 'heroku'
     source "#{Rails.root}/config/application_heroku.yml"
+  elsif Rails.env == 'heroku_staging'
+    source "#{Rails.root}/config/application_heroku_staging.yml"
   else
     source "#{Rails.root}/config/application.yml"
   end
