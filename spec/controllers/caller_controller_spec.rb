adefd210084d08861713de8cb2e6e8aa041a8b14
Nikhil Fernandes <nikhil@activesphere.com>
2012-01-06 14:40:22 +0530

    Catching exception to avaoid memory leak

--- spec/controllers/caller_controller_spec.rb
+++ spec/controllers/caller_controller_spec.rb
@@ -37,7 +37,7 @@ describe CallerController do
   end
 
   describe "preview dial" do
-    let(:campaign) { Factory(:campaign) }
+    let(:campaign) { Factory(:campaign, start_time: Time.now - 6.hours, end_time: Time.now + 6.hours) }
 
     before(:each) do
       login_as(caller)
@@ -242,7 +242,7 @@ describe CallerController do
     let(:caller) { Factory(:caller, :is_phones_only => true, :name => "caller name", :pin => "78453") }
     describe "preview mode" do
       before(:each) do
-        @campaign = Factory(:campaign, :robo => false, :predictive_type => 'preview')
+        @campaign = Factory(:campaign, :robo => false, :predictive_type => 'preview', start_time: Time.now - 6.hours, end_time: Time.now + 6.hours)
         @caller_session = Factory(:caller_session, :caller => caller, :campaign => @campaign, :session_key => "sessionkey")
         @current_voter = Factory(:voter, :campaign => @campaign)
       end
