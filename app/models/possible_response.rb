65bccc20f565401775ed66b40e85285f368fce17
Nikhil Fernandes <nikhil@activesphere.com>
2012-01-09 22:06:57 +0530

    Benchmarking for memory leaks

--- app/models/possible_response.rb
+++ app/models/possible_response.rb
@@ -2,8 +2,8 @@ class PossibleResponse < ActiveRecord::Base
   belongs_to :question
   has_many :answers
   
-  def stats(from_date, to_date, total_answers, campaign)
-    number_of_answers = answers.within(from_date, to_date).belong_to(campaign.all_voters.collect{|v| v.id}).length
+  def stats(from_date, to_date, total_answers, voter_ids)
+    number_of_answers = answers.within(from_date, to_date).belong_to(voter_ids).length
     {answer: value, number: number_of_answers, percentage:  total_answers == 0 ? 0 : (number_of_answers * 100 / total_answers)}
   end
   
