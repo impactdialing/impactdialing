3c6dc5922e3722d6490129549721f1e57b5b432f
Sreehari <sreehari@Sreeharis-MacBook-Pro.local>
2012-01-09 18:04:36 +0530

    monitor index page fix the same id for campaign row and caller row

--- public/javascripts/monitors/assign_caller_to_campaign.js
+++ public/javascripts/monitors/assign_caller_to_campaign.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$('.assign_campaign').live('change', function(){
 		var campaign_id = this.value;
-		var caller_id = $(this).closest('tr').attr('id');
+		var caller_id = $(this).closest('tr').attr('caller_id');
 		var session_id = $(this).closest('tr').attr('session_id');
 		$.ajax({
         url : "/client/callers/"+ caller_id + "/reassign_to_campaign?campaign_id=" + campaign_id + "&session_id=" + session_id,
