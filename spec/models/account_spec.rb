6fd2c4a5e5a5b05aa9dcf95c3419c5e458457eba
Aninda Kundu <aninda@c42.in>
2012-01-12 16:04:17 +0530

    voter custom fields

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,7 +5,7 @@ describe Account do
     Factory(:account, :activated => true).paid?.should be_true
     Factory(:account, :activated => false).paid?.should be_false
   end
-  
+
   it "can toggle the call_recording setting" do
     account = Factory(:account, :record_calls => true)
     account.record_calls?.should be_true
@@ -14,4 +14,13 @@ describe Account do
     account.toggle_call_recording!
     account.record_calls?.should be_true
   end
+
+  it "lists all custom fields" do
+    account = Factory(:account)
+    field1 = Factory(:custom_voter_field, :name => "field1", :account => account)
+    field2 = Factory(:custom_voter_field, :name => "field2", :account => account)
+    field3 = Factory(:custom_voter_field, :name => "field3", :account => account)
+    account.custom_fields.should == [field1, field2, field3]
+  end
+
 end
