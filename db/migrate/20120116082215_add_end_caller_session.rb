a0b15a60917ac81618c7d055438ecdb1e3d4cf34
Nikhil Fernandes <nikhil@activesphere.com>
2012-01-16 14:22:55 +0530

    Not starting a new conference is caller_Session has endtime set

--- db/migrate/20120116082215_add_end_caller_session.rb
+++
@@ -1,9 +0,0 @@
-class AddEndCallerSession < ActiveRecord::Migration
-  def self.up
-    add_column :caller_sessions, :ended, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :caller_sessions, :ended
-  end
-end
