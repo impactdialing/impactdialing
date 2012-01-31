633d28e64492c337042af1cdb52e2b4a242a3a83
Sreehari <sreehari@Sreeharis-MacBook-Pro.local>
2012-01-31 12:46:59 +0530

    bug fix - monitor page call_status does not change for phones-only caller, after sumbmit results

--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -422,11 +422,19 @@ describe Voter do
       pending_question = Factory(:question, :script => script)
       voter.unanswered_questions.should == [pending_question]
     end
+    
+    it "sends pusher event to moderator as 'voter_response_submitted' " do
+      voter.update_attributes(:last_call_attempt => Factory(:call_attempt, :campaign => campaign, :caller_session => Factory(:caller_session)))
+      answered_question = Factory(:question, :script => script)
+      Factory(:answer, :voter => voter, :question => answered_question, :possible_response => Factory(:possible_response, :question => answered_question))
+      Moderator.should_receive(:publish_event).with(campaign, 'voter_response_submitted', anything)
+      voter.unanswered_questions.should == []
+    end
 
     describe "phones only" do
       let(:script) { Factory(:script) }
       let(:campaign) { Factory(:campaign, :script => script) }
-      let(:voter) { Factory(:voter, :campaign => campaign) }
+      let(:voter) { Factory(:voter, :campaign => campaign, :last_call_attempt => Factory(:call_attempt, :caller_session => Factory(:caller_session))) }
       let(:question) { Factory(:question, :script => script) }
 
       it "captures a voter response" do
