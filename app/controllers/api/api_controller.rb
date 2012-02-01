fbe0a59145bf56e69ade9347e5ed79ab2b7e7326
Aninda Kundu <spacefugitive@gmail.com>
2012-02-01 17:58:49 +0530

    api changes to pass tests

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,21 +1,23 @@
-class ApiController < ApplicationController
+module Api
+  class ApiController < ApplicationController
     before_filter :authenticate_account
-    
+
     def authenticate_account
       if params[:api_key] != '1mp@ctd1@l1ng'
-        render_json_response({status: 'error',code: "401", message: "UnauthorizedAccess"})
+        render_json_response({status: 'error', code: "401", message: "UnauthorizedAccess"})
         return
       end
     end
-    
-    def render_json_response(response)
-      json_structure = { 
-        :status => response[:status], 
-        :message => response[:message], 
-        }
 
-      render_options = {:json => json_structure}  
+    def render_json_response(response)
+      json_structure = {
+          :status => response[:status],
+          :message => response[:message],
+      }
+
+      render_options = {:json => json_structure}
       render_options[:status] = response[:code]
       render(render_options)
     end
+  end
 end
