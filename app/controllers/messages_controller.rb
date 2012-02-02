434fe813f65abaf0064fbae87892d17567fc2c28
Aninda Kundu <spacefugitive@gmail.com>
2012-02-02 13:05:14 +0530

    refactor

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ClientController
   layout 'v2'
+
   def new
     @script = Script.new
     @script.robo_recordings.build
@@ -7,8 +8,11 @@ class MessagesController < ClientController
 
   def create
     @script = Script.new(params[:script].merge(:robo => true, :for_voicemail => true, :active => true, :account => account))
-    render :action => 'new' unless @script.save
-    redirect_to message_path(@script) if @script.save
+    if @script.save
+      redirect_to message_path(@script)
+    else
+      render :action => 'new'
+    end
   end
 
   def show
