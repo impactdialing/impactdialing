d8b3cf35e54ff432ca9a840c7a3be40a896cfd22
Aninda Kundu <spacefugitive@gmail.com>
2012-02-08 12:43:07 +0530

    using white labeling while starting broadcast campaigns

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -45,7 +45,7 @@ class UserMailer
     to_email = "michael@impactdialing.com"
     subject = I18n.t(:broadcast_campaign_started)
     content = "<br/>#{I18n.t(:broadcast_campaign_started)} : #{campaign.name} by #{user.email}"
-    @uakari.send_email({ :message => { :subject => subject, :text => content, :html => content, :from_name => white_labeled_title(campaign.account.domain), :from_email => white_labeled_email(current_user.domain), :to_email => [to_email]} })
+    @uakari.send_email({ :message => { :subject => subject, :text => content, :html => content, :from_name => white_labeled_title(campaign.account.domain), :from_email => white_labeled_email(user.domain), :to_email => [to_email]} })
   end
 
   def deliver_download(user, download_link)
