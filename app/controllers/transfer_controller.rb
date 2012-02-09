e80b4d10a981ffe35ab6c42b5fb91be70f93372d
Nikhil Fernandes <nikhil@activesphere.com>
2012-02-09 16:43:27 +0530

    Logs to check redirect

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -38,20 +38,24 @@ class TransferController < ApplicationController
   end
   
   def callee
+    puts "enetered callee"
     response = Twilio::Verb.new do |v|
       v.dial(:hangupOnStar => true, :action => gather_response_caller_url(caller, :host => Settings.host, :port => Settings.port, :session_id => id)) do
         v.conference(params[:session_key], :startConferenceOnEnter => true, :endConferenceOnExit => true, :beep => false, :waitUrl => hold_call_url(:host => Settings.host, :port => Settings.port, :version => HOLD_VERSION), :waitMethod => 'GET')
       end.response
     end
+    puts response
     response    
   end
   
   def caller
+        puts "enetered caller"
     response = Twilio::Verb.new do |v|
       v.dial(:hangupOnStar => true, :action => gather_response_caller_url(caller, :host => Settings.host, :port => Settings.port, :session_id => id)) do
         v.conference(params[:session_key], :startConferenceOnEnter => true, :endConferenceOnExit => false, :beep => false, :waitUrl => hold_call_url(:host => Settings.host, :port => Settings.port, :version => HOLD_VERSION), :waitMethod => 'GET')
       end.response    
     end
+    puts response
     response
   end
     
