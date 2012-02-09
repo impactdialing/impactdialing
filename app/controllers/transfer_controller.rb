37df9079bdce0fe7c734cd680ccafe248f73953b
Nikhil Fernandes <nikhil@activesphere.com>
2012-02-09 18:25:25 +0530

    Removing action url from dial for transfer

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -40,7 +40,7 @@ class TransferController < ApplicationController
   def callee
     puts "enetered callee"
     response = Twilio::Verb.new do |v|
-      v.dial(:hangupOnStar => true, :action => gather_response_caller_url(caller, :host => Settings.host, :port => Settings.port, :session_id => id)) do
+      v.dial(:hangupOnStar => true) do
         v.conference(params[:session_key], :startConferenceOnEnter => true, :endConferenceOnExit => true, :beep => false, :waitUrl => hold_call_url(:host => Settings.host, :port => Settings.port, :version => HOLD_VERSION), :waitMethod => 'GET')
       end.response
     end
@@ -51,7 +51,7 @@ class TransferController < ApplicationController
   def caller
         puts "enetered caller"
     response = Twilio::Verb.new do |v|
-      v.dial(:hangupOnStar => true, :action => gather_response_caller_url(caller, :host => Settings.host, :port => Settings.port, :session_id => id)) do
+      v.dial(:hangupOnStar => true) do
         v.conference(params[:session_key], :startConferenceOnEnter => true, :endConferenceOnExit => false, :beep => false, :waitUrl => hold_call_url(:host => Settings.host, :port => Settings.port, :version => HOLD_VERSION), :waitMethod => 'GET')
       end.response    
     end
