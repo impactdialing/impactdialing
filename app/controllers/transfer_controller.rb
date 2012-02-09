176cfc1df7b80980c6e3af8bbdcc86d37418ba63
Nikhil Fernandes <nikhil@activesphere.com>
2012-02-09 18:33:38 +0530

    Fixing airbrake for transfer call

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -42,8 +42,8 @@ class TransferController < ApplicationController
     response = Twilio::Verb.new do |v|
       v.dial(:hangupOnStar => true) do
         v.conference(params[:session_key], :startConferenceOnEnter => true, :endConferenceOnExit => true, :beep => false, :waitUrl => hold_call_url(:host => Settings.host, :port => Settings.port, :version => HOLD_VERSION), :waitMethod => 'GET')
-      end.response
-    end
+      end
+    end.response
     puts response
     response    
   end
@@ -51,10 +51,10 @@ class TransferController < ApplicationController
   def caller
         puts "enetered caller"
     response = Twilio::Verb.new do |v|
-      v.dial(:hangupOnStar => true) do
+      v.dial(:hangupOnStar => true, :action => gather_response_caller_url(caller, :host => Settings.host, :port => Settings.port, :session_id => id)) do
         v.conference(params[:session_key], :startConferenceOnEnter => true, :endConferenceOnExit => false, :beep => false, :waitUrl => hold_call_url(:host => Settings.host, :port => Settings.port, :version => HOLD_VERSION), :waitMethod => 'GET')
-      end.response    
-    end
+      end    
+    end.response
     puts response
     response
   end
