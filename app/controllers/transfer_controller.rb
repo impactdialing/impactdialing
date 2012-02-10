52a9a12eb2dfe7aa0fd065248848b2cc4e4dfd46
Nikhil Fernandes <nikhil@activesphere.com>
2012-02-10 16:32:00 +0530

    Start conference again for warm transfer

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -42,13 +42,15 @@ class TransferController < ApplicationController
     render nothing: true
   end
   
-  def move_to_next_voter
+  def start_conference
     
   end
   
   def callee
+    caller_session = CallerSession.find(params[:caller_session])
+    caller = Caller.find(caller_session.caller_id)
     response = Twilio::Verb.new do |v|
-      v.dial(:hangupOnStar => true) do
+      v.dial(:hangupOnStar => true, action: pause_caller_url(caller, :host => Settings.host, :port => Settings.port, :session_id => caller_session.id)) do
         v.conference(params[:session_key], :startConferenceOnEnter => true, :endConferenceOnExit => true, :beep => false, :waitUrl => hold_call_url(:host => Settings.host, :port => Settings.port, :version => HOLD_VERSION), :waitMethod => 'GET')
       end
     end.response
