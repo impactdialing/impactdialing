fdc8e188b19f41fbd86c056cc10ba9406a31c436
Asif S Kalam <asif@c42.in>
2012-02-20 11:23:49 +0530

    Refactor scopes for Answer.

--- app/models/possible_response.rb
+++ app/models/possible_response.rb
@@ -3,7 +3,7 @@ class PossibleResponse < ActiveRecord::Base
   has_many :answers
   
   def stats(from_date, to_date, total_answers, campaign_id)
-    number_of_answers = answers.within(from_date, to_date, campaign_id).size
+    number_of_answers = answers.within(from_date, to_date).with_campaign_id(campaign_id).size
     {answer: value, number: number_of_answers, percentage:  total_answers == 0 ? 0 : (number_of_answers * 100 / total_answers)}
   end
   
