d1d520ee7136f87eab2a0847c5b0d23e6e6053ad
Aninda Kundu <aninda@c42.in>
2012-02-21 22:14:14 +0530

    fixing migration error

--- db/migrate/20120221162611_allow_null_in_answer.rb
+++ db/migrate/20120221162611_allow_null_in_answer.rb
@@ -1,9 +1,9 @@
 class AllowNullInAnswer < ActiveRecord::Migration
   def self.up
-    change_column(:answers, :caller_id, :integer, :null => false)
+    change_column(:answers, :caller_id, :integer, :null => true)
   end
 
   def self.down
-    change_column(:answers, :caller_id, :integer, :null => true)
+    change_column(:answers, :caller_id, :integer, :null => false)
   end
 end
