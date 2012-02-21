7d89ac8798610f97ef1aa5d82e1d935429f185b0
Aninda Kundu <aninda@c42.in>
2012-02-21 22:02:34 +0530

    null caller ids allowed in answers

---
+++ db/migrate/20120221162611_allow_null_in_answer.rb
@@ -0,0 +1,9 @@
+class AllowNullInAnswer < ActiveRecord::Migration
+  def self.up
+    change_column(:answers, :caller_id, :integer, :null => false)
+  end
+
+  def self.down
+    change_column(:answers, :caller_id, :integer, :null => true)
+  end
+end
