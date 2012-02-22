c9500e16948b16939d168f567acf3c8f85993222
Nikhil Fernandes <nikhil@activesphere.com>
2012-02-22 14:41:34 +0530

    Indexes for call_attempts

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -72,7 +72,6 @@ class CallerController < ApplicationController
   end
 
   def gather_response
-    start_time = Time.now
     caller = Caller.find(params[:id])
     caller_session = caller.caller_sessions.find(params[:session_id])
     question = Question.find_by_id(params[:question_id])
