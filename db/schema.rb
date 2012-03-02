96ee8f4e6dd6d820d0255a3b9a94061ede250e47
Nikhil Fernandes <nikhil@activesphere.com>
2012-03-02 14:38:33 +0530

    Copying twilio stats for broadcast campaigns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120222085859) do
+ActiveRecord::Schema.define(:version => 20120302084830) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "card_verified"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(:version => 20120222085859) do
     t.integer  "possible_response_id", :null => false
     t.datetime "created_at"
     t.integer  "campaign_id"
-    t.integer  "caller_id",            :null => false
+    t.integer  "caller_id"
   end
 
   add_index "answers", ["voter_id", "question_id"], :name => "index_answers_on_voter_id_and_question_id"
