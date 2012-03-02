c73fbe6665895a3bcc6513bbe11ef0529a0bedef
Nikhil Fernandes <nikhil@activesphere.com>
2012-03-02 14:58:16 +0530

    Copying twilio stats for broadcast campaigns

---
+++ db/migrate/20120302092545_copy_twilio_statsfor_robo_campaigns.rb
@@ -0,0 +1,9 @@
+class CopyTwilioStatsforRoboCampaigns < ActiveRecord::Migration
+  def self.up
+     CallAttempt.connection.execute("update call_attempts set connecttime = tStartTime , call_end = tEndTime where connecttime is null and call_end is null and campaign_id in (select id from campaigns where robo = true)");
+  end
+
+  def self.down
+    CallAttempt.connection.execute("update call_attempts set connecttime = null , call_end = null where connecttime is not null and call_end is not null and campaign_id in (select id from campaigns where robo = true)");
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120302084830) do
+ActiveRecord::Schema.define(:version => 20120302092545) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "card_verified"
