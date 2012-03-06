c46d08f242c138372ce47ecd7c8063bf9f4b2c1c
Nikhil Fernandes <nikhil@activesphere.com>
2012-03-06 16:36:55 +0530

    Api for voter list upload

--- app/controllers/api/callers_controller.rb
+++ app/controllers/api/callers_controller.rb
@@ -1,30 +1,8 @@
 module Api
   class CallersController < ApiController
 
-
     def validate_params
-      begin
-        Campaign.find(params[:campaign_id])
-        Account.find(params[:account_id])
-      rescue Exception => err
-        render_json_response({status: 'error', code: '404', message: err.to_s})
-        return false
-      end
-      
-      if !params[:campaign_id].blank? && !params[:account_id].blank?
-        campaign = Campaign.find(params[:campaign_id])
-        if campaign.account_id.to_s != params[:account_id]
-          render_json_response({status: 'error', code: '400' , message: 'Campaign is not assigned to the account'})
-          return false
-        end
-      end    
-      
-      if params[:email].blank?
-        render_json_response({status: 'error', code: '400', message: 'Email cannot be blank'})
-        return false
-      end
-      return true
-    
+      validate_campaign(params[:campaign_id]) && validate_account(params[:account_id]) && validate_campaign_belongs_to_account(params[:campaign_id], params[:account_id]) && validate_email_not_blank(params[:email])
     end
     
     def create
