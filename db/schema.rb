103fab1c3c87f2713878a5cda74056533057e67c
Michael Kaiser-Nyman <michael@impactdialing.com>
2012-03-20 12:00:04 -0700

    account views

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(:version => 20120312080357) do
     t.boolean  "record_calls",              :default => false
     t.string   "recurly_account_code"
     t.string   "subscription_name"
+    t.integer  "subscription_count"
     t.boolean  "subscription_active",       :default => false
     t.string   "recurly_subscription_uuid"
-    t.integer  "subscription_count"
     t.boolean  "autorecharge_enabled",      :default => false
     t.float    "autorecharge_trigger"
     t.float    "autorecharge_amount"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(:version => 20120312080357) do
     t.integer  "possible_response_id", :null => false
     t.datetime "created_at"
     t.integer  "campaign_id"
-    t.integer  "caller_id"
+    t.integer  "caller_id",            :null => false
   end
 
   add_index "answers", ["voter_id", "question_id"], :name => "index_answers_on_voter_id_and_question_id"
@@ -327,14 +327,12 @@ ActiveRecord::Schema.define(:version => 20120312080357) do
 
   create_table "payments", :force => true do |t|
     t.float    "amount_paid"
-    t.integer  "account_id"
     t.float    "amount_remaining"
-    t.integer  "recurly_account_code"
+    t.integer  "recurly_transaction_uuid"
+    t.integer  "account_id"
     t.string   "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type"
-    t.string   "recurly_transaction_uuid"
   end
 
   create_table "possible_responses", :force => true do |t|
