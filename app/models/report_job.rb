479f7f8418e99fe264e072ab4d9d39f3d886805e
Nikhil Fernandes <nikhil@activesphere.com>
2012-03-26 15:34:42 +0530

    Escape / in download reports to prevent breakage

--- app/models/report_job.rb
+++ app/models/report_job.rb
@@ -15,6 +15,7 @@ class ReportJob < Struct.new(:campaign, :user, :selected_voter_fields, :selected
     FileUtils.mkdir_p(Rails.root.join("tmp"))
     uuid = UUID.new.generate
     @campaign_name = "#{uuid}_report_#{campaign.name}"
+    @campaign.tr("/\000", "")
     filename = "#{Rails.root}/tmp/#{@campaign_name}.csv"
     report_csv = @report.split("\n")
     file = File.open(filename, "w")
