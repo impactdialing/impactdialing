f2d2026b13af4d4939914e84ad4fd3fccb4cd182
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-02 12:36:55 +0530

    Dialer log

--- lib/predictive_dialer.rb
+++ lib/predictive_dialer.rb
@@ -1,14 +1,16 @@
 #require File.join("../", 'config/environment')
 RAILS_ROOT = File.expand_path('../..', __FILE__)
 require File.join(RAILS_ROOT, 'config/environment')
-#ActiveRecord::Base.logger = DIALER_LOGGER #why would you want to log all the sql ??
-ActiveRecord::Base.logger = nil #why would you want to log all the sql ??
+DIALER_ROOT = ENV['DIALER_ROOT'] || File.expand_path('..', __FILE__)
+FileUtils.mkdir_p(File.join(DIALER_ROOT, 'log'), :verbose => true)
+ActiveRecord::Base.logger = Logger.new(File.open(File.join(DIALER_ROOT, 'log', "dialer_#{RAILS_ENV}.log"), 'a'))
+
 
 loop do
   begin
-    DIALER_LOGGER.info "starting dialer"
+    ActiveRecord::Base.logger.info "starting dialer"
     logged_in_campaigns = ActiveRecord::Base.connection.execute("select distinct campaign_id from caller_sessions where on_call=1")
-    DIALER_LOGGER.info "============ logged_in_campaigns: #{logged_in_campaigns.num_rows} ============"
+    ActiveRecord::Base.logger.info "============ logged_in_campaigns: #{logged_in_campaigns.num_rows} ============"
     logged_in_campaigns.each do |k|
       campaign = Campaign.find(k.first)
       if campaign.predictive_type != Campaign::Type::PREVIEW && campaign.predictive_type != Campaign::Type::PROGRESSIVE
@@ -18,11 +20,11 @@ loop do
     sleep 3
   rescue Exception => e
     if e.class==SystemExit
-      DIALER_LOGGER.info "============ EXITING  ============"
+      ActiveRecord::Base.logger.info "============ EXITING  ============"
       exit 
     end
-    DIALER_LOGGER.info "DIALER EXCEPTION Rescued - #{ e } (#{ e.class })!"
-    DIALER_LOGGER.info "DIALER EXCEPTION Backtrace : #{e.backtrace}"
+    ActiveRecord::Base.logger.info "DIALER EXCEPTION Rescued - #{ e } (#{ e.class })!"
+    ActiveRecord::Base.logger.info "DIALER EXCEPTION Backtrace : #{e.backtrace}"
     ActiveRecord::Base.connection.reconnect!
   end
 end
