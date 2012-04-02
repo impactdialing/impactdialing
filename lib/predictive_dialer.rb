dff65139d53f513cc7e45b60874a81f228566e6e
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-02 12:50:28 +0530

    Dialer log

--- lib/predictive_dialer.rb
+++ lib/predictive_dialer.rb
@@ -5,13 +5,10 @@ DIALER_ROOT = ENV['DIALER_ROOT'] || File.expand_path('..', __FILE__)
 FileUtils.mkdir_p(File.join(DIALER_ROOT, 'log'), :verbose => true)
 ActiveRecord::Base.logger = Logger.new(File.open(File.join(DIALER_ROOT, 'log', "dialer_#{RAILS_ENV}.log"), 'a'))
 
-puts "dialer"
 loop do
   begin
-    puts "dialer123"
-    ActiveRecord::Base.logger.info "starting dialer"
     logged_in_campaigns = ActiveRecord::Base.connection.execute("select distinct campaign_id from caller_sessions where on_call=1")
-    ActiveRecord::Base.logger.info "============ logged_in_campaigns: #{logged_in_campaigns.num_rows} ============"
+    puts "============ logged_in_campaigns: #{logged_in_campaigns.num_rows} ============"
     logged_in_campaigns.each do |k|
       campaign = Campaign.find(k.first)
       if campaign.predictive_type != Campaign::Type::PREVIEW && campaign.predictive_type != Campaign::Type::PROGRESSIVE
@@ -21,11 +18,11 @@ loop do
     sleep 3
   rescue Exception => e
     if e.class==SystemExit
-      ActiveRecord::Base.logger.info "============ EXITING  ============"
+      puts "============ EXITING  ============"
       exit 
     end
-    ActiveRecord::Base.logger.info "DIALER EXCEPTION Rescued - #{ e } (#{ e.class })!"
-    ActiveRecord::Base.logger.info "DIALER EXCEPTION Backtrace : #{e.backtrace}"
+    puts "DIALER EXCEPTION Rescued - #{ e } (#{ e.class })!"
+    puts "DIALER EXCEPTION Backtrace : #{e.backtrace}"
     ActiveRecord::Base.connection.reconnect!
   end
 end
