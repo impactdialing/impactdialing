a22919920d4db3dfa8bfcfe76b6c6c17c0fac3c3
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-03 19:13:34 +0530

    preview  voter on start calling

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -145,6 +145,7 @@ class CallerController < ApplicationController
       @session = @caller.create_caller_session(@identity.session_key, params[:CallSid])
       Moderator.caller_connected_to_campaign(@caller, @caller.campaign, @session)
       @session.publish('start_calling', {caller_session_id: @session.id}) 
+      @session.preview_voter
       render xml:  @caller.is_on_call? ? @caller.already_on_call : @session.start
     end
   end
--- app/controllers/callin_controller.rb
+++ app/controllers/callin_controller.rb
@@ -23,6 +23,7 @@ class CallinController < ApplicationController
 
       Moderator.caller_connected_to_campaign(@caller, @caller.campaign, @session)
       @session.publish('start_calling', {caller_session_id: @session.id}) 
+      @session.preview_voter
       render :xml => @caller.is_phones_only? ? @caller.ask_instructions_choice(@session) : @session.start
     else
       render :xml => Caller.ask_for_pin(params[:attempt].to_i)
