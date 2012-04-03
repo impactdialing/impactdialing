ea1d08d518f0a38eace7711b228d71b3ef4e6655
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-03 14:23:42 +0530

    Revert "Bug fix for end session and wtf"
    This reverts commit 37a36e649ea5b73565b3e2bd490cb1220c2082da.

--- db/migrate/20120402081433_add_websocet_connected_to_caller_session.rb
+++
@@ -1,9 +0,0 @@
-class AddWebsocetConnectedToCallerSession < ActiveRecord::Migration
-  def self.up
-    add_column(:caller_sessions, :websocket_connected, :boolean)
-  end
-
-  def self.down
-    remove_column(:caller_sessions, :websocket_connected)
-  end
-end
