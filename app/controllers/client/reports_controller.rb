2bf15cf8c4bab189f20cfcd23e88d1251c8d3ff4
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-04 18:31:25 +0530

    Dial reports issues

--- app/controllers/client/reports_controller.rb
+++ app/controllers/client/reports_controller.rb
@@ -16,7 +16,7 @@ module Client
     def dials
       set_date_range
       @total_voters_count = @campaign.all_voters.count
-      dialed_voters_ids = Voter.find(:all, :select => 'id' ,:conditions => [ "(voters.campaign_id = ?) AND (last_call_attempt_time BETWEEN  ? AND ?) ", @campaign.id, @from_date, (@to_date + 1.day)])
+      dialed_voters_ids = Voter.find(:all, :select => 'id' ,:conditions => [ "(voters.campaign_id = ?) AND (last_call_attempt_time BETWEEN  ? AND ?) ", @campaign.id, @from_date, @to_date])
       unless dialed_voters_ids.empty?
         @answered = @campaign.answered_count(dialed_voters_ids)
         @no_answer = @campaign.all_voters.last_call_attempt_within(@from_date, @to_date).by_status(CallAttempt::Status::NOANSWER).count
