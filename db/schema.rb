4c35bdc040a8e04f0e9c67d1fa1b6a26435f257e
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-06 14:10:45 +0530

    changing null recycle rate to 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120406082851) do
+ActiveRecord::Schema.define(:version => 20120406083908) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "card_verified"
