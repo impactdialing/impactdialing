88917cd2b74157f1638102cb91cc3ed109062547
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-06 17:40:28 +0530

    reports download date issues

--- app/models/transfer_attempt.rb
+++ app/models/transfer_attempt.rb
@@ -5,7 +5,7 @@ class TransferAttempt < ActiveRecord::Base
   belongs_to :call_attempt
   belongs_to :campaign
   include Rails.application.routes.url_helpers
-  scope :within, lambda { |from, to, campaign_id| where(:created_at => from..(to + 1.day)).where(campaign_id: campaign_id)}
+  scope :within, lambda { |from, to, campaign_id| where(:created_at => from..to).where(campaign_id: campaign_id)}
   scope :between, lambda { |from_date, to_date| {:conditions => {:created_at => from_date..to_date}} }
 
   
