79750f133fbe4f4663c7d44ebf2bf6d3869b9ada
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-12 15:56:21 +0530

    download report on user browser

--- app/models/downloaded_report.rb
+++ app/models/downloaded_report.rb
@@ -3,7 +3,14 @@ class DownloadedReport < ActiveRecord::Base
     belongs_to :campaign
     
     
-
+   def self.active_reports(campaign_id)
+     DownloadedReport.find_by_campaign_id(campaign_id, ,:conditions => [ "(created_at > ?)", 12.hours.ago])
+   end
+   
+   def self.active_reports_count(campaign_id)
+     DownloadedReport.count(:conditions => [ "campaign_id = ? AND (created_at > ?)", campaign_id, 12.hours.ago])
+   end
+   
     
     
 end
