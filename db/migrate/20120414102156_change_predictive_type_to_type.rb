7188cb8ccdb74b5aa0c8132ec6fe5259a20933cc
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-16 16:23:54 +0530

    answer reports with ton of questions

--- db/migrate/20120414102156_change_predictive_type_to_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangePredictiveTypeToType < ActiveRecord::Migration
-  def self.up
-    rename_column :campaigns, :predictive_type, :type
-  end
-
-  def self.down
-    rename_column :campaigns, :type, :predictive_type
-  end
-end
