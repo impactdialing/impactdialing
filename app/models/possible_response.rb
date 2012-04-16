071fe7ef0576fcc4d2641f44ab75f566675a0c26
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-16 16:23:21 +0530

    answer reports with ton of questions

--- app/models/possible_response.rb
+++ app/models/possible_response.rb
@@ -2,9 +2,10 @@ class PossibleResponse < ActiveRecord::Base
   belongs_to :question
   has_many :answers
   
-  def stats(from_date, to_date, total_answers, campaign_id)
-    number_of_answers = answers.within(from_date, to_date).with_campaign_id(campaign_id).size
+  def stats(answer_count, total_count)
+    number_of_answers = answer_count[self.id]
+    total_answers = total_count[question_id]
     {answer: value, number: number_of_answers, percentage:  total_answers == 0 ? 0 : (number_of_answers * 100 / total_answers)}
   end
-    
+      
 end
