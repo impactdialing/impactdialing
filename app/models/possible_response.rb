7a4e5ed630e84daf4a6f2d6ed4f1942109ca5340
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-17 09:40:24 +0530

    airbrake

--- app/models/possible_response.rb
+++ app/models/possible_response.rb
@@ -4,7 +4,7 @@ class PossibleResponse < ActiveRecord::Base
   
   def stats(answer_count, total_count)
     number_of_answers = answer_count[self.id] || 0
-    total_answers = total_count[question_id] 
+    total_answers = total_count[question_id] || 1
     {answer: value, number: number_of_answers, percentage:  total_answers == 0 ? 0 : (number_of_answers * 100 / total_answers)}
   end
       
