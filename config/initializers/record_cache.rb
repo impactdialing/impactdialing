cdbbcc6b30cb80c8cbb01a9e3794252e040ea766
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-20 13:07:05 +0530

    merge

--- config/initializers/record_cache.rb
+++ config/initializers/record_cache.rb
@@ -2,7 +2,7 @@
 # All Workers that use the Record Cache should point to the same Version Store
 # E.g. a MemCached cluster or a Redis Store (defaults to Rails.cache)
 if Rails.env == 'heroku' || Rails.env == "heroku_staging"
-  RecordCache::Base.version_store = Rails.cache
+  RecordCache::Base.version_store = :dalli
   RecordCache::Base.register_store(:local, Rails.cache)
   RecordCache::Base.register_store(:shared, Rails.cache)
 else
