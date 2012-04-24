9f4b5efa4f42a127d4a60144c0684638688f8e7e
michael <michaelrkn@gmail.com>
2012-04-24 09:06:47 -0700

    removed old helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,40 +2,6 @@
 module ApplicationHelper
   include WhiteLabeling
 
-  def cms(key)
-    s = Seo.find_by_crmkey_and_active_and_version(key, 1, session[:seo_version])
-    s = Seo.find_by_crmkey_and_active_and_version(key, 1, nil) if s.blank?
-
-    if s.blank?
-      ""
-    else
-      s.content
-    end
-  end
-
-  def float_sidebar
-    @floatSidebar="<script>
-    $('content').style.float='right';
-    $('sidebar').style.float='left';
-
-      var obj = document.getElementById('sidebar');
-      if (obj.style.styleFloat) {
-          obj.style.styleFloat = 'left';
-      } else {
-          obj.style.cssFloat = 'left';
-      }
-
-      var obj = document.getElementById('content');
-      if (obj.style.styleFloat) {
-          obj.style.styleFloat = 'right';
-      } else {
-          obj.style.cssFloat = 'right';
-      }
-
-      </script>";
-    ""
-  end
-
   def send_rt(channel, key, post_data)
     require 'pusher'
     Pusher.app_id = PUSHER_APP_ID
