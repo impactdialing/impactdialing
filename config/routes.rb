20fa49ca842b109a7bce44566ee8c3b0371d42e8
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-25 21:12:37 +0530

    adding submit resultsto routes

--- config/routes.rb
+++ config/routes.rb
@@ -187,6 +187,9 @@ ImpactDialing::Application.routes.draw do
     member do
       post :flow
       post :hangup
+      post :submit_result
+      post :submit_result_and_stop
+      
     end
   end
 
