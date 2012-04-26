530deaa27e483c2ee4c4f190e53d609d0f6a5b24
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-26 22:56:21 +0530

    redirect caller after submit result modifying flow url

--- app/models/call.rb
+++ app/models/call.rb
@@ -151,7 +151,7 @@ class Call < ActiveRecord::Base
   
   def redirect_caller
     Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
-    Twilio::Call.redirect(caller_session.sid, flow_caller_url(:host => Settings.host, :port => Settings.port, session_id: caller_session.id, event: "start_conf"))
+    Twilio::Call.redirect(caller_session.sid, flow_caller_url(caller_session.caller, :host => Settings.host, :port => Settings.port, session_id: caller_session.id, event: "start_conf"))
   end
   
   
