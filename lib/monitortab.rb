95cc6e2cf1f4f44c171d480d53ec2ac372aa1ba0
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-30 16:33:28 +0530

    monitor tab sync request

--- lib/monitortab.rb
+++ lib/monitortab.rb
@@ -5,15 +5,12 @@ require 'em-http-request'
 loop do
   begin
     monitors = Moderator.active
-    puts monitors
     monitors.each do |monitor|
       monitor.account.campaigns.each do |campaign|
-        puts campaign
         caller_sessions = CallerSession.on_call.on_campaign(campaign)
-        puts caller_sessions
         caller_sessions.each do |caller_session|
           Moderator.publish_event_sync(campaign, 'voter_event', {caller_session_id:  caller_session.id, campaign_id:  campaign.id, caller_id:  caller_session.caller.id, call_status: caller_session.attempt_in_progress.try(:status)})      
-          Moderator.publish_event_sync(campaign, 'update_dials_in_progress', {:campaign_id => campaign.id, :dials_in_progress => campaign.call_attempts.not_wrapped_up.size, :voters_remaining => Voter.remaining_voters_count_for('campaign_id', campaign.id)})            
+          Moderator.publish_event_sync(campaign, 'update_dials_in_progress', {:campaign_id => campaign.id, :dials_in_progress => campaign.call_attempts.current_ringing_lines_count, :voters_remaining => Voter.remaining_voters_count_for('campaign_id', campaign.id)})            
         end
       end      
     end
