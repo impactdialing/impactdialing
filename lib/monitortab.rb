f6f8052adee3ee245d4b887ad1a9087ff6083421
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-30 17:04:12 +0530

    monitor tab ringing lines

--- lib/monitortab.rb
+++ lib/monitortab.rb
@@ -10,7 +10,7 @@ loop do
         caller_sessions = CallerSession.on_call.on_campaign(campaign)
         caller_sessions.each do |caller_session|
           Moderator.publish_event_sync(campaign, 'voter_event', {caller_session_id:  caller_session.id, campaign_id:  campaign.id, caller_id:  caller_session.caller.id, call_status: caller_session.attempt_in_progress.try(:status)})      
-          Moderator.publish_event_sync(campaign, 'update_dials_in_progress', {:campaign_id => campaign.id, :dials_in_progress => campaign.call_attempts.current_ringing_lines_count, :voters_remaining => Voter.remaining_voters_count_for('campaign_id', campaign.id)})            
+          Moderator.publish_event_sync(campaign, 'update_dials_in_progress', {:campaign_id => campaign.id, :dials_in_progress => campaign.call_attempts.not_wrapped_up.size, :voters_remaining => Voter.remaining_voters_count_for('campaign_id', campaign.id)})            
         end
       end      
     end
