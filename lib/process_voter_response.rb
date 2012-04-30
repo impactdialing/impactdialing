e8138bd508e64a0cbf7ed8578bc5a848d97768de
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-30 12:57:13 +0530

    process voter response

--- lib/process_voter_response.rb
+++ lib/process_voter_response.rb
@@ -7,7 +7,7 @@ loop do
     call_attempts.each do |call_attempt|
       call_attempt.voter.persist_answers(call_attempt.call.questions, call_attempt)
       call_attempt.voter.persist_notes(call_attempt.call.notes)
-      call_attempt.update_attributs(voter_response_processed: true, result_date: Time.now)
+      call_attempt.update_attributes(voter_response_processed: true, result_date: Time.now)
     end
     
   rescue Exception => e
