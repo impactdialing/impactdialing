f917ecacd2561b68e2126296be033947b8eca78a
Nikhil Fernandes <nikhil@activesphere.com>
2012-04-30 16:37:24 +0530

    monitor tab sync request

--- lib/monitortab.rb
+++ lib/monitortab.rb
@@ -10,7 +10,7 @@ loop do
         caller_sessions = CallerSession.on_call.on_campaign(campaign)
         caller_sessions.each do |caller_session|
           Moderator.publish_event_sync(campaign, 'voter_event', {caller_session_id:  caller_session.id, campaign_id:  campaign.id, caller_id:  caller_session.caller.id, call_status: caller_session.attempt_in_progress.try(:status)})      
-          Moderator.publish_event_sync(campaign, 'update_dials_in_progress', {:campaign_id => campaign.id, :dials_in_progress => campaign.call_attempts.current_ringing_lines_count, :voters_remaining => Voter.remaining_voters_count_for('campaign_id', campaign.id)})            
+          Moderator.publish_event_sync(campaign, 'update_dials_in_progress', {:campaign_id => campaign.id, :dials_in_progress => campaign.call_attempts.between(20.seconds.ago, Time.now).with_status(CallAttempt::Status::RINGING).size, :voters_remaining => Voter.remaining_voters_count_for('campaign_id', campaign.id)})            
         end
       end      
     end
