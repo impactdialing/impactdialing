f347cdb688ca5bb78e36518ae92e133dabfef10c
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-01 14:14:45 +0530

    fixing all tests

--- app/models/phones_only_caller_session.rb
+++ app/models/phones_only_caller_session.rb
@@ -162,7 +162,7 @@ class PhonesOnlyCallerSession < CallerSession
   
   
   def select_voter(voter)
-    voter ||= campaign.next_voter_in_dial_queue(current_voter)
+    voter ||= campaign.next_voter_in_dial_queue(voter)
     update_attributes(voter_in_progress: voter)
   end
   
