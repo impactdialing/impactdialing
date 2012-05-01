a6f8627fad36f14f0055f9d3158125340c27f333
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-01 12:32:13 +0530

    airbrake for lead events

--- app/models/lead_events.rb
+++ app/models/lead_events.rb
@@ -6,12 +6,16 @@ module LeadEvents
   module InstanceMethods
     
     def publish_voter_connected
-      event_hash = campaign.voter_connected_event(self.call)
-      caller_session.publish_async(event_hash[:event], event_hash[:data])
+      unless caller_session.caller.is_phones_only?
+        event_hash = campaign.voter_connected_event(self.call)
+        caller_session.publish_async(event_hash[:event], event_hash[:data])
+      end
     end    
     
     def publish_voter_disconnected
-      caller_session.publish_async('voter_disconnected',{})
+      unless caller_session.caller.is_phones_only?
+        caller_session.publish_async('voter_disconnected',{})
+      end
     end
     
   end
