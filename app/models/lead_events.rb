bb40c89643a5d93f2fad5ad9da325f3c9438bc1b
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-02 13:34:30 +0530

    Airbrake NoMethodError: private method 'caller' called for nil:NilClass

--- app/models/lead_events.rb
+++ app/models/lead_events.rb
@@ -6,19 +6,21 @@ module LeadEvents
   module InstanceMethods
     
     def publish_voter_connected      
-      EM.run {
-        unless caller_session.caller.is_phones_only?
-          event_hash = campaign.voter_connected_event(self.call)        
-          caller_deferrable = Pusher[caller_session.session_key].trigger_async(event_hash[:event], event_hash[:data].merge!(:dialer => campaign.type))
-          caller_deferrable.callback {}
-          caller_deferrable.errback { |error| }
-        end
-        campaign.account.moderators.last_hour.active.each do |moderator|
-          moderator_deferrable = Pusher[moderator.session].trigger_async('voter_event', {caller_session_id:  caller_session.id, campaign_id:  campaign.id, caller_id:  caller_session.caller.id, call_status: caller_session.attempt_in_progress.try(:status)})      
-          moderator_deferrable.callback {}
-          moderator_deferrable.errback { |error| }          
-        end              
-         }
+      unless caller_session.nil?
+        EM.run {
+          unless caller_session.caller.is_phones_only?
+            event_hash = campaign.voter_connected_event(self.call)        
+            caller_deferrable = Pusher[caller_session.session_key].trigger_async(event_hash[:event], event_hash[:data].merge!(:dialer => campaign.type))
+            caller_deferrable.callback {}
+            caller_deferrable.errback { |error| }
+          end
+          campaign.account.moderators.last_hour.active.each do |moderator|
+            moderator_deferrable = Pusher[moderator.session].trigger_async('voter_event', {caller_session_id:  caller_session.id, campaign_id:  campaign.id, caller_id:  caller_session.caller.id, call_status: caller_session.attempt_in_progress.try(:status)})      
+            moderator_deferrable.callback {}
+            moderator_deferrable.errback { |error| }          
+          end              
+           }
+      end
     end    
     
     def publish_voter_disconnected
