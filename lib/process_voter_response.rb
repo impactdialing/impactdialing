393c7b6b0ef056dac1ed1a4084f8f4ae745875c4
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-03 18:29:52 +0530

    airbrake

--- lib/process_voter_response.rb
+++ lib/process_voter_response.rb
@@ -3,15 +3,14 @@ require File.join(RAILS_ROOT, 'config/environment')
 
 loop do
   begin
-    CallAttempt.results_not_processed.limit(10) do |call_attempts|
-      call_attempts.each do |call_attempt|
-        call_attempt.voter.persist_answers(call_attempt.call.questions, call_attempt)
-        call_attempt.voter.persist_notes(call_attempt.call.notes)
-        call_attempt.update_attribute(:voter_response_processed, true)
-        call_attempt.voter.update_attribute(:result_date, Time.now)
-      end
-    GC.start  
+    CallAttempt.results_not_processed.limit(10).each do |call_attempt|
+      call_attempt.voter.persist_answers(call_attempt.call.questions, call_attempt)
+      call_attempt.voter.persist_notes(call_attempt.call.notes)
+      call_attempt.update_attributes(voter_response_processed: true)
+      call_attempt.voter.update_attribute(:result_date, Time.now)
     end
+    GC.start
   rescue Exception => e
+    puts e.backtrace
   end
 end
