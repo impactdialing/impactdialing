1add682892bd44133cc65ce0e38905e8ae56accc
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-04 12:17:54 +0530

    wrong error message saving campaign without caller id

--- app/controllers/client/campaigns_controller.rb
+++ app/controllers/client/campaigns_controller.rb
@@ -59,7 +59,7 @@ module Client
       begin
         @campaign.save!      
       rescue ActiveRecord::RecordInvalid => e
-        flash_message(:error, e)
+        flash_message(:error, e.to_s)
         redirect_to :back
         return
       end
