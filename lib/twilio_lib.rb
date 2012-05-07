d2de7b3fa0da316bafe5c7b324212ede44df887d
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-07 16:49:52 +0530

    adding method param to redirect url

--- lib/twilio_lib.rb
+++ lib/twilio_lib.rb
@@ -32,7 +32,7 @@ class TwilioLib
     # http.use_ssl=true
     params = {'Url'=> redirect_url, "method" => 'POST'}
     deferrable = EM::DefaultDeferrable.new
-    http = EventMachine::HttpRequest.new("https://#{@server}:#{@port}#{@root}Calls/#{call_sid}").post({:head => {'authorization' => [@http_user, @http_password]} })
+    http = EventMachine::HttpRequest.new("https://#{@server}:#{@port}#{@root}Calls/#{call_sid}").post({:head => {'authorization' => [@http_user, @http_password]},query: params })
     http.callback {
             begin
               deferrable.succeed
