44421786c3fd6800c1e6428adfb862abd3506c95
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-09 12:40:32 +0530

    reverting script changes

--- spec/models/call_spec.rb
+++ spec/models/call_spec.rb
@@ -706,7 +706,7 @@ describe Call do
       end
       
       it "should wrapup call_attempt" do
-        call = Factory(:call, answered_by: "human", call_attempt: @call_attempt, state: 'call_answered_by_lead')
+        call = Factory(:call, answered_by: "human", call_attempt: @call_attempt, state: 'call_answered_by_lead', all_states: "")
         call.call_attempt.should_receive(:publish_moderator_response_submited)
         call.call_attempt.should_receive(:redirect_caller)
         call.submit_result!
@@ -724,7 +724,7 @@ describe Call do
       end
       
       it "should wrapup call_attempt" do
-        call = Factory(:call, answered_by: "human", call_attempt: @call_attempt, state: 'call_answered_by_lead')
+        call = Factory(:call, answered_by: "human", call_attempt: @call_attempt, state: 'call_answered_by_lead', all_states: "")
         call.submit_result_and_stop!
         call.call_attempt.wrapup_time.should_not be_nil
       end
