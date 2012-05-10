7efc3bfd644fafbd16ee66f2f8ec48fc74a1b146
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-10 14:42:19 +0530

    optimizing mysql cpu utilization

--- simulator/newest_simulator.rb
+++ simulator/newest_simulator.rb
@@ -216,11 +216,11 @@ end
 
 loop do
   begin
-    logged_in_campaigns = ActiveRecord::Base.connection.execute("select distinct campaign_id from caller_sessions where on_call=1")
-    logged_in_campaigns.each do |k|     
-      puts "Simulating #{k.first}"
-      campaign = Campaign.find(k.first)      
-      simulate(k.first) if campaign.predictive_type == Campaign::Type::PREDICTIVE
+    logged_in_campaigns = CallerSession.campaigns_on_call
+    logged_in_campaigns.each do |c|     
+      puts "Simulating #{c.campaign_id}"
+      campaign = Campaign.find(c.campaign_id)      
+      simulate(c.campaign_id) if campaign.predictive_type == Campaign::Type::PREDICTIVE
     end
     sleep 3
   rescue Exception => e
