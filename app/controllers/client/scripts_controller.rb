9aaab2a44bcd682acd58204ea5cb9eeb894c5eb2
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-14 14:07:20 +0530

    scripts performance

--- app/controllers/client/scripts_controller.rb
+++ app/controllers/client/scripts_controller.rb
@@ -36,9 +36,11 @@ module Client
     def show
       @script = @user.account.scripts.find(params[:id])
       @script.questions << [Question.new(possible_responses: [PossibleResponse.new])]  if @script.questions.empty?
-      #@voter_fields = ["CustomID","FirstName","MiddleName","LastName","Suffix","Address","City","State/Province","Zip/Postal Code","Country","Phone","Email"]
       @voter_fields = VoterList::VOTER_DATA_COLUMNS.values
       @voter_fields.concat(@user.account.custom_voter_fields.collect{ |field| field.name})
+      Answer.select("possible_response_id").where("campaign_id = ?", self.id).within(from_date, to_date).group("possible_response_id").count
+      @answered_questions = Question.select("id").where("script_id = ?",self.id).group("id").count
+      @answered_possible_response = PossibleResponse.select("id").where("question_id in (?)",@script.questions.collect{|q| q.id}).group("id").count
       if @script.voter_fields!=nil
         begin
           @voter_field_values = JSON.parse(@script.voter_fields)
