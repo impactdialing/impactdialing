3f970be510cb1f48a10e1c80b7274ca7630d0f54
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-21 19:09:10 +0530

    report job error

--- jobs/report_download_job.rb
+++ jobs/report_download_job.rb
@@ -6,7 +6,7 @@ class ReportDownloadJob
 
 
    def self.perform(campaign, user, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
-     report_job = new ReportJob(campaign, user, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
+     report_job = ReportJob.new(campaign, user, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
      report_job.perform     
    end
 end
