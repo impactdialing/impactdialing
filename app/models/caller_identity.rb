0eac67e84eb982e27ca89beeadb5800ce272461f
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-22 15:28:42 +0530

    Caller Identity issue

--- app/models/caller_identity.rb
+++ app/models/caller_identity.rb
@@ -3,9 +3,10 @@ class CallerIdentity < ActiveRecord::Base
  
  def self.create_uniq_pin
    uniq_pin=0
+   
    while uniq_pin==0 do
      pin = rand.to_s[2..6]
-     check = CallerIndentity.find_by_pin(pin)
+     check = find_by_pin(pin)
      uniq_pin=pin if check.blank?
    end
    uniq_pin
