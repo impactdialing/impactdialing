5a812c0af5743ebad080c2f1a69a5205b6fecbcf
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-22 12:01:51 +0530

    stale object stop calling

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -32,12 +32,7 @@ class CallerController < ApplicationController
   end
   
   def stop_calling
-    begin
-      @caller_session.process('stop_calling') unless @caller_session.nil?
-    rescue ActiveRecord::StaleObjectError
-      caller_session = CallerSession.find_by_id(params[:session_id]) || CallerSession.find_by_sid(params[:CallSid])
-      caller_session.process('stop_calling')
-    end
+    @caller_session.process('stop_calling') unless @caller_session.nil?
     render :nothing => true
   end
   
