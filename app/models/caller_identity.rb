2fa159aa3ac64923b7d9d34bc44091b19e3363b9
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-22 15:11:43 +0530

    campaign closed message

--- app/models/caller_identity.rb
+++ app/models/caller_identity.rb
@@ -1,3 +1,14 @@
 class CallerIdentity < ActiveRecord::Base 
  belongs_to :caller  
+ 
+ def self.create_uniq_pin
+   uniq_pin=0
+   while uniq_pin==0 do
+     pin = rand.to_s[2..6]
+     check = CallerIndentity.find_by_pin(pin)
+     uniq_pin=pin if check.blank?
+   end
+   uniq_pin
+ end
+ 
 end
