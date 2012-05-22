19e057cf96ff36fe12db1cd7a04f566e0cfd71e0
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-22 16:45:18 +0530

    heroku autoscale requiring module

--- jobs/voter_list_upload_job.rb
+++ jobs/voter_list_upload_job.rb
@@ -1,5 +1,6 @@
+require 'heroku_resque_auto_scale'
 class VoterListUploadJob 
-  extend HerokuResqueAutoScale
+  extend ::HerokuResqueAutoScale
   @queue = :worker_job
 
    def self.perform(separator, column_headers, csv_to_system_map, filename, voter_list_name, campaign_id, account_id, domain, email, callback_url, strategy="webui")
