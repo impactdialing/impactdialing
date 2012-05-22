ea22d018312ffc08df77726d300cd778b159edde
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-22 13:46:25 +0530

    moving to resque director

--- jobs/report_download_job.rb
+++ jobs/report_download_job.rb
@@ -1,8 +1,8 @@
 require 'resque/plugins/lock'
 class ReportDownloadJob 
   extend Resque::Plugins::Director
-  direct :min_workers => 0, :max_workers => 10, :max_time => 60, :max_queue => 0, :wait_time => 30
-  @queue = :report_download
+  direct :min_workers => 1, :max_workers => 10, :max_time => 60, :max_queue => 1, :wait_time => 30
+  @queue = :worker_job
 
 
    def self.perform(campaign_id, user_id, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
--- jobs/voter_list_upload_job.rb
+++ jobs/voter_list_upload_job.rb
@@ -2,8 +2,8 @@ require 'resque/plugins/lock'
 
 class VoterListUploadJob 
   extend Resque::Plugins::Director
-  direct :min_workers => 0, :max_workers => 10, :max_time => 60, :max_queue => 0, :wait_time => 30
-  @queue = :voter_list
+  direct :min_workers => 1, :max_workers => 10, :max_time => 60, :max_queue => 1, :wait_time => 30
+  @queue = :worker_job
 
    def self.perform(separator, column_headers, csv_to_system_map, filename, voter_list_name, campaign_id, account_id, domain, email, callback_url, strategy="webui")
      job = VoterListJob.new(separator, column_headers, csv_to_system_map, filename, voter_list_name, campaign_id, account_id, domain, email, callback_url, strategy="webui")
