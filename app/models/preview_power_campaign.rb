a0c7689de71a8a3aac039189beba83d95ea0e34c
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-23 15:36:24 +0530

    they had numbers left in their campaign but it said they were out

--- app/models/preview_power_campaign.rb
+++ app/models/preview_power_campaign.rb
@@ -1,25 +1,28 @@
 module PreviewPowerCampaign
   
-  def next_voter_in_dial_queue(current_voter_id = nil)
+  def next_voter_in_dial_queue(current_voter_id = nil, caller_session)
+    begin
+      voter = next_voter(current_voter_id)
+    rescue ActiveRecord::StaleObjectError
+      caller_session.redirect_caller
+    end
+    voter
+  end
+  
+  def next_voter(current_voter_id)
     voter = all_voters.priority_voters.first
     voter||= all_voters.scheduled.first
     voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.not_skipped.where("voters.id > #{current_voter_id}").first unless current_voter_id.blank?
     voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.not_skipped.first
     voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.where("voters.id != #{current_voter_id}").first unless current_voter_id.blank?
-    voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.first
-    unless voter.nil?
-      begin
-        voter.update_attributes(status: CallAttempt::Status::READY)
-      rescue ActiveRecord::StaleObjectError
-        next_voter_in_dial_queue(voter.id)
-      end
-    end
+    voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.first    
+    voter.update_attributes(status: CallAttempt::Status::READY) unless voter.nil?
     voter
   end
   
     
-  def caller_conference_started_event
-    next_voter = next_voter_in_dial_queue
+  def caller_conference_started_event(current_voter_id)
+    next_voter = next_voter_in_dial_queue(current_voter_id)
     {event: 'conference_started', data: next_voter.nil? ? {} : next_voter.info}                     
   end
   
