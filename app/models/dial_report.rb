3208c1b3c901669c1597de11ba64a208b7222a12
Nikhil Fernandes <nikhil@activesphere.com>
2012-05-31 18:45:49 +0530

    dial report

---
+++ app/models/dial_report.rb
@@ -0,0 +1,15 @@
+
+  module DialReport
+    module ClassMethods
+      
+    end
+    
+    module InstanceMethods
+      
+    end
+    
+    def self.included(receiver)
+      receiver.extend         ClassMethods
+      receiver.send :include, InstanceMethods
+    end
+  end
