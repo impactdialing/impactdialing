4fe88e527358e772d5cfc60218a58ed7bbaf7afb
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-03 14:35:56 +0530

    em fiber dialer

--- app/models/predictive.rb
+++ app/models/predictive.rb
@@ -3,13 +3,13 @@ class Predictive < Campaign
     
   def dial
     num_to_call = number_of_voters_to_dial
-    Rails.logger.info "num_to_call #{num_to_call}"    
+    Rails.logger.info "Campaign: #{self.id} - num_to_call #{num_to_call}"    
     return if  num_to_call <= 0
     EM.synchrony do
       concurrency = 4
       voters_to_dial = choose_voters_to_dial(num_to_call)
       EM::Synchrony::Iterator.new(voters_to_dial, concurrency).map do |voter, iter|
-        voter.dial_predictive1(iter)
+        voter.dial_predictive_em(iter)
       end
       EventMachine.stop
     end
