c916cdffd9dfb9078020a9a5d93daa80ec7c5685
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-04 15:59:59 +0530

    dials report attempts

--- app/models/dial_report.rb
+++ app/models/dial_report.rb
@@ -6,13 +6,14 @@ class DialReport
     @campaign = campaign
     @leads_grouped_by_status = @campaign.all_voters.group("status").count
     @leads_grouped_by_status_filtered = @campaign.all_voters.last_call_attempt_within(@from_date, @to_date).group("status").count
+    @attempts_grouped_by_status_filtered = @campaign.call_attempts.between(@from_date, @to_date).group("status").count
     overview_summary
     per_lead_dials
-    @lead_dials = @campaign.all_voters.last_call_attempt_within(@from_date, @to_date).group("status").count
+    per_attempt_dials
   end
   
   def dialed_and_completed
-    sanitize_dials(@leads_grouped_by_status[CallAttempt::Status::SUCCESS]) + sanitize_dials(@leads_grouped_by_status[CallAttempt::Status::FAILED]) + sanitize_dials(@leads_grouped_by_status['retry'])
+    sanitize_dials(@leads_grouped_by_status[CallAttempt::Status::SUCCESS]) + sanitize_dials(@leads_grouped_by_status[CallAttempt::Status::FAILED])
   end
   
   def scheduled_for_now
@@ -43,8 +44,6 @@ class DialReport
   end
   
   def per_lead_dials      
-    # @total_voters_count = @campaign.all_voters.last_call_attempt_within(@from_date, @to_date).count         
-    # @total_lead_dials = ((@total_voters_count == 0) ? 1 : @total_voters_count)
     @total_dials_made_leads = total_dials(leads_grouped_by_status_filtered)
   end
   
@@ -52,12 +51,16 @@ class DialReport
     @total_dials_made_leads
   end
   
+  def attempts_grouped_by_status_filtered
+    @attempts_grouped_by_status_filtered
+  end
+  
   def per_attempt_dials
-    @total_attempts_count = @campaign.call_attempts.between(@from_date, @to_date).count
-    @per_attempt_dials = @campaign.call_attempts.between(@from_date, @to_date).group("status").count
-    @total_attempt_dials = ((@total_attempts_count == 0) ? 1 : @total_attempts_count)
-    @ready_to_dial_attempts = params[:from_date] ? 0 : sanitize_dials(@per_attempt_dials[CallAttempt::Status::READY])
-    @total_dials_made_attempts = total_dials(@per_attempt_dials)
+    @total_dials_made_attempts = total_dials(attempts_grouped_by_status_filtered)
+  end
+  
+  def total_dials_made_attempts
+    @total_dials_made_attempts
   end
   
   def sanitize_dials(dial_count)
