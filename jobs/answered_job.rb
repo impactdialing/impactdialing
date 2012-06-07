bcb5ff02ed3495bd2fa14e6cf245907264a930c4
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-07 20:07:34 +0530

    moving answers to resque job

--- jobs/answered_job.rb
+++ jobs/answered_job.rb
@@ -1,16 +1,17 @@
 require 'resque/plugins/lock'
+require 'resque-loner'
 class AnsweredJob 
   extend Resque::Plugins::Lock
-  @queue = :answered
+  include Resque::Plugins::UniqueJob
+  @queue = :answered_worker
   
    def self.perform     
-     CallAttempt.results_not_processed.limit(10).each do |call_attempt|
+     CallAttempt.results_not_processed.limit(100).each do |call_attempt|
        begin
          call_attempt.voter.persist_answers(call_attempt.call.questions, call_attempt)
          call_attempt.voter.persist_notes(call_attempt.call.notes, call_attempt)
          call_attempt.update_attributes(voter_response_processed: true)
          call_attempt.voter.update_attribute(:result_date, Time.now)
-         # Redis.decr('answer_counter')
        rescue Exception => e
          puts e.backtrace
        end      
