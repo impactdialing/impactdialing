7884d31d190eb8a8adf337feb9b60f259013ae9a
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-07 13:58:47 +0530

    report delivery strategy

--- jobs/report_download_job.rb
+++ jobs/report_download_job.rb
@@ -5,7 +5,7 @@ class ReportDownloadJob
 
 
    def self.perform(campaign_id, user_id, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
-     report_job = ReportJob.new(campaign_id, user_id, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
+     report_job = NewReportJob.new(campaign_id, user_id, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
      report_job.perform     
    end
    
