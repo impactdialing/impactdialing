06201ae3f4d5f9b3f23ed3e212cc14eef489e4a7
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-07 16:40:58 +0530

    In the Dials and Download report, add Ringing and Ready To Dial to Not Dialed

---
+++ app/models/report_api_strategy.rb
@@ -0,0 +1,25 @@
+class ReportApiStrategy
+  require 'net/http'
+  
+  def initialize(result, account_id, campaign_id, callback_url)
+    @result = result
+    @account_id = account_id
+    @campaign_id = campaign_id
+    @callback_url = callback_url
+  end
+  
+  def response(params)
+    if @result == "success"
+      link = AWS::S3::S3Object.url_for("#{params[:campaign_name]}.csv", "download_reports", :expires => expires_in_12_hours)
+    else
+      link = ""
+    end
+    uri = URI.parse(@callback_url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl=true
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data({message: @result, download_link: link, account_id: @account_id, campaign_id: @campaign_id})
+    http.start{http.request(request)}
+  end
+  
+end
