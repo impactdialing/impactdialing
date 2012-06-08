9ecae0a21f35ed0d800bcf93548f4194e0d6c491
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-08 13:24:50 +0530

    Including jobs in rb file

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,4 +5,4 @@ require 'resque_scheduler'
 redis_config = YAML.load_file(rails_root + '/config/redis.yml')
 Resque.redis = redis_config[rails_env]
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
-require '#{Rails.root}/jobs/*.rb'
+require "#{Rails.root}/jobs/*.rb"
