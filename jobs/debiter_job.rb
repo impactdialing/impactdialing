207e3c1fade35f341f30e9f211cb765ad39a91a9
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-08 17:32:06 +0530

    debit job every five minutes

--- jobs/debiter_job.rb
+++ jobs/debiter_job.rb
@@ -1,7 +1,10 @@
 require 'resque/plugins/lock'
+require 'resque-loner'
+
 class DebiterJob 
   extend Resque::Plugins::Lock
-  @queue = :debiter
+  include Resque::Plugins::UniqueJob
+  @queue = :background_worker_job
 
    def self.perform
      call_attempts = CallAttempt.debit_not_processed
@@ -10,12 +13,13 @@ class DebiterJob
        call_attempt.debit
        call_attempt.update_attribute(:debited, true)
      end
-
+     
      caller_sessions = CallerSession.debit_not_processed
+     
      caller_sessions.each do |caller_session|
        caller_session.debit
        caller_session.update_attribute(:debited, true)
      end
-     
+          
    end
 end
