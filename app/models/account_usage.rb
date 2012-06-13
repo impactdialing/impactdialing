5522ba038ca6b43a1cb5ddfed89851338465ece3
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-13 18:07:44 +0530

    account usage total minutes

--- app/models/account_usage.rb
+++ app/models/account_usage.rb
@@ -4,29 +4,27 @@ class AccountUsage
     @account = account
     @from_date = from_date
     @to_date = to_date
+    @campaigns = @account.campaigns
+    @campaign_ids = @campaigns.collect{|x| x.id}    
   end
   
   def billable_usage
-    campaigns = @account.campaigns
-    campaign_ids = campaigns.collect{|x| x.id}
-    caller_times = CallerSession.where("campaign_id in (?)",campaign_ids).between(@from_date, @to_date).where("tCaller is NOT NULL").group("campaign_id").sum('ceil(TIMESTAMPDIFF(SECOND ,starttime,endtime)/60)')      
-    lead_times =   CallAttempt.where("campaign_id in (?)",campaign_ids).between(@from_date, @to_date).group("campaign_id").sum('ceil(TIMESTAMPDIFF(SECOND ,connecttime,call_end)/60)')
-    transfer_times = TransferAttempt.where("campaign_id in (?)",campaign_ids).between(@from_date, @to_date).group("campaign_id").sum('ceil(TIMESTAMPDIFF(SECOND ,connecttime,call_end)/60)')
-    calculate_total_billable_times(campaign_ids, caller_times, lead_times, transfer_times)
+    caller_times = CallerSession.where("campaign_id in (?)",@campaign_ids).between(@from_date, @to_date).where("tCaller is NOT NULL").group("campaign_id").sum('ceil(TIMESTAMPDIFF(SECOND ,starttime,endtime)/60)')      
+    lead_times =   CallAttempt.where("campaign_id in (?)",@campaign_ids).between(@from_date, @to_date).group("campaign_id").sum('ceil(TIMESTAMPDIFF(SECOND ,connecttime,call_end)/60)')
+    transfer_times = TransferAttempt.where("campaign_id in (?)",@campaign_ids).between(@from_date, @to_date).group("campaign_id").sum('ceil(TIMESTAMPDIFF(SECOND ,connecttime,call_end)/60)')
+    calculate_total_billable_times(caller_times, lead_times, transfer_times)
   end
   
   def total_minutes
-    time_logged_in = CallerSession.where("campaign_id in (?)",campaign_ids).between(@from_date, @to_date).group('campaign_id').sum('TIMESTAMPDIFF(SECOND ,starttime,endtime)')
-    time_on_call = CallAttempt.where("campaign_id in (?)",campaign_ids).between(@from_date, @to_date).without_status([CallAttempt::Status::VOICEMAIL, CallAttempt::Status::ABANDONED]).group('campaign_id').sum('TIMESTAMPDIFF(SECOND ,connecttime,call_end)')
-    time_in_wrapup = CallAttempt.where("campaign_id in (?)",campaign_ids).between(@from_date, @to_date).without_status([CallAttempt::Status::VOICEMAIL, CallAttempt::Status::ABANDONED]).group('campaign_id').sum('TIMESTAMPDIFF(SECOND ,call_end,wrapup_time)')
-    
-    time_onhold = round_for_utilization(CallerSession.time_logged_in(nil, @campaign, @from_date, @to_date).to_f - CallAttempt.time_on_call(nil, @campaign, @from_date, @to_date).to_f - CallAttempt.time_in_wrapup(nil, @campaign, @from_date, @to_date).to_f)
-    
+    total_usage = {}
+    time_logged_in = CallerSession.where("campaign_id in (?)",@campaign_ids).between(@from_date, @to_date).group('campaign_id').sum('TIMESTAMPDIFF(SECOND ,starttime,endtime)')    
+    @campaign_ids.each {|campaign_id| total_usage[campaign_id] = sanitize(total_usage[campaign_id]).to_i}
+    total_usage
   end
   
-  def calculate_total_billable_times(campaign_ids, caller_times, lead_times, transfer_times)
+  def calculate_total_billable_times(caller_times, lead_times, transfer_times)
     total_times = {}      
-    campaign_ids.each do |campaign_id|
+    @campaign_ids.each do |campaign_id|
       total_times[campaign_id] = sanitize(caller_times[campaign_id]).to_i + sanitize(lead_times[campaign_id]).to_i + sanitize(transfer_times[campaign_id]).to_i
     end
     total_times    
