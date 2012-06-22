302e00254f4418874118be7d95f58dfac80fb517
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-22 12:31:58 +0530

    debugging blank report

--- app/models/caller_campaign_report_strategy.rb
+++ app/models/caller_campaign_report_strategy.rb
@@ -36,7 +36,11 @@ class CallerCampaignReportStrategy < CampaignReportStrategy
   
   def download_for_date_range_lead
     @campaign.all_voters.last_call_attempt_within(@from_date, @to_date).order('created_at').find_in_batches(:batch_size => 100) do |voters|
-      voters.each {|voter| @csv << csv_for(voter)}
+      voters.each do |voter|
+         test = csv_for(voter)
+         puts test.inspect
+         @csv << test
+      end
     end
   end
   
