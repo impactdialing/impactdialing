d4873f5925f9b3596daeb6c6a2bc6444f97a7654
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-22 16:09:24 +0530

    report download issue

--- app/models/caller_campaign_report_strategy.rb
+++ app/models/caller_campaign_report_strategy.rb
@@ -35,16 +35,9 @@ class CallerCampaignReportStrategy < CampaignReportStrategy
   end
   
   def download_for_date_range_lead
-    puts "ffff"
-    puts @from_date
-    puts @to_date
-    puts @campaign.all_voters.last_call_attempt_within(@from_date, @to_date).order('created_at').size
-    
     @campaign.all_voters.last_call_attempt_within(@from_date, @to_date).order('created_at').find_in_batches(:batch_size => 100) do |voters|
       voters.each do |voter|
-         test = csv_for(voter)
-         puts test.inspect
-         @csv << test
+         @csv << csv_for(voter)
       end
     end
   end
