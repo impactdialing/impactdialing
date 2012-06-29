e3282eed7a90c50222358c4238065383d8588f08
Nikhil Fernandes <nikhil@activesphere.com>
2012-06-29 13:06:52 +0530

    indexes to reduce creation of temp tables

--- db/migrate/20120629071704_add_indexes_for_group_by_status.rb
+++ db/migrate/20120629071704_add_indexes_for_group_by_status.rb
@@ -1,6 +1,6 @@
 class AddIndexesForGroupByStatus < ActiveRecord::Migration
   def self.up
-    add_index(:voters, [:campaign_id,:status])
+    add_index(:voters, [:campaign_id,:status,:last_call_attempt_time])
   end
 
   def self.down
