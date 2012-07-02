ada2fced993097287242fa3018e7433881a04f81
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-02 09:46:59 +0530

    migration failure

--- db/migrate/20120629071704_add_indexes_for_group_by_status.rb
+++ db/migrate/20120629071704_add_indexes_for_group_by_status.rb
@@ -1,6 +1,6 @@
 class AddIndexesForGroupByStatus < ActiveRecord::Migration
   def self.up    
-    add_index(:voters, [:campaign_id,:status,:last_call_attempt_time])
+    add_index(:voters, [:campaign_id,:status,:last_call_attempt_time], :name => 'voters_campaign_status_time')
   end
 
   def self.down
