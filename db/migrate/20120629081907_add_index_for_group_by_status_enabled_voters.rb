df554dced01bfc2158dfe3729d58c16ad3c8940f
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-10 11:02:22 +0530

    indexes for call_details

--- db/migrate/20120629081907_add_index_for_group_by_status_enabled_voters.rb
+++ db/migrate/20120629081907_add_index_for_group_by_status_enabled_voters.rb
@@ -1,8 +1,10 @@
 class AddIndexForGroupByStatusEnabledVoters < ActiveRecord::Migration
   def self.up
-    add_index(:voters, [:enabled, :campaign_id,:last_call_attempt_time,:status], :name => 'voters_enabled_campaign_time_status')    
+    add_index(:answers, [:question_id, :campaign_id], :name => 'index_answers_distinct_question')    
+    add_index(:answers, [:possible_response_id, :campaign_id, :caller_id, :created_at], :name => 'index_answers_count_possible_response')    
   end
 
   def self.down
   end
 end
+
