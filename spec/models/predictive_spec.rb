6b15fd491245c138141b3b50ebac37122943e607
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-12 10:00:23 +0530

    limit best dials to 3

--- spec/models/predictive_spec.rb
+++ spec/models/predictive_spec.rb
@@ -129,7 +129,7 @@ describe Predictive do
    it "should return best dials  as 4 if  best_dials simulated_values is greater than 4" do
       simulated_values = SimulatedValues.create(best_dials: 6.0, best_conversation: 0, longest_conversation: 0)
       campaign = Factory(:predictive, :simulated_values => simulated_values)
-      campaign.best_dials_simulated.should eq(4)
+      campaign.best_dials_simulated.should eq(3)
     end
 
 
