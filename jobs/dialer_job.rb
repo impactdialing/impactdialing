9b0f3ed156a742ab77b468999b1708d15770878b
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-13 13:15:06 +0530

    resque dialer error

--- jobs/dialer_job.rb
+++ jobs/dialer_job.rb
@@ -10,7 +10,7 @@ class DialerJob
      campaign = Campaign.find(campaign_id)
      EM.synchrony do
        concurrency = 8
-       voters_to_dial = campaign.choose_voters_to_dial(num_to_call)
+       voters_to_dial = campaign.choose_voters_to_dial(nums_to_call)
        EM::Synchrony::Iterator.new(voters_to_dial, concurrency).map do |voter, iter|
          voter.dial_predictive_em(iter)
        end
