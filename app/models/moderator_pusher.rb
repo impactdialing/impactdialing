dc54481d655090c807d4c0aa53fd4186ce28701b
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-16 20:05:36 +0530

    merging

--- app/models/moderator_pusher.rb
+++ app/models/moderator_pusher.rb
@@ -1,13 +1,5 @@
 class ModeratorPusher
   
   def push_to_screen(campaign)
-    Moderator.active_moderators(campaign).each do |moderator|
-      EM.run {
-        deferrable = Pusher[moderator.session].trigger_async(event, data)
-        deferrable.callback { 
-          }
-        deferrable.errback { |error|
-        }
-      }    
   end
 end
