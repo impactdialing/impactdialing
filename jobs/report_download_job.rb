a661479cfb22024eb282aedb7b71b7eaeb403931
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-16 14:27:21 +0530

    limiting report download jobs

--- jobs/report_download_job.rb
+++ jobs/report_download_job.rb
@@ -20,13 +20,13 @@ class ReportDownloadJob
    end 
    
    def after_perform_scale_down(*args)
-     Scaler.workers(@queue.to_s,1) if Scaler.working_job_count(@queue.to_s) == 1
+     Scaler.workers('report_download_worker_job',1) if Scaler.working_job_count('report_download_worker_job') == 1
    end
    
    def after_enqueue_scale_up(*args)
-      workers_to_scale = Scaler.working_job_count(@queue.to_s) + Scaler.pending_job_count(@queue.to_s) - Scaler.worker_count(@queue.to_s)
-      if workers_to_scale > 0 && Scaler.working_job_count(@queue.to_s) <= 3
-        Scaler.workers('report_download_worker_job', Scaler.working_job_count(@queue.to_s) + Scaler.pending_job_count(@queue.to_s) + 1)
+      workers_to_scale = Scaler.working_job_count('report_download_worker_job') + Scaler.pending_job_count('report_download_worker_job') - Scaler.worker_count('report_download_worker_job')
+      if workers_to_scale > 0 && Scaler.working_job_count('report_download_worker_job') <= 3
+        Scaler.workers('report_download_worker_job', Scaler.working_job_count('report_download_worker_job') + Scaler.pending_job_count('report_download_worker_job'))
       end
     end
    
