f33462c17d0d37fe457424f0f4f2b204fc24fe65
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-21 15:43:59 +0530

    moderator job spec

--- jobs/moderator_job.rb
+++ jobs/moderator_job.rb
@@ -3,10 +3,10 @@ class ModeratorJob
   sidekiq_options :queue => :monitor_worker
   
   
-   def self.perform(campaign_id, caller_session_id, event, time_now) 
+   def self.perform(campaign_id, event, time_now) 
      redis = Redis.current
-     redis.hmget "moderator:#{campaign_id}", "timestamp"      
-     return if time_now < redis.hmget "moderator:#{campaign_id}", timestamp
+     time_refreshed = redis.hmget "moderator:#{campaign_id}", "timestamp"      
+     return if time_now < time_refreshed[0].to_time
      pub_sub = MonitorPubSub.new  
      pub_sub.push_to_monitor_screen(campaign_id, event, time_now)
    end
