dc6642e6f2cf68343faddbf52c8d7dd1eef442f7
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-22 22:45:10 +0530

    monitor job class name issue

--- jobs/monitor_job.rb
+++ jobs/monitor_job.rb
@@ -11,4 +11,13 @@ class MonitorJob
      pub_sub = MonitorPubSub.new  
      pub_sub.push_to_monitor_screen(campaign_id, event, time_now)
    end
+   
+   def perform(campaign_id, event, time_now) 
+     redis = RedisConnection.monitor_connection
+     time_refreshed = redis.hmget "moderator:#{campaign_id}", "timestamp"      
+     return if time_now < time_refreshed[0].to_time
+     pub_sub = MonitorPubSub.new  
+     pub_sub.push_to_monitor_screen(campaign_id, event, time_now)
+   end
+   
 end
