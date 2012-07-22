7bf0e98ee5cd269b34b221936b1cd50745b977da
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-22 22:54:48 +0530

    connecting sidekiq to redis ec2

--- jobs/monitor_job.rb
+++ jobs/monitor_job.rb
@@ -4,18 +4,10 @@ class MonitorJob
   include Sidekiq::Worker
   sidekiq_options :queue => :monitor_worker
   
-   def self.perform(campaign_id, event, time_now) 
-     redis = RedisConnection.monitor_connection
-     time_refreshed = redis.hmget "moderator:#{campaign_id}", "timestamp"      
-     return if time_now < time_refreshed[0].to_time
-     pub_sub = MonitorPubSub.new  
-     pub_sub.push_to_monitor_screen(campaign_id, event, time_now)
-   end
-   
    def perform(campaign_id, event, time_now) 
      redis = RedisConnection.monitor_connection
      time_refreshed = redis.hmget "moderator:#{campaign_id}", "timestamp"      
-     return if time_now < time_refreshed[0].to_time
+     return if time_now.to_time < time_refreshed[0].to_time
      pub_sub = MonitorPubSub.new  
      pub_sub.push_to_monitor_screen(campaign_id, event, time_now)
    end
