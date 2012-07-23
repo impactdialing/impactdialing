80d7d0debb7112bfec8d8891b17c1bfdca71f7fa
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-23 12:21:57 +0530

    monitor pubsub em http

--- jobs/monitor_job.rb
+++ jobs/monitor_job.rb
@@ -11,7 +11,9 @@ class MonitorJob
      time_refreshed = redis.hmget "moderator:#{campaign_id}", "timestamp"      
      return if time_now.to_time < time_refreshed[0].to_time
      pub_sub = MonitorPubSub.new  
-     pub_sub.push_to_monitor_screen(campaign_id, event, time_now)
+     EM.run {
+       pub_sub.push_to_monitor_screen(campaign_id, event, time_now)
+     }
    end
    
 end
