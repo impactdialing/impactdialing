4bcc52a8b9676af0e929e35f6181b93ed5bfe3c7
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-24 11:44:14 +0530

    using simple redis connsection for tests

--- lib/redis_connection.rb
+++ lib/redis_connection.rb
@@ -1,10 +1,17 @@
+require "eventmachine"
+require 'em-http'
+require 'em-hiredis'
 class RedisConnection
   
   def self.monitor_connection
     rails_env = ENV['RAILS_ENV'] || 'development'
     redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
-    uri = URI.parse(redis_config[rails_env])
-    EM::Hiredis.connect(:host => uri.host, :port => uri.port)
+    if rails_env = "test"
+      uri = URI.parse(redis_config[rails_env])
+      Redis.new(:host => uri.host, :port => uri.port)      
+    else
+      EM::Hiredis.connect(redis_config[rails_env])
+    end
   end
   
 end
