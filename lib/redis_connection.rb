f5eb32fe92d83754018bcd1e93e437cccd95ff22
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-24 12:54:07 +0530

    2 redis connections for monitor

--- lib/redis_connection.rb
+++ lib/redis_connection.rb
@@ -6,12 +6,8 @@ class RedisConnection
   def self.monitor_connection
     rails_env = ENV['RAILS_ENV'] || 'development'
     redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
-    if rails_env == "test"
-      uri = URI.parse(redis_config[rails_env])
-      Redis.new(:host => uri.host, :port => uri.port)      
-    else
-      EM::Hiredis.connect(redis_config[rails_env])
-    end
+    uri = URI.parse(redis_config[rails_env])
+    Redis.new(:host => uri.host, :port => uri.port)      
   end
   
   def self.monitor_connection_em
