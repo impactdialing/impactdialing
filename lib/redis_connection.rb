304200cf77e0a5bb6afdc6ac3b80123d17ff0d6c
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-24 11:58:17 +0530

    redis connection error

--- lib/redis_connection.rb
+++ lib/redis_connection.rb
@@ -6,7 +6,7 @@ class RedisConnection
   def self.monitor_connection
     rails_env = ENV['RAILS_ENV'] || 'development'
     redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
-    if rails_env = "test"
+    if rails_env == "test"
       uri = URI.parse(redis_config[rails_env])
       Redis.new(:host => uri.host, :port => uri.port)      
     else
