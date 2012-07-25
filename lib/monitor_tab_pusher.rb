9b26a16517e51408526912e9694509727a6b9a5f
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-25 14:53:59 +0530

    sending caller data when caller connects

--- lib/monitor_tab_pusher.rb
+++ lib/monitor_tab_pusher.rb
@@ -37,7 +37,11 @@ module MonitorTab
     end
     
     def self.add_caller(channel, campaign_id, caller_session_id, event)
-      caller_deferrable = ::Pusher[channel].trigger_async('caller_connected', {campaign_id: campaign_id, caller_session: caller_session_id, event: event})
+      caller = CallerSession.find(caller_session_id).caller
+      caller.email = caller.identity_name
+      caller_info = caller.info      
+      caller_info.merge!({campaign_id: campaign_id, caller_session: caller_session_id, event: event})
+      caller_deferrable = ::Pusher[channel].trigger_async('caller_connected', caller_info)
       caller_deferrable.callback {}
       caller_deferrable.errback { |error| puts error }                                
     end
