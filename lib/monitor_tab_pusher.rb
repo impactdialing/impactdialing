8b08c5b5e150e8447f9bc4467b0185bf3bff3248
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-25 09:34:15 +0530

    monitor caller details pusher

--- lib/monitor_tab_pusher.rb
+++ lib/monitor_tab_pusher.rb
@@ -11,15 +11,24 @@ module MonitorTab
 
     def self.next
       redis.blpop('monitor_notifications', 0).callback do |list, data|
-        notification = JSON.parse(data)
+        notification = JSON.parse(data)        
         channel = notification.delete('channel')
         campaign_id = notification.delete('campaign')
+        type = notification.delete('type')
         
-        redis.hgetall("moderator:#{campaign_id}").callback { |campaign_info|
-          caller_deferrable = ::Pusher[channel].trigger_async('update_campaign_info', Hash[*campaign_info.flatten])
+        if type == "campaign"        
+          redis.hgetall("moderator:#{campaign_id}").callback { |campaign_info|
+            campaign_deferrable = ::Pusher[channel].trigger_async('update_campaign_info', Hash[*campaign_info.flatten])
+            campaign_deferrable.callback {}
+            campaign_deferrable.errback { |error| puts error }                    
+           }
+        else
+          caller_session_id = notification.delete('caller_session')
+          event = notification.delete('event')          
+          caller_deferrable = ::Pusher[channel].trigger_async('update_caller_info', {caller_session: caller_session_id, event: event})
           caller_deferrable.callback {}
           caller_deferrable.errback { |error| puts error }                    
-         }
+        end
       EM.next_tick(&method(:next))   
       end      
     end
