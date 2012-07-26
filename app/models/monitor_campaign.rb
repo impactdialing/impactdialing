d45152413034b06e9df37703a93a09b19acc1a3e
Nikhil Fernandes <nikhil@activesphere.com>
2012-07-26 14:13:34 +0530

    fixing tests

--- app/models/monitor_campaign.rb
+++ app/models/monitor_campaign.rb
@@ -5,59 +5,59 @@ class MonitorCampaign
     num_available, num_remaining)
     campaign = Campaign.find(campaign_id)
     redis = RedisConnection.monitor_connection
-    redis.hset "monitor:#{campaign_id}", "timestamp", Time.now
-    redis.hset "monitor:#{campaign_id}", "name", campaign.name
-    redis.hset "monitor:#{campaign_id}", "id", campaign.id
-    redis.hdel  "monitor:#{campaign_id}", "callers_logged_in"
-    redis.hdel  "monitor:#{campaign_id}", "on_call"
-    redis.hdel  "monitor:#{campaign_id}", "on_hold"
-    redis.hdel  "monitor:#{campaign_id}", "ringing_lines"
-    redis.hdel  "monitor:#{campaign_id}", "wrapup"
-    redis.hdel  "monitor:#{campaign_id}", "live_lines"
-    redis.hdel  "monitor:#{campaign_id}", "available"
-    redis.hdel  "monitor:#{campaign_id}", "remaining"    
+    redis.hset "monitor_campaign:#{campaign_id}", "timestamp", Time.now
+    redis.hset "monitor_campaign:#{campaign_id}", "name", campaign.name
+    redis.hset "monitor_campaign:#{campaign_id}", "id", campaign.id
+    redis.hdel  "monitor_campaign:#{campaign_id}", "callers_logged_in"
+    redis.hdel  "monitor_campaign:#{campaign_id}", "on_call"
+    redis.hdel  "monitor_campaign:#{campaign_id}", "on_hold"
+    redis.hdel  "monitor_campaign:#{campaign_id}", "ringing_lines"
+    redis.hdel  "monitor_campaign:#{campaign_id}", "wrapup"
+    redis.hdel  "monitor_campaign:#{campaign_id}", "live_lines"
+    redis.hdel  "monitor_campaign:#{campaign_id}", "available"
+    redis.hdel  "monitor_campaign:#{campaign_id}", "remaining"    
     
-    redis.hincrby "monitor:#{campaign_id}", "callers_logged_in", num_callers_logged_in
-    redis.hincrby "monitor:#{campaign_id}", "on_call", num_on_call
-    redis.hincrby "monitor:#{campaign_id}", "on_hold", num_on_hold
-    redis.hincrby "monitor:#{campaign_id}", "ringing_lines", num_ringing_lines    
-    redis.hincrby "monitor:#{campaign_id}", "wrapup", num_wrapup    
-    redis.hincrby "monitor:#{campaign_id}", "live_lines", num_live_lines        
-    redis.hincrby "monitor:#{campaign_id}", "available", num_available        
-    redis.hincrby "monitor:#{campaign_id}", "remaining", num_remaining            
-    redis.zadd("monitoring", Time.now.to_i,  "monitor:#{campaign_id}")
+    redis.hincrby "monitor_campaign:#{campaign_id}", "callers_logged_in", num_callers_logged_in
+    redis.hincrby "monitor_campaign:#{campaign_id}", "on_call", num_on_call
+    redis.hincrby "monitor_campaign:#{campaign_id}", "on_hold", num_on_hold
+    redis.hincrby "monitor_campaign:#{campaign_id}", "ringing_lines", num_ringing_lines    
+    redis.hincrby "monitor_campaign:#{campaign_id}", "wrapup", num_wrapup    
+    redis.hincrby "monitor_campaign:#{campaign_id}", "live_lines", num_live_lines        
+    redis.hincrby "monitor_campaign:#{campaign_id}", "available", num_available        
+    redis.hincrby "monitor_campaign:#{campaign_id}", "remaining", num_remaining            
+    redis.zadd("monitoring", Time.now.to_i,  "monitor_campaign:#{campaign_id}")
   end
   
   ['callers_logged_in', 'on_call', 'on_hold', 'wrapup', 'live_lines', 'ringing_lines', 'available', 'remaining' ].each do |value|
     define_singleton_method("increment_#{value}") do |campaign_id, num|
       redis = RedisConnection.monitor_connection
-      redis.hincrby "monitor:#{campaign_id}", value, num
+      redis.hincrby "monitor_campaign:#{campaign_id}", value, num
     end
     
     define_singleton_method("decrement_#{value}") do |campaign_id, num|
       redis = RedisConnection.monitor_connection
-      redis.hincrby "monitor:#{campaign_id}", value, -num
+      redis.hincrby "monitor_campaign:#{campaign_id}", value, -num
     end
     
     define_singleton_method("#{value}") do |campaign_id|
       redis = RedisConnection.monitor_connection
-      redis.hmget("monitor:#{campaign_id}", value)[0]
+      redis.hmget("monitor_campaign:#{campaign_id}", value)[0]
     end
   end
   
   def self.name(campaign_id)
     redis = RedisConnection.monitor_connection
-    redis.hmget("monitor:#{campaign_id}", 'name')[0]    
+    redis.hmget("monitor_campaign:#{campaign_id}", 'name')[0]    
   end
   
   def self.add_caller_status(caller_id, status)
     redis = RedisConnection.monitor_connection 
-    redis.hset "monitor:#{campaign_id}" "caller:#{caller_id}", status
+    redis.hset "monitor_campaign:#{campaign_id}" "caller:#{caller_id}", status
   end
   
   def self.remove_caller(caller_id)
     redis = RedisConnection.monitor_connection 
-    redis.hdel "monitor:#{campaign_id}" "caller:#{caller_id}"   
+    redis.hdel "monitor_campaign:#{campaign_id}" "caller:#{caller_id}"   
   end
   
   def self.campaign_overview_info(campaign)
