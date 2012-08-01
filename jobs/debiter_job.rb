146262739c64a1b1538a2593326c3f1f91fa08ef
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-01 16:15:07 +0530

    limiting debiter job to 100

--- jobs/debiter_job.rb
+++ jobs/debiter_job.rb
@@ -6,20 +6,24 @@ class DebiterJob
   include Resque::Plugins::UniqueJob
   @queue = :debit_worker_job
 
-   def self.perform
-     call_attempts = CallAttempt.debit_not_processed.limit(100)
-          
-     call_attempts.each do |call_attempt|
-       call_attempt.debit
-       call_attempt.update_attribute(:debited, true)
-     end
+   def self.perform     
+     begin         
+       call_attempts = CallAttempt.debit_not_processed.limit(100) 
+       call_attempts.each do |call_attempt|
+         call_attempt.debit
+         call_attempt.update_attribute(:debited, true)
+       end
      
-     caller_sessions = CallerSession.debit_not_processed
+       caller_sessions = CallerSession.debit_not_processed     
+       caller_sessions.each do |caller_session|
+         caller_session.debit
+         caller_session.update_attribute(:debited, true)
+       end
      
-     caller_sessions.each do |caller_session|
-       caller_session.debit
-       caller_session.update_attribute(:debited, true)
-     end
+     rescue Exception => e
+       puts e.backtrace
+     end      
+     
           
    end
 end
