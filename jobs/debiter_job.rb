0a783921948e7599cfd57a5608a1f6cc19353730
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-03 13:39:20 +0530

    catching exceptions debiter jobs

--- jobs/debiter_job.rb
+++ jobs/debiter_job.rb
@@ -7,23 +7,24 @@ class DebiterJob
   @queue = :debit_worker_job
 
    def self.perform     
-     begin         
-       call_attempts = CallAttempt.debit_not_processed.limit(1000) 
-       call_attempts.each do |call_attempt|
+     call_attempts = CallAttempt.debit_not_processed.limit(5000)        
+     call_attempts.each do |call_attempt|
+       begin
          call_attempt.debit
          call_attempt.update_attribute(:debited, true)
+       rescue Exception => e
+         call_attempt.update_attribute(:debited, true)
        end
+     end
      
-       caller_sessions = CallerSession.debit_not_processed.limit(1000)     
-       caller_sessions.each do |caller_session|
-         caller_session.debit
+    caller_sessions = CallerSession.debit_not_processed.limit(5000)     
+    caller_sessions.each do |caller_session|
+      begin
+        caller_session.debit
+        caller_session.update_attribute(:debited, true)
+       rescue Exception=>e
          caller_session.update_attribute(:debited, true)
        end
-     
-     rescue Exception => e
-       puts e.backtrace
-     end      
-     
-          
+    end
    end
 end
