7394405683ed21ad911bb0674d406e63231ebd5e
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-16 19:37:01 +0530

    including redis connection in rails env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require Rails.root.join("lib/redis_connection")
 
 # Initialize the rails application
 ImpactDialing::Application.initialize!
