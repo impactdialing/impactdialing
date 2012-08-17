e5c43e412827e46f2f5b2fcfa032dfb4ae4c3ea6
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-17 17:08:32 +0530

    sidekiq for dialer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,5 +3,5 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
 Sidekiq.configure_server do |config|
-  config.redis = { :url => redis_config[rails_env]['resque_sidekiq'], :namespace => 'resque' }
+  config.redis = { :url => redis_config[rails_env], :namespace => 'resque' }
 end
