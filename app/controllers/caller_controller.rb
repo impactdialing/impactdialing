0fc05c8d0703473063a99d4c638e85c1b194b809
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-17 15:47:56 +0530

    single redis connection for application

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -27,7 +27,7 @@ class CallerController < ApplicationController
   def call_voter
     caller = Caller.find(params[:id])
     caller_session = caller.caller_sessions.find(params[:session_id])    
-    voter = RedisVoter.read(params[:voter_id], $redis_call_flow_connection)
+    voter = RedisVoter.read(params[:voter_id])
     caller_session.publish_calling_voter
     Twillio.dial(voter, caller_session)
     # caller_session.dial_em(Voter.find(params[:voter_id])) unless params[:voter_id].blank?
