56b599aa87c735a1ca8bf906369937e53cd3314c
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-17 19:01:11 +0530

    separate instance for redis sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,9 +4,9 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
 
 Sidekiq.configure_server do |config|
-  config.redis = { :url => redis_config[rails_env]}
+  config.redis = { :url => redis_config[rails_env]['resque_sidekiq']}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => redis_config[rails_env]}
+  config.redis = { :url => redis_config[rails_env]['resque_sidekiq']}
 end
