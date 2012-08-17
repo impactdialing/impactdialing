f7368fc93b838789d0ba7029373b02218d3d44ff
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-17 16:13:19 +0530

    moving to self hosted redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,8 +2,9 @@ rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 require 'resque_scheduler'
 
-redis_config = YAML.load_file(rails_root + '/config/redis.yml')
-Resque.redis = redis_config[rails_env]['resque_sidekiq']
+redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
+uri = URI.parse(redis_config[rails_env]['resque_sidekiq'])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port)
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
 
 Dir[File.dirname(__FILE__) + '/../jobs/*.rb'].each do |file| 
