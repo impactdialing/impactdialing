5dcab88389edcb9f4cac879dd10f603bde7830f6
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-17 19:53:19 +0530

    sidekiq configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,10 +3,18 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
 
-Sidekiq.configure_server do |config|
-  config.redis = { :url => redis_config[rails_env]['resque_sidekiq']}
-end
+if rails_env == 'development' || rails_env == "test"
+  
+else
+  
+  Sidekiq.configure_server do |config|
+    config.redis = { :url => redis_config[rails_env]['resque_sidekiq'] }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { :url => redis_config[rails_env]['resque_sidekiq']}
-end
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => redis_config[rails_env]['resque_sidekiq']}
+  end  
+  
+end  
+
+  
