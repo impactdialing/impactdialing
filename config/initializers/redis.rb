b03e116851cd3ca3da08fc9836fd142d9a89d079
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-18 13:04:05 +0530

    new redis connection for monitor tab

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,4 +4,8 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 redis_config = YAML.load_file(rails_root + '/config/redis.yml')
 # Redis = redis_config[rails_env]
 
-redis = redis_config[rails_env]
+redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
+uri = URI.parse(redis_config[rails_env]['monitor_redis'])
+
+$redis_monitor_connection = Redis.new(:host => uri.host, :port => uri.port)      
+
