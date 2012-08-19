25fd12393d3927701947acf07d419684e3d8b57e
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-19 21:53:28 +0530

    dialer job

---
+++ app/models/redis/redis_call.rb
@@ -0,0 +1,8 @@
+class RedisCall
+  
+  def self.call_completed(call_attempt_id, voter_id)
+    $redis_call_flow_connection.rpush('call_completed', {call_attempt_id: call_attempt_id, voter_id: voter_id}.to_json)
+  end
+  
+
+end
