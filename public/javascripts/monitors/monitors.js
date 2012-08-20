380ae06bb2b9c37235f066f504f65e5efdb66214
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-20 19:46:25 +0530

    start timer on page load monitor page

--- public/javascripts/monitors/monitors.js
+++ public/javascripts/monitors/monitors.js
@@ -10,8 +10,8 @@ var Monitors = function(channel){
 	this.remove_caller();
 	this.bind_caller_actions();
 	this.call_status = {"Call in progress": "On call", "Call completed with success.": "Wrap up", "On hold": "On hold", "Ringing":"On hold" }
-	var timers = $('.timer');
-	$.each(timers, function(){
+	alert($('.timer').length);
+	$.each( $('.timer'), function(){
 		$(this).stopwatch('start');
 	});
 	
