d84bbd86e41e765a92b5a5462621eeddd4970705
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-21 11:34:34 +0530

    script text sections

---
+++ app/models/script_text.rb
@@ -0,0 +1,3 @@
+class ScriptText < ActiveRecord::Base
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716080402) do
+ActiveRecord::Schema.define(:version => 20120821053423) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "card_verified"
@@ -308,6 +308,10 @@ ActiveRecord::Schema.define(:version => 20120716080402) do
     t.string   "campaign_id"
   end
 
+  create_table "moderator_campaigns", :force => true do |t|
+    t.string "name"
+  end
+
   create_table "moderators", :force => true do |t|
     t.integer  "caller_session_id"
     t.string   "call_sid"
@@ -384,6 +388,11 @@ ActiveRecord::Schema.define(:version => 20120716080402) do
     t.string   "file_content_type"
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
+  end
+
+  create_table "script_texts", :force => true do |t|
+    t.integer "script_id"
+    t.text    "section"
   end
 
   create_table "scripts", :force => true do |t|
