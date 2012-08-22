81fbf0c76edd023276588ed07d37a4593a6eea3d
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-22 12:54:24 +0530

    migration to copy script contents

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120821090107) do
+ActiveRecord::Schema.define(:version => 20120822072207) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "card_verified"
