a13ec0d2767ea0b249a0b960c65f5a3f514d8dd8
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-22 15:31:14 +0530

    resque redis

--- lib/resque_predictive_dialer.rb
+++ lib/resque_predictive_dialer.rb
@@ -3,6 +3,7 @@ require File.join(RAILS_ROOT, 'config/environment')
 DIALER_ROOT = ENV['DIALER_ROOT'] || File.expand_path('..', __FILE__)
 FileUtils.mkdir_p(File.join(DIALER_ROOT, 'log'), :verbose => true)
 ActiveRecord::Base.logger = Logger.new(File.open(File.join(DIALER_ROOT, 'log', "dialer_#{RAILS_ENV}.log"), 'a'))
+rails_env = ENV['RAILS_ENV'] || 'development'
 redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
 uri = URI.parse(redis_config[rails_env])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port)
