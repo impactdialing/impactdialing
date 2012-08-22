37663f8df4f06ae1da6260ee9a0ed95db10dfd4b
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-22 20:48:30 +0530

    Adding oncall

--- public/javascripts/monitors/monitors.js
+++ public/javascripts/monitors/monitors.js
@@ -79,12 +79,7 @@ Monitors.prototype.update_caller_info = function(){
       Pusher.log(data)		
 	  var caller_selector = 'tr#caller_'+data.caller_session;
 	  status = self.call_status[data.event]
-	  if (status == 'On call') {
-	    $(caller_selector).attr('on_call', true)	
-	  }
-	  else {
-		$(caller_selector).attr('on_call', true)			
-	  }
+	  $(caller_selector).attr('on_call', status == 'On call')	
 	  self.update_status_and_duration(caller_selector, self.call_status[data.event]);
 	}	
   });
