ff08ac6a03ff067ef59d2599709b150d4cf7549c
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-22 21:25:05 +0530

    switch mode

--- app/models/monitor_campaign.rb
+++ app/models/monitor_campaign.rb
@@ -80,5 +80,22 @@ class MonitorCampaign
     dial_count.nil? ? 0 : dial_count
   end
   
+  def self.get_conference_id(caller_session)
+     Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
+     conferences = Twilio::Conference.list({"FriendlyName" => caller_session.session_key})
+     confs = conferences.parsed_response['TwilioResponse']['Conferences']['Conference']
+     conference_sid = ""
+     conference_sid = confs.class == Array ? confs.last['Sid'] : confs['Sid']
+  end
+  
+  def self.switch_monitor_mode(caller_session, type)
+    conference_sid = get_conference_id(caller_session)
+    if type == "breakin"
+      Twilio::Conference.unmute_participant(conference_sid, call_sid)
+    else
+      Twilio::Conference.mute_participant(conference_sid, call_sid)
+    end
+  end
+  
    
 end
