ff3e43b53cab1e9daaf5d97ebdc032c14788afc3
michael <michaelrkn@gmail.com>
2012-08-25 10:20:35 -0600

    refactor scripts controller and views

--- app/controllers/client/scripts_controller.rb
+++ app/controllers/client/scripts_controller.rb
@@ -1,44 +1,70 @@
 module Client
   class ScriptsController < ::ScriptsController
-    skip_before_filter :load_script, :apply_changes, :questions_answered
+    skip_before_filter :apply_changes, :questions_answered
+    before_filter :load_script, :only => [:show, :update, :destroy]
+    before_filter :load_voter_fields, :only => [:new, :show]
 
     layout 'client'
-
-    def deleted
-      render 'scripts/deleted'
-    end
 
     def index
       @scripts = @user.scripts.manual.active.paginate(:page => params[:page])
     end
 
     def new
-      @script = Script.new(:robo => false, questions: [Question.new(possible_responses: [PossibleResponse.new])], script_texts: [ScriptText.new])
-      @voter_fields = VoterList::VOTER_DATA_COLUMNS.values
-      @voter_fields.concat(@user.account.custom_voter_fields.collect{ |field| field.name})
-      @voter_field_values=[]
+      @script = Script.new(robo: false,
+                           questions: [Question.new(possible_responses: [PossibleResponse.new])],
+                           script_texts: [ScriptText.new])
     end
 
     def create
-      params[:script][:voter_fields] =  params[:voter_field] ? params[:voter_field].to_json : nil
-      @script = Script.new(params[:script])
-      @voter_fields = Voter.upload_fields
-      @voter_field_values = params[:voter_field] || []
-      if @script.save
-        @user.account.scripts << @script
-        flash_message(:notice, "Script saved")
-        redirect_to :action=>"index"
-      else
-        render :action=>"new"
-      end
+      @script = account.scripts.new
+      @error_action = 'new'
+      save_script
     end
 
     def show
-      @script = @user.account.scripts.find(params[:id])
-      @script.questions << [Question.new(possible_responses: [PossibleResponse.new])]  if @script.questions.empty?
+    end
+
+    def update
+      @error_action = 'show'
+      save_script
+    end
+
+    def destroy
+      if @user.account.campaigns.active.find_by_script_id(@script.id).nil?
+        @script.update_attributes(:active => false)
+        flash_message(:notice, "Script deleted")
+      else
+        flash_message(:notice, I18n.t(:script_cannot_be_deleted))
+      end
+      redirect_to :action => "index"
+    end
+
+    def questions_answered
+      render :json => { :data => Question.question_count_script(params[:id]) }
+    end
+
+    def possible_responses_answered
+      render :json => { :data => PossibleResponse.possible_response_count(params[:question_ids]) }
+    end
+
+    def deleted
+      render 'scripts/deleted'
+    end
+
+    def load_deleted
+      self.instance_variable_set("@#{type_name.pluralize}", Script.deleted.for_account(@user.account).paginate(:page => params[:page], :order => 'id desc'))
+    end
+
+    private
+
+    def load_script
+      @script = Script.find(params[:id])
+    end
+
+    def load_voter_fields
       @voter_fields = VoterList::VOTER_DATA_COLUMNS.values
       @voter_fields.concat(@user.account.custom_voter_fields.collect{ |field| field.name})
-      @answered_questions = Question.question_count_script(@script.id)
       if @script.voter_fields!=nil
         begin
           @voter_field_values = JSON.parse(@script.voter_fields)
@@ -49,82 +75,15 @@ module Client
         @voter_field_values=[]
       end
     end
-    
 
-    def update
-      @script = @user.account.scripts.find(params[:id])
+    def save_script
       params[:script][:voter_fields] =  params[:voter_field] ? params[:voter_field].to_json : nil
-      @voter_fields = VoterList::VOTER_DATA_COLUMNS.values
-      @voter_fields.concat(@user.account.custom_voter_fields.collect{ |field| field.name})
-      @voter_field_values = (JSON.parse(@script.voter_fields) if @script.voter_fields) || []
-      @answered_questions = Question.question_count_script(@script.id)
-      begin
-        params[:save_as] ? save_as : @script = account.scripts.find_by_id(params[:id])
-        if params[:save_as]
-          redirect_to client_script_path(@script)          
-        elsif !params[:save_as] && @script.update_attributes!(params[:script])
-          flash_message(:notice, "Script updated")
-          redirect_to :action=>"index"
-        else
-          render :show
-        end
-      rescue Exception => e
-        flash_message(:notice, "Script not saved. Error:" + e.message)
-        render :show
+      if @script.update_attributes(params[:script])
+        flash_message(:notice, "Script saved")
+        redirect_to :action=>"index"
+      else
+        render :action => @error_action
       end
     end
-
-    def destroy
-      @script = @user.account.scripts.manual.find(params[:id])
-      unless @script.nil?
-        campaign = @user.account.campaigns.active.find_by_script_id(@script.id)
-        if campaign.nil?
-          @script.update_attributes(:active => false)
-          flash_message(:notice, "Script deleted")
-        else
-          flash_message(:notice, I18n.t(:script_cannot_be_deleted))
-        end
-      end
-      redirect_to :action => "index"
-    end
-    
-    def questions_answered
-      render :json => { :data => Question.question_count_script(params[:id]) }
-    end
-    
-    def possible_responses_answered
-      render :json => { :data => PossibleResponse.possible_response_count(params[:question_ids]) }
-    end
-    
-    def load_deleted
-      self.instance_variable_set("@#{type_name.pluralize}", Script.deleted.for_account(@user.account).paginate(:page => params[:page], :order => 'id desc'))
-    end    
-    
-
-    private
-
-    def save_as
-      @script = Script.new(:name => "", :active => true, :account => @user.account, :script => params[:script][:script], :voter_fields => params[:script][:voter_fields])
-      @script.save(:validate => false)
-      params[:script][:questions_attributes].each_value.each do |q|
-        if q[:_destroy] == "false"
-          question = @script.questions.new(:text => q[:text])
-          question.save!
-          q[:possible_responses_attributes].each_value.each do |ps|
-            if ps[:_destroy] == "false"
-              possible_response = question.possible_responses.new(:value => ps[:value], :keypad => ps[:keypad], :retry => ps[:retry])
-              possible_response.save!
-            end
-          end
-        end
-      end
-      params[:script][:notes_attributes].try(:each_value).try(:each) do |n|
-        if n[:_destroy] == "false"
-          note = @script.notes.new(:note => n[:note])
-          note.save!
-        end
-      end
-    end
-
   end
 end
