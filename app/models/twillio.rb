f98737f50b72fc69766f34ba87db52c6fcd111a8
Nikhil Fernandes <nikhil@activesphere.com>
2012-08-25 20:05:50 +0530

    moving predictive load to redis

--- app/models/twillio.rb
+++ app/models/twillio.rb
@@ -19,6 +19,36 @@ class Twillio
     end    
   end
   
+  def self.dial_predictive_em(iter, voter)
+    call_attempt = setup_call_predictive(voter, self.campaign.type)
+    twilio_lib = TwilioLib.new(TWILIO_ACCOUNT, TWILIO_AUTH)  
+    Rails.logger.info "#{call_attempt.id} - before call"        
+    http = twilio_lib.make_call(campaign, self, call_attempt)
+    http.callback { 
+      Rails.logger.info "#{call_attempt.id} - after call"    
+      response = JSON.parse(http.response)  
+      if response["RestException"]
+        handle_failed_call(call_attempt, nil, voter)
+      else
+        RedisCallAttempt.update_call_sid(call_attempt.id, response["sid"])
+      end
+      iter.return(http)      
+       }
+    http.errback { iter.return(http) }    
+    
+  end
+  
+  def self.setup_call_predictive(voter, mode = 'robo')
+    call_attempt = voter.call_attempts.create(campaign:  self.campaign, dialer_mode:  mode, status:  CallAttempt::Status::RINGING, call_start:  Time.now)
+    $redis_call_flow_connection.pipelined do
+      RedisCallAttempt.load_call_attempt_info(attempt.id, attempt)
+      RedisVoter.setup_call(voter.id, attempt.id, nil)
+    end
+    Call.create(call_attempt: attempt, all_states: "")    
+    call_attempt
+  end
+  
+  
   
   def self.setup_call(voter, caller_session, campaign)
     attempt = voter.call_attempts.create(:campaign => campaign, :dialer_mode => campaign.type, :status => CallAttempt::Status::RINGING, :caller_session => caller_session, :caller => caller_session.caller, call_start:  Time.now)    
@@ -35,7 +65,7 @@ class Twillio
     $redis_call_flow_connection.pipelined do
       RedisCallAttempt.failed_call(attempt.id)
       RedisVoter.failed_call(voter.id)
-      RedisAvailableCaller.add_caller(campaign_id, caller_session.id)
+      RedisAvailableCaller.add_caller(campaign_id, caller_session.id) unless caller_session.nil?
     end
     # update_attributes(:on_call => true, :available_for_call => true, :attempt_in_progress => nil)
     caller_session.redirect_caller
