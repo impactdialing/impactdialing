99bea515625cb432cec6ea5056307553fe1ebab8
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-03 22:56:50 +0530

    api rails 3 respond_with

--- config/routes.rb
+++ config/routes.rb
@@ -147,7 +147,7 @@ ImpactDialing::Application.routes.draw do
 
     [:campaigns, :scripts, :callers].each do |type_plural|
       get "/deleted_#{type_plural}", :to => "#{type_plural}#deleted", :as => "deleted_#{type_plural}"
-      resources type_plural, :only => [:new, :index, :show, :destroy, :create, :update] do
+      resources type_plural, :only => [:new, :index, :show, :destroy, :create, :update, :edit] do
         put 'restore', :to => "#{type_plural}#restore"
       end
     end
