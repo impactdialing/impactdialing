183105d441057a00ec1968454fcb74b3f0e039ff
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-03 15:08:57 +0530

    fixing tests

--- app/models/script_text.rb
+++ app/models/script_text.rb
@@ -2,7 +2,7 @@ class ScriptText < ActiveRecord::Base
   attr_accessible :section, :script_id, :script_order
 
   validates :section, presence: true
-  validates :script, presence: true
+  validates :script_id, presence: true
   validates :script_order, presence: true, numericality: true
 
   belongs_to :script
--- spec/models/script_text_spec.rb
+++ spec/models/script_text_spec.rb
@@ -4,7 +4,6 @@ describe ScriptText do
   context 'validations' do
     it {should validate_presence_of :section}
     it {should validate_presence_of :script_id}
-    it {should validate_numericality_of :script_id}
     it {should validate_presence_of :script_order}
     it {should validate_numericality_of :script_order}
   end
