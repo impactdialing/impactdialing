f89fbe1a4619057a3982d6b32ac5a18f46bfd11c
michael <michaelrkn@gmail.com>
2012-09-04 10:38:18 -0700

    validate parent, not parent_id, to allow saving nested attributes on new forms

--- app/models/script_text.rb
+++ app/models/script_text.rb
@@ -1,8 +1,8 @@
 class ScriptText < ActiveRecord::Base
-  attr_accessible :section, :script_id, :script_order
+  attr_accessible :content, :script_id, :script_order
 
-  validates :section, presence: true
-  validates :script_id, presence: true
+  validates :content, presence: true
+  validates :script, presence: true
   validates :script_order, presence: true, numericality: true
 
   belongs_to :script
