8d355424bb54eb856f3fba4e9cf99dc740c6401f
michael <michaelrkn@gmail.com>
2012-09-04 12:48:55 -0700

    fix failing specs, mostly for parent_id, also some old client controller cruft

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,7 +5,7 @@ describe Question do
 
   context 'validations' do
     it {should validate_presence_of :text}
-    it {should validate_presence_of :script_id}
+    it {should validate_presence_of :script}
     it {should validate_presence_of :script_order}
     it {should validate_numericality_of :script_order}
   end
