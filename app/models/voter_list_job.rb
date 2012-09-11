b5bf2a063803c06d9b105205f5bc4bc9164d8ed5
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-11 12:25:59 +0530

    voter list custom attributes error

--- app/models/voter_list_job.rb
+++ app/models/voter_list_job.rb
@@ -30,7 +30,6 @@ class VoterListJob
                                         @separator)
       response['success'] << "Upload complete. #{result[:successCount]} out of #{result[:successCount]+result[:failedCount]} records imported successfully."
     rescue Exception => err
-      puts err.backtrace
       @voter_list.destroy
       response['errors'] << "Invalid CSV file. Could not import."
     ensure
