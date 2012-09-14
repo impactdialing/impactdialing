d7b6f076bce2d479226f00cf3cdad1ca1d50ee9b
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-14 15:27:41 +0530

    adding calculate dials job

---
+++ jobs/calculate_dials_job.rb
@@ -0,0 +1,18 @@
+require 'em-http-request'
+require "em-synchrony"
+require "em-synchrony/em-http"
+
+class CalculateDialsJob 
+  @queue = :calculate_dials_worker
+
+
+   def self.perform(campaign_id)
+     campaign = Campaign.find(campaign_id)
+     num_to_call = campaign.number_of_voters_to_dial
+     Rails.logger.info "Campaign: #{campaign.id} - num_to_call #{num_to_call}"    
+     return if num_to_call <= 0    
+     campaign.set_calls_in_progress
+     voters_to_dial = campaign.choose_voters_to_dial(num_to_call)
+     voters_to_dial.each_slice(10).to_a.each do {|voters| Resque.enqueue(DialerJob, campaign.id, voters_to_dial) }     
+   end
+end
--- jobs/dialer_job.rb
+++ jobs/dialer_job.rb
@@ -6,16 +6,10 @@ class DialerJob
   @queue = :dialer_worker
 
 
-   def self.perform(campaign_id)
-     campaign = Campaign.find(campaign_id)
-     num_to_call = campaign.number_of_voters_to_dial
-     Rails.logger.info "Campaign: #{campaign.id} - num_to_call #{num_to_call}"    
-     return if num_to_call <= 0    
-     campaign.set_calls_in_progress     
+   def self.perform(campaign_id, voters_to_dial)
      begin
        EM.synchrony do
-         concurrency = 8
-         voters_to_dial = campaign.choose_voters_to_dial(num_to_call)
+         concurrency = 10
          EM::Synchrony::Iterator.new(voters_to_dial, concurrency).map do |voter, iter|
            voter.dial_predictive_em(iter)
          end
