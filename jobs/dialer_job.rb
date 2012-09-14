ea6ad7fa0224aa5978858a9169006c6e36d01421
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-14 18:27:40 +0530

    calculate dials

--- jobs/dialer_job.rb
+++ jobs/dialer_job.rb
@@ -9,16 +9,16 @@ class DialerJob
    def self.perform(campaign_id, voter_ids)
      campaign = Campaign.find(campaign_id)     
      voters_to_dial = Voter.where("id in (?)" ,voter_ids)
-     
      begin
        EM.synchrony do
          concurrency = 10
          EM::Synchrony::Iterator.new(voters_to_dial, concurrency).map do |voter, iter|
+           campaign.decrement_campaign_dial_count(voters_to_dial.size)
            voter.dial_predictive_em(iter)
          end
          EventMachine.stop
        end
-       campaign.decrement_campaign_dial_count(voters_to_dial.size)
+       
       rescue Exception => e
         EventMachine.stop
         puts e        
