884e6786d364d22f9d9629aa991064cb6a239473
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-14 14:19:47 +0530

    dialer job

--- jobs/dialer_job.rb
+++ jobs/dialer_job.rb
@@ -15,7 +15,7 @@ class DialerJob
      begin
        EM.synchrony do
          concurrency = 8
-         voters_to_dial = campaign.choose_voters_to_dial(nums_to_call)
+         voters_to_dial = campaign.choose_voters_to_dial(num_to_call)
          EM::Synchrony::Iterator.new(voters_to_dial, concurrency).map do |voter, iter|
            voter.dial_predictive_em(iter)
          end
