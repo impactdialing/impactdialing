b2f86a4c1aa40932f52895068b89b5d323db0a05
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-14 18:27:40 +0530

    calculate dials

--- jobs/calculate_dials_job.rb
+++ jobs/calculate_dials_job.rb
@@ -3,19 +3,19 @@ require "em-synchrony"
 require "em-synchrony/em-http"
 
 class CalculateDialsJob 
+  extend Resque::Plugins::Lock
+  include Resque::Plugins::UniqueJob
+  
   @queue = :calculate_dials_worker
 
 
    def self.perform(campaign_id)
      campaign = Campaign.find(campaign_id)
-     num_to_call = campaign.number_of_voters_to_dial
+     num_to_call = campaign.number_of_voters_to_dial - campaign.dialing_count
      Rails.logger.info "Campaign: #{campaign.id} - num_to_call #{num_to_call}"    
-     if num_to_call <= 0    
-       Resque.redis.del("dial_count:#{campaign.id}")
-       return
-     end
+     return if num_to_call <= 0    
      voters_to_dial = campaign.choose_voters_to_dial(num_to_call).collect {|voter| voter.id}          
-     campaign.increment_campaign_dial_count(voters_to_dial.size - 1)
+     campaign.increment_campaign_dial_count(voters_to_dial.size)
      voters_to_dial.each_slice(10).to_a.each {|voters| Resque.enqueue(DialerJob, campaign.id, voters) }     
    end
 end
