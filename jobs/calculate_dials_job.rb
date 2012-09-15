d853475f1974c971e4b9dc8eb5c93331a60af7a2
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-15 13:30:14 +0530

    calc dialing sequentially

--- jobs/calculate_dials_job.rb
+++ jobs/calculate_dials_job.rb
@@ -21,6 +21,6 @@ class CalculateDialsJob
      else
        voters_to_dial.each_slice(10).to_a.each {|voters| Resque.enqueue(DialerJob, campaign.id, voters) }     
      end                    
-     
+     Resque.redis.del("dial_calculate:#{self.id}")
    end
 end
--- lib/resque_predictive_dialer.rb
+++ lib/resque_predictive_dialer.rb
@@ -14,7 +14,7 @@ loop do
     logged_in_campaigns = CallerSession.campaigns_on_call
     logged_in_campaigns.each do |c|
       campaign = Campaign.find(c.campaign_id)
-      if campaign.type != Campaign::Type::PREVIEW && campaign.type != Campaign::Type::PROGRESSIVE
+      if campaign.type != Campaign::Type::PREVIEW && campaign.type != Campaign::Type::PROGRESSIVE && !campaign.calculate_dialing?
         campaign.dial_resque
       end
     end
