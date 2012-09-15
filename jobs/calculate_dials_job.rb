4ee7a370809d9fc2f941203d508f0244f0f42ac8
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-15 13:25:58 +0530

    resque dialer multifork

--- jobs/calculate_dials_job.rb
+++ jobs/calculate_dials_job.rb
@@ -14,8 +14,13 @@ class CalculateDialsJob
      num_to_call = campaign.number_of_voters_to_dial - campaign.dialing_count
      Rails.logger.info "Campaign: #{campaign.id} - num_to_call #{num_to_call}"    
      return if num_to_call <= 0    
-     voters_to_dial = campaign.choose_voters_to_dial(num_to_call).collect {|voter| voter.id}          
+     voters_to_dial = campaign.choose_voters_to_dial(num_to_call).collect {|voter| voter.id}
      campaign.increment_campaign_dial_count(voters_to_dial.size)
-     voters_to_dial.each_slice(10).to_a.each {|voters| Resque.enqueue(DialerJob, campaign.id, voters) }     
+     if voters_to_dial.size <=10
+       DialerJob.perform(campaign.id, voters_to_dial)
+     else
+       voters_to_dial.each_slice(10).to_a.each {|voters| Resque.enqueue(DialerJob, campaign.id, voters) }     
+     end                    
+     
    end
 end
