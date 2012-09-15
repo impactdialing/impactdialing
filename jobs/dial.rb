47d4834c292f0239e2b981856a7c79c1a248389a
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-15 14:08:19 +0530

    dialer error

--- jobs/dial.rb
+++ jobs/dial.rb
@@ -10,10 +10,10 @@ class Dial
     begin
       EM.synchrony do
         concurrency = 10
+        campaign.decrement_campaign_dial_count(voters_to_dial.size)
         EM::Synchrony::Iterator.new(voters_to_dial, concurrency).map do |voter, iter|
-          campaign.decrement_campaign_dial_count(voters_to_dial.size)
           voter.dial_predictive_em(iter)
-        end
+        end        
         EventMachine.stop
       end
       
