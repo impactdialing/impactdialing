d21d25b06de0a2a1208f4b00c9ab94fbdcc85329
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-15 14:22:17 +0530

    dialer error

--- jobs/dial.rb
+++ jobs/dial.rb
@@ -7,10 +7,11 @@ class Dial
   def self.perform(campaign_id, voter_ids)
     campaign = Campaign.find(campaign_id)     
     voters_to_dial = Voter.where("id in (?)" ,voter_ids)
+    campaign.decrement_campaign_dial_count(voters_to_dial.size)
     begin
       EM.synchrony do
         concurrency = 10
-        campaign.decrement_campaign_dial_count(voters_to_dial.size)
+        
         EM::Synchrony::Iterator.new(voters_to_dial, concurrency).map do |voter, iter|
           voter.dial_predictive_em(iter)
         end        
