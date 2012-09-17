3ba2cd1b07d5525741e0822bb3ecba84adb328e8
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-17 17:57:14 +0530

    adding call connected to proc file

--- app/models/redis/redis_call_notification.rb
+++ app/models/redis/redis_call_notification.rb
@@ -1,34 +1,34 @@
 class RedisCallNotification
   
   def self.connected(call_attempt_id)
-    RedisConnection.common_connection.rpush('connected_call_notification', {call_attempt: call_attempt_id, event: "call_connected"}.to_json)
+    RedisConnection.common_connection.rpush('connected_call_notification', {call_attempt_id: call_attempt_id, event: "call_connected"}.to_json)
   end
   
   def self.end_answered_call(call_attempt_id)
-    RedisConnection.common_connection.rpush('connected_call_notification', {call_attempt: call_attempt_id, event: "end_answered_call"}.to_json)
+    RedisConnection.common_connection.rpush('connected_call_notification', {call_attempt_id: call_attempt_id, event: "end_answered_call"}.to_json)
   end
   
   
   def self.wrapup(call_attempt_id)
-    RedisConnection.common_connection.rpush('connected_call_notification', {call_attempt: call_attempt_id, event: "wrapup"}.to_json)
+    RedisConnection.common_connection.rpush('connected_call_notification', {call_attempt_id: call_attempt_id, event: "wrapup"}.to_json)
   end
   
   
   def self.abandoned(call_attempt_id)
-    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt: call_attempt_id, event: "call_abandoned"}.to_json)
+    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt_id: call_attempt_id, event: "call_abandoned"}.to_json)
   end
   
   
   def self.answered_by_machine(call_attempt_id)
-    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt: call_attempt_id, event: "answered_by_machine"}.to_json)    
+    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt_id: call_attempt_id, event: "answered_by_machine"}.to_json)    
   end
   
   def self.end_answered_by_machine(call_attempt_id)
-    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt: call_attempt_id, event: "end_answered_by_machine"}.to_json)    
+    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt_id: call_attempt_id, event: "end_answered_by_machine"}.to_json)    
   end
   
   def self.end_unanswered_call(call_attempt_id)
-    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt: self.id, event: "end_unanswered_call"}.to_json)
+    RedisConnection.common_connection.rpush('notconnected_call_notification', {call_attempt_id: self.id, event: "end_unanswered_call"}.to_json)
   end
   
   
