cfc7c36feaa38c85fc00f0b70e7a28f7146c7216
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-17 17:41:35 +0530

    redis call mysql tests

--- app/models/redis/redis_call_mysql.rb
+++ app/models/redis/redis_call_mysql.rb
@@ -3,7 +3,7 @@ class RedisCallMysql
   def self.call_completed(call_attempt_id)
     redis_call_attempt = RedisCallAttempt.read(call_attempt_id)
     update_call_attempt(call_attempt_id)
-    # update_voter(redis_call_attempt['voter_id'])
+    update_voter(redis_call_attempt['voter_id'])
     # update_caller_session(redis_call_attempt['caller_session_id'])
   end
   
