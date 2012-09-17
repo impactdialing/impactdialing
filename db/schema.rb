e0cc48b90061a4e856538324f64fb34092c48542
michael <michaelrkn@gmail.com>
2012-09-17 12:52:06 +0530

    fix migration index_callers_on_call_group_by_campaign

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120917070247) do
+ActiveRecord::Schema.define(:version => 20120917072028) do
 
   create_table "accounts", :force => true do |t|
     t.boolean  "card_verified"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(:version => 20120917070247) do
   end
 
   add_index "caller_sessions", ["caller_id"], :name => "index_caller_sessions_on_caller_id"
+  add_index "caller_sessions", ["campaign_id", "on_call"], :name => "index_callers_on_call_group_by_campaign"
   add_index "caller_sessions", ["campaign_id"], :name => "index_caller_sessions_on_campaign_id"
   add_index "caller_sessions", ["sid"], :name => "index_caller_sessions_on_sid"
 
@@ -560,7 +561,6 @@ ActiveRecord::Schema.define(:version => 20120917070247) do
   add_index "voters", ["attempt_id"], :name => "index_voters_on_attempt_id"
   add_index "voters", ["caller_session_id"], :name => "index_voters_on_caller_session_id"
   add_index "voters", ["campaign_id", "active", "status", "call_back"], :name => "index_voters_on_campaign_id_and_active_and_status_and_call_back"
-  add_index "voters", ["campaign_id", "enabled", "priority", "status"], :name => "index_callers_on_call_group_by_campaign"
   add_index "voters", ["campaign_id", "enabled", "priority", "status"], :name => "index_priority_voters"
   add_index "voters", ["campaign_id", "status", "id"], :name => "index_voters_on_campaign_id_and_status_and_id"
   add_index "voters", ["campaign_id", "status", "last_call_attempt_time"], :name => "voters_campaign_status_time"
