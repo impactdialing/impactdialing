df8ad65f20a95d275bbf7e1746cbe99037e74bdd
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-18 00:28:26 +0530

    catching exceptions for failed jobs

--- jobs/calculate_dials_job.rb
+++ jobs/calculate_dials_job.rb
@@ -13,16 +13,20 @@ class CalculateDialsJob
 
 
    def self.perform(campaign_id)
-     campaign = Campaign.find(campaign_id)
-     num_to_call = campaign.number_of_voters_to_dial - campaign.dialing_count
-     Rails.logger.info "Campaign: #{campaign.id} - num_to_call #{num_to_call}"    
-     return if num_to_call <= 0    
-     voters_to_dial = campaign.choose_voters_to_dial(num_to_call).collect {|voter| voter.id}
-     campaign.increment_campaign_dial_count(voters_to_dial.size)
-     if voters_to_dial.size <=10
-       Dial.perform(campaign_id, voters_to_dial)
-     else
-       voters_to_dial.each_slice(10).to_a.each {|voters| Resque.enqueue(DialerJob, campaign.id, voters) }     
+     begin
+       campaign = Campaign.find(campaign_id)
+       num_to_call = campaign.number_of_voters_to_dial - campaign.dialing_count
+       Rails.logger.info "Campaign: #{campaign.id} - num_to_call #{num_to_call}"    
+       return if num_to_call <= 0    
+       voters_to_dial = campaign.choose_voters_to_dial(num_to_call).collect {|voter| voter.id}
+       campaign.increment_campaign_dial_count(voters_to_dial.size)
+       if voters_to_dial.size <=10
+         Dial.perform(campaign_id, voters_to_dial)
+       else
+         voters_to_dial.each_slice(10).to_a.each {|voters| Resque.enqueue(DialerJob, campaign.id, voters) }     
+       end
+     rescue Exception => e
+       
      end                    
    end
 end
