0cacbc5e87c9b67c8f96ff2bd4bf1713a0feaa82
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-17 19:04:01 +0530

    resque dialer

--- lib/resque_predictive_dialer.rb
+++ lib/resque_predictive_dialer.rb
@@ -14,7 +14,7 @@ loop do
     logged_in_campaigns = CallerSession.campaigns_on_call
     logged_in_campaigns.each do |c|
       campaign = Campaign.find(c.campaign_id)
-      if campaign.type != Campaign::Type::PREDICTIVE
+      if campaign.type == Campaign::Type::PREDICTIVE
         campaign.dial_resque
       end
     end
