fd023b92b9e72d22aa05ddc96b6787b16a3f30ac
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-17 17:53:05 +0530

    adding call connected to proc file

---
+++ lib/connected_call.rb
@@ -0,0 +1,54 @@
+RAILS_ROOT = File.expand_path('../..', __FILE__)
+require File.join(RAILS_ROOT, 'config/environment')
+require File.join(RAILS_ROOT, 'lib/redis_connection')
+require "eventmachine"
+require 'em-http'
+require 'em-hiredis'
+require 'em-http-request'
+
+module Connected
+  module Call
+    
+    def self.redis
+      redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
+      @redis ||= EM::Hiredis.connect(redis_config[ENV['RAILS_ENV']]['common'])      
+    end
+
+    def self.next
+      redis.blpop('connected_call_notification', 0).callback do |list, data|
+        notification = JSON.parse(data)        
+        call_attempt_id = notification.delete('call_attempt_id')
+        event = notification.delete('event')
+        self.send(event, call_attempt_id)
+        EM.next_tick(&method(:next))   
+      end      
+    end
+    
+    def self.call_connected(call_attempt_id)
+      call_attempt = CallAttempt.find(call_attempt_id)
+      campaign = call_attempt.campaign
+      # RedisCampaignCall.move_ringing_to_inprogress(campaign.id, call_attempt.id)
+      # MonitorEvent.incoming_call_request(campaign)      
+    end
+    
+    def self.end_answered_call(call_attempt_id)
+      call_attempt = CallAttempt.find(call_attempt_id)
+      campaign = call_attempt.campaign            
+      # RedisCampaignCall.move_inprogress_to_wrapup(campaign.id, call_attempt.id)      
+    end    
+    
+    def self.wrapup(call_attempt_id)
+      call_attempt = CallAttempt.find(call_attempt_id)
+      campaign = call_attempt.campaign                  
+      RedisCallMysql.call_completed(call_attempt.id)      
+      # MonitorEvent.voter_response_submitted(campaign)
+      # RedisCampaignCall.move_inprogress_to_wrapup(campaign.id, call_attempt.id)      
+    end    
+    
+    
+  end
+end
+
+EM.run do
+  Connected::Call.next
+end
