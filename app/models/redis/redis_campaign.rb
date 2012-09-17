aa50f5be7c7f4b1373880387af289b3b1ac81e48
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-17 20:15:35 +0530

    changes

--- app/models/redis/redis_campaign.rb
+++ app/models/redis/redis_campaign.rb
@@ -17,6 +17,12 @@ class RedisCampaign
     campaign_set << campaign_id if type == Campaign::Type::PREDICTIVE
   end
   
+  def self.remove_running_predictive_campaign(campaign_id)
+    campaign_set = Redis::Set.new("running_campaigns", $redis_call_flow_connection)    
+    campaign_set.delete(campaign_id)
+  end
+  
+  
   def self.running_campaigns
     campaign_set = Redis::Set.new("running_campaigns", $redis_call_flow_connection)
     campaign_set.members
