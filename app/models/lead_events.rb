b8a026128615ccaa5b37d5d746b0d5ee99eef184
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-21 16:35:00 +0530

    em

--- app/models/lead_events.rb
+++ app/models/lead_events.rb
@@ -13,8 +13,8 @@ module LeadEvents
           if !caller.is_phones_only? && !caller_session.nil?
             event_hash = campaign.voter_connected_event(self.call)        
             caller_deferrable = Pusher[caller_session.session_key].trigger_async(event_hash[:event], event_hash[:data].merge!(:dialer => campaign.type))
-            caller_deferrable.callback {}
-            caller_deferrable.errback { |error| }
+            caller_deferrable.callback {EM.stop}
+            caller_deferrable.errback { |error| EM.stop }
           end
           # Moderator.active_moderators(campaign).each do |moderator|
           #   moderator_deferrable = Pusher[moderator.session].trigger_async('voter_event', {caller_session_id:  caller_session.id, campaign_id:  campaign.id, caller_id:  caller_session.caller.id, call_status: caller_session.attempt_in_progress.try(:status)})      
@@ -35,8 +35,8 @@ module LeadEvents
           now = Time.now
           unless caller_session.caller.is_phones_only?      
             caller_deferrable = Pusher[caller_session.session_key].trigger_async("voter_disconnected", {})
-            caller_deferrable.callback {}
-            caller_deferrable.errback { |error| puts error.inspect}
+            caller_deferrable.callback {EM.stop}
+            caller_deferrable.errback { |error| EM.stop}
           end
           # Moderator.active_moderators(campaign).each do |moderator|
           #   moderator_deferrable = Pusher[moderator.session].trigger_async('voter_event', {caller_session_id:  caller_session.id, campaign_id:  campaign.id, caller_id:  caller_session.caller.id, call_status: caller_session.attempt_in_progress.try(:status)})      
