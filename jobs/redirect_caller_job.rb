943d3c5e320dda09fae864bca5ee34789aef7ff5
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-21 16:12:23 +0530

    redirect caller

--- jobs/redirect_caller_job.rb
+++ jobs/redirect_caller_job.rb
@@ -3,7 +3,7 @@ class RedirectCallerJob
   @queue = :redirect_caller_job
   
    def self.perform(call_attempt_id)    
-     call_attempt = CallAttempt.find(id)
+     call_attempt = CallAttempt.find(call_attempt_id)
      call_attempt.redirect_caller
    end
 end
