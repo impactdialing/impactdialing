cfd437eaa35596c4c9e0fff9db81f383d7fc81df
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-21 19:35:28 +0530

    power preview dials job

--- jobs/preview_power_dial_job.rb
+++ jobs/preview_power_dial_job.rb
@@ -1,7 +1,7 @@
 require 'em-http-request'
 
-class PowerPreviewDialJob
-  @queue = :power_preview_dial_job
+class PreviewPowerDialJob
+  @queue = :preview_power_dial_job
   
   def self.perform(caller_session_id, voter_id)    
     caller_session = CallerSession.find(caller_session_id)
