97113fdc4ce65af43d7d52cad00017254f0c80ab
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-21 17:57:22 +0530

    start conference

--- app/models/webui_caller_session.rb
+++ app/models/webui_caller_session.rb
@@ -9,7 +9,7 @@ class WebuiCallerSession < CallerSession
             
       state :connected do                
         before(:always) { publish_start_calling; start_conference }
-        after(:success) { publish_caller_conference_started }
+        after(:success) { Resque.enqueue(CallerPusherJob, self.id, "publish_caller_conference_started") }
         event :pause_conf, :to => :disconnected, :if => :disconnected?
         event :pause_conf, :to => :paused, :if => :call_not_wrapped_up?
         event :start_conf, :to => :connected
