fad38fb6fd34a91db22d7b3915fbef15b57b664a
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-24 13:02:55 +0530

    read slave block

--- jobs/simulator_job.rb
+++ jobs/simulator_job.rb
@@ -147,7 +147,7 @@ class SimulatorJob
        call_attempts_from_start_time = campaign.call_attempts.last(1000)
      end
 
-
+     puts "Simulating #{call_attempts_from_start_time.size} call attempts"
      observed_conversations = call_attempts_from_start_time.where(:status => "Call completed with success.").map{|attempt| OpenStruct.new(:length => attempt.duration_wrapped_up, time_to_wrapup: attempt.time_to_wrapup, :counter => 0)}
      observed_dials = call_attempts_from_start_time.map{|attempt| OpenStruct.new(:length => attempt.ringing_duration, :counter => 0, :answered? => attempt.status == 'Call completed with success.') }
      ActiveRecord::Base.logger.info observed_conversations
