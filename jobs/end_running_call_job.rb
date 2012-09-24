8f1ae9bdedce756ed0601557221cc28d8fb09f38
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-24 16:12:48 +0530

    end running call job

--- jobs/end_running_call_job.rb
+++ jobs/end_running_call_job.rb
@@ -2,7 +2,7 @@ class EndRunningCallJob
   @queue = :end_running_call
   
    def self.perform(call_sid)
-     t = TwilioLib.new(account, auth)    
+     t = TwilioLib.new    
      t.end_call_sync(call_sid)              
    end
 end
