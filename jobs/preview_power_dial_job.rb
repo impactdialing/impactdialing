322cb603d9ae175b3c1973161eb777ea29f724b2
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-25 14:27:58 +0530

    preview power campaign fix

--- jobs/preview_power_dial_job.rb
+++ jobs/preview_power_dial_job.rb
@@ -1,11 +1,7 @@
 require 'em-http-request'
-require 'resque/plugins/lock'
-require 'resque-loner'
 
 
 class PreviewPowerDialJob
-  extend Resque::Plugins::Lock
-  include Resque::Plugins::UniqueJob
   @queue = :preview_power_dial_job
   
   def self.perform(caller_session_id, voter_id)    
