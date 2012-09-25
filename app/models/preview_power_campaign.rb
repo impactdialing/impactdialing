a4b575d04261bd6f126b2add3f78cb5db583f579
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-25 12:27:42 +0530

    using retry instead of recursion

--- app/models/preview_power_campaign.rb
+++ app/models/preview_power_campaign.rb
@@ -1,17 +1,16 @@
 module PreviewPowerCampaign
   
   def next_voter_in_dial_queue(current_voter_id = nil)
-    voter = all_voters.priority_voters.first
-    voter||= all_voters.scheduled.first
-    voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.not_skipped.where("voters.id > #{current_voter_id}").first unless current_voter_id.blank?
-    voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.not_skipped.first
-    voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.where("voters.id != #{current_voter_id}").first unless current_voter_id.blank?
-    voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.first    
     begin
+      voter = all_voters.priority_voters.first
+      voter||= all_voters.scheduled.first
+      voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.not_skipped.where("voters.id > #{current_voter_id}").first unless current_voter_id.blank?
+      voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.not_skipped.first
+      voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.where("voters.id != #{current_voter_id}").first unless current_voter_id.blank?
+      voter||= all_voters.last_call_attempt_before_recycle_rate(recycle_rate).to_be_dialed.first    
       update_voter_status_to_ready(voter)
     rescue ActiveRecord::StaleObjectError
-      puts "Entered recursion xxxxxxxxxxxxxxxxxxxxxxxx"
-      voter = next_voter_in_dial_queue(current_voter_id)
+      retry
     end
     voter
   end  
