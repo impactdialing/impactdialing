82daae6c1a5f1ba2a2237039335b7ddd8baecc84
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-25 13:13:07 +0530

    call end task and change in proc file

--- app/models/redis_call.rb
+++ app/models/redis_call.rb
@@ -1,7 +1,6 @@
 class RedisCall
   
   def self.store_call_details(params)
-    puts params
-    Resque.redis.rpush("impactdialing_calls", params)
+    Resque.enqueue(CallEndJob, params);    
   end
 end
---
+++ jobs/call_end_job.rb
@@ -0,0 +1,11 @@
+class CallEndJob 
+  @queue = :call_end_job
+  
+   def self.perform(params)    
+     call_id = params['id']
+     unless call_id.blank?
+       call = Call.find(call_id)
+       call.end_unanswered_call(params['call_status'])
+     end
+   end
+end
