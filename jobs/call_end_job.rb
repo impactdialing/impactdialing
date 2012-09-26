0920d730b20028cc79a2302de4d5d503de43851f
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-26 18:26:23 +0530

    call end event for answered by machine calls

--- jobs/call_end_job.rb
+++ jobs/call_end_job.rb
@@ -5,7 +5,8 @@ class CallEndJob
      call_id = params['id']
      unless call_id.blank?
        call = Call.find(call_id)
-       call.end_unanswered_call(params['call_status'])
+       call.end_unanswered_call(params['call_status']) if ["no-answer", "busy", "failed"].include?(params['call_status'])
+       call.end_answered_by_machine if params['answered_by'] == "machine"
      end
    end
 end
