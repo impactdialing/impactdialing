8eba19eb190bc2ad5a8d759105aa4ef03105d359
Nikhil Fernandes <nikhil@activesphere.com>
2012-09-27 14:06:01 +0530

    fixing active record stale object in preview power

--- jobs/preview_power_dial_job.rb
+++ jobs/preview_power_dial_job.rb
@@ -3,6 +3,10 @@ class PreviewPowerDialJob
   
   def self.perform(caller_session_id, voter_id)    
     caller_session = CallerSession.find(caller_session_id)
-    caller_session.dial(Voter.find(voter_id)) 
+    begin
+      caller_session.dial(Voter.find(voter_id)) 
+    rescue ActiveRecord::StaleObjectError 
+      caller_session.redirect_caller
+    end
   end
 end
