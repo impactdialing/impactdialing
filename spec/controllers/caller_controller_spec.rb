36c6bcc055858f704c08d15a7acd31006caf3baf
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-01 17:15:22 +0530

    adding specs for caller controller

--- spec/controllers/caller_controller_spec.rb
+++ spec/controllers/caller_controller_spec.rb
@@ -24,16 +24,29 @@ describe CallerController do
   
   describe "start calling" do
     it "should start a new caller conference" do
-      caller = Factory(:caller, campaign: Factory(:predictive))
+      caller = Factory(:caller, campaign: Factory(:predictive), account: Factory(:account))
       caller_identity = Factory(:caller_identity)
-      caller_session = Factory(:webui_caller_session, session_key: caller_identity.session_key, caller_type: CallerSession::CallerType::TWILIO_CLIENT)
+      caller_session = Factory(:webui_caller_session, session_key: caller_identity.session_key, caller_type: CallerSession::CallerType::TWILIO_CLIENT, caller: caller)
+      Caller.should_receive(:find).and_return(caller)
       caller.should_receive(:create_caller_session).and_return(caller_session)
       RedisCampaign.should_receive(:add_running_predictive_campaign).with(caller.campaign_id, caller.campaign.type)
       RedisCaller.should_receive(:add_caller).with(caller.campaign.id, caller_session.id)
-      RedisCallNotification.should_receive(:caller_connected(session.id)
-      
-      post :start_calling, caller_id: caller.id, session_key: caller_identity.session_key, CallSid: "abc"
-      
+      RedisCallNotification.should_receive(:caller_connected).with(caller_session.id)      
+      post :start_calling, caller_id: caller.id, session_key: caller_identity.session_key, CallSid: "abc"      
+      response.body.should eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Say>Your account has insufficent funds</Say><Hangup/></Response>")
+    end
+  end
+  
+  describe "call voter" do
+    it "should call voter" do
+      campaign =  Factory(:predictive)
+      caller = Factory(:caller, campaign: campaign, account: Factory(:account))
+      caller_identity = Factory(:caller_identity)
+      caller_session = Factory(:webui_caller_session, session_key: caller_identity.session_key, caller_type: CallerSession::CallerType::TWILIO_CLIENT, caller: caller)
+      voter = Factory(:voter,campaign: campaign)
+      Resque.should_receive(:enqueue).with(CallerPusherJob, caller_session.id, "publish_calling_voter")   
+      Resque.should_receive(:enqueue).with(PreviewPowerDialJob, caller_session.id, voter.id.to_s)    
+      post :call_voter, id: caller.id, voter_id: voter.id, session_id: caller_session.id
     end
   end
  
