6b848a9741a1c45bf35f5b37f788ac498565796c
michael <michaelrkn@gmail.com>
2012-10-01 17:54:49 +0300

    rename upload and download jobs to make them more sane

--- Procfile
+++ Procfile
@@ -1,18 +1,18 @@
-web:  bundle exec rails server thin -p $PORT
+web: bundle exec rails server thin -p $PORT
 
 dialer_loop: bundle exec ruby lib/dialer_loop.rb
 simulator_loop: bundle exec ruby simulator/simulator_loop.rb
 
-dialer_worker: rake environment resque:work QUEUE=dialer
-simulator_worker: rake environment resque:work QUEUE=simulator
+dialer_worker: rake environment resque:work QUEUE=dialer_worker
+simulator_worker: rake environment resque:work QUEUE=simulator_worker
 
-voter_list_upload_worker_job: rake environment resque:work QUEUE=voter_list_upload_worker_job
-report_download_worker_job: rake environment resque:work QUEUE=report_download_worker_job
+list_upload: rake environment resque:work QUEUE=list_upload
+report_download: rake environment resque:work QUEUE=report_download
 
-background_worker_job: rake environment resque:work QUEUE=background_worker_job
+background_worker: rake environment resque:work QUEUE=background_worker
 
-answered_worker_job: rake environment resque:work QUEUE=answered_worker
-debit_worker_job: rake environment resque:work QUEUE=debit_worker
+answered_worker: rake environment resque:work QUEUE=answered_worker
+debit_worker: rake environment resque:work QUEUE=debit_worker
 
 call_flow: rake environment resque:work QUEUE=call_flow
 call_end: rake environment resque:work QUEUE=call_end
@@ -20,4 +20,4 @@ call_end: rake environment resque:work QUEUE=call_end
 moderator_flow: rake environment resque:work QUEUE=moderator_caller
 moderator_flow: rake environment resque:work QUEUE=moderator_call
 
-clock:   rake environment resque:scheduler
+clock: rake environment resque:scheduler
--- jobs/report_download_job.rb
+++ jobs/report_download_job.rb
@@ -1,7 +1,7 @@
 require Rails.root.join("jobs/heroku_resque_auto_scale")
 class ReportDownloadJob
   extend ::HerokuResqueAutoScale
-  @queue = :report_download_worker_job
+  @queue = :report_download
 
 
    def self.perform(campaign_id, user_id, voter_fields, custom_fields, all_voters,lead_dial, from, to, callback_url, strategy="webui")
@@ -10,13 +10,13 @@ class ReportDownloadJob
    end
 
    def self.after_perform_scale_down(*args)
-     HerokuResqueAutoScale::Scaler.workers('report_download_worker_job',1) if HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') == 1
+     HerokuResqueAutoScale::Scaler.workers('report_download',1) if HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') == 1
    end
 
    def self.after_enqueue_scale_up(*args)
-      workers_to_scale = HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download_worker_job') - HerokuResqueAutoScale::Scaler.worker_count('report_download_worker_job')
-      if workers_to_scale > 0 && HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') < 2
-        HerokuResqueAutoScale::Scaler.workers('report_download_worker_job', (HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download_worker_job')))
+      workers_to_scale = HerokuResqueAutoScale::Scaler.working_job_count('report_download') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download_worker_job') - HerokuResqueAutoScale::Scaler.worker_count('report_download_worker_job')
+      if workers_to_scale > 0 && HerokuResqueAutoScale::Scaler.working_job_count('report_download') < 2
+        HerokuResqueAutoScale::Scaler.workers('report_download', (HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download_worker_job')))
       end
     end
 
