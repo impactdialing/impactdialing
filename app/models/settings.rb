8f9c885450a658b72bce7ad9e76ef3d3133d5989
michael <michaelrkn@gmail.com>
2012-10-01 13:51:19 +0300

    make production use application.yml instead of application_heroku.yml

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,6 +1,6 @@
 class Settings < Settingslogic
-  if Rails.env == 'heroku'
-    source "#{Rails.root}/config/application_heroku.yml"
+  if Rails.env == 'heroku_preproduction'
+    source "#{Rails.root}/config/application_heroku_preproduction.yml"
   elsif Rails.env == 'heroku_staging'
     source "#{Rails.root}/config/application_heroku_staging.yml"
   else
