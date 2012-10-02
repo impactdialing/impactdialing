2317ebf83b7cd25161b183c91713962c6b20732c
michael <michaelrkn@gmail.com>
2012-10-02 12:12:30 +0300

    rename jobs to match procfile

--- jobs/report_download_job.rb
+++ jobs/report_download_job.rb
@@ -10,13 +10,13 @@ class ReportDownloadJob
    end
 
    def self.after_perform_scale_down(*args)
-     HerokuResqueAutoScale::Scaler.workers('report_download',1) if HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') == 1
+     HerokuResqueAutoScale::Scaler.workers('report_download',1) if HerokuResqueAutoScale::Scaler.working_job_count('report_download') == 1
    end
 
    def self.after_enqueue_scale_up(*args)
-      workers_to_scale = HerokuResqueAutoScale::Scaler.working_job_count('report_download') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download_worker_job') - HerokuResqueAutoScale::Scaler.worker_count('report_download_worker_job')
+      workers_to_scale = HerokuResqueAutoScale::Scaler.working_job_count('report_download') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download') - HerokuResqueAutoScale::Scaler.worker_count('report_download')
       if workers_to_scale > 0 && HerokuResqueAutoScale::Scaler.working_job_count('report_download') < 2
-        HerokuResqueAutoScale::Scaler.workers('report_download', (HerokuResqueAutoScale::Scaler.working_job_count('report_download_worker_job') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download_worker_job')))
+        HerokuResqueAutoScale::Scaler.workers('report_download', (HerokuResqueAutoScale::Scaler.working_job_count('report_download') + HerokuResqueAutoScale::Scaler.pending_job_count('report_download')))
       end
     end
 
