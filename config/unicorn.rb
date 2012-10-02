cf553424b2e39f827dffca683b8d5c8d92d32566
gregolsen <anotheroneman@yahoo.com>
2012-10-02 13:52:53 +0300

    unicord added

---
+++ config/unicorn.rb
@@ -0,0 +1,21 @@
+worker_processes 3
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+
+  sleep 1
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection_proxy.instance_variable_get(:@shards).each do |k,v|
+      v.clear_reloadable_connections!
+    end
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+end
