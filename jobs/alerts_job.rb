b0e6b4dc2a281d3cb29c141d3e22a23d82d056f6
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-03 19:56:29 +0530

    alerts for different scenarios

--- jobs/alerts_job.rb
+++ jobs/alerts_job.rb
@@ -2,18 +2,33 @@ class CallEndJob
   @queue = :background_worker
   
    def self.perform
-     predictive_campaign_ids = CallerSession.campaigns_on_call.where("type = 'Predictive'").pluck(:id)     
-     simulator_not_run_campaigns = SimulatedValues.where("campaign_id in (?) and updated_at < ?", predictive_campaign_ids, 2.minutes.ago)
+     campaign_ids = CallerSession.campaigns_on_call.pluck(:id)     
+     predictive_campaign_ids = Campaign.where("type = 'Predictive' and id in (?)", campaign_ids).pluck(:id)
+     alert_on_hold_callers(predictive_campaign_ids)
+     alert_not_simulated_campaigns(predictive_campaign_ids)
+     alert_dials_not_being_made_for_campaign(predictive_campaign_ids)
+   end
+   
+   def self.alert_dials_not_being_made_for_campaign(predictive_campaign_ids)
+     calls_made_in_2_minutes = CallAttempt.where("campaign_id in (?)", predictive_campaign_ids).between(2.minutes.ago, Time.now).group("campaign_id").count
+     no_dials = calls_made_in_2_minutes.select {|k,v| v == 0}
+     unless no_dials.empty?
+       email_all("Dials not made for the following campaigns in the last 2 minutes", no_dials.join(" , ").to_s)
+     end     
+   end
+   
+   def self.alert_not_simulated_campaigns(predictive_campaign_ids)
+     simulator_not_run_campaigns = SimulatedValues.where("campaign_id in (?) and updated_at < ?", predictive_campaign_ids, 2.minutes.ago).pluck(:campaign_id)
      unless simulator_not_run_campaigns.blank?
-       simulator_not_run_campaigns.pluck(:campaign_id)
+       email_all("Campaigns not Simulated in the last 2 minutes", simulator_not_run_campaigns.join(" , ").to_s)
      end
    end
    
-   def self.alter_on_hold_callers(predictive_campaign_ids)
-     caller_sessions_on_hold_high = CallerSession.available.where("updated_at < ? and campaign_id in (?)", 2.minutes.ago, predictive_campaign_ids)
+   
+   def self.alert_on_hold_callers(predictive_campaign_ids)
+     caller_sessions_on_hold_high = CallerSession.available.where("updated_at < ? and campaign_id in (?)", 2.minutes.ago, predictive_campaign_ids).pluck(:id)
      unless caller_sessions_on_hold_high.blank?
-       caller_session_ids = caller_sessions_on_hold_high.collect{|c| c.id}
-       email_all("Callers on hold for long", caller_session_ids.join(" , ").to_s)
+       email_all("Callers on hold for long", caller_sessions_on_hold_high.join(" , ").to_s)
      end         
    end
    
