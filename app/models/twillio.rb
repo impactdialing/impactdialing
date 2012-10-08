a077c680f472676eb6c3b1cb7d022b392df5a3a5
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-08 21:40:19 +0530

    caller on call state

--- app/models/twillio.rb
+++ app/models/twillio.rb
@@ -46,7 +46,7 @@ class Twillio
     attempt = voter.call_attempts.create(:campaign => campaign, :dialer_mode => campaign.type, :status => CallAttempt::Status::RINGING, :caller_session => caller_session, :caller => caller_session.caller, call_start:  Time.now)    
     voter.update_attributes(:last_call_attempt_id => attempt.id, :last_call_attempt_time => Time.now, :caller_session_id => caller_session.id, status: CallAttempt::Status::RINGING)
     Call.create(call_attempt: attempt, all_states: "", state: "initial")
-    enqueue_dial_flow(CampaignStatusJob, ["dialing", campaign.id, call_attempt.id, caller_session.id])
+    enqueue_dial_flow(CampaignStatusJob, ["dialing", campaign.id, attempt.id, caller_session.id])
     attempt    
   end
   
