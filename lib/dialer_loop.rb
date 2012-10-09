486c436eec858eb05a5d3b2abc67ef3ac2db5f44
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-09 17:12:21 +0530

    setup beta to dial only few campaigns

--- lib/dialer_loop.rb
+++ lib/dialer_loop.rb
@@ -14,7 +14,7 @@ loop do
     logged_in_campaigns = RedisPredictiveCampaign.running_campaigns
     logged_in_campaigns.each do |campaign_id|
       campaign = Campaign.find(campaign_id)
-      campaign.dial_resque if !campaign.calculate_dialing?
+      campaign.dial_resque if !campaign.calculate_dialing? && campaign.dial_campaign?
     end
     sleep 3
   rescue Exception => e
