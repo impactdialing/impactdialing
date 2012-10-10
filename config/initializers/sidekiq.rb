c2214b778ae196eb58749b10332f37d5eb758f9c
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-11 02:03:22 +0530

    reducing concurency in sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,10 +4,10 @@ redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
 if rails_env == 'development' || rails_env == "test"  
 else  
   Sidekiq.configure_server do |config|
-    config.redis = { :url => redis_config[rails_env]['sidekiq'], :namespace => 'resque', :size => (Sidekiq.options[:concurrency] + 2) }    
+    config.redis = { :url => redis_config[rails_env]['sidekiq'], :namespace => 'resque') }    
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { :url => redis_config[rails_env]['sidekiq'], :namespace => 'resque', :size => (Sidekiq.options[:concurrency] + 2)}
+    config.redis = { :url => redis_config[rails_env]['sidekiq'], :namespace => 'resque')}
   end    
 end
