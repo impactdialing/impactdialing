9a86312ccd831bdaaefeb5665d819ab1dad8b55e
gregolsen <anotheroneman@yahoo.com>
2012-10-11 21:06:55 +0300

    persistence job optimized

--- jobs/persist_calls.rb
+++ jobs/persist_calls.rb
@@ -53,10 +53,11 @@ class PersistCalls
   end
   
   def self.machine_calls(call_attempts, voters)
-    unanswered_calls = multipop(RedisCall.end_answered_by_machine_call_list, 100)
-    unanswered_calls.each do |unanswered_call|
+    unanswered_calls = multipop(RedisCall.end_answered_by_machine_call_list, 100).sort_by { |a| a['id'] }
+    calls = Call.where(id: unanswered_calls.map { |c| c['id'] }).
+      includes(call_attempt: :voter).order(:id)
+    calls.zip(unanswered_calls).each do |call, unanswered_call|
       begin
-        call = Call.find(unanswered_call['id'])
         connect_time = RedisCall.processing_by_machine_call_hash[unanswered_call['id']]
         call_attempt = call.call_attempt
         voter = call_attempt.voter
@@ -66,15 +67,15 @@ class PersistCalls
         voters << voter
       rescue Exception
       end
-      
     end    
   end
   
   def self.disconnected_calls(call_attempts, voters)
-    disconnected_calls = multipop(RedisCall.disconnected_call_list ,100)
-    disconnected_calls.each do |disconnected_call|
+    disconnected_calls = multipop(RedisCall.disconnected_call_list ,100).sort_by { |a| a['id'] }
+    calls = Call.where(id: disconnected_calls.map { |c| c['id'] }).
+      includes(call_attempt: :voter).order(:id)
+    calls.zip(disconnected_calls).each do |call, disconnected_call|
       begin
-        call = Call.find(disconnected_call['id'])
         call_attempt = call.call_attempt
         voter = call_attempt.voter
         call_attempt.disconnect_call(disconnected_call['current_time'], disconnected_call['recording_duration'], disconnected_call['recording_url'], disconnected_call['caller_id'] )
@@ -87,15 +88,14 @@ class PersistCalls
   end
   
   def self.wrapped_up_calls(call_attempts, voters)    
-    wrapped_up_calls = multipop(RedisCall.wrapped_up_call_list ,100)
-    wrapped_up_calls.each do |wrapped_up_call|
+    wrapped_up_calls = multipop(RedisCall.wrapped_up_call_list ,100).sort_by { |a| a['id'] }
+    call_attempts = CallAttempt.where(id: wrapped_up_calls.map  { |c| c['id'] }).order(:id)
+    call_attempts.zip(wrapped_up_calls).each do |call_attempt, wrapped_up_call|
       begin
-        call_attempt = CallAttempt.find(wrapped_up_call['id'])
         call_attempt.wrapup_now(wrapped_up_call['current_time'], wrapped_up_call['caller_type'])
         call_attempts << call_attempt
       rescue
       end
-        
     end    
   end
   
