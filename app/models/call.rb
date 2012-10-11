f2d708133c73e5aa16fffa6d195a87929647ca91
gregolsen <anotheroneman@yahoo.com>
2012-10-11 16:34:39 +0300

    check added so that no voter_connected published in case phones only

--- app/models/call.rb
+++ app/models/call.rb
@@ -34,7 +34,9 @@ class Call < ActiveRecord::Base
         
         before(:always) {  
           connect_call;
-          enqueue_call_flow(VoterConnectedPusherJob, [caller_session.id, self.id])
+          if !Caller.where(caller_session_id: call_attempt.caller_session_id).select(:is_phones_only).is_phones_only?
+            enqueue_call_flow(VoterConnectedPusherJob, [caller_session.id, self.id])
+          end
           # enqueue_dial_flow(CampaignStatusJob, ["connected", campaign.id, call_attempt.id, caller_session.id])          
         }
         
