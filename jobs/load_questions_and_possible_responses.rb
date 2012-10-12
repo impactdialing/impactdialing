247f565f482f933a1a18f53210b1f99563558c34
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-12 20:24:32 +0530

    getting question order right

--- jobs/load_questions_and_possible_responses.rb
+++ jobs/load_questions_and_possible_responses.rb
@@ -3,10 +3,10 @@ class LoadQuestionsAndPossibleResponses
   def perform
     Script.all.each do |script|
       RedisQuestion.clear_list(script.id)
-      script.questions.each do|question|        
+      script.questions.reverse.each do|question|        
         RedisQuestion.persist_questions(script.id, question.id, question.text)
         RedisPossibleResponse.clear_list(question.id)
-        question.possible_responses.each do |possible_response|          
+        question.possible_responses.reverse.each do |possible_response|          
           RedisPossibleResponse.persist_possible_response(question.id, possible_response.keypad, possible_response.value)
         end
       end
