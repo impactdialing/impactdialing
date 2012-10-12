4528ad76e51d331de98df8068949cf278e0ad89f
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-12 22:08:44 +0530

    redis conf

--- jobs/persist_phones_only_answers.rb
+++ jobs/persist_phones_only_answers.rb
@@ -8,10 +8,13 @@ class PersistPhonesOnlyAnswers
       lists = []
       answers_list = multipop($redis_phones_ans_uri_connection, 'phones_only_answer_list', 100).sort_by{|a| a['voter_id']}
       answers_list.each do |answer_list|
-        voter = Voter.find(answer_list['voter_id'])
-        caller_session = CallerSession.find(answer_list['caller_session_id'])
-        question = Question.find(answer_list['question_id'])
-        lists << voter.answer(question, answer_list['digit'], caller_session)        
+        begin
+          voter = Voter.find(answer_list['voter_id'])
+          caller_session = CallerSession.find(answer_list['caller_session_id'])
+          question = Question.find(answer_list['question_id'])
+          lists << voter.answer(question, answer_list['digit'], caller_session)        
+        rescue Exception
+        end
       end
       Answer.import lists.compact      
     end
