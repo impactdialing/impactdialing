c2daf0a33eb88bdd9d081cc5f7a1bb84795de795
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-12 19:51:16 +0530

    adding multi to saving to redis

--- app/controllers/client/scripts_controller.rb
+++ app/controllers/client/scripts_controller.rb
@@ -131,14 +131,16 @@ module Client
     end
     
     def persist_in_redis
-      RedisQuestion.clear_list(@script.id)
-      @script.questions.each do|question|
-        RedisQuestion.persist_questions(script.id, question.id, question.text)
-        RedisPossibleResponse.clear_list(question.id)
-        question.possible_responses.each do |possible_response|
-          RedisPossibleResponse.persist_possible_response(question.id, possible_response.keypad, possible_response.value)
-        end
-      end      
+      $redis_question_pr_uri_connection.multi do
+        RedisQuestion.clear_list(@script.id)
+        @script.questions.each do|question|
+          RedisQuestion.persist_questions(script.id, question.id, question.text)
+          RedisPossibleResponse.clear_list(question.id)
+          question.possible_responses.each do |possible_response|
+            RedisPossibleResponse.persist_possible_response(question.id, possible_response.keypad, possible_response.value)
+          end
+        end      
+      end
     end
   end
 end
