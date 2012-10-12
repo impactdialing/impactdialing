2bd966b07b08a57e402a399b15408e9da4d75c4b
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-12 21:38:22 +0530

    persisting answer in redis and then mysql

--- jobs/persist_phones_only_answers.rb
+++ jobs/persist_phones_only_answers.rb
@@ -5,21 +5,21 @@ class PersistPhonesOnlyAnswers
     @queue = :persist_jobs
     
     def self.perform
-      answers = []
-      answers_list = multipop(RedisPhonesOnlyAnswer.phones_only_answers_list, 100).sort_by{|a| a['voter_id']}
+      lists = []
+      answers_list = multipop($redis_phones_ans_uri_connection, 'phones_only_answer_list', 100).sort_by{|a| a['voter_id']}
       answers_list.each do |answer_list|
         voter = Voter.find(answer_list['voter_id'])
         caller_session = CallerSession.find(answer_list['caller_session_id'])
         question = Question.find(answer_list['question_id'])
-        answers << voter.answer(question, answer_list['digit'], caller_session)        
+        lists << voter.answer(question, answer_list['digit'], caller_session)        
       end
-      Answer.import answers      
+      Answer.import lists.compact      
     end
     
-    def self.multipop(list, num)
+    def self.multipop(connection, list_name, num)
       result = []
       num.times do |x|
-        element = list.shift
+        element = connection.rpop list_name
         result << JSON.parse(element) unless element.nil?
       end
       result
