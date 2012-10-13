7c45bac006629b5b1109a1bd7b2052bd260c16f5
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-13 21:31:13 +0530

    fixing redis script reverse order

--- app/controllers/client/scripts_controller.rb
+++ app/controllers/client/scripts_controller.rb
@@ -133,10 +133,10 @@ module Client
     def persist_in_redis
       $redis_question_pr_uri_connection.multi do
         RedisQuestion.clear_list(@script.id)
-        @script.questions.each do|question|
+        @script.questions.reverse.each do|question|
           RedisQuestion.persist_questions(@script.id, question.id, question.text)
           RedisPossibleResponse.clear_list(question.id)
-          question.possible_responses.each do |possible_response|
+          question.possible_responses.reverse.each do |possible_response|
             RedisPossibleResponse.persist_possible_response(question.id, possible_response.keypad, possible_response.value)
           end
         end      
