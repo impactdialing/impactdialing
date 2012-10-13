9a9ef2949ba419675a734db54c795975bc2a2bee
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-13 10:26:46 +0530

    increasing number of calls to be persisted in each run

--- jobs/persist_phones_only_answers.rb
+++ jobs/persist_phones_only_answers.rb
@@ -6,7 +6,7 @@ class PersistPhonesOnlyAnswers
     
     def self.perform
       lists = []
-      answers_list = multipop($redis_phones_ans_uri_connection, 'phones_only_answer_list', 100).sort_by{|a| a['voter_id']}
+      answers_list = multipop($redis_phones_ans_uri_connection, 'phones_only_answer_list', 1000).sort_by{|a| a['voter_id']}
       answers_list.each do |answer_list|
         begin
           voter = Voter.find(answer_list['voter_id'])
