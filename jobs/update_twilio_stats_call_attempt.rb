4db2e01e22abdb3a72a01080e06391aad0260f00
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-14 19:55:00 +0530

    update twilio stats

---
+++ jobs/update_twilio_stats_call_attempt.rb
@@ -0,0 +1,16 @@
+require 'resque-loner'
+class UpdateTwilioStatsCallAttempt
+  include Resque::Plugins::UniqueJob
+  @queue = :twilio_stats_attempt
+  
+  def self.perform
+    call_attempts = []
+    twillio_lib = TwilioLib.new    
+    CallAttempt.where("tPrice is NULL and (tStatus is NULL or tStatus = 'completed') and sid is not null").limit(500).each do |attempt|
+      call_attempts << twillio_lib.update_twilio_stats_by_model(attempt)
+    end
+      CallAttempt.import call_attempts, :on_duplicate_key_update=>[:tCallSegmentSid, :tAccountSid,
+                                        :tCalled, :tCaller, :tPhoneNumberSid, :tStatus, :tStartTime, :tEndTime, :tDuration, :tPrice, :tFlags]
+  end
+  
+end
