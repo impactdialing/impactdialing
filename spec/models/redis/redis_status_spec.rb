09e24cbee786188ef738e08815791af12a06e791
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-15 18:09:38 +0530

    monitor page status and time

--- spec/models/redis/redis_status_spec.rb
+++ spec/models/redis/redis_status_spec.rb
@@ -4,7 +4,20 @@ describe RedisStatus do
   
   it "should return back state and time" do
     RedisStatus.set_state_changed_time(1, "On hold", 1)
-    RedisStatus.state_time(1, 1).should eq([])
+    RedisStatus.state_time(1, 1).should eq(["On hold", "0:0:0"])
+  end
+  
+  it "should delete state" do
+    RedisStatus.set_state_changed_time(1, "On hold", 1)
+    RedisStatus.delete_state(1,1)
+    RedisStatus.state_time(1, 1).should be_nil
+  end
+  
+  it "should return count by status" do
+    RedisStatus.set_state_changed_time(1, "On hold", 1)
+    RedisStatus.set_state_changed_time(1, "Wrap up", 2)
+    RedisStatus.set_state_changed_time(1, "On call", 3)
+    RedisStatus.count_by_status(1, [1,4,3,2]).should eq([1, 1, 1])
   end
   
 end
