29f8ced2261bf21ad24955b9010d3c09451331f1
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-15 19:54:30 +0530

    report download failure

--- jobs/update_twilio_stats_call_attempt.rb
+++ jobs/update_twilio_stats_call_attempt.rb
@@ -6,8 +6,7 @@ class UpdateTwilioStatsCallAttempt
   def self.perform
     call_attempts = []
     twillio_lib = TwilioLib.new    
-    CallAttempt.where("status in (?) and tPrice is NULL and (tStatus is NULL or tStatus = 'completed') and sid is not null", ['Hangup or answering machine']).limit(2000).each do |attempt|
-
+    CallAttempt.where("status in (?) and tPrice is NULL and (tStatus is NULL or tStatus = 'completed') and sid is not null", ['Message delivered', 'Call completed with success.', 'Call abandoned', 'Hangup or answering machine']).limit(2000).each do |attempt|
       call_attempts << twillio_lib.update_twilio_stats_by_model(attempt)
     end
       CallAttempt.import call_attempts, :on_duplicate_key_update=>[:tCallSegmentSid, :tAccountSid,
