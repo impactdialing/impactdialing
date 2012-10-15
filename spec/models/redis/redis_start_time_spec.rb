0c700fb2928d1a2f7b84028b7ab0d0384a9f4b4d
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-15 12:44:32 +0530

    time in status monitor page

--- spec/models/redis/redis_start_time_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe RedisStartTime do
-  
-  it "should give time in state" do
-    RedisStartTime.set_state_changed_time(1)
-    RedisStartTime.time_in_state(1).should eq("")
-  end
-end
