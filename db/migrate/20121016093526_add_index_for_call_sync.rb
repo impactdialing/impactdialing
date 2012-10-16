2c86aa2ec77a83bf583c773d3dfd6b98c87193f6
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-16 15:11:04 +0530

    index for sync calls

---
+++ db/migrate/20121016093526_add_index_for_call_sync.rb
@@ -0,0 +1,8 @@
+class AddIndexForCallSync < ActiveRecord::Migration
+  def up
+    add_index :call_attempts, [:status, :tPrice, :tStatus, sid], :name => "index_sync_calls"
+  end
+
+  def down
+  end
+end
