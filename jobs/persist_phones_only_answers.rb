ecf37fbef72c052085d8232fcfb9193e93548baf
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-16 12:57:37 +0530

    answer report for phones only

--- jobs/persist_phones_only_answers.rb
+++ jobs/persist_phones_only_answers.rb
@@ -6,7 +6,7 @@ class PersistPhonesOnlyAnswers
     
     def self.perform
       lists = []
-      answers_list = multipop($redis_phones_ans_uri_connection, 'phones_only_answer_list', 1000).sort_by{|a| a['voter_id']}
+      answers_list = multipop($redis_phones_ans_uri_connection, 'phones_only_answer_list', 2000).sort_by{|a| a['voter_id']}
       answers_list.each do |answer_list|
         begin
           voter = Voter.find(answer_list['voter_id'])
@@ -20,8 +20,10 @@ class PersistPhonesOnlyAnswers
     end
     
     def self.multipop(connection, list_name, num)
-      result = []
-      num.times do |x|
+      num_of_elements = connection.llen list_name
+      num_to_pop = num_of_elements < num ? num_of_elements : num
+      result = []      
+      num_to_pop.times do |x|
         element = connection.rpop list_name
         result << JSON.parse(element) unless element.nil?
       end
