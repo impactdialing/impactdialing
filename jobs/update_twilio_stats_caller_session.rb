20e79b1b6bd5d64c0ac5abfd1a1f7e089c632cf8
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-16 16:41:58 +0530

    report now from twillio data and syncing job

--- jobs/update_twilio_stats_caller_session.rb
+++ jobs/update_twilio_stats_caller_session.rb
@@ -7,7 +7,7 @@ class UpdateTwilioStatsCallerSession
     caller_sessions = []
     twillio_lib = TwilioLib.new
     
-    WebuiCallerSession.where("tPrice is NULL and (tStatus is NULL or tStatus = 'completed')").limit(100).each do |session|
+    WebuiCallerSession.where("endtime is not NULL and tPrice is NULL and (tStatus is NULL or tStatus = 'completed')").limit(100).each do |session|
         caller_sessions << twillio_lib.update_twilio_stats_by_model(session)
     end
     WebuiCallerSession.import caller_sessions, :on_duplicate_key_update=>[:tCallSegmentSid, :tAccountSid,
@@ -17,7 +17,7 @@ class UpdateTwilioStatsCallerSession
     caller_sessions = []
     twillio_lib = TwilioLib.new
 
-    PhonesOnlyCallerSession.where("tPrice is NULL and (tStatus is NULL or tStatus = 'completed')").limit(1000).each do |session|
+    PhonesOnlyCallerSession.where("endtime is not NULL and tPrice is NULL and (tStatus is NULL or tStatus = 'completed')").limit(100).each do |session|
         caller_sessions << twillio_lib.update_twilio_stats_by_model(session)
     end
     PhonesOnlyCallerSession.import caller_sessions, :on_duplicate_key_update=>[:tCallSegmentSid, :tAccountSid,
