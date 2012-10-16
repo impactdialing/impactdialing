0e2bc07679e6d6cd623aff0488becc20f5d5e0f9
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-16 13:11:29 +0530

    divide sync jobs

---
+++ jobs/update_stats_em_abandoned.rb
@@ -0,0 +1,32 @@
+require 'resque-loner'
+require 'em-http-request'
+require "em-synchrony"
+require "em-synchrony/em-http"
+
+
+class UpdateStatsEmAbandoned
+  include Resque::Plugins::UniqueJob
+  @queue = :twilio_stats_attempt
+  
+  def self.perform
+    results = []
+    twillio_lib = TwilioLib.new    
+    call_attempts = CallAttempt.where("status in (?) and tPrice is NULL and (tStatus is NULL or tStatus = 'completed') and sid is not null", ['Call abandoned']).limit(500)
+      EM.synchrony do
+        concurrency = 500
+        EM::Synchrony::Iterator.new(call_attempts, concurrency).map do |attempt, iter|
+          http = twillio_lib.update_twilio_stats_by_model_em(attempt)
+          http.callback { 
+            twillio_lib.twilio_xml_parse(http.response, attempt)
+            results << attempt
+            iter.return(http)      
+             }
+          http.errback { iter.return(http) }
+        end        
+        CallAttempt.import results, :on_duplicate_key_update=>[:tCallSegmentSid, :tAccountSid,
+                                          :tCalled, :tCaller, :tPhoneNumberSid, :tStatus, :tStartTime, :tEndTime, :tDuration, :tPrice, :tFlags]
+        EventMachine.stop
+      end
+  end
+  
+end
