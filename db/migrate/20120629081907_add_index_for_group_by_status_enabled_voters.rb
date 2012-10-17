e517db9fdfef7e87ed0d79008bcde53331e01fac
Maxim Bondaruk <maxim.bondaruk@railsware.com>
2012-10-17 16:59:18 +0000

    wq[#37164573] Fix schema.rb

--- db/migrate/20120629081907_add_index_for_group_by_status_enabled_voters.rb
+++ db/migrate/20120629081907_add_index_for_group_by_status_enabled_voters.rb
@@ -1,7 +1,7 @@
 class AddIndexForGroupByStatusEnabledVoters < ActiveRecord::Migration
   def self.up
     add_index(:answers, [:question_id, :campaign_id], :name => 'index_answers_distinct_question')    
-    add_index(:answers, [:possible_response_id, :campaign_id, :caller_id, :created_at], :name => 'index_answers_count_possible_response')    
+    #add_index(:answers, [:possible_response_id, :campaign_id, :caller_id, :created_at], :name => 'index_answers_count_possible_response')    
   end
 
   def self.down
