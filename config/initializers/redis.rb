61da39d91cd6c06985f6cfd298ed8d029297bc9c
Andrii Krykunenko <andriy.krykunenko@gmail.com>
2012-10-19 11:09:51 +0300

    redis password for staging

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -13,25 +13,25 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 redis_config = YAML.load_file(RAILS_ROOT + "/config/redis.yml")
 
 call_flow_uri = URI.parse(redis_config[rails_env]['call_flow'])
-$redis_call_flow_connection = Redis.new(:host => call_flow_uri.host, :port => call_flow_uri.port)      
+$redis_call_flow_connection = Redis.new(:host => call_flow_uri.host, :port => call_flow_uri.port, :password => call_flow_uri.password)
 
 call_end_uri = URI.parse(redis_config[rails_env]['call_end'])
-$redis_call_end_connection = Redis.new(:host => call_end_uri.host, :port => call_end_uri.port)      
+$redis_call_end_connection = Redis.new(:host => call_end_uri.host, :port => call_end_uri.port, :password => call_end_uri.password)
 
 
 monitor_uri = URI.parse(redis_config[rails_env]['monitor'])
-$redis_dialer_connection = Redis.new(:host => monitor_uri.host, :port => monitor_uri.port)
+$redis_dialer_connection = Redis.new(:host => monitor_uri.host, :port => monitor_uri.port, :password => monitor_uri.password)
 
 on_hold_uri = URI.parse(redis_config[rails_env]['on_hold_callers'])
-$redis_on_hold_connection = Redis.new(:host => on_hold_uri.host, :port => on_hold_uri.port)
+$redis_on_hold_connection = Redis.new(:host => on_hold_uri.host, :port => on_hold_uri.port, :password => on_hold_uri.password)
 
 
 question_pr_uri = URI.parse(redis_config[rails_env]['question_pr'])
-$redis_question_pr_uri_connection = Redis.new(:host => question_pr_uri.host, :port => question_pr_uri.port)
+$redis_question_pr_uri_connection = Redis.new(:host => question_pr_uri.host, :port => question_pr_uri.port, :password => question_pr_uri.password)
 
 
 phones_only_uri = URI.parse(redis_config[rails_env]['phones_only_ans'])
-$redis_phones_ans_uri_connection = Redis.new(:host => phones_only_uri.host, :port => phones_only_uri.port)
+$redis_phones_ans_uri_connection = Redis.new(:host => phones_only_uri.host, :port => phones_only_uri.port, :password => phones_only_uri.password)
 
 
 
