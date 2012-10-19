9d63836bb655dbef46270b0fc1563e9b252ea0ee
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-19 14:16:29 +0530

    checking for column while delete it

--- db/migrate/20121017091544_delete_unecessary_columns_calls.rb
+++ db/migrate/20121017091544_delete_unecessary_columns_calls.rb
@@ -1,33 +1,33 @@
 class DeleteUnecessaryColumnsCalls < ActiveRecord::Migration
   def up
-    remove_column :calls, :conference_name unless column_exists? :conference_name
-    remove_column :calls, :account_sid unless column_exists? :account_sid
-    remove_column :calls, :conference_history unless column_exists? :conference_history
-    remove_column :calls, :to_zip unless column_exists? :to_zip
-    remove_column :calls, :from_state unless column_exists? :from_state
-    remove_column :calls, :called unless column_exists? :called
-    remove_column :calls, :from_country unless column_exists? :from_city
-    remove_column :calls, :caller_country unless column_exists? :caller_country
-    remove_column :calls, :called_zip unless column_exists? :called_zip
-    remove_column :calls, :direction unless column_exists? :direction
-    remove_column :calls, :from_city unless column_exists? :from_city
-    remove_column :calls, :called_country unless column_exists? :called_country     
-    remove_column :calls, :caller_state unless column_exists? :caller_state
-    remove_column :calls, :called_state unless column_exists? :called_state
-    remove_column :calls, :from unless column_exists? :from
-    remove_column :calls, :caller_zip unless column_exists? :caller_zip    
-    remove_column :calls, :from_zip    unless column_exists? :from_zip
-    remove_column :calls, :application_sid unless column_exists? :application_sid
-    remove_column :calls, :to_city unless column_exists? :to_city
-    remove_column :calls, :to_state unless column_exists? :to_state
-    remove_column :calls, :to unless column_exists? :to
-    remove_column :calls, :to_country unless column_exists? :to_country
-    remove_column :calls, :caller_city unless column_exists? :caller_city
-    remove_column :calls, :api_version unless column_exists? :api_version
-    remove_column :calls, :caller unless column_exists? :caller
-    remove_column :calls, :called_city unless column_exists? :called_city
-    remove_column :calls, :waiting_at unless column_exists? :waiting_at
-    remove_column :calls, :ended_at unless column_exists? :ended_at
+    remove_column :calls, :conference_name unless column_exists? :calls, :conference_name
+    remove_column :calls, :account_sid unless column_exists? :calls, :account_sid
+    remove_column :calls, :conference_history unless column_exists? :calls, :conference_history
+    remove_column :calls, :to_zip unless column_exists? :calls, :to_zip
+    remove_column :calls, :from_state unless column_exists? :calls, :from_state
+    remove_column :calls, :called unless column_exists? :calls, :called
+    remove_column :calls, :from_country unless column_exists? :calls, :from_city
+    remove_column :calls, :caller_country unless column_exists? :calls, :caller_country
+    remove_column :calls, :called_zip unless column_exists? :calls, :called_zip
+    remove_column :calls, :direction unless column_exists? :calls, :direction
+    remove_column :calls, :from_city unless column_exists? :calls, :from_city
+    remove_column :calls, :called_country unless column_exists? :calls, :called_country     
+    remove_column :calls, :caller_state unless column_exists? :calls, :caller_state
+    remove_column :calls, :called_state unless column_exists? :calls, :called_state
+    remove_column :calls, :from unless column_exists? :calls, :from
+    remove_column :calls, :caller_zip unless column_exists? :calls, :caller_zip    
+    remove_column :calls, :from_zip    unless column_exists? :calls, :from_zip
+    remove_column :calls, :application_sid unless column_exists? :calls, :application_sid
+    remove_column :calls, :to_city unless column_exists? :calls, :to_city
+    remove_column :calls, :to_state unless column_exists? :calls, :to_state
+    remove_column :calls, :to unless column_exists? :calls, :to
+    remove_column :calls, :to_country unless column_exists? :calls, :to_country
+    remove_column :calls, :caller_city unless column_exists? :calls, :caller_city
+    remove_column :calls, :api_version unless column_exists? :calls, :api_version
+    remove_column :calls, :caller unless column_exists? :calls, :caller
+    remove_column :calls, :called_city unless column_exists? :calls, :called_city
+    remove_column :calls, :waiting_at unless column_exists? :calls, :waiting_at
+    remove_column :calls, :ended_at unless column_exists? :calls, :ended_at
   end
 
   def down
