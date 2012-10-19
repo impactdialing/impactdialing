86262686831ef8476527f56d24af364850ab01c1
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-19 16:07:22 +0530

    fixing simulator loop r14

--- simulator/simulator_loop.rb
+++ simulator/simulator_loop.rb
@@ -1,14 +1,7 @@
-require 'active_record'
-require "ostruct"
-require 'yaml'
-require 'logger'
-require 'fileutils'
 require 'octopus'
-
 RAILS_ROOT = File.expand_path('../..', __FILE__)
 require File.join(RAILS_ROOT, 'config/environment')
-SIMULATOR_ROOT = ENV['SIMULATOR_ROOT'] || File.expand_path('..', __FILE__)
-ActiveRecord::Base.logger = Logger.new(File.open(File.join(SIMULATOR_ROOT, 'log', "simulator_#{ENV['RAILS_ENV']}.log"), 'a'))
+
 
 loop do
   begin
@@ -23,10 +16,10 @@ loop do
     sleep 30
   rescue Exception => e
     if e.class == SystemExit || e.class == Interrupt
-      ActiveRecord::Base.logger.info "============ EXITING  ============"
+      puts "============ EXITING  ============"
       exit
     end
-    ActiveRecord::Base.logger.info "Rescued - #{ e } (#{ e.class })!"
-    ActiveRecord::Base.logger.info e.backtrace
+    puts "Rescued - #{ e } (#{ e.class })!"
+    puts e.backtrace
   end
 end
