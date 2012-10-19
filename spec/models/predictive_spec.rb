6aedc161996fee8835703a57dac47da4c5b4ed93
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-19 12:29:57 +0530

    call_end optimization

--- spec/models/predictive_spec.rb
+++ spec/models/predictive_spec.rb
@@ -133,13 +133,13 @@ describe Predictive do
     it "should return best dials  if  best_dials simulated_values is has a value" do
       simulated_values = SimulatedValues.create(best_dials: 1.8, best_conversation: 0, longest_conversation: 0)
       campaign = Factory(:predictive, :simulated_values => simulated_values)
-      campaign.best_dials_simulated.should eq(1.8)
+      campaign.best_dials_simulated.should eq(2)
     end
     
    it "should return best dials  as 5 if  best_dials simulated_values is greater than 3" do
       simulated_values = SimulatedValues.create(best_dials: 10.0, best_conversation: 0, longest_conversation: 0)
       campaign = Factory(:predictive, :simulated_values => simulated_values)
-      campaign.best_dials_simulated.should eq(1.8)
+      campaign.best_dials_simulated.should eq(4)
     end
 
 
