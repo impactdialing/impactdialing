77ece88c4f33e9dd263a35a5004b9959d97dff5f
gregolsen <anotheroneman@yahoo.com>
2012-10-22 17:11:22 +0300

    logs added to download report restart

--- jobs/report_download_job.rb
+++ jobs/report_download_job.rb
@@ -12,6 +12,7 @@ class ReportDownloadJob
         report_job = NewReportJob.new(campaign_id, user_id, voter_fields, custom_fields, all_voters, lead_dial, from, to, callback_url, strategy)
         report_job.perform
       rescue Resque::TermException
+        puts "Restarting Downlod report job campaign_id: #{campaign_id}, user_id: #{user_id}, from: #{from}, to: #{to}, strategy: #{strategy}"
         Resque.enqueue(self, campaign_id, user_id, voter_fields, custom_fields, all_voters, lead_dial, from, to, callback_url, strategy)
       end
     end
