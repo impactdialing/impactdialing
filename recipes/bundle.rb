351dc5e088f957b9475d326e0516b67af82b524c
Maxim Bondaruk <maxim.bondaruk@railsware.com>
2012-10-26 13:10:54 +0300

    [#38291475] switch call_end_callback, incoming_callback to aws

--- recipes/bundle.rb
+++ recipes/bundle.rb
@@ -1,6 +1,3 @@
-
-puts "sfSDF"
-
 namespace :bundle do
   task :install, :except => { :no_release => true } do
     #run "bundle install --gemfile #{current_path}/Gemfile --path #{shared_path}/bundle #{fetch(:bundle_flags,'')}"
