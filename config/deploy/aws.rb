50eac309e04d4cca88d52e65e99d16dcb2e47506
Maxim Bondaruk <maxim.bondaruk@railsware.com>
2012-10-26 18:32:44 +0300

    [#38291475] Added instance to aws

--- config/deploy/aws.rb
+++ config/deploy/aws.rb
@@ -4,3 +4,4 @@ ssh_options[:keys] = ["config/deploy/ssh-keys/app-aws.pem"]
 
 server "ec2-107-22-30-120.compute-1.amazonaws.com", :app, :web, :db, :primary => true
 server "ec2-184-73-125-26.compute-1.amazonaws.com", :app, :web 
+server 'ec2-50-19-174-132.compute-1.amazonaws.com', :app, :web 
