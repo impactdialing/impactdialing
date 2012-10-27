df13c1ceee9c55c79cff670f9ab50644d77407e4
Stanislav Doroshok <stanislav.doroshok@railsware.com>
2012-10-27 16:16:13 +0300

    Added new servers to aws

--- config/deploy/aws.rb
+++ config/deploy/aws.rb
@@ -1,11 +1,12 @@
 ssh_options[:user] = "impactdialing"
 ssh_options[:keys] = ["config/deploy/ssh-keys/app-aws.pem"]
 
-#server "ec2-107-22-30-120.compute-1.amazonaws.com", :app, :web, :db, :primary => true
-#server "ec2-184-73-125-26.compute-1.amazonaws.com", :app, :web 
-server 'ec2-50-19-174-132.compute-1.amazonaws.com', :app, :web 
-server 'ec2-54-242-82-13.compute-1.amazonaws.com', :app, :web 
-server 'ec2-184-73-108-160.compute-1.amazonaws.com', :app, :web 
-server 'ec2-23-21-18-102.compute-1.amazonaws.com', :app, :web 
-
-
+server 'ec2-50-19-174-132.compute-1.amazonaws.com', :app, :web
+server 'ec2-54-242-82-13.compute-1.amazonaws.com', :app, :web
+server 'ec2-184-73-108-160.compute-1.amazonaws.com', :app, :web
+server 'ec2-23-21-18-102.compute-1.amazonaws.com', :app, :web
+server 'ec2-174-129-154-129.compute-1.amazonaws.com', :app, :web
+server 'ec2-50-17-179-238.compute-1.amazonaws.com' :app, :web
+server 'ec2-50-16-137-110.compute-1.amazonaws.com' :app, :web
+server 'ec2-23-22-168-37.compute-1.amazonaws.com' :app, :web
+server 'ec2-75-101-190-60.compute-1.amazonaws.com' :app, :web
