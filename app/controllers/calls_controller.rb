d911b677e60031199413929c171b6cb5e45342f8
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-29 19:59:24 +0530

    sending dc codes while making calls

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,14 +1,11 @@
 class CallsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter :parse_params, :except => [:datacentre]
+  before_filter :parse_params
   before_filter :find_and_update_call, :only => [:destroy, :incoming, :call_ended, :disconnected]
   before_filter :find_and_update_answers_and_notes_and_scheduled_date, :only => [:submit_result, :submit_result_and_stop]
   before_filter :find_call, :only => [:hangup, :call_ended]
 
     
-  def datacentre
-    render :json => {:data_centres => RedisPredictiveCampaign.data_centres(params[:campaign_id])}
-  end
     
   def incoming
     if Campaign.predictive_campaign?(params['campaign_type']) && @call.answered_by_human? 
