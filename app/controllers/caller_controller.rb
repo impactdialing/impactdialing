2deb5079bb131b4ef46266edee1f3edd34a4b1e5
Nikhil Fernandes <nikhil@activesphere.com>
2012-10-30 04:06:06 +0530

    wrong param name caller_dc

--- app/controllers/caller_controller.rb
+++ app/controllers/caller_controller.rb
@@ -26,7 +26,7 @@ class CallerController < ApplicationController
   end
   
   def ready_to_call
-    render xml: @caller_session.ready_to_call(DataCentre.code(params[:callerdc]))
+    render xml: @caller_session.ready_to_call(DataCentre.code(params[:caller_dc]))
   end
   
   def continue_conf
