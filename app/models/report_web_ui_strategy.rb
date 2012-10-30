61dac1e4ff2f0745e0848cad4c86cef8da41c7b7
Andrii Krykunenko <andriy.krykunenko@gmail.com>
2012-10-30 17:44:02 +0200

    web ui report save fix

--- app/models/report_web_ui_strategy.rb
+++ app/models/report_web_ui_strategy.rb
@@ -13,7 +13,7 @@ class ReportWebUIStrategy
     if @result == "success"
       expires_in_24_hours = (Time.now + 24.hours).to_i
       link = AWS::S3::S3Object.url_for("#{params[:campaign_name]}.csv", "download_reports", :expires => expires_in_24_hours)
-      DownloadedReport.create(link: link, user: @user, campaign_id: @campaign.id)
+      DownloadedReport.using(:master).create(link: link, user: @user, campaign_id: @campaign.id)
       @mailer.deliver_download(@user, link)
     else
       @mailer.deliver_download_failure(@user, @campaign, @campaign.account_id, @exception) unless Rails.env.development?
