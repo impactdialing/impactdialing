40257f4407cce27e4f674a52a07d45dc44e7312c
Nikhil Fernandes <nikhil@activesphere.com>
2012-11-01 02:47:50 +0530

    error in datacentre redis

--- app/models/redis/redis_data_centre.rb
+++ app/models/redis/redis_data_centre.rb
@@ -1,16 +1,16 @@
 class RedisDataCentre
   
   def self.set_datacentres_used(campaign_id, datacentre)
-    $redis_dialer_connection.lpush 'data_centre:#{campaign_id}', datacentre
+    $redis_dialer_connection.lpush "data_centre:#{campaign_id}", datacentre
   end
   
   def self.data_centres(campaign_id)
-    dcs = $redis_dialer_connection.lrange 'data_centre:#{campaign_id}', 0, -1    
+    dcs = $redis_dialer_connection.lrange "data_centre:#{campaign_id}", 0, -1    
     dcs.uniq.join(",")
   end
   
   def self.remove_data_centre(campaign_id, data_centre)
-    $redis_dialer_connection.lrem 'data_centre:#{campaign_id}', 1, data_centre
+    $redis_dialer_connection.lrem "data_centre:#{campaign_id}", 1, data_centre
   end
   
   def self.add_to_voxeo_dc_campaigns(campaign_id)
