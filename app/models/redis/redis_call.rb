74d266f6437fb3054974059a6d73f3585df194e2
gregolsen <anotheroneman@yahoo.com>
2012-11-01 20:01:11 +0200

    answered job fixed

--- app/models/redis/redis_call.rb
+++ app/models/redis/redis_call.rb
@@ -2,38 +2,47 @@ class RedisCall
   
   def self.set_request_params(call_id, options)
     id = "call_flow:#{call_id}"
-    data = $redis_call_uri_connection.get(id) || '{}'
+    data = call_data(call_id) || '{}'
     hash = JSON.parse(data)
     $redis_call_uri_connection.set id, hash.merge(options).to_json
   end
   
   def self.answered_by(call_id)
-    data = $redis_call_uri_connection.get "call_flow:#{call_id}"
+    data = call_data(call_id)
     hash = JSON.parse(data)
     hash["answered_by"]    
   end
   
   def self.call_status(call_id)
-    data = $redis_call_uri_connection.get "call_flow:#{call_id}"
+    data = call_data(call_id)
     hash = JSON.parse(data)
     hash["call_status"]    
   end
   
   def self.questions(call_id)
-    data = $redis_call_uri_connection.get "call_flow:#{call_id}"
+    data = call_data(call_id)
     hash = JSON.parse(data)
     hash["questions"]        
   end
 
   def self.notes(call_id)
-    data = $redis_call_uri_connection.get "call_flow:#{call_id}"
+    data = call_data(call_id)
     hash = JSON.parse(data)
     hash["notes"]        
+  end
+
+  def self.questions_and_notes(call_id)
+    data = call_data(call_id)
+    return nil unless data
+    JSON.parse(data)
   end
   
   def self.delete(call_id)
     $redis_call_uri_connection.del "call_flow:#{call_id}"
   end
-  
+
+  def self.call_data(call_id)
+    $redis_call_uri_connection.get "call_flow:#{call_id}"
+  end
   
 end
