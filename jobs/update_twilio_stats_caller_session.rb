4865ee8e0ad2c34c8f195ba60cb6a8703ea3ac7b
Nikhil Fernandes <nikhil@activesphere.com>
2012-11-02 16:43:26 +0530

    callersession update stats

--- jobs/update_twilio_stats_caller_session.rb
+++ jobs/update_twilio_stats_caller_session.rb
@@ -8,7 +8,14 @@ class UpdateTwilioStatsCallerSession
     twillio_lib = TwilioLib.new
     
     WebuiCallerSession.where("endtime is not NULL and tPrice is NULL and (tStatus is NULL or tStatus = 'completed') ").limit(100).each do |session|
-        caller_sessions << twillio_lib.update_twilio_stats_by_model(session)
+        if !session.sid.nil? && !session.sid.starts_with?("CA")
+          session.tEndTime = session.endtime
+          session.tStartTime = session.starttime
+          session.tPrice = 0.0
+          caller_sessions << session
+        else
+          caller_sessions << twillio_lib.update_twilio_stats_by_model(session)
+        end              
     end
     WebuiCallerSession.import caller_sessions, :on_duplicate_key_update=>[:tCallSegmentSid, :tAccountSid,
                                         :tCalled, :tCaller, :tPhoneNumberSid, :tStatus, :tStartTime, :tEndTime, :tDuration, :tPrice, :tFlags]    
@@ -18,7 +25,7 @@ class UpdateTwilioStatsCallerSession
     twillio_lib = TwilioLib.new
 
     PhonesOnlyCallerSession.where("endtime is not NULL and tPrice is NULL and (tStatus is NULL or tStatus = 'completed') ").limit(100).each do |session|
-      if !session.sid.nil? && session.sid.starts_with?("VX")
+      if !session.sid.nil? && !session.sid.starts_with?("CA")
         session.tEndTime = session.endtime
         session.tStartTime = session.starttime
         session.tPrice = 0.0
