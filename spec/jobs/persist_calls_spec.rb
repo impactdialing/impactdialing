55655044b9546a2111622f3f0082108846b360fb
gregolsen <anotheroneman@yahoo.com>
2012-11-02 23:35:41 +0200

    PersistCalls job fixed

--- spec/jobs/persist_calls_spec.rb
+++ spec/jobs/persist_calls_spec.rb
@@ -12,6 +12,7 @@ describe PersistCalls do
   context ".abandoned_calls" do
     before(:each) do
       $redis_call_flow_connection.lpush "abandoned_call_list", {id: call.id, current_time: time}.to_json
+      $redis_call_flow_connection.lpush "abandoned_call_list", {id: 123, current_time: Time.now - 1.day}.to_json
       PersistCalls.perform
     end
 
@@ -56,8 +57,12 @@ describe PersistCalls do
   end
   
   context ".disconnected" do
+    let!(:new_call_attempt) { Factory(:call_attempt, voter: voter, campaign: campaign) }
+    let!(:new_call) { Factory(:call, call_attempt: new_call_attempt) }
     before(:each) do
       $redis_call_flow_connection.lpush "disconnected_call_list" , {id: call.id, recording_duration: 15, recording_url: "url", caller_id: 1, current_time: time}.to_json
+      $redis_call_flow_connection.lpush "disconnected_call_list" , {id: call.id, recording_duration: 15, recording_url: "url", caller_id: 1, current_time: time}.to_json
+      $redis_call_flow_connection.lpush "disconnected_call_list" , {id: new_call.id, recording_duration: 15, recording_url: "url", caller_id: 3, current_time: time}.to_json
       PersistCalls.perform
     end
 
@@ -67,6 +72,14 @@ describe PersistCalls do
       its(:recording_url) { should == "url" }
       its(:recording_duration) { should == 15 }
       its(:caller_id) { should == 1 }
+    end
+
+    context "new_call_attempt" do
+      subject { new_call_attempt.reload }
+      its(:status) { should == CallAttempt::Status::SUCCESS }
+      its(:recording_url) { should == "url" }
+      its(:recording_duration) { should == 15 }
+      its(:caller_id) { should == 3 }
     end
 
     context "voter" do
