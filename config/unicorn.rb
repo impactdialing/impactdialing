fd4c4d4c664eae73c04fa8cb51f703d5e79a7a15
gregolsen <anotheroneman@yahoo.com>
2012-11-04 01:43:41 +0200

    fixing bad sybmol

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,9 +8,7 @@ worker_processes (ENV['UNICORN_WORKERS'] ? ENV['UNICORN_WORKERS'].to_i : 3)
 timeout (ENV['UNICORN_TIMEOUT'] ? ENV['UNICORN_TIMEOUT'].to_i : 30)
 preload_app true
 
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = File.join(ENV['PWD'], 'Gemfile')
-end
+before_exec { |server| ENV["BUNDLE_GEMFILE"] = File.join(ENV['PWD'], 'Gemfile') }
 
 before_fork do |server, worker|
   if defined?(ActiveRecord::Base)
