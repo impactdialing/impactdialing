e474c1ff1506c7a9a7b56527b37004118c8a34f0
gregolsen <anotheroneman@yahoo.com>
2012-11-03 23:11:38 +0200

    updating voters based on id

--- jobs/end_caller_session_job.rb
+++ jobs/end_caller_session_job.rb
@@ -5,7 +5,8 @@ class EndCallerSessionJob
    def perform(caller_session_id)
      caller_session = CallerSession.find(caller_session_id)
      caller_id = caller_session.caller_id
-     Voter.where(caller_id: caller_id, status: CallAttempt::Status::READY).update_all(status: 'not called')
+     voter_ids = Voter.where(caller_id: caller_id, status: CallAttempt::Status::READY).pluck(:id)
+     Voter.where(id: voter_ids).update_all(status: 'not called')
      CallAttempt.wrapup_calls(caller_id)
    end
 end
