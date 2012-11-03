ceb3f498e6b91e702f87035f1608d161760615fe
Nikhil Fernandes <nikhil@activesphere.com>
2012-11-03 23:37:30 +0530

    only processing CA callersession for twilio

--- jobs/phantom_caller_job.rb
+++ jobs/phantom_caller_job.rb
@@ -6,10 +6,12 @@ class PhantomCallerJob
   
    def self.perform
      t = TwilioLib.new(TWILIO_ACCOUNT,TWILIO_AUTH)
-     CallerSession.on_call.where("updated_at < ? ", 5.minutes.ago).each do |cs|       
+     CallerSession.on_call.where("updated_at < ? ", 5.minutes.ago).each do |cs|              
        begin
-         call_response = Hash.from_xml(t.call("GET", "Calls/" + cs.sid, {}))['TwilioResponse']['Call']
-         cs.end_running_call if call_response.try(:[],"Status") == 'completed'
+         if cs.sid.starts_with?("CA")
+           call_response = Hash.from_xml(t.call("GET", "Calls/" + cs.sid, {}))['TwilioResponse']['Call']
+           cs.end_running_call if call_response.try(:[],"Status") == 'completed'
+         end
         rescue
           puts cs.id
         end
