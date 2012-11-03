9b3d06cfef5af2e64becff56ff82555f8c9667c7
gregolsen <anotheroneman@yahoo.com>
2012-11-03 23:29:33 +0200

    fixing job to use voters on campaign index

--- jobs/end_caller_session_job.rb
+++ jobs/end_caller_session_job.rb
@@ -5,8 +5,7 @@ class EndCallerSessionJob
    def perform(caller_session_id)
      caller_session = CallerSession.find(caller_session_id)
      caller_id = caller_session.caller_id
-     voter_ids = Voter.where(caller_id: caller_id, status: CallAttempt::Status::READY).pluck(:id)
-     Voter.where(id: voter_ids).update_all(status: 'not called')
+     Voter.where(campaign_id: caller_session.caller.campaign_id, caller_id: caller_id, status: CallAttempt::Status::READY).update_all(status: 'not called')
      CallAttempt.wrapup_calls(caller_id)
    end
 end
