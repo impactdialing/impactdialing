a99679819afe7e1efe9321bc5d246e2d5c2ca14f
Nikhil Fernandes <nikhil@activesphere.com>
2012-11-05 17:20:24 +0530

    deleting large xml files from voxeo.

--- jobs/parse_voxeo_data.rb
+++ jobs/parse_voxeo_data.rb
@@ -18,17 +18,5 @@ class ParseVoxeoData
         end
       end
     end
-    # contents = file.read
-    # sessions_array =  Hash.from_xml(contents)["sessions"]["session"]
-    # sessions_array.each do |session|
-    #   if session['applicationName'] == 'inboundConference'        
-    #     start_time = session['startDate'] + " " + session['startTime']
-    #     end_time = Time.parse(start_time) + session['durationMinutes'].to_f
-    #     puts " #{session['direction']} - #{session['sessionId']} - #{start_time}  - #{end_time}  - #{session['durationMinutes']} "         
-    #     caller_session = CallerSession.find_by_sid(session['sessionId'])
-    #     caller_session.update_attributes(starttime: start_time, tStartTime: start_time, end_time: end_time, tEndTime: end_time)
-    #   end
-    # end
-   # 2012-11-02 - 17:58:32 - 32.2 
   end
 end
