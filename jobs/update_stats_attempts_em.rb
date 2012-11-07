7c3ac198f48f643f36fdc7ce0f4b18c5bf8b0902
Nikhil Fernandes <nikhil@activesphere.com>
2012-11-07 18:45:22 +0530

    running job with larger data

--- jobs/update_stats_attempts_em.rb
+++ jobs/update_stats_attempts_em.rb
@@ -11,7 +11,7 @@ class UpdateStatsAttemptsEm
   def self.perform
     results = []
     twillio_lib = TwilioLib.new    
-    call_attempts = CallAttempt.where("status in (?) and tPrice is NULL and (tStatus is NULL or tStatus = 'completed') and sid is not null ", ['Message delivered', 'Call completed with success.', 'Call abandoned', 'Hangup or answering machine']).limit(1000)
+    call_attempts = CallAttempt.where("status in (?) and tPrice is NULL and (tStatus is NULL or tStatus = 'completed') and sid is not null ", ['Message delivered', 'Call completed with success.', 'Call abandoned', 'Hangup or answering machine']).limit(10000)
     
     voxeo_call_attempts = call_attempts.select {|call_attempt| call_attempt.sid.starts_with?("VX")}
     voxeo_call_attempts.each do |attempt|
