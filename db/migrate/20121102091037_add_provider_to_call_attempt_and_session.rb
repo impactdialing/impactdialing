6b8af932c66484604c5a08e46e18b22432871424
Nikhil Fernandes <nikhil@activesphere.com>
2012-11-29 18:03:50 +0530

    remove migration

--- db/migrate/20121102091037_add_provider_to_call_attempt_and_session.rb
+++
@@ -1,6 +0,0 @@
-class AddProviderToCallAttemptAndSession < ActiveRecord::Migration
-  def change
-    add_column :call_attempts, :service_provider, :string
-    add_column :caller_sessions, :service_provider, :string
-  end
-end
