a91e70cc65379d9c5548c71b1447a1177e7dac0c
Nikhil Fernandes <nikhil@activesphere.com>
2013-02-15 14:55:09 +0530

    campaign 2756 issue

---
+++ spec/integration/callin_preview_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+describe "CallinPreview" do  
+  describe "Callin Preview Campaign" do
+    
+    before(:each) do
+      @account = Factory(:account)
+      @campaign = Factory(:preview, account: @account)
+      @caller = Factory(:caller, campaign: @campaign, account: @account, email: "test@impactdialing.com", password: "demo123")      
+    end
+    
+    
+    it "should see the start calling button" do
+      visit "/caller/login"
+      page.should have_content('Log in')
+      fill_in "email", with: @caller.email
+      fill_in "password", with: @caller.password
+      click_link_or_button 'Log in'
+      page.should have_content('Start calling')
+    end
+    
+    it "should start calling and s" do
+      visit "/caller/login"
+      page.should have_content('Log in')
+      fill_in "email", with: caller.email
+      fill_in "password", with: caller.password
+      click_link_or_button 'Log in'
+      page.should have_content('Start calling')
+    end
+    
+  end
+end
