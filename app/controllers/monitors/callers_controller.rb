c7c129f5acad44d34cbe691e0f4263fca47b8970
Nikhil Fernandes <nikhil@activesphere.com>
2013-03-07 16:10:29 +0530

    skip verify authencity token

--- app/controllers/monitors/callers_controller.rb
+++ app/controllers/monitors/callers_controller.rb
@@ -1,7 +1,7 @@
 module Monitors
   class CallersController < ClientController
     skip_before_filter :check_login, :only => [:kick_off]
-    skip_before_filter :verify_authenticity_token
+    skip_before_filter :verify_authenticity_token, :only => [:start]
     respond_to :json
 
     def index
