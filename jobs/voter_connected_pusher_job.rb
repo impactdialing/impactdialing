bbe4620095f6cd4cd7bdbc64eb3b538b10d9f620
Nikhil Fernandes <nikhil@activesphere.com>
2013-03-13 14:19:55 +0530

    track failures for sidekiq jobs

--- jobs/voter_connected_pusher_job.rb
+++ jobs/voter_connected_pusher_job.rb
@@ -1,10 +1,11 @@
-class VoterConnectedPusherJob 
+class VoterConnectedPusherJob
   include Sidekiq::Worker
   sidekiq_options :retry => false
-  
-  def perform(caller_session_id, call_id)    
+  sidekiq_options :failures => true
+
+  def perform(caller_session_id, call_id)
     caller_session = CallerSession.find(caller_session_id)
     caller_session.send('publish_voter_connected', call_id)
   end
-  
+
 end
