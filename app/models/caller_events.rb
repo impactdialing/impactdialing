e56501ac3bdfca3bef17f764b5b72f6980b1b238
Nikhil Fernandes <nikhil@activesphere.com>
2013-03-26 10:15:13 +0530

    preview power dial error if voter info does not reach caller screen

--- app/models/caller_events.rb
+++ app/models/caller_events.rb
@@ -1,49 +1,49 @@
 module CallerEvents
-  
+
   module ClassMethods
   end
-  
+
   module InstanceMethods
-            
+
     def publish_start_calling
         publish_sync('start_calling', {caller_session_id: id})
-    end    
-    
-    def publish_voter_connected(call_id)            
+    end
+
+    def publish_voter_connected(call_id)
       call = Call.find(call_id)
       unless caller.is_phones_only?
         event_hash = campaign.voter_connected_event(call)
         Pusher[session_key].trigger!(event_hash[:event], event_hash[:data].merge!(:dialer => campaign.type))
       end
-    end    
-    
+    end
+
     def publish_voter_disconnected
-      unless caller.is_phones_only?      
+      unless caller.is_phones_only?
         Pusher[session_key].trigger!("voter_disconnected", {})
-      end  
+      end
     end
-        
+
     def publish_caller_conference_started
-      unless caller.is_phones_only? 
-        event_hash = campaign.caller_conference_started_event(voter_in_progress.try(:id))     
+      unless caller.is_phones_only?
+        event_hash = campaign.caller_conference_started_event(voter_in_progress.try(:id))
         Pusher[session_key].trigger!(event_hash[:event], event_hash[:data].merge!(:dialer => campaign.type))
-     end      
+     end
     end
-    
+
     def publish_calling_voter
-      Pusher[session_key].trigger!('calling_voter', {}) unless caller.is_phones_only? 
+      Pusher[session_key].trigger!('calling_voter', {}) unless caller.is_phones_only?
     end
-        
-    def publish_caller_disconnected      
+
+    def publish_caller_disconnected
       Pusher[session_key].trigger!("caller_disconnected", {}) unless caller.is_phones_only?
-    end   
-    
-    
+    end
+
+
   end
-  
+
   def self.included(receiver)
     receiver.extend         ClassMethods
     receiver.send :include, InstanceMethods
   end
-  
+
 end
