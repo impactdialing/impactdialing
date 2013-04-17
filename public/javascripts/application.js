66176558c2646a74cd7380e2a7105601ddc3a208
Nikhil Fernandes <nikhil@activesphere.com>
2013-04-17 12:54:57 +0530

    js errors

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
 
 $(document).ajaxSend(function(e, xhr, options) {
   var token = $("meta[name='csrf-token']").attr("content");
@@ -10,3 +13,4 @@ function delete_entity(form_id){
 	if (confirm('Are you sure?'))
 		$(form_id).submit();
 }
+
