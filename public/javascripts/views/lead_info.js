06b52439ed66a133c05866c6e7c2a1dd606a668f
Nikhil Fernandes <nikhil@activesphere.com>
2013-04-18 12:55:13 +0530

    displaying custom voter fields

--- public/javascripts/views/lead_info.js
+++ public/javascripts/views/lead_info.js
@@ -5,8 +5,12 @@ ImpactDialing.Views.LeadInfo = Backbone.View.extend({
   },
 
   render: function () {
+    this.model.handleCustomFields();
+    console.log(this.model.toJSON())
     $(this.el).html(Mustache.to_html($('#caller-campaign-script-lead-info-template').html(), this.model.toJSON()));
     return this;
   },
+
+
 
 });
