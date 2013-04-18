644522c2c9633f178caf6e02278bf3a8687accab
Nikhil Fernandes <nikhil@activesphere.com>
2013-04-18 16:16:54 +0530

    js error after caller disconnects

--- public/javascripts/models/lead_info.js
+++ public/javascripts/models/lead_info.js
@@ -3,14 +3,15 @@ ImpactDialing.Models.LeadInfo = Backbone.Model.extend({
   handleCustomFields: function(){
      var custom_fields_array = [];
      var self = this;
-     console.log(this.get("custom_fields"))
-    _.each(_.keys(this.get("custom_fields")) , function(ele){
-      custom_fields_array.push({
-      'key' : ele,
-      'value' : self.get("custom_fields")[ele]
-     });
-    })
-    this.set("custom_lead_info", custom_fields_array)
+     if(_.isObject(this.get("custom_fields"))){
+      _.each(_.keys(this.get("custom_fields")) , function(ele){
+        custom_fields_array.push({
+        'key' : ele,
+        'value' : self.get("custom_fields")[ele]
+       });
+      });
+      this.set("custom_lead_info", custom_fields_array)
+    }
   },
 
 });
