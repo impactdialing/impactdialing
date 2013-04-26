f4a424c936ec649ff61ade079826d27f357eff1b
Nikhil Fernandes <nikhil@activesphere.com>
2013-04-26 09:39:30 +0530

    moving establish connection after writing to file

--- app/models/caller_campaign_report_strategy.rb
+++ app/models/caller_campaign_report_strategy.rb
@@ -142,7 +142,7 @@ class CallerCampaignReportStrategy < CampaignReportStrategy
     Octopus.using(OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do
       first_voter = Voter.by_campaign(@campaign).order('id').first
       @possible_responses = get_possible_responses
-      Voter.by_campaign(@campaign).order('last_call_attempt_time').find_in_hashes(:batch_size => 1000, start: start_position(first_voter), shard: OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do |voters|
+      Voter.by_campaign(@campaign).order('last_call_attempt_time').find_in_hashes(:batch_size => 5000, start: start_position(first_voter), shard: OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do |voters|
         process_voters(voters)
       end
     end
@@ -152,7 +152,7 @@ class CallerCampaignReportStrategy < CampaignReportStrategy
     Octopus.using(OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do
       first_attempt = CallAttempt.for_campaign(@campaign).order('id').first
       @possible_responses = get_possible_responses
-      CallAttempt.from('call_attempts use index (index_call_attempts_on_campaign_created_id)').for_campaign(@campaign).order('created_at').includes(:answers, :note_responses).find_in_hashes(:batch_size => 1000, start: start_position(first_attempt), shard: OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do |attempts|
+      CallAttempt.from('call_attempts use index (index_call_attempts_on_campaign_created_id)').for_campaign(@campaign).order('created_at').includes(:answers, :note_responses).find_in_hashes(:batch_size => 5000, start: start_position(first_attempt), shard: OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do |attempts|
         process_attempts(attempts)
       end
     end
@@ -162,7 +162,7 @@ class CallerCampaignReportStrategy < CampaignReportStrategy
     Octopus.using(OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do
       first_voter = Voter.by_campaign(@campaign).last_call_attempt_within(@from_date, @to_date).order('id').first
       @possible_responses = get_possible_responses
-      Voter.by_campaign(@campaign).last_call_attempt_within(@from_date, @to_date).order('created_at').find_in_hashes(:batch_size => 1000, start: start_position(first_voter), shard: OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do |voters|
+      Voter.by_campaign(@campaign).last_call_attempt_within(@from_date, @to_date).order('created_at').find_in_hashes(:batch_size => 5000, start: start_position(first_voter), shard: OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do |voters|
         process_voters(voters)
       end
     end
@@ -172,7 +172,7 @@ class CallerCampaignReportStrategy < CampaignReportStrategy
     Octopus.using(OctopusConnection.dynamic_shard(:read_slave1, :read_slave2)) do
       first_attempt = CallAttempt.for_campaign(@campaign).between(@from_date, @to_date).order('id').first
       @possible_responses = get_possible_responses
-      CallAttempt.from('call_attempts use index (index_call_attempts_on_campaign_created_id)').for_campaign(@campaign).between(@from_date, @to_date).order('created_at').includes(:answers, :note_responses).find_in_batches(:batch_size => 1000, start: start_position(first_attempt)) do |attempts|
+      CallAttempt.from('call_attempts use index (index_call_attempts_on_campaign_created_id)').for_campaign(@campaign).between(@from_date, @to_date).order('created_at').includes(:answers, :note_responses).find_in_batches(:batch_size => 5000, start: start_position(first_attempt)) do |attempts|
         process_attempts(attempts)
       end
     end
