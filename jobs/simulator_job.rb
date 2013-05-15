d34930fe9a2600616c14b29d28c0d0d8e6ce4875
Nikhil Fernandes <nikhil@activesphere.com>
2013-05-15 16:25:50 +0530

    moving debitter job to background worker

--- jobs/simulator_job.rb
+++ jobs/simulator_job.rb
@@ -1,19 +1,19 @@
 require 'octopus'
 require 'resque-loner'
 
-class SimulatorJob 
+class SimulatorJob
   include Resque::Plugins::UniqueJob
   @queue = :simulator_worker
-  
+
 
    def self.perform(campaign_id)
-    begin 
+    begin
      target_abandonment = Campaign.using(:simulator_slave).find(campaign_id).acceptable_abandon_rate
      start_time = 60 * 60
      simulator_length = 60 * 60
      abandon_count = 0
      dials_needed = 1
-     best_dials = 1  
+     best_dials = 1
      increment = 10.0
      outer_loop = 0
      inner_loop = 0
@@ -28,17 +28,17 @@ class SimulatorJob
 
        if outer_loop == 2
          expected_wrapup_time = 0
-       end        
+       end
 
        while inner_loop < increment
-         idle_time = active_time = 0.0 
+         idle_time = active_time = 0.0
          t = 0
          active_dials =  []
          finished_dials = []
          active_conversations = []
          finished_conversations = []
 
-         while(t <= simulator_length)  
+         while(t <= simulator_length)
 
            active_conversations.clone.each do |call_attempt|
              if call_attempt.counter == call_attempt.length
@@ -71,15 +71,15 @@ class SimulatorJob
            end
 
            active_wrapups = []
-           active_conversations.each do |active_conversation| 
+           active_conversations.each do |active_conversation|
               if active_conversation.counter > active_conversation.length
                 active_wrapups <<  (active_conversation.counter - active_conversation.length)
               end
-           end               
+           end
 
-           available_callers = caller_statuses.count(&:available?) + 
+           available_callers = caller_statuses.count(&:available?) +
                              active_conversations.count{|active_conversation| (active_conversation.counter > expected_conversation) && (active_conversation.counter < longest_conversation)} +
-                             active_wrapups.count{|active_wrapup| (active_wrapup > expected_wrapup_time)}          
+                             active_wrapups.count{|active_wrapup| (active_wrapup > expected_wrapup_time)}
 
            ringing_lines = active_dials.length
            dials_to_make = (( dials_needed * available_callers ) - ringing_lines).to_i
@@ -88,22 +88,22 @@ class SimulatorJob
            active_time += caller_statuses.select(&:unavailable?).size
            finished_dials.each{|dial| dial.counter += 1}
            finished_conversations.each{|call_attempt| call_attempt.counter += 1}
-           t += 1     
+           t += 1
 
         end
 
         finished_conversations_answered_count = finished_conversations.count(&:answered?)
         simulated_abandonment = abandon_count / (finished_conversations_answered_count == 0 ? 1 : finished_conversations_answered_count)
 
-        if simulated_abandonment <= target_abandonment     
-          utilization = active_time / ( active_time + idle_time )     
+        if simulated_abandonment <= target_abandonment
+          utilization = active_time / ( active_time + idle_time )
           if utilization > best_utilization
             best_utilization = utilization
             best_dials = dials_needed if outer_loop == 0
             best_conversation = expected_conversation if outer_loop == 1
             best_wrapup_time = expected_wrapup_time if outer_loop == 2
           end
-        end   
+        end
         answered_observed_dials = observed_dials.count(&:answered?)
         answer_ratio =  answered_observed_dials == 0 ? 1 : (observed_dials.size  / answered_observed_dials)
        if outer_loop == 0
@@ -119,7 +119,7 @@ class SimulatorJob
        end
 
        inner_loop += 1
-     end  
+     end
        dials_needed = best_dials
        expected_conversation = best_conversation
        expected_wrapup_time = best_wrapup_time
@@ -128,17 +128,17 @@ class SimulatorJob
     puts "Simulated_#{campaign_id} , Lines To Dial: #{best_dials}, Expected Call Time: #{best_conversation}, Expected Wrapup Time: #{best_wrapup_time} "
     SimulatedValues.find_or_create_by_campaign_id(campaign_id).update_attributes(best_dials: best_dials, best_conversation: best_conversation, longest_conversation: longest_conversation, best_wrapup_time: best_wrapup_time)
    rescue Exception => e
-   end  
-     
    end
-   
-   
-   
+
+   end
+
+
+
    def self.simulator_campaign_base_values(campaign_id, start_time)
        Octopus.using(:simulator_slave) do
          caller_statuses = CallerSession.where(:campaign_id => campaign_id,
-                   :on_call => true).size.times.map{ CallerStatus.new('available') }            
-               
+                   :on_call => true).size.times.map{ CallerStatus.new('available') }
+
          campaign = Campaign.find(campaign_id)
 
          call_attempts_from_start_time = campaign.call_attempts.between((Time.now - start_time.seconds), Time.now).limit(500)
@@ -164,10 +164,10 @@ class SimulatorJob
          expected_wrapup_time = longest_wrapup_time
          [expected_conversation, longest_conversation, best_conversation, mean_conversation, expected_wrapup_time, longest_wrapup_time, best_wrapup_time, caller_statuses, observed_conversations, observed_dials]
       end
-   end   
-   
-   
-   
+   end
+
+
+
 end
 
 
