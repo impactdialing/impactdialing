b5e70ac239ff0f003dea95eabf755e464cdf7d76
Nikhil Fernandes <nikhil@activesphere.com>
2013-07-15 14:42:06 +0530

    url for download report

--- app/models/report_web_ui_strategy.rb
+++ app/models/report_web_ui_strategy.rb
@@ -12,7 +12,7 @@ class ReportWebUIStrategy
   def response(params)
     if @result == "success"
       expires_in_24_hours = (Time.now + 24.hours).to_i
-      link = AmazonS3.new.object("download_reports", "#{params[:campaign_name]}.csv").url_for(:read, :expires => (Time.now + 24.hours).to_i)
+      link = AmazonS3.new.object("download_reports", "#{params[:campaign_name]}.csv").url_for(:read, :expires => (Time.now + 24.hours).to_i).to_s
       DownloadedReport.using(:master).create(link: link, user: @user, campaign_id: @campaign.id)
       @mailer.deliver_download(@user, link)
     else
