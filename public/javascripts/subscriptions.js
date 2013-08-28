1b8f9ade6be0a21a586a94a413c528b695d03978
Nikhil Fernandes <fernandes.nikhil@gmail.com>
2013-08-28 13:17:32 +0530

    subscription total cost

--- public/javascripts/subscriptions.js
+++ public/javascripts/subscriptions.js
@@ -60,14 +60,14 @@ Subscriptions.prototype.subscriptionTypeChangeEvent = function(){
 }
 
 Subscriptions.prototype.calculatedTotalMonthlyCost = function(){
-  $("#monthly-cost").hide();
+  $("#cost-subscription").hide();
   if ($("#subscription_type").val() == "PerMinute"){
     return;
   }
   var subscriptions = {"Basic": 49.00, "Pro": 99.00, "Business": 199.00}  
   var value = (subscriptions[$("#subscription_type").val()] * $("#number_of_callers").val())
   $("#monthly-cost").html(value);
-  $("#monthly-cost").show();
+  $("#cost-subscription").show();
 }
 
 Subscriptions.prototype.number_of_callers_reduced = function(){
