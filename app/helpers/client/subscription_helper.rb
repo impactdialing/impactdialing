9fc5ca06274b62d4e867e2f3f472c5e32b31a969
Jeremiah Heller <jeremiah@impactdialing.com>
2013-09-12 08:40:08 -0700

    hide upgrade btw & list 1 btw per line

--- app/helpers/client/subscription_helper.rb
+++ app/helpers/client/subscription_helper.rb
@@ -1,20 +1,89 @@
 module Client::SubscriptionHelper
   def subscription_type_options_for_select
-    Subscription::Type::PAID_SUBSCRIPTIONS.map do |type|
-      [type.underscore.humanize, type]
+    return Subscription::Type::PAID_SUBSCRIPTIONS.map do |type|
+      [subscription_human_type(type), type]
     end
   end
 
   def subscription_upgrade_button(subscription)
+    button = []
     if subscription.stripe_customer_id.present?
-      html_opts = {class: 'action primary confirm'}
-      return link_to 'Upgrade', client_subscription_path(subscription), html_opts
-    else
-      html_opts = {
-        class: 'disabled',
-        title: 'Please provide billing info before upgrading.'
-      }
-      return content_tag :span, 'Upgrade', html_opts
+      button << 'Upgrade'
+      button << client_subscription_path(subscription)
+      button << button_html_opts
     end
+    return button
+  end
+
+  def subscription_update_billing_button(subscription)
+    return [
+      'Update billing info',
+      update_billing_client_subscription_path(subscription),
+      button_html_opts
+    ]
+  end
+
+  def subscription_cancel_button(subscription)
+    button = []
+    if not subscription.trial?
+      button << 'Cancel subscription'
+      button << {
+        action: 'cancel',
+        id: subscription.id
+      }
+      button << {
+        method: 'put',
+        class: 'action secondary',
+        confirm: 'Are you sure you want to cancel your subscription?'
+      }
+    end
+    return button
+  end
+
+  def subscription_add_to_balance_button(subscription)
+    return [
+      'Add to your balance',
+      add_funds_client_subscription_path(subscription),
+      {class: 'action primary'}
+    ]
+  end
+
+  def subscription_configure_auto_recharge_button(subscription)
+    return [
+      'Configure auto-recharge',
+      configure_auto_recharge_client_subscription_path(subscription),
+      {class: 'action primary'}
+    ]
+  end
+
+  def subscription_human_type(type_or_subscription)
+    type = if type_or_subscription.kind_of? ActiveRecord::Base
+             type_or_subscription.type
+           else
+             type_or_subscription
+           end
+    return type.underscore.humanize
+  end
+
+  def subscription_buttons(subscription)
+    buttons = []
+    if subscription.per_agent?
+      buttons << subscription_update_billing_button(subscription)
+      upgrade_button = subscription_upgrade_button(subscription)
+      buttons << upgrade_button if not upgrade_button.empty?
+      cancel_button = subscription_cancel_button(subscription)
+      buttons << cancel_button if not cancel_button.empty?
+    elsif subscription.per_minute?
+      buttons << subscription_add_to_balance_button(subscription)
+      buttons << subscription_configure_auto_recharge_button(subscription)
+      buttons << subscription_update_billing_button(subscription)
+    end
+    return buttons
+  end
+
+private
+
+  def button_html_opts
+    return @button_html_opts ||= {class: 'action primary confirm'}
   end
 end
--- spec/helpers/client/subscription_helper_spec.rb
+++ spec/helpers/client/subscription_helper_spec.rb
@@ -11,7 +11,194 @@ describe Client::SubscriptionHelper do
     end
   end
 
-  describe '#subscription_upgrade_button(subscription, enabled=true)' do
+  describe '#subscription_update_billing_button(subscription)' do
+    let(:subscription) do
+      create(:trial, {
+        number_of_callers: 1
+      })
+    end
+
+    it 'returns an array of link_to args to update billing info for subscription' do
+      actual = helper.subscription_update_billing_button(subscription)
+
+      actual[0].should eq 'Update billing info'
+      actual[1].should eq update_billing_client_subscription_path(subscription)
+      actual[2].should eq({class: 'action primary confirm'})
+    end
+  end
+
+  describe 'Per minute subscription buttons' do
+    let(:subscription) do
+      create(:per_minute, {
+        number_of_callers: 1
+      })
+    end
+
+    describe '#subscription_configure_auto_recharge_button(subscription)' do
+      it 'returns an array of link_to args to update auto recharge settings' do
+        actual = helper.subscription_configure_auto_recharge_button(subscription)
+        actual.should eq [
+          'Configure auto-recharge',
+          configure_auto_recharge_client_subscription_path(subscription),
+          {class: 'action primary'}
+        ]
+      end
+    end
+
+    describe '#subscription_add_to_balance_button' do
+      it 'returns an array of link_to args to add to balance' do
+        actual = helper.subscription_add_to_balance_button(subscription)
+        actual.should eq [
+          'Add to your balance',
+          add_funds_client_subscription_path(subscription),
+          {class: 'action primary'}
+        ]
+      end
+    end
+  end
+
+  describe '#subscription_buttons(subscription)' do
+    context 'subscription is Per minute' do
+      let(:subscription) do
+        create(:per_minute, {
+          number_of_callers: 1
+        })
+      end
+
+      before do
+        @actual = helper.subscription_buttons(subscription)
+      end
+
+      it 'returns an array of 3 items, one for each button to render' do
+        @actual.size.should eq 3
+      end
+
+      it 'first button is for add to balance' do
+        @actual[0].should eq helper.subscription_add_to_balance_button(subscription)
+      end
+
+      it 'second button is for configure auto-recharge' do
+        @actual[1].should eq helper.subscription_configure_auto_recharge_button(subscription)
+      end
+
+      it 'third button is for update billing info' do
+        @actual[2].should eq helper.subscription_update_billing_button(subscription)
+      end
+    end
+
+    context 'subscription is Per agent' do
+      context 'and Trial' do
+        let(:subscription) do
+          create(:trial, {
+            number_of_callers: 1
+          })
+        end
+
+        context 'with no billing info' do
+          before do
+            @actual = helper.subscription_buttons(subscription)
+          end
+
+          it 'returns args for a single link' do
+            @actual.size.should eq 1
+          end
+
+          it 'returns args to update billing info' do
+            @actual.should eq [helper.subscription_update_billing_button(subscription)]
+          end
+        end
+
+        context 'with billing info' do
+          before do
+            subscription.stripe_customer_id = 1
+            @actual = helper.subscription_buttons(subscription)
+          end
+
+          it 'returns args for 2 links' do
+            @actual.size.should eq 2
+          end
+
+          it 'returns args to update billing info' do
+            @actual[0].should eq helper.subscription_update_billing_button(subscription)
+          end
+
+          it 'returns args to upgrade plan' do
+            @actual[1].should eq helper.subscription_upgrade_button(subscription)
+          end
+        end
+      end
+
+      context 'and not Trial' do
+        let(:subscription) do
+          create(:basic, {
+            number_of_callers: 1,
+            stripe_customer_id: 3
+          })
+        end
+
+        before do
+          @actual = helper.subscription_buttons(subscription)
+        end
+
+        it 'returns args for 3 links' do
+          @actual.size.should eq 3
+        end
+
+        it 'returns args to update billing info' do
+          @actual[0].should eq helper.subscription_update_billing_button(subscription)
+        end
+
+        it 'returns args to upgrade plan' do
+          @actual[1].should eq helper.subscription_upgrade_button(subscription)
+        end
+
+        it 'returns args to cancel subscription' do
+          @actual[2].should eq helper.subscription_cancel_button(subscription)
+        end
+      end
+    end
+  end
+
+  describe '#subscription_cancel_button(subscription)' do
+    context 'when subscription is a trial' do
+      let(:subscription) do
+        create(:trial, {
+          number_of_callers: 1
+        })
+      end
+
+      it 'returns an empty array' do
+        actual = helper.subscription_cancel_button(subscription)
+
+        actual.should eq []
+      end
+    end
+
+    context 'when subscription is not a trial' do
+      let(:subscription) do
+        create(:basic, {
+          number_of_callers: 1
+        })
+      end
+
+      it 'returns an array of link_to args to cancel subscription' do
+        actual = helper.subscription_cancel_button(subscription)
+
+        actual[0].should eq 'Cancel subscription'
+        actual[1].should eq({
+          action: 'cancel',
+          id: subscription.id
+        })
+        actual[2].should eq({
+          method: 'put',
+          class: 'action secondary',
+          confirm: 'Are you sure you want to cancel your subscription?'
+        })
+      end
+    end
+  end
+
+  describe '#subscription_upgrade_button(subscription)' do
     let(:subscription) do
       create(:trial, {
         number_of_callers: 1
@@ -22,23 +209,20 @@ describe Client::SubscriptionHelper do
       before do
         subscription.stripe_customer_id = 1
       end
-      it 'returns a link to subscription' do
+      it 'returns an array of link_to args to subscription' do
         actual = helper.subscription_upgrade_button(subscription)
 
-        actual.should have_text 'Upgrade'
-        actual.should have_css 'a'
-        actual.should have_css "[href$='#{client_subscription_path(subscription)}']"
-        actual.should have_css "[class='action primary confirm']"
+        actual[0].should eq 'Upgrade'
+        actual[1].should eq client_subscription_path(subscription)
+        actual[2].should eq({class: 'action primary confirm'})
       end
     end
 
     context 'when a subscription does not have a stripe customer id' do
-      it 'returns a span button look alike' do
+      it 'returns an empty array' do
         actual = helper.subscription_upgrade_button(subscription)
 
-        actual.should have_text 'Upgrade'
-        actual.should have_css 'span'
-        actual.should have_css "[class='disabled']"
+        actual.should eq []
       end
     end
   end
