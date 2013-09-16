28db3ac5fc96e81a91710106c268ef25d0216c12
Jeremiah Heller <jeremiah@impactdialing.com>
2013-09-16 12:30:08 -0700

    whitespace

--- app/models/redis/redis_status.rb
+++ app/models/redis/redis_status.rb
@@ -1,22 +1,22 @@
 class RedisStatus
-  
+
   def self.set_state_changed_time(campaign_id, status, caller_session_id)
     $redis_dialer_connection.hset "campaign:#{campaign_id}:status", caller_session_id, {status: status, time: Time.now.to_s}.to_json
   end
-  
+
   def self.state_time(campaign_id, caller_session_id)
     time_status = $redis_dialer_connection.hget "campaign:#{campaign_id}:status", caller_session_id
     unless time_status.nil?
       element = JSON.parse(time_status)
-      time_spent = Time.now - Time.parse(element['time'] || Time.now.to_s)    
+      time_spent = Time.now - Time.parse(element['time'] || Time.now.to_s)
       [element['status'], seconds_fraction_to_time(time_spent)]
     end
   end
-  
+
   def self.delete_state(campaign_id, caller_session_id)
-    $redis_dialer_connection.hdel "campaign:#{campaign_id}:status", caller_session_id    
+    $redis_dialer_connection.hdel "campaign:#{campaign_id}:status", caller_session_id
   end
-  
+
   def self.seconds_fraction_to_time(time_difference)
     days = hours = mins = 0
     mins = (time_difference / 60).to_i
@@ -26,19 +26,19 @@ class RedisStatus
     hours = (hours % 24).to_i
     "#{hours.to_s.rjust(2, '0')}:#{mins.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
   end
-  
+
   def self.count_by_status(campaign_id, *caller_session_ids)
     elements = $redis_dialer_connection.hmget "campaign:#{campaign_id}:status" , *caller_session_ids.flatten
     on_hold = 0
     on_call = 0
     wrap_up = 0
     elements.compact.each do |element|
-     ele = JSON.parse(element)
-     on_hold = on_hold + 1 if ele['status'] == 'On hold'
-     on_call = on_call + 1 if ele['status'] == 'On call'
-     wrap_up = wrap_up + 1 if ele['status'] == 'Wrap up'
-   end
-  [on_hold, on_call, wrap_up]
- end
-  
+      ele = JSON.parse(element)
+      on_hold = on_hold + 1 if ele['status'] == 'On hold'
+      on_call = on_call + 1 if ele['status'] == 'On call'
+      wrap_up = wrap_up + 1 if ele['status'] == 'Wrap up'
+    end
+    [on_hold, on_call, wrap_up]
+  end
+
 end
