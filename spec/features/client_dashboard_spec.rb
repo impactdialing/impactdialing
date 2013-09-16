95fac8ba9382ba5faa645b1968f9b4c2d46b3a9b
Jeremiah Heller <jeremiah@impactdialing.com>
2013-09-16 12:36:08 -0700

    add failing test for #107
    - reassign campaign select menus do not reflect reality

--- spec/features/client_dashboard_spec.rb
+++ spec/features/client_dashboard_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
 
+def within_nth_campaign_cell(row_n, cell_n, &block)
+  within "#campaigns-monitor tbody tr:nth-of-type(#{row_n}) td:nth-of-type(#{cell_n})" do
+    yield
+  end
+end
+
+def within_nth_caller_cell(row_n, cell_n, &block)
+  within "#callers-monitor tbody tr:nth-of-type(#{row_n}) td:nth-of-type(#{cell_n})" do
+    yield
+  end
+end
+
 describe 'Client Web Dashboard (/client/index)' do
+  let(:user) do
+    create(:user)
+  end
   before do
-    create_user_and_login
+    web_login_as(user)
   end
 
   context 'when a user is logged in' do
@@ -23,6 +38,126 @@ describe 'Client Web Dashboard (/client/index)' do
       click_on 'Turn call recording off'
 
       page.should have_content 'Call recording turned off.'
+    end
+
+    describe 'listing active campaigns and callers', js: true do
+      let(:account){ user.account }
+
+      let(:power_no_calls) do
+        create(:power, {
+          account: account
+        })
+      end
+
+      let(:power_extra) do
+        create(:power, {
+          account: account
+        })
+      end
+
+      let(:power_campaign) do
+        create(:power, {
+          account: account
+        })
+      end
+
+      let(:callers) do
+        create_list(:caller, 6, {
+          account: account,
+          campaign: power_campaign
+        })
+        Caller.all
+      end
+
+      let(:extra_callers) do
+        create_list(:caller, 3, {
+          account: account,
+          campaign: power_extra
+        })
+      end
+
+      before do
+        redis_states = ['On call', 'On call', 'On hold']
+        3.times do |i|
+          session = create(:webui_caller_session, {
+            caller: callers[i],
+            campaign: power_campaign,
+            on_call: true,
+            available_for_call: redis_states[i] == 'On call' ? false : true
+          })
+
+          c = power_campaign
+          RedisStatus.set_state_changed_time(c.id, redis_states[i], session.id)
+        end
+
+        2.times do |i|
+          session = create(:webui_caller_session, {
+            caller: extra_callers[i],
+            campaign: power_extra,
+            on_call: true,
+            available_for_call: i == 0 ? true : false
+          })
+
+          c = power_extra
+          RedisStatus.set_state_changed_time(c.id, 'On hold', session.id)
+        end
+
+        visit client_root_path
+      end
+
+      context 'Active Campaigns' do
+        it 'only lists campaigns w/ active callers logged in' do
+          within '#campaigns-monitor' do
+            page.should have_content power_campaign.name
+            page.should_not have_content power_no_calls.name
+          end
+        end
+        it 'lists the number of callers logged in for the specified campaign' do
+          within_nth_campaign_cell 2, 2 do
+            page.should have_content power_campaign.caller_sessions.count
+          end
+        end
+        it 'lists the number of callers on call' do
+          within_nth_campaign_cell 2, 3 do
+            page.should have_content 2
+          end
+        end
+        it 'lists the number of callers in wrap up' do
+          within_nth_campaign_cell 2, 4 do
+            page.should have_content 0
+          end
+        end
+        it 'lists the number of callers on hold' do
+          within_nth_campaign_cell 2, 5 do
+            page.should have_content 1
+          end
+        end
+        it 'lists the number of ringing lines'
+        it 'lists the number of numbers remaining in the call list'
+      end
+
+      context 'Active Callers' do
+        it 'lists the caller email address' do
+          within_nth_caller_cell 2, 1 do
+            page.should have_content callers.first.username
+          end
+        end
+        it 'displays the campaign the caller is logged in for in a drop down menu' do
+          within_nth_caller_cell 2, 2 do
+            page.find("select.reassign-campaign option[value='#{power_campaign.id}']").should be_selected
+          end
+        end
+        it 'lists the calls status e.g. on hold' do
+          within_nth_caller_cell 2, 3 do
+            page.should have_content 'On call'
+          end
+        end
+        it 'lists the time the call has been in the current status' do
+          within_nth_caller_cell 2, 4 do
+            page.body.should =~ /\d\d:\d\d:\d\d/
+          end
+        end
+      end
     end
   end
 end
