ea830a8ef4ccf3eff6fab9843224f8bd1a578984
Jeremiah Heller <jeremiah@impactdialing.com>
2013-11-07 11:56:11 -0800

    parameterize query params to avoid SQLi
    fixes #76

--- app/models/note_response.rb
+++ app/models/note_response.rb
@@ -6,11 +6,11 @@ class NoteResponse < ActiveRecord::Base
 
   scope :for, lambda{|note| where(["note_id = ?",note.id])}
   scope :for_notes, lambda{|note_ids| where("note_id in (?) ", note_ids)}
-  
+
   def self.note_ids(campaign_id)
-    NoteResponse.all(:select=>"distinct note_id", :conditions=>"campaign_id = #{campaign_id}", :order => "note_id").collect{|nr| nr.note_id }    
+    NoteResponse.all(:select=>"distinct note_id", :conditions=>["campaign_id = ?", campaign_id], :order => "note_id").collect{|nr| nr.note_id }
   end
-  
+
   def self.response_texts(note_ids, note_responses)
     note_responses ||= []
     responses = note_responses.each_with_object({}) do |response, memo|
