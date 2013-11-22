f7f7fc36fa5fa2ddb7400ad3befa67a0e4e74117
Jeremiah Heller <jeremiah@impactdialing.com>
2013-11-22 13:08:22 -0800

    remove Transfer#dial

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer < ActiveRecord::Base
   belongs_to :script
   has_many :transfer_attempts
-  include Rails.application.routes.url_helpers
+
   validates_presence_of :phone_number
   validates_length_of :phone_number, :minimum => 10, :unless => Proc.new{|transfer| transfer.phone_number && transfer.phone_number.start_with?("+")}
   before_validation :sanitize_phone
@@ -18,38 +18,8 @@ class Transfer < ActiveRecord::Base
     self.phone_number = Transfer.sanitize_phone(phone_number) if phone_number
   end
 
-
-
   module Type
     WARM = "warm"
     COLD = "cold"
   end
-
-
-
-
-  def dial(caller_session, call_attempt, voter, type)
-    transfer_attempt = TransferAttempt.create(session_key: generate_session_key, transfer: self, campaign_id: caller_session.campaign_id, :status => CallAttempt::Status::RINGING, caller_session_id: caller_session.id, call_attempt_id: call_attempt.id, transfer_type: type)
-    Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
-    params = {'FallbackUrl' => TWILIO_ERROR, 'StatusCallback' => end_transfer_url(transfer_attempt, :host => Settings.twilio_callback_host, :port => Settings.twilio_callback_port, :protocol => "http://"),'Timeout' => "30"}
-    response = Twilio::Call.make(voter.phone, phone_number, connect_transfer_url(transfer_attempt, :host => Settings.twilio_callback_host, :port => Settings.twilio_callback_port, :protocol => "http://"),params)
-    if response["TwilioResponse"]["RestException"]
-      transfer_attempt.update_attributes(status: CallAttempt::Status::FAILED)
-      return
-    end
-    transfer_attempt.update_attributes(:sid => response["TwilioResponse"]["Call"]["Sid"])
- end
-
- def generate_session_key
-   secure_digest(Time.now, (1..10).map{ rand.to_s })
- end
-
- def secure_digest(*args)
-   Digest::SHA1.hexdigest(args.flatten.join('--'))
- end
-
-
-
-
-
 end
--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -14,40 +14,4 @@ describe Transfer do
     end
 
   end
-
-  describe "dial" do
-    it "should make a call and update attempt with call sid" do
-      voter = create(:voter, phone: "1234567890")
-      transfer = create(:transfer, phone_number: "9878987654")
-      caller_session = create(:caller_session)
-      call_attempt = create(:call_attempt)
-      voter = create(:voter)
-      Twilio::Call.should_receive(:make).with(voter.phone, transfer.phone_number, anything, anything).and_return("TwilioResponse"=> {"Call" => {"Sid" => 'sid'}})
-      transfer.dial(caller_session, call_attempt, voter, Transfer::Type::WARM)
-      transfer.transfer_attempts.first.sid.should eq('sid')
-    end
-
-    it "should create a transfer attempt" do
-      voter = create(:voter, phone: "1234567890")
-      transfer = create(:transfer, phone_number: "9878987654")
-      caller_session = create(:caller_session)
-      call_attempt = create(:call_attempt)
-      voter = create(:voter)
-      Twilio::Call.should_receive(:make).with(voter.phone, transfer.phone_number, anything, anything).and_return("TwilioResponse"=> {"Call" => {"Sid" => 'sid'}})
-      transfer.dial(caller_session, call_attempt, voter, Transfer::Type::WARM)
-      transfer.transfer_attempts.first.status.should eq(CallAttempt::Status::RINGING)
-    end
-
-    it "should create a transfer attempt with status failed if cannot make call" do
-      voter = create(:voter, phone: "1234567890")
-      transfer = create(:transfer, phone_number: "9923700678")
-      caller_session = create(:caller_session)
-      call_attempt = create(:call_attempt)
-      voter = create(:voter)
-      Twilio::Call.should_receive(:make).with(voter.phone, transfer.phone_number, anything, anything).and_return("TwilioResponse"=> {"RestException" => {}})
-      transfer.dial(caller_session, call_attempt, voter, Transfer::Type::WARM)
-      transfer.transfer_attempts.first.status.should eq(CallAttempt::Status::FAILED)
-    end
-
-  end
 end
