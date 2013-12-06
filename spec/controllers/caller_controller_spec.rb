d02c3e2ae06ecf9bd1ce0dbf0c847ca0ef830806
Jeremiah Heller <jeremiah@impactdialing.com>
2013-12-06 12:41:28 -0800

    add tests for :kick_caller_off_conference

--- spec/controllers/caller_controller_spec.rb
+++ spec/controllers/caller_controller_spec.rb
@@ -1,6 +1,13 @@
 require "spec_helper"
 
 describe CallerController do
+  include TwilioRequests
+  include TwilioResponses
+
+  before do
+    WebMock.disable_net_connect!
+  end
+
   let(:account) { create(:account) }
   let(:user) { create(:user, :account => account) }
 
@@ -20,23 +27,23 @@ describe CallerController do
       response.should redirect_to(caller_login_path)
     end
 
-  end  
-  
+  end
+
   describe "start calling" do
     it "should start a new caller conference" do
       account = create(:account)
-      campaign = create(:predictive, account: account)      
+      campaign = create(:predictive, account: account)
       caller = create(:caller, campaign: campaign, account: account)
       caller_identity = create(:caller_identity)
       caller_session = create(:webui_caller_session, session_key: caller_identity.session_key, caller_type: CallerSession::CallerType::TWILIO_CLIENT, caller: caller, campaign: campaign)
       Caller.should_receive(:find).and_return(caller)
       caller.should_receive(:create_caller_session).and_return(caller_session)
       RedisPredictiveCampaign.should_receive(:add).with(caller.campaign_id, caller.campaign.type)
-      post :start_calling, caller_id: caller.id, session_key: caller_identity.session_key, CallSid: "abc"      
+      post :start_calling, caller_id: caller.id, session_key: caller_identity.session_key, CallSid: "abc"
       response.body.should eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Dial hangupOnStar=\"true\" action=\"http://#{Settings.twilio_callback_host}:#{Settings.twilio_callback_port}/caller/#{caller.id}/pause?session_id=#{caller_session.id}\"><Conference startConferenceOnEnter=\"false\" endConferenceOnExit=\"true\" beep=\"true\" waitUrl=\"hold_music\" waitMethod=\"GET\"/></Dial></Response>")
     end
   end
-  
+
   describe "call voter" do
     it "should call voter" do
       account = create(:account)
@@ -50,5 +57,74 @@ describe CallerController do
       post :call_voter, id: caller.id, voter_id: voter.id, session_id: caller_session.id
     end
   end
- 
+
+  describe "kick_caller_off_conference" do
+    let(:account){ create(:account) }
+    let(:campaign){ create(:predictive, account: account) }
+    let(:caller){ create(:caller, campaign: campaign, account: account) }
+    let(:caller_identity){ create(:caller_identity) }
+    let(:voter){ create(:voter, campaign: campaign) }
+
+    let(:caller_session) do
+      create(:webui_caller_session, {
+        session_key: caller_identity.session_key,
+        caller_type: CallerSession::CallerType::TWILIO_CLIENT,
+        caller: caller,
+        campaign: campaign,
+        sid: '123abc'
+      })
+    end
+    let(:url_opts) do
+      {
+        host: Settings.twilio_callback_host,
+        port: Settings.twilio_callback_port,
+        protocol: "http://",
+        session_id: caller_session.id
+      }
+    end
+    let(:conference_sid){ 'CFww834eJSKDJFjs328JF92JSDFwe' }
+    let(:call_sid){ caller_session.sid }
+    let(:conference_name){ caller_session.session_key }
+    let(:valid_response) do
+      double('TwilioResponseObject', {
+        :[] => {
+          'TwilioResponse' => {}
+        },
+        :conference_sid => conference_sid
+      })
+    end
+    before do
+      @conf_list_request = stub_request(:get, twilio_conference_by_name_url(conference_name)).
+        to_return({
+          :status => 200,
+          :body => conference_by_name_response,
+          :headers => {
+            'Content-Type' => 'text/xml'
+          }
+        })
+      @kick_request = stub_request(:delete, twilio_conference_kick_participant_url(conference_sid, call_sid)).
+        to_return({
+          :status => 204
+        })
+      @redirect_request = stub_request(:post, twilio_call_url(call_sid)).
+        with(:body => request_body(pause_caller_url(caller, url_opts))).
+        to_return({
+          :status => 200,
+          :body => updated_call_response,
+          :headers => {
+            'Content-Type' => 'text/xml'
+          }
+        })
+      post :kick_caller_off_conference, id: caller.id, caller_session: caller_session.id
+    end
+    it 'kicks caller off conference' do
+      @kick_request.should have_been_made
+    end
+    it 'redirects caller to pause url' do
+      @redirect_request.should have_been_made
+    end
+    it 'renders nothing' do
+      response.body.should be_blank
+    end
+  end
 end
