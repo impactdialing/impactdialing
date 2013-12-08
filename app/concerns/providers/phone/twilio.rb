e3769bad5903be63edf293fd7851fa3a7bfbaf3b
Jeremiah Heller <jeremiah@impactdialing.com>
2013-12-07 18:54:01 -0800

    refactor

--- app/concerns/providers/phone/twilio.rb
+++ app/concerns/providers/phone/twilio.rb
@@ -1,29 +1,23 @@
 module Providers::Phone::Twilio
-  def self.connect
+  def self.connect(&block)
     Twilio.connect(TWILIO_ACCOUNT, TWILIO_AUTH)
+    xml = yield
+    return Response.new(xml)
   end
 
   def self.redirect(call_sid, url)
-    connect
-    xml = Twilio::Call.redirect(call_sid, url)
-    return Response.new(xml)
+    connect{ Twilio::Call.redirect(call_sid, url) }
   end
 
   def self.make(from, to, url, params)
-    connect
-    xml = Twilio::Call.make(from, to, url, params)
-    return Response.new(xml)
+    connect{ Twilio::Call.make(from, to, url, params) }
   end
 
   def self.conference_list(search_options={})
-    connect
-    xml = Twilio::Conference.list(search_options)
-    return Response.new(xml)
+    connect{ Twilio::Conference.list(search_options) }
   end
 
   def self.kick(conference_sid, call_sid)
-    connect
-    bodyless_response = Twilio::Conference.kick_participant(conference_sid, call_sid)
-    return Response.new(bodyless_response)
+    connect{ Twilio::Conference.kick_participant(conference_sid, call_sid) }
   end
 end
