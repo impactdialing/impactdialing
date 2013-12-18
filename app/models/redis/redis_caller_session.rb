92f9c2009276820d36257ec221da061101e734bc
Jeremiah Heller <jeremiah@impactdialing.com>
2013-12-17 19:06:43 -0800

    add active_transfer flag & refactor

--- app/models/redis/redis_caller_session.rb
+++ app/models/redis/redis_caller_session.rb
@@ -1,63 +1,89 @@
 class RedisCallerSession
-  
+
+  def self.redis
+    $redis_caller_session_uri_connection
+  end
+
+  ## active_transfer_*
+  # todo: refactor active_transfer_* to own obj
+  # todo: refactor RedisCallerSession & other redis classes
+  #
+  # active_transfer_* methods are for managing
+  # the active_transfer flag which is useful
+  # to determine which TwiML to render.
+  #
+  def self.active_transfer_key(session_key)
+    "caller_sessions.#{session_key}.active_transfer"
+  end
+
+  def self.active_transfer(caller_session_key)
+    redis.get(active_transfer_key(caller_session_key))
+  end
+
+  def self.active_transfer?(caller_session_key)
+    on_off = active_transfer(caller_session_key)
+    on_off.to_i > 0
+  end
+
+  def self.activate_transfer(caller_session_key)
+    redis.set(active_transfer_key(caller_session_key), 1)
+  end
+
+  def self.deactivate_transfer(caller_session_key)
+    redis.del(active_transfer_key(caller_session_key))
+  end
+
   def self.set_request_params(caller_session_id, options)
-    $redis_caller_session_uri_connection.set "caller_session_flow:#{caller_session_id}", options.to_json
+    redis.set "caller_session_flow:#{caller_session_id}", options.to_json
   end
-  
+
   def self.delete(caller_session_id)
-    $redis_caller_session_uri_connection.del "caller_session_flow:#{caller_session_id}"
+    redis.del "caller_session_flow:#{caller_session_id}"
   end
-  
-  
+
   def self.get_request_params(caller_session_id)
-    $redis_caller_session_uri_connection.get "caller_session_flow:#{caller_session_id}"
+    redis.get "caller_session_flow:#{caller_session_id}"
   end
-  
-  
+
   def self.digit(caller_session_id)
-    data = $redis_caller_session_uri_connection.get "caller_session_flow:#{caller_session_id}"
+    data = redis.get "caller_session_flow:#{caller_session_id}"
     hash = JSON.parse(data)
     hash["digit"]
   end
-  
+
   def self.question_id(caller_session_id)
-    data = $redis_caller_session_uri_connection.get "caller_session_flow:#{caller_session_id}"
+    data = redis.get "caller_session_flow:#{caller_session_id}"
     hash = JSON.parse(data)
     hash["question_id"]
   end
 
   def self.question_number(caller_session_id)
-    data = $redis_caller_session_uri_connection.get "caller_session_flow:#{caller_session_id}"
+    data = redis.get "caller_session_flow:#{caller_session_id}"
     hash = JSON.parse(data)
     hash["question_number"]
   end
-  
+
   def self.set_datacentre(caller_session_id, caller_dc)
-    $redis_caller_session_uri_connection.set "caller_dc:#{caller_session_id}", caller_dc
+    redis.set "caller_dc:#{caller_session_id}", caller_dc
   end
-  
+
   def self.datacentre(caller_session_id)
-    $redis_caller_session_uri_connection.get "caller_dc:#{caller_session_id}"
+    redis.get "caller_dc:#{caller_session_id}"
   end
-  
+
   def self.remove_datacentre(caller_session_id)
-    $redis_caller_session_uri_connection.del "caller_dc:#{caller_session_id}"
+    redis.del "caller_dc:#{caller_session_id}"
   end
-  
+
   def self.add_phantom_callers(caller_session_id)
-    $redis_caller_session_uri_connection.lpush "phantom_callers", caller_session_id
+    redis.lpush "phantom_callers", caller_session_id
   end
-  
+
   def self.phantom_callers
-    $redis_caller_session_uri_connection.lrange "phantom_callers", 0, -1
+    redis.lrange "phantom_callers", 0, -1
   end
-  
+
   def self.remove_phantom_caller(caller_session_id)
-    $redis_caller_session_uri_connection.del "phantom_callers", caller_session_id
+    redis.del "phantom_callers", caller_session_id
   end
-  
-  
-  
-  
-  
 end
--- spec/models/redis/redis_caller_session_spec.rb
+++ spec/models/redis/redis_caller_session_spec.rb
@@ -1,27 +1,70 @@
 require "spec_helper"
 
 describe RedisCallerSession do
-  
+  subject{ RedisCallerSession }
+
   it "should set options" do
     RedisCallerSession.set_request_params(1, {digit: 1, question_number: 2, question_id: 3})
     result = RedisCallerSession.get_request_params(1)
     JSON.parse(result).should eq({"digit"=>1, "question_number"=>2, "question_id"=>3})
   end
-  
+
   it "should get digit" do
     RedisCallerSession.set_request_params(1, {digit: 1, question_number: 2, question_id: 3})
     RedisCallerSession.digit(1).should eq(1)
   end
-  
+
   it "should get question number" do
     RedisCallerSession.set_request_params(1, {digit: 1, question_number: 2, question_id: 3})
     RedisCallerSession.question_number(1).should eq(2)
   end
-  
+
   it "should get question id" do
     RedisCallerSession.set_request_params(1, {digit: 1, question_number: 2, question_id: 3})
     RedisCallerSession.question_id(1).should eq(3)
   end
-  
-  
+
+  describe 'manage an active transfer flag' do
+    let(:session_key){ 'session-key.abc123' }
+    before do
+      subject.deactivate_transfer(session_key)
+    end
+    after do
+      subject.deactivate_transfer(session_key)
+    end
+    describe '#activate_transfer(caller_session_key)' do
+      it 'sets caller_sessions.#{caller_session_key}.active_transfer = "1"' do
+        subject.activate_transfer(session_key)
+        subject.active_transfer(session_key).should eq '1'
+      end
+    end
+
+    describe '#deactivate_transfer(caller_session_key)' do
+      it 'deletes caller_sessions.#{caller_session_key}.active_transfer' do
+        subject.deactivate_transfer(session_key)
+        subject.active_transfer(session_key).should be_nil
+      end
+    end
+
+    describe '#active_transfer(caller_session_key)' do
+      it 'returns the current value of caller_sessions.#{caller_session_key}.active_transfer' do
+        subject.active_transfer(session_key).should be_nil
+
+        subject.activate_transfer(session_key)
+        subject.active_transfer(session_key).should eq '1'
+      end
+    end
+
+    describe '#active_transfer?(caller_session_key)' do
+      it 'returns true when caller_sessions.#{caller_session_key}.active_transfer == "1"' do
+        subject.activate_transfer(session_key)
+        subject.active_transfer?(session_key).should be_true
+      end
+
+      it 'returns false when caller_sessions.#{caller_session_key}.active_transfer != "1"' do
+        subject.active_transfer?(session_key).should be_false
+        subject.active_transfer(session_key).should be_nil
+      end
+    end
+  end
 end
