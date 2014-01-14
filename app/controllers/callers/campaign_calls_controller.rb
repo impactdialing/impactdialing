9d6263db9ec90c58df7d964c01196b320bd3f460
Jeremiah Heller <jeremiah@impactdialing.com>
2014-01-14 14:52:13 -0800

    readability

--- app/controllers/callers/campaign_calls_controller.rb
+++ app/controllers/callers/campaign_calls_controller.rb
@@ -12,22 +12,43 @@ module Callers
     end
 
     def token
-
       @campaign = @caller.campaign
+
       unless @caller.account.current_subscription.can_dial?
         render :json => "Your account is not funded. Please contact your account administrator.", :status => 422
         return
       end
+
       if @campaign.time_period_exceeded?
-        render json: I18n.t(:campaign_time_period_exceed, :start_time => @campaign.start_time.hour <= 12 ? "#{@campaign.start_time.hour} AM" : "#{@campaign.start_time.hour-12} PM",
-         :end_time => @campaign.end_time.hour <= 12 ? "#{@campaign.end_time.hour} AM" : "#{@campaign.end_time.hour-12} PM")
+        start_time = if @campaign.start_time.hour <= 12
+                       "#{@campaign.start_time.hour} AM"
+                     else
+                      "#{@campaign.start_time.hour-12} PM"
+                     end
+        end_time = if @campaign.end_time.hour <= 12
+                     "#{@campaign.end_time.hour} AM"
+                   else
+                     "#{@campaign.end_time.hour-12} PM"
+                   end
+        render(
+          json: I18n.t(:campaign_time_period_exceed, {
+            :start_time => start_time,
+            :end_time => end_time
+          })
+        )
         return
       end
       @caller_identity = @caller.create_caller_identity(generate_session_key)
       twilio_capability = Twilio::Util::Capability.new(TWILIO_ACCOUNT, TWILIO_AUTH)
       twilio_capability.allow_client_outgoing(TWILIO_APP_SID)
-      render json: @caller_identity.as_json({root: false}).merge({twilio_token: twilio_capability.generate,
-       phone_number: number_to_phone(Settings.callin_phone, :area_code => true), campaign_id: @campaign.id})
+
+      additional_json = {
+        campaign_id: @campaign.id,
+        twilio_token: twilio_capability.generate,
+        phone_number: number_to_phone(Settings.callin_phone, :area_code => true)
+      }
+
+      render json: @caller_identity.as_json({root: false}).merge(additional_json)
     end
 
   end
