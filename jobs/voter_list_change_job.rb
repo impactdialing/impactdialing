967b42a63fda46285dab3a5486cfea9a1198b46c
Jeremiah Heller <jeremiah@impactdialing.com>
2014-02-02 11:06:08 -0800

    Requeue & notify on deadlock victims
    [#63217030]

--- jobs/voter_list_change_job.rb
+++ jobs/voter_list_change_job.rb
@@ -8,17 +8,34 @@ class VoterListChangeJob
 
     def perform(voter_list_id, enabled)
       begin
+        p "VoterListChangeJob performing..."
         voter_list = VoterList.find(voter_list_id)
         voter_list.voter_ids.each_slice(500) do |ids|
           Voter.where(id: ids).update_all(enabled: enabled)
         end
-      rescue Resque::TermException
-        requeue(voter_list_id, enabled)
+        p "VoterListChangeJob done."
+      rescue Resque::TermException, ActiveRecord::StatementInvalid => exception
+        handle_exception(voter_list_id, enabled, exception)
       end
     end
 
     def requeue(voter_list_id, enabled)
       Resque.enqueue(self, voter_list_id, enabled)
+    end
+
+    def handle_exception(voter_list_id, enabled, exception)
+      should_requeue = false
+      if exception.kind_of? ActiveRecord::StatementInvalid
+        mailer         = ExceptionMailer.new(exception)
+        mailer.notify_if_deadlock_detected
+        if mailer.deadlock_detected?
+          requeue(voter_list_id, enabled)
+        else
+          raise exception
+        end
+      else
+        requeue(voter_list_id, enabled)
+      end
     end
 
     def after_perform_scale_down(*args)
--- spec/jobs/voter_list_change_job_spec.rb
+++ spec/jobs/voter_list_change_job_spec.rb
@@ -4,17 +4,75 @@ require 'resque/errors'
 
 describe VoterListChangeJob do
   let(:subject){ VoterListChangeJob }
-  let(:voter) do
-    create(:voter)
-  end
   let(:voter_list) do
-    voter.voter_list
+    create(:voter_list)
+  end
+  let(:new_voter_list) do
+    create(:voter_list, {
+      csv_to_system_map: {
+        'VANID' => 'custom_id',
+        'FirstName' => 'first_name',
+        'LastName' => 'last_name',
+        'Phone' => 'phone'
+      },
+      s3path: "test/uploads/voter_list/custom-id-numbers.csv"
+    })
+  end
+  let(:voters) do
+    create_list(:voter, 3, {
+      first_name: 'Frank',
+      last_name: 'Moody',
+      voter_list: voter_list
+    })
   end
   let(:enabled){ false }
+  before do
+    voter_list.voters = voters
+    voter_list.save!
+  end
+  after do
+    VoterList.destroy_all
+    Voter.destroy_all
+  end
+
   it 'properly requeues itself if the worker is stopped during a run' do
     Resque.should_receive(:enqueue).with(subject, voter_list.id, enabled)
-    Voter.stub_chain(:where, :update_all){ raise Resque::TermException, 'KILL' }
-    subject.perform(voter_list.id, false)
+    Voter.stub_chain(:where, :update_all){ raise Resque::TermException, 'TERM' }
+    subject.perform(voter_list.id, enabled)
+  end
+
+  context 'Deadlock victims' do
+    let(:msg){ 'Mysql2::Error: Deadlock found when trying to get lock; try restarting transaction: UPDATE `voters` SET `enabled` = 0 WHERE `voters`.`id` IN (123, 321)' }
+
+    let(:mailer) do
+      double('ExceptionMailer', {
+        notify_if_deadlock_detected: nil,
+        deadlock_detected?: true
+      })
+    end
+    before do
+      Voter.stub_chain(:where, :update_all){ raise ActiveRecord::StatementInvalid, msg }
+      Resque.stub(:enqueue).with(subject, voter_list.id, anything)
+      ExceptionMailer.stub(:new){ mailer }
+    end
+
+    it 'are requeued' do
+      Resque.should_receive(:enqueue).with(subject, voter_list.id, anything)
+      subject.perform(voter_list.id, enabled)
+    end
+
+    it 'emails the innodb status' do
+      mailer.should_receive(:notify_if_deadlock_detected)
+      subject.perform(voter_list.id, enabled)
+    end
+  end
+
+  context 'other exceptions' do
+    before do
+      Voter.stub_chain(:where, :update_all){ raise ActiveRecord::StatementInvalid, "Syntax error" }
+    end
+    it 'fall through' do
+      expect{subject.perform(voter_list.id, enabled)}.to raise_error(ActiveRecord::StatementInvalid)
+    end
   end
 end
-
