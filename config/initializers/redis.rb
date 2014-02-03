7d4aef1e25edf85d88e153e8e0228d813f6090ed
Jeremiah Heller <jeremiah@impactdialing.com>
2014-02-03 13:37:39 -0800

    clean up

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,12 @@
-RAILS_ROOT = File.expand_path('../../..', __FILE__)
-require File.join(RAILS_ROOT, 'config/environment')
+require File.join(Rails.root.to_s, 'config/environment')
 require "eventmachine"
 require 'em-http'
 require 'em-hiredis'
 require 'em-http-request'
 
-
-rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-redis_config = YAML.load_file(RAILS_ROOT + "/config/redis.yml")
+redis_config = YAML.load_file(Rails.root.to_s + "/config/redis.yml")
 
 call_flow_uri = URI.parse(redis_config[rails_env]['call_flow'])
 $redis_call_flow_connection = Redis.new(:host => call_flow_uri.host, :port => call_flow_uri.port, :password => call_flow_uri.password)
