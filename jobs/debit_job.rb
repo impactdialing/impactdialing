47844330239e4ba9dfad26c9ea698bd07b24d0e7
Jeremiah Heller <jeremiah@impactdialing.com>
2014-03-04 15:07:21 -0800

    start debiting accounts again + debug

--- jobs/debit_job.rb
+++ jobs/debit_job.rb
@@ -13,16 +13,26 @@ class DebitJob
   def self.batch_process(klass, call_times)
     results = []
     call_times.each do |call_time|
-      # begin
-        account = call_time.campaign.account
-        subscription = account.debitable_subscription
-        debit = Debit.new(call_time, account, subscription)
-        results << debit.process
-      # rescue Exception => e
-      #   UserMailer.new.deliver_exception_notification('DebitJob Exception', e)
-      # end
+      account = call_time.campaign.account
+      subscription = account.debitable_subscription
+      # debug
+      before = [subscription.minutes_utlized, subscription.available_minutes]
+
+      debit = Debit.new(call_time, account, subscription)
+      results << debit.process
+
+      # debug
+      after = [subscription.minutes_utlized, subscription.available_minutes]
+      # debug
+      unless call_time.debited
+        m = (call_time.tDuration/60.0).ceil
+        d = call_time.debited
+        Rails.logger.error "DebitJob: Account[#{account.id}] Sub[#{subscription.id}] #{klass}[#{call_time.id}] Before[#{before[0]}:#{before[1]}] After[#{after[0]}:#{after[1]}] CallTimeMinutes[#{m}] CallTimeDebited[#{d}]"
+      end
     end
-    klass.import results, on_duplicate_key_update: [:debited]
+    import_result = klass.import results, on_duplicate_key_update: [:debited]
+    # debug
+    Rails.logger.error "DebitJob: Inserts[#{import_result.num_inserts}] Failed[#{import_result.failed_instances.size}]"
   end
 
   def self.perform
@@ -32,7 +42,7 @@ class DebitJob
       klass = klass_name.constantize
       call_times = klass.debit_pending.includes({campaign: :account}).limit(limit)
       Rails.logger.error "DebitJob: processing #{klass_name} #{call_times.count} - #{call_times.first.try(:id)} thru #{call_times.last.try(:id)}"
-      # batch_process(klass, call_times)
+      batch_process(klass, call_times)
     end
   end
 end
