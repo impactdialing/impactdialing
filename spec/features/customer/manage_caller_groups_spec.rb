069cc1ee354ff290ebbda05e0d7318ef05076e3b
Jeremiah Heller <jeremiah@impactdialing.com>
2014-04-01 14:36:35 -0700

    Fix auth/z check (naming).
    [fixes #68673942]

---
+++ spec/features/customer/manage_caller_groups_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'Caller Group Management', rack: true do
+  let(:customer) do
+    create(:user)
+  end
+  before do
+    web_login_as(customer)
+  end
+  it 'displays a link on the callers index page' do
+    visit '/client/callers'
+    click_on 'Manage caller groups'
+    page.should have_content 'Caller Groups'
+    click_on 'Add new caller group' # double-check it is the right page
+  end
+
+  it 'allows callers to be assigned to a group when edited' do
+    campaign = create(:power, {account: customer.account})
+    group = create(:caller_group, {
+      campaign: campaign,
+      account: customer.account
+    })
+    caller = create(:caller, {
+      account: customer.account,
+      campaign: campaign
+    })
+    visit "/client/callers/#{caller.id}/edit"
+    save_and_open_page
+    select "#{group.name}", from: 'Caller group'
+  end
+end
