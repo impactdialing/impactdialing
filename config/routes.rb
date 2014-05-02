eb9d11e8e5e94b28dc52524e1b066dc244f35233
Jeremiah Heller <jeremiah@impactdialing.com>
2014-05-02 00:06:34 -0700

    stop clobbering :session_id for /stop_calling

--- config/routes.rb
+++ config/routes.rb
@@ -81,7 +81,7 @@ ImpactDialing::Application.routes.draw do
   post 'call_center/api/:id/hangup', :to => 'calls#hangup'
   post 'call_center/api/:id/call_voter', :to => 'caller#call_voter'
   post 'call_center/api/transfer/dial', :to => 'transfer#dial'
-  post 'call_center/api/:session_id/stop_calling', :to => 'caller#stop_calling'
+  post 'call_center/api/:id/stop_calling', :to => 'caller#stop_calling'
   post 'call_center/api/:id/skip_voter', :to => 'caller#skip_voter'
   post 'call_center/api/:id/kick', :to => 'caller#kick'
   # /new api rough draft
