8db1e6e50f253477f2f28096579db7d8505d19c4
Jeremiah Heller <jeremiah@impactdialing.com>
2014-05-04 13:04:50 -0700

    handle abort caller conditions
    - dialer access disabled
    - account funded
    - acceptable time of day for campaign

--- app/controllers/callers/station_controller.rb
+++ app/controllers/callers/station_controller.rb
@@ -11,6 +11,38 @@ private
       @current_ability ||= Ability.new(@caller.account)
     end
 
+    def abort_json
+      if cannot?(:access_dialer, @caller)
+        return({json: {message: I18n.t('dialer.access.denied')}, status: 403})
+      end
+      if cannot? :start_calling, @caller
+        return({json: {message: I18n.t('dialer.account.not_funded')}, status: 402})
+      end
+      if @caller.campaign.time_period_exceeded?
+        start_time = if @caller.campaign.start_time.hour <= 12
+                       "#{@caller.campaign.start_time.hour} AM"
+                     else
+                      "#{@caller.campaign.start_time.hour-12} PM"
+                     end
+        end_time = if @caller.campaign.end_time.hour <= 12
+                     "#{@caller.campaign.end_time.hour} AM"
+                   else
+                     "#{@caller.campaign.end_time.hour-12} PM"
+                   end
+        return({
+          json: {
+            message: I18n.t('dialer.campaign.time_period_exceeded', {
+              start_time: start_time,
+              end_time: end_time
+            })
+          },
+          status: 403
+        })
+      end
+
+      {}
+    end
+
 public
     # draft new html/client entry point
     def show
@@ -33,6 +65,11 @@ public
 
     # draft new api - eventually moving to grape abstraction
     def create
+      abort = abort_json
+      unless abort.empty?
+        render abort and return
+      end
+
       campaign = @caller.campaign
       caller_session_key = generate_session_key
       caller_identity = @caller.create_caller_identity(caller_session_key)
