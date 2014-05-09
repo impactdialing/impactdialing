415cb30579144854bdd862e47726e113f4ce1983
Jeremiah Heller <jeremiah@impactdialing.com>
2014-05-09 09:05:07 -0700

    workaround failing redirect issue
    [#71031730]

--- jobs/redirect_caller_job.rb
+++ jobs/redirect_caller_job.rb
@@ -5,6 +5,11 @@ class RedirectCallerJob
 
    def perform(caller_session_id)
      caller_session = CallerSession.find_by_id_cached(caller_session_id)
-     Providers::Phone::Call.redirect_for(caller_session)
+     # Providers::Phone::Call.redirect_for(caller_session)
+
+     twilio = Twilio::REST::Client.new(TWILIO_ACCOUNT, TWILIO_AUTH)
+     call   = twilio.account.calls.get(caller_session.sid)
+     params = Providers::Phone::Call::Params::CallerSession.new(caller_session)
+     call.redirect_to(params.url)
    end
 end
