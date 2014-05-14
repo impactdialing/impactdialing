186e63c8c62647b49814ed9820bafa8b670e0a44
Jeremiah Heller <jeremiah@impactdialing.com>
2014-05-13 23:45:14 -0700

    and resque to use env var REDIS_URL

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,10 +3,15 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 require 'resque_scheduler'
 require 'octopus'
 
-redis_config = YAML.load_file(rails_root + '/config/redis.yml')
-Resque.redis = redis_config[rails_env]['resque']
+unless ENV['REDIS_URL'].nil? || ENV['REDIS_URL'].size.zero?
+  url = ENV['REDIS_URL']
+else
+  redis_config = YAML.load_file(rails_root + '/config/redis.yml')
+  url          = redis_config[rails_env]['resque']
+end
+Resque.redis    = url
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
 
-Dir[File.dirname(__FILE__) + '/../jobs/*.rb'].each do |file| 
+Dir[File.dirname(__FILE__) + '/../jobs/*.rb'].each do |file|
   require File.basename(file, File.extname(file))
 end
