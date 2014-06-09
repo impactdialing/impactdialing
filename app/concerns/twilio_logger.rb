5abff06ae3d94ff8e6f45f92fe998bc214bea951
Jeremiah Heller <jeremiah@impactdialing.com>
2014-06-09 16:00:10 -0700

    improve message when RestException prop not present

--- app/concerns/twilio_logger.rb
+++ app/concerns/twilio_logger.rb
@@ -1,9 +1,13 @@
 class TwilioLogger
   def self.error(response)
-    exception = response['RestException'] || response
-    msg = "TwilioRestException #{exception['Message']}" +
-          " [#{exception['Code']}] [#{exception['Status']}]" +
-          " (#{exception['MoreInfo']})"
+    exception = response['RestException']
+    if exception['Message'] || exception['MoreInfo']
+      msg = "TwilioRestException #{exception['Message']}" +
+            " [#{exception['Code']}] [#{exception['Status']}]" +
+            " (#{exception['MoreInfo']})"
+    else
+      msg = "TwilioRestException #{response}"
+    end
     Rails.logger.error(msg)
   end
 end
