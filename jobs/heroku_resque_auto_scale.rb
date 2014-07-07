74c80ea79c2f5167a69011887b64a5ec8ba24558
Jeremiah Heller <jeremiah@impactdialing.com>
2014-07-07 15:54:35 -0700

    uninstall deprecated heroku gem & recurly

--- jobs/heroku_resque_auto_scale.rb
+++ jobs/heroku_resque_auto_scale.rb
@@ -1,32 +1,37 @@
-require 'heroku'
+# require 'heroku'
 
 module HerokuResqueAutoScale
   module Scaler
     class << self
-      @@heroku = Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASS'])
+      # @@heroku = Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASS'])
 
       def worker_count(queue_name)
-        Resque.workers.count { |worker| worker.queues.include?(queue_name) }
+        0
+        # Resque.workers.count { |worker| worker.queues.include?(queue_name) }
       end
-      
+
       def working_job_count(queue_name)
-        Resque.working.count { |worker| worker.job["queue"] == queue_name }
+        0
+        # Resque.working.count { |worker| worker.job["queue"] == queue_name }
       end
-      
+
      def workers(queue_name, qty)
-        @@heroku.ps_scale(ENV['HEROKU_APP'], :type=>queue_name, :qty=>qty)
+      1
+        # @@heroku.ps_scale(ENV['HEROKU_APP'], :type=>queue_name, :qty=>qty)
      end
 
       def pending_job_count(queue_name)
-        Resque.size(queue_name)
+        0
+        # Resque.size(queue_name)
       end
-      
+
       def restart_web_dyno(dyno)
-        @heroku.restart(ENV['HEROKU_APP'], dyno)
+        true
+        # @heroku.restart(ENV['HEROKU_APP'], dyno)
       end
-      
+
     end
   end
-  
- 
+
+
 end
