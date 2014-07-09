d7ce10c063941650defb115baaaee9b10f86846c
Jeremiah Heller <jeremiah@impactdialing.com>
2014-07-09 10:25:05 -0700

    rm HerokuResqueAutoScale & stop autoscaling email jobs

--- jobs/deliver_invitation_email_job.rb
+++ jobs/deliver_invitation_email_job.rb
@@ -1,6 +1,4 @@
-require Rails.root.join("jobs/heroku_resque_auto_scale")
 class DeliverInvitationEmailJob
-  extend ::HerokuResqueAutoScale
   @queue = :background_worker
 
 
@@ -10,16 +8,4 @@ class DeliverInvitationEmailJob
     user_mailer = UserMailer.new
     user_mailer.deliver_invitation(new_user, current_user)
   end
-
-  def after_perform_scale_down(*args)
-    Scaler.workers(@queue.to_s,1) if Scaler.working_job_count(@queue.to_s) == 1
-  end
-
-  def after_enqueue_scale_up(*args)
-     workers_to_scale = Scaler.working_job_count(@queue.to_s) + Scaler.pending_job_count(@queue.to_s) - Scaler.worker_count(@queue.to_s)
-     if workers_to_scale > 0
-       Scaler.workers(@queue.to_s, Scaler.working_job_count(@queue.to_s) + Scaler.pending_job_count(@queue.to_s) + 1)
-     end
-   end
-
 end
--- jobs/reset_password_email_job.rb
+++ jobs/reset_password_email_job.rb
@@ -1,6 +1,5 @@
 require Rails.root.join("jobs/heroku_resque_auto_scale")
 class ResetPasswordEmailJob
-  extend ::HerokuResqueAutoScale
   @queue = :background_worker
 
 
@@ -9,16 +8,4 @@ class ResetPasswordEmailJob
     user_mailer = UserMailer.new
     user_mailer.reset_password(user)
   end
-  def after_perform_scale_down(*args)
-    Scaler.workers(@queue.to_s,1) if Scaler.working_job_count(@queue.to_s) == 1
-  end
-
-  def after_enqueue_scale_up(*args)
-     workers_to_scale = Scaler.working_job_count(@queue.to_s) + Scaler.pending_job_count(@queue.to_s) - Scaler.worker_count(@queue.to_s)
-     if workers_to_scale > 0
-       Scaler.workers(@queue.to_s, Scaler.working_job_count(@queue.to_s) + Scaler.pending_job_count(@queue.to_s) + 1)
-     end
-   end
-
-
 end
