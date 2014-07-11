ace95c0a915498c5fb4f7f597a0e16e29d3c0f40
Jeremiah Heller <jeremiah@impactdialing.com>
2014-07-11 14:11:43 -0700

    refactor for performance; create factories before all

--- spec/concerns/call_stats/velocity_spec.rb
+++ spec/concerns/call_stats/velocity_spec.rb
@@ -1,30 +1,36 @@
 require 'spec_helper'
 
 describe CallStats::Velocity do
-  let(:campaign){ create(:power) }
-
-  before do
+  # let(:campaign){ create(:power) }
+  before(:all) do
+    @campaign_for_all = create(:power)
     create_list(:caller, rand(10) + 2, {
-      campaign: campaign
+      campaign: @campaign_for_all
     })
     create_list(:webui_caller_session, rand(10) + 1, {
-      campaign: campaign,
+      campaign: @campaign_for_all,
       caller: Caller.all.sample,
       tDuration: rand(60*20) + 1200 # <= at least 5 minutes each session
     })
     create_list(:phones_only_caller_session, rand(10) + 1, {
-      campaign: campaign,
+      campaign: @campaign_for_all,
       caller: Caller.all.sample,
       tDuration: rand(60*20) + 1200 # <= at least 5 minutes each session
     })
     create_list(:call_attempt, rand(100) + 20, {
-      campaign: campaign,
+      campaign: @campaign_for_all,
       caller_session: CallerSession.all.sample,
       tDuration: rand(60*20) + 60 # <= at least 1 minute each session
     })
   end
 
-  subject{ CallStats::Velocity.new(campaign) }
+  after(:all) do
+    Campaign.destroy_all
+    CallerSession.destroy_all
+    CallAttempt.destroy_all
+  end
+
+  subject{ CallStats::Velocity.new(@campaign_for_all) }
 
   describe '#dial_rate' do
     it 'returns an Integer indicating the number of dials made per caller per hour' do
@@ -49,7 +55,7 @@ describe CallStats::Velocity do
       calling_hours   = calling_hours < 1 ? 1 : calling_hours
       expected        = (dials / calling_hours).round
 
-      velocity = CallStats::Velocity.new(campaign, {from_date: from_date, to_date: to_date})
+      velocity = CallStats::Velocity.new(@campaign_for_all, {from_date: from_date, to_date: to_date})
       velocity.dial_rate.should eq expected
     end
   end
@@ -62,7 +68,6 @@ describe CallStats::Velocity do
       answers         = CallAttempt.where(status: CallAttempt::Status::SUCCESS).count
       calling_seconds = CallerSession.sum('tDuration')
       calling_hours   = (calling_seconds / 3600.0)
-      calling_hours   = calling_hours < 1 ? 1 : calling_hours
       expected        = (answers / calling_hours).round
 
       subject.answer_rate.should eq expected
