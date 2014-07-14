351eea8bc02521091aa576c024a490f9f5c896a2
Jeremiah Heller <jeremiah@impactdialing.com>
2014-07-14 00:49:23 -0700

    cruft

--- config/application.rb
+++ config/application.rb
@@ -16,16 +16,12 @@ SUPPORT_EMAIL    = "support@impactdialing.com"
 
 module ImpactDialing
   class Application < Rails::Application
-  #  config.time_zone = "Pacific Time (US & Canada)"
-    #config.active_record.default_timezone = :local
-
-
     if ["heroku", "heroku_staging"].include?(Rails.env)
       config.logger = Logger.new(STDOUT)
       config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ?  ENV['LOG_LEVEL'].upcase : 'INFO')
     end
+
     config.filter_parameters << :password << :card_number << :card_verification << :cc << :code
-    # config.time_zone = 'UTC'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -36,22 +32,9 @@ module ImpactDialing
     config.autoload_paths += %W(#{config.root}/app/concerns)
     config.autoload_paths += %W(#{config.root}/app/models/redis)
 
-    # Specify gems that this application depends on and have them installed with rake gems:install
-    # config.gem "bj"
-    # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-    # config.gem "sqlite3-ruby", :lib => "sqlite3"
-    # config.gem "aws-s3", :lib => "aws/s3"
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
     # Skip frameworks you're not going to use. To use Rails without a database,
     # you must remove the Active Record framework.
     # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-    # Activate observers that should always be running
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names.
