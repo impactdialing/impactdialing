3fc0b2ef7431fa682cf043d25ab5d920176b4a3d
Jeremiah Heller <jeremiah@impactdialing.com>
2014-08-29 21:50:22 -0700

    refactor

--- app/models/redis/redis_possible_response.rb
+++ app/models/redis/redis_possible_response.rb
@@ -1,18 +1,28 @@
 class RedisPossibleResponse
-  
+  def self.redis
+    $redis_question_pr_uri_connection
+  end
+
+  def self.key(question_id)
+    "possible_response:#{question_id}"
+  end
+
   def self.persist_possible_response(question_id, keypad, value)
-    $redis_question_pr_uri_connection.lpush "possible_response:#{question_id}", {id: question_id, keypad: keypad, value: value}.to_json
+    redis.lpush key(question_id), {id: question_id, keypad: keypad, value: value}.to_json
   end
   
   def self.possible_responses(question_id)
-    possible_responses = $redis_question_pr_uri_connection.lrange "possible_response:#{question_id}", 0, -1
+    possible_responses = redis.lrange key(question_id), 0, -1
     unless possible_responses.blank?
       possible_responses.collect {|x| JSON.parse(x)}      
     end
   end
   
   def self.clear_list(question_id)
-    $redis_question_pr_uri_connection.del("possible_response:#{question_id}")
+    redis.del(key(question_id))
   end
-  
+
+  def self.cached?(question_id)
+    redis.llen(key(question_id)) > 0
+  end
 end
