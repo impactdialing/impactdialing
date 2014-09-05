4b5bd213e1fafa03c337d875261fe01193283285
Jeremiah Heller <jeremiah@impactdialing.com>
2014-09-04 20:58:31 -0700

    requiring deleted files is Not Good
    #changelog:fixup remove missed ref to heroku_resque_auto_scale
    
    [fixes #78171182]

--- jobs/reset_password_email_job.rb
+++ jobs/reset_password_email_job.rb
@@ -1,8 +1,6 @@
-require Rails.root.join("jobs/heroku_resque_auto_scale")
 class ResetPasswordEmailJob
   @queue = :background_worker
-
-
+  
   def self.perform(user_id)
     user = User.find(user_id)
     user_mailer = UserMailer.new
