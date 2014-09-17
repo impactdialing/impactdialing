f5aff59181c5b1432bdac734fbc78fe302080319
Jeremiah Heller <jeremiah@impactdialing.com>
2014-09-17 14:59:24 -0700

    workaround Out of Range error
    DataCentre-specific code occasionally threw Out of Range error
    when loading subset of voters to dial. Workaround by simply
    dialing all voters to dial at once, which bypasses DataCentre code-path,
    which is fine since we only use one DataCentre (twilio) and no longer
    interact w/ the other DataCentre (voxeo).
    
    [delivers #76752150]

--- jobs/dial.rb
+++ jobs/dial.rb
@@ -4,30 +4,37 @@ require "em-synchrony/em-http"
 
 class Dial
   
+  # def self.bad_perform(campaign_id, voter_ids)
+  #   campaign = Campaign.find(campaign_id)     
+  #   voters_to_dial = Voter.where("id in (?)" ,voter_ids)
+  #   data_centres = RedisDataCentre.data_centres_array(campaign_id)
+  #   dcs_hash = {}
+  #   total = 0
+  #   data_centres.each do |dc|
+  #     on_hold = RedisOnHoldCaller.length(campaign_id, dc)
+  #     dcs_hash[dc] = on_hold  
+  #     total = total + on_hold
+  #   end
+  #   puts "Campaign: #{campaign_id}, Total: #{total}, #{dcs_hash.inspect}"
+  #   start = 0
+  #   dcs_hash.each_pair do |key, value|
+  #     number_to_dial = (voters_to_dial.size.to_f / total.to_f) * value
+  #     puts "Campaign: #{campaign_id}, Numbers to Dial: #{number_to_dial}"
+  #     em_dial(voters_to_dial[start, number_to_dial], key)
+  #     start = number_to_dial
+  #   end
+  # end
+
   def self.perform(campaign_id, voter_ids)
-    campaign = Campaign.find(campaign_id)     
+    campaign       = Campaign.find(campaign_id)     
     voters_to_dial = Voter.where("id in (?)" ,voter_ids)
-    data_centres = RedisDataCentre.data_centres_array(campaign_id)
-    dcs_hash = {}
-    total = 0
-    data_centres.each do |dc|
-      on_hold = RedisOnHoldCaller.length(campaign_id, dc)
-      dcs_hash[dc] = on_hold  
-      total = total + on_hold
-    end
-    puts "Campaign: #{campaign_id}, Total: #{total}, #{dcs_hash.inspect}"
-    start = 0
-    dcs_hash.each_pair do |key, value|
-      number_to_dial = (voters_to_dial.size.to_f / total.to_f) * value
-      puts "Campaign: #{campaign_id}, Numbers to Dial: #{number_to_dial}"
-      em_dial(voters_to_dial[start, number_to_dial], key)
-      start = number_to_dial
-    end
     
+    puts "Campaign: #{campaign_id}, Numbers to Dial: #{voter_ids.size}"
     
+    em_dial(voters_to_dial)
   end
   
-  def self.em_dial(voters_to_dial, key)
+  def self.em_dial(voters_to_dial, key='twilio')
     begin
       EM.synchrony do
         concurrency = 10        
