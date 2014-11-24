1cfd0acb2fee2aa165dc2fd44ad185a5402b8109
Jeremiah Heller <jeremiah@impactdialing.com>
2014-11-24 12:44:06 -0800

    fix-up dashboard ‘remaining’ count
    [#81337986]

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -308,7 +308,7 @@ public
   end
 
   def leads_available_now
-    sanitize_dials(all_voters.enabled.avialable_to_be_retried(recycle_rate).count  + all_voters.by_status(CallAttempt::Status::ABANDONED).count)
+    sanitize_dials(all_voters.live.avialable_to_be_retried(recycle_rate).count  + all_voters.live.by_status(CallAttempt::Status::ABANDONED).count)
   end
 
   def sanitize_dials(dial_count)
@@ -346,7 +346,7 @@ public
     end
 
     ringing_lines = call_attempts.with_status(CallAttempt::Status::RINGING).between(15.seconds.ago, Time.now).size
-    num_remaining = all_voters.by_status('not called').enabled.count
+    num_remaining = all_voters.by_status('not called').live.count
     num_available = leads_available_now + num_remaining
     {callers_logged_in: callers_logged_in, on_call: status_count[1], wrap_up: status_count[2], on_hold: status_count[0], ringing_lines: ringing_lines, available: num_available  }
   end
