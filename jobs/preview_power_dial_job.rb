7b56e124bac87ac467d27708fa68920fe5d3eda8
Jeremiah Heller <jeremiah@impactdialing.com>
2014-12-11 15:11:16 -0800

    update PreviewPowerDialJob to handle phone numbers instead of voters

--- jobs/preview_power_dial_job.rb
+++ jobs/preview_power_dial_job.rb
@@ -1,5 +1,5 @@
 ##
-# Attempt to dial a +Voter+ identified by given `voter_id` and connect them to the
+# Attempt to dial a +Household+ identified by given `phone` & `caller_session_id` and connect them to the
 # +Caller+ of the +CallerSession+ identified by given `caller_session_id`.
 # This job is queued from +Caller#calling_voter_preview_power+,
 # +PhonesOnlyCallerSession#conference_started_phones_only_power+
@@ -23,34 +23,8 @@ class PreviewPowerDialJob
   sidekiq_options :retry => false
   sidekiq_options :failures => true
 
-  def perform(caller_session_id, voter_id)
-    caller_session = CallerSession.find_by_id_cached(caller_session_id)
-
-    voter = Voter.find(voter_id)
-
-    campaign = voter.campaign
-
-    Rails.logger.error "JID-#{jid} RecycleRate PreviewPowerDialJob CallerSession[#{caller_session_id}] Voter[#{voter_id}] #{campaign.try(:type) || 'Campaign'}[#{campaign.try(:id)}]"
-
-    if campaign.present? && campaign.within_recycle_rate?(voter)
-      msg = "JID-#{jid} PreviewPowerDialJob - RecycleRateError:" +
-            " CallerSession[#{caller_session.id}]" +
-            " Voter[#{voter.id}] Campaign[#{campaign.id}]"
-      Rails.logger.error msg
-    end
-
-    begin
-      Twillio.dial(voter, caller_session)
-      # if campaign.within_recycle_rate?(voter)
-      #   # Twillio.dial(voter, caller_session)
-      # else
-      #   # tell the caller what happened
-      #   # redirect the caller to trigger an update to the lead info
-      #   # Providers::Phone::Call.redirect_for(caller_session, :voter_banned_by_recycle_rate)
-      # end
-    rescue ActiveRecord::StaleObjectError => e
-      Providers::Phone::Call.redirect_for(caller_session)
-      # rescue from any exception > tell the caller what happened > redirect
-    end
+  def perform(caller_session_id, phone)
+    caller_session = CallerSession.find_by_id_cached(caller_session_id, includes: [:campaign])
+    Twillio.dial(phone, caller_session)
   end
 end
--- spec/jobs/preview_power_dial_job_spec.rb
+++ spec/jobs/preview_power_dial_job_spec.rb
@@ -2,13 +2,15 @@ require "spec_helper"
 
 describe PreviewPowerDialJob do  
   it "should dial voter" do
-    account = create(:account)
-    caller = create(:caller, account: account)
+    account        = create(:account)
+    caller         = create(:caller, account: account)
     caller_session = create(:caller_session, caller: caller)
+    phone          = Forgery(:address).phone
+
     expect(CallerSession).to receive(:find_by_id_cached).and_return(caller_session)
-    voter = create(:voter)
-    expect(Twillio).to receive(:dial)
     
-    PreviewPowerDialJob.new.perform(caller_session.id, voter.id)
+    expect(Twillio).to receive(:dial).with(phone, caller_session)
+    
+    PreviewPowerDialJob.new.perform(caller_session.id, phone)
   end
 end
