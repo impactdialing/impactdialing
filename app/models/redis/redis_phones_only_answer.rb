17f9ddfc8a3b66dcf03405d69f5fa07f064e05bf
Jeremiah Heller <jeremiah@impactdialing.com>
2015-01-06 11:22:58 -0800

    refactor redis key names

--- app/models/redis/redis_phones_only_answer.rb
+++ app/models/redis/redis_phones_only_answer.rb
@@ -1,10 +1,17 @@
 class RedisPhonesOnlyAnswer
-  
+  def self.keys
+    {
+      master: 'phones_only_answer_list',
+      pending: 'phones_only_answer_list:pending',
+      partial: 'phones_only_answer_list:partial'
+    }
+  end
+
   def self.push_to_list(voter_id, caller_session_id, digit, question_id)
-    $redis_phones_ans_uri_connection.lpush "phones_only_answer_list", {voter_id: voter_id, caller_session_id: caller_session_id, question_id: question_id, digit: digit}.to_json
+    $redis_phones_ans_uri_connection.lpush keys[:master], {voter_id: voter_id, caller_session_id: caller_session_id, question_id: question_id, digit: digit}.to_json
   end
   
   def self.phones_only_answers_list
-    $redis_phones_ans_uri_connection.lrange "phones_only_answer_list", 0, -1       
+    $redis_phones_ans_uri_connection.lrange keys[:master], 0, -1       
   end
 end
