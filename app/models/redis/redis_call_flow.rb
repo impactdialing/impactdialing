1540ac7b5ddce21b53959a64b805bd3f00a9b405
Jeremiah Heller <jeremiah@impactdialing.com>
2015-01-09 17:58:35 -0800

    fix time zone jitters (use UTC when saving times to redis)

--- app/models/redis/redis_call_flow.rb
+++ app/models/redis/redis_call_flow.rb
@@ -1,20 +1,24 @@
 require 'redis/list'
 class RedisCallFlow
   include Redis::Objects
+
+  def self.current_time
+    Time.now.utc.to_s
+  end
   
   # done
   def self.push_to_not_answered_call_list(call_id, call_status)   
-    $redis_call_end_connection.lpush "not_answered_call_list", {id: call_id, call_status: call_status, current_time: Time.now.to_s}.to_json
+    $redis_call_end_connection.lpush "not_answered_call_list", {id: call_id, call_status: call_status, current_time: current_time}.to_json
   end
   
   # done
   def self.push_to_abandoned_call_list(call_id)    
-    $redis_call_flow_connection.lpush "abandoned_call_list", {id: call_id, current_time: Time.now.to_s}.to_json
+    $redis_call_flow_connection.lpush "abandoned_call_list", {id: call_id, current_time: current_time}.to_json
   end
   
   # done
   def self.push_to_processing_by_machine_call_hash(call_id)        
-    processing_by_machine_call_hash.store(call_id, Time.now.to_s) 
+    processing_by_machine_call_hash.store(call_id, current_time) 
   end
 
   def self.record_message_drop_info(call_id, recording_id, drop_type)
@@ -28,15 +32,15 @@ class RedisCallFlow
   
   # done
   def self.push_to_end_by_machine_call_list(call_id)    
-    $redis_call_flow_connection.lpush "end_answered_by_machine_call_list", {id: call_id, current_time: Time.now.to_s}.to_json
+    $redis_call_flow_connection.lpush "end_answered_by_machine_call_list", {id: call_id, current_time: current_time}.to_json
   end
     
   def self.push_to_disconnected_call_list(call_id, recording_duration, recording_url, caller_id)    
-    $redis_call_flow_connection.lpush "disconnected_call_list", {id: call_id, recording_duration: recording_duration, recording_url: recording_url, caller_id: caller_id, current_time: Time.now.to_s}.to_json
+    $redis_call_flow_connection.lpush "disconnected_call_list", {id: call_id, recording_duration: recording_duration, recording_url: recording_url, caller_id: caller_id, current_time: current_time}.to_json
   end
   
   def self.push_to_wrapped_up_call_list(call_id, caller_type, voter_id)
-    $redis_call_flow_connection.lpush "wrapped_up_call_list", {id: call_id, caller_type: caller_type, voter_id: voter_id, current_time: Time.now.to_s}.to_json
+    $redis_call_flow_connection.lpush "wrapped_up_call_list", {id: call_id, caller_type: caller_type, voter_id: voter_id, current_time: current_time}.to_json
   end
   
   def self.not_answered_call_list
--- spec/models/redis/redis_call_flow_spec.rb
+++ spec/models/redis/redis_call_flow_spec.rb
@@ -7,36 +7,36 @@ describe RedisCallFlow, :type => :model do
   it "should add the call params to the not answered list" do
     RedisCallFlow.push_to_not_answered_call_list(1234, "busy")
     expect(RedisCallFlow.not_answered_call_list.length).to eq(1)
-    expect(RedisCallFlow.not_answered_call_list.pop).to eq("{\"id\":1234,\"call_status\":\"busy\",\"current_time\":\"#{Time.now.to_s}\"}")
+    expect(RedisCallFlow.not_answered_call_list.pop).to eq("{\"id\":1234,\"call_status\":\"busy\",\"current_time\":\"#{Time.now.utc.to_s}\"}")
   end
   
   it "should add the call params to the abandoned list" do
     RedisCallFlow.push_to_abandoned_call_list(1234)
     expect(RedisCallFlow.abandoned_call_list.length).to eq(1)
-    expect(RedisCallFlow.abandoned_call_list.pop).to eq("{\"id\":1234,\"current_time\":\"#{Time.now.to_s}\"}")
+    expect(RedisCallFlow.abandoned_call_list.pop).to eq("{\"id\":1234,\"current_time\":\"#{Time.now.utc.to_s}\"}")
   end
 
 
   it "should add the call params to the processing_by_machine list" do
     RedisCallFlow.push_to_processing_by_machine_call_hash(1234)
-    expect(RedisCallFlow.processing_by_machine_call_hash['1234']).to eq(Time.now.to_s)
+    expect(RedisCallFlow.processing_by_machine_call_hash['1234']).to eq(Time.now.utc.to_s)
   end
   
   it "should add the call params to the end_by_machine list" do
     RedisCallFlow.push_to_end_by_machine_call_list(1234)
     expect(RedisCallFlow.end_answered_by_machine_call_list.length).to eq(1)
-    expect(RedisCallFlow.end_answered_by_machine_call_list.pop).to eq("{\"id\":1234,\"current_time\":\"#{Time.now.to_s}\"}")
+    expect(RedisCallFlow.end_answered_by_machine_call_list.pop).to eq("{\"id\":1234,\"current_time\":\"#{Time.now.utc.to_s}\"}")
   end
   
   it "should add the call params to the answered call list" do
     RedisCallFlow.push_to_disconnected_call_list(1234, 15, "url", 1)
     expect(RedisCallFlow.disconnected_call_list.length).to eq(1)
-    expect(RedisCallFlow.disconnected_call_list.pop).to eq("{\"id\":1234,\"recording_duration\":15,\"recording_url\":\"url\",\"caller_id\":1,\"current_time\":\"#{Time.now.to_s}\"}")
+    expect(RedisCallFlow.disconnected_call_list.pop).to eq("{\"id\":1234,\"recording_duration\":15,\"recording_url\":\"url\",\"caller_id\":1,\"current_time\":\"#{Time.now.utc.to_s}\"}")
   end
   
   it "should add the call params to the wrapup call list" do
     RedisCallFlow.push_to_wrapped_up_call_list(1234, CallerSession::CallerType::TWILIO_CLIENT, 42)
     expect(RedisCallFlow.wrapped_up_call_list.length).to eq(1)
-    expect(RedisCallFlow.wrapped_up_call_list.pop).to eq("{\"id\":1234,\"caller_type\":\"Twilio client\",\"voter_id\":42,\"current_time\":\"#{Time.now.to_s}\"}")
+    expect(RedisCallFlow.wrapped_up_call_list.pop).to eq("{\"id\":1234,\"caller_type\":\"Twilio client\",\"voter_id\":42,\"current_time\":\"#{Time.now.utc.to_s}\"}")
   end  
 end
