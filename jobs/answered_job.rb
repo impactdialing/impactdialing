98aeab19c5702c46a4aa63823443e6b621265079
Jeremiah Heller <jeremiah@impactdialing.com>
2015-01-13 23:57:14 -0800

    stop rescuing Exception when persisting web submitted answer data

--- jobs/answered_job.rb
+++ jobs/answered_job.rb
@@ -31,33 +31,24 @@ class AnsweredJob
 
   def self.perform
     ActiveRecord::Base.verify_active_connections!
-    success_count = 0
-    not_found = 0
+
     CallAttempt.results_not_processed.where('call_id IS NOT NULL AND voter_id IS NOT NULL').reorder('call_attempts.id DESC').includes(:call, :voter).find_each do |call_attempt|
-      begin
-        call = call_attempt.call
-        answers_data = RedisCall.questions_and_notes(call.id)
-        if answers_data && (answers_data["questions"] || answers_data["notes"])
-          questions = answers_data["questions"]
-          notes     = answers_data["notes"]
+      call = call_attempt.call
+      answers_data = RedisCall.questions_and_notes(call.id)
+      if answers_data && (answers_data["questions"] || answers_data["notes"])
+        questions = answers_data["questions"]
+        notes     = answers_data["notes"]
 
-          call_attempt.voter.persist_answers(questions, call_attempt)
-          call_attempt.voter.persist_notes(notes, call_attempt)
-          call_attempt.update_attributes(voter_response_processed: true)
-          call_attempt.voter.update_attribute(:result_date, Time.now)
+        call_attempt.voter.persist_answers(questions, call_attempt)
+        call_attempt.voter.persist_notes(notes, call_attempt)
+        call_attempt.update_attributes(voter_response_processed: true)
+        call_attempt.voter.update_attribute(:result_date, Time.now)
 
-          RedisCall.delete(call.id)
-          success_count += 1
-        else
-          if call_attempt.created_at < 10.minutes.ago
-            call_attempt.update_column(:voter_response_processed, true)
-          else
-            not_found += 1
-          end
+        RedisCall.delete(call.id)
+      else
+        if call_attempt.created_at < 10.minutes.ago
+          call_attempt.update_column(:voter_response_processed, true)
         end
-      rescue Exception => e
-        Rails.logger.error("#{self} Exception: #{e.class}: #{e.message}")
-        Rails.logger.error("#{self} Exception Backtrace: #{e.backtrace}")
       end
     end
   end
