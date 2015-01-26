4970dc70e7fd951e89fd237f5379908db4652bdf
Jeremiah Heller <jeremiah@impactdialing.com>
2015-01-25 21:04:14 -0800

    output houseless & phenols Voter counts

--- jobs/householding/seed_dial_queue.rb
+++ jobs/householding/seed_dial_queue.rb
@@ -12,8 +12,8 @@ module Householding
       if voter_list.enabled?
         p "Seeding DialQueue Campaign[#{campaign_id}] EnabledList[#{voter_list_id}] Voter[#{lower_voter_id}..#{upper_voter_id}]"
         voters = voter_list.voters.where(id: (lower_voter_id..upper_voter_id)).includes({campaign: :account, custom_voter_field_values: :custom_voter_field}, :voter_list, :household)
-        p "Phoneless Voter count: #{voters.where('phone IS NULL or phone = ""').count}"
-        campaign.dial_queue.cache_all(voters)
+        p "Phoneless/Houseless Voter count: #{voters.where('phone IS NULL or phone = ""').count} / #{voters.where('household_id IS NULL').count}"
+        campaign.dial_queue.cache_all(voters.where('phone IS NOT NULL AND household_id IS NOT NULL'))
       else
         p "Skip seeding DialQueue Campaign[#{campaign_id}] DisabledList[#{voter_list_id}] Voter[#{lower_voter_id}..#{upper_voter_id}]"
       end
