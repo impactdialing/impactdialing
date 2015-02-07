7241abc078bba33cd60748ba8811427733b0ead3
Jeremiah Heller <jeremiah@impactdialing.com>
2015-02-06 18:09:19 -0800

    fix-up 0533e25

--- app/models/archival/jobs/campaign.rb
+++ app/models/archival/jobs/campaign.rb
@@ -1,13 +1,13 @@
 module Archival::Jobs
   class Campaign
     @queue = :background_worker
-    
+
     def self.add_to_queue(campaign_id)
       Resque.enqueue(self, campaign_id)
     end
 
     def self.perform(campaign_id)
-      campaign = Campaign.find campaign_id
+      campaign = ::Campaign.find campaign_id
       campaign.callers.update_all(campaign_id: nil)
     end
   end
