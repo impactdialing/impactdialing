9b58acda33f138e16be4dd44284f1638ea17a4bd
Jeremiah Heller <jeremiah@impactdialing.com>
2015-02-06 20:16:59 -0800

    job to archive campaigns that have had no activity in the last 30 days
    campaigns are considered to have no activity when:
    - updated_at timestamp is older than 30 days
    - AND last call attempt is older than 30 days

---
+++ app/models/archival/jobs/campaign_sweeper.rb
@@ -0,0 +1,25 @@
+module Archival::Jobs
+  class CampaignSweeper
+    extend LibratoResque
+    @queue = :background_worker
+
+    def self.perform
+      time_threshold               = 30.days.ago
+      recently_called_campaign_ids = CallAttempt.where('created_at > ?', time_threshold).select('DISTINCT(campaign_id)').pluck(:campaign_id)
+
+      Campaign.where(active: true).
+        where('updated_at < ?', time_threshold).
+        where('id <> ?', recently_called_campaign_ids).
+        find_in_batches(batch_size: 500) do |inactive_campaigns|
+          inactive_campaigns.each do |campaign|
+            campaign.active = false
+
+            unless campaign.save
+              extra = "ac-#{campaign.account_id}.ca-#{campaign.id}"
+              ImpactPlatform::Metrics.count('job_status.invalid_record', 1, source(extra))
+            end
+          end
+        end
+    end
+  end
+end
---
+++ spec/models/archival/jobs/campaign_sweeper_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe 'Archival::Jobs::CampaignSweeper' do
+  subject{ Archival::Jobs::CampaignSweeper }
+  let(:account){ create(:account) }
+  let(:script){ create(:script, account: account) }
+  let(:inactive_campaign) do
+    create(:bare_preview, {
+      account: account,
+      script: script,
+      active: true,
+      created_at: 60.days.ago,
+      updated_at: 31.days.ago
+    })
+  end
+  let(:active_campaign) do
+    create(:bare_predictive, {account: account, script: script, active: true})
+  end
+  let!(:caller1){ create(:caller, account: account, campaign: inactive_campaign) }
+  let!(:caller2){ create(:caller, account: account, campaign: active_campaign) }
+  before do
+    Redis.new.flushall # some tests are not cleaning up after themselves...
+    create(:bare_call_attempt, {campaign: inactive_campaign, created_at: 30.days.ago - 1.hour})
+    create(:bare_call_attempt, {campaign: active_campaign, created_at: 30.days.ago + 1.hour})
+    inactive_campaign.dial_queue.cache(create(:voter, campaign: inactive_campaign))
+    active_campaign.dial_queue.cache(create(:voter, campaign: active_campaign))
+  end
+
+  after do
+    Redis.new.flushall
+  end
+
+  it 'archives campaigns where the last call attempt is older than 30 days' do
+    subject.perform
+    expect(Campaign.archived.count).to eq 1
+    expect(Campaign.archived.first).to eq inactive_campaign
+    expect(resque_jobs(:background_worker)).to include({
+      'class' => 'CallFlow::DialQueue::Jobs::Purge',
+      'args' => [inactive_campaign.id]
+    })
+    expect(resque_jobs(:background_worker)).to include({
+      'class' => 'Archival::Jobs::CampaignArchived',
+      'args' => [inactive_campaign.id]
+    })
+  end
+
+  it 'does nothing with campaigns where the last call attempt is younger than 30 days' do
+    subject.perform
+    expect(Campaign.active.count).to eq 1
+    expect(Campaign.active.first).to eq active_campaign
+    expect(resque_jobs(:background_worker)).to_not include({
+      'class' => 'CallFlow::DialQueue::Jobs::Purge',
+      'args' => [active_campaign.id]
+    })
+    expect(resque_jobs(:background_worker)).to_not include({
+      'class' => 'Archival::Jobs::CampaignArchived',
+      'args' => [active_campaign.id]
+    })
+  end
+
+  it 'does nothing with campaigns that were updated in the last 30 days' do
+    inactive_campaign.updated_at = 29.days.ago
+    inactive_campaign.save!
+    subject.perform
+
+    expect(Campaign.active.count).to eq 2
+    expect(Campaign.active.all).to include inactive_campaign
+    expect(resque_jobs(:background_worker)).to_not include({
+      'class' => 'CallFlow::DialQueue::Jobs::Purge',
+      'args' => [inactive_campaign.id]
+    })
+    expect(resque_jobs(:background_worker)).to_not include({
+      'class' => 'Archival::Jobs::CampaignArchived',
+      'args' => [inactive_campaign.id]
+    })
+  end
+end
