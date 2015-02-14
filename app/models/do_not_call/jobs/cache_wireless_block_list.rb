26b37710c9a4ab7a78b059086823059512fba51e
Jeremiah Heller <jeremiah@impactdialing.com>
2015-02-13 17:55:16 -0800

    CacheWirelessBlockLists: add job cache wireless block list to s3
    [#83776230]

---
+++ app/models/do_not_call/jobs/cache_wireless_block_list.rb
@@ -0,0 +1,49 @@
+# https://www.qscdl.com/download/nalennd_block.zip
+
+require 'tempfile'
+require 'uri'
+require 'zip'
+
+module DoNotCall::Jobs
+  class CacheWirelessBlockList
+    @queue = :upload_download
+
+    def self.url
+      ENV['DO_NOT_CALL_WIRELESS_BLOCK_LIST_PROVIDER_URL']
+    end
+
+    def self.perform(s3_root_path)
+      temp_file      = download
+      filename, file = *unzip(temp_file)
+      dest_path      = "#{s3_root_path}/#{filename}"
+      AmazonS3.new.write(dest_path, file)
+    end
+
+    def self.download
+      uri     = URI.parse(url)
+      faraday = Faraday.new("#{uri.scheme}://#{uri.host}")
+      faraday.basic_auth(uri.user, uri.password)
+      response = faraday.get(uri.path)
+      
+      save_temp_file(response.body)
+    end
+
+    def self.save_temp_file(contents)
+      temp_file = Tempfile.new('wireless_block_list', encoding: 'ascii-8bit')
+      temp_file.write(contents)
+      temp_file.close
+      temp_file
+    end
+
+    def self.unzip(temp_file)
+      unzipped_file     = nil
+      unzipped_filename = nil
+      ::Zip::File.open(temp_file.path) do |zip_file|
+        entry             = zip_file.glob('*.csv').first
+        unzipped_filename = entry.name
+        unzipped_file     = entry.get_input_stream.read
+      end
+      return [unzipped_filename, unzipped_file]
+    end
+  end
+end
---
+++ spec/models/do_not_call/jobs/cache_wireless_block_list_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe DoNotCall::Jobs::CacheWirelessBlockList, data_heavy: true do
+  subject{ DoNotCall::Jobs::CacheWirelessBlockList }
+  let(:s3_config){ YAML::load(File.open("#{Rails.root}/config/amazon_s3.yml")) }
+  let(:s3_connection){ AWS::S3.new(access_key_id: s3_config['access_key_id'], secret_access_key: s3_config['secret_access_key']) }
+
+  it 'downloads the nalennd_block.zip file' do
+    VCR.use_cassette('cache wireless block list download file') do
+      file = subject.download
+      expect(file.size).to be > 3_000_000
+    end
+  end
+  it 'unzips the nalennd_block.zip file' do
+    # used cached request from previous test
+    VCR.use_cassette('cache wireless block list download file') do
+      file = subject.download
+      expect(subject.unzip(file).last.size).to be > 20_000_000
+    end
+  end
+  it 'caches the unzipped nalennd_block file to s3' do
+    VCR.use_cassette('cache wireless block list to s3') do
+      filename     = 'nalennd_block.csv'
+      s3_root_path = "_system/do_not_call/test"
+
+      subject.perform(s3_root_path)
+
+      s3_keys = s3_connection.buckets[s3_config['bucket']].objects.with_prefix(s3_root_path).map(&:key)
+      expect(s3_keys).to include "#{s3_root_path}/#{filename}"
+    end
+  end
+end
