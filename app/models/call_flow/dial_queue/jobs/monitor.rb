764223583dfa04ef6b8e0e2ce13afd6da735163e
Jeremiah Heller <jeremiah@impactdialing.com>
2015-03-03 17:31:13 -0800

    monitor size of presented & recycled dial queue sets
    [#89556670]

--- app/models/call_flow/dial_queue/jobs/monitor.rb
+++ app/models/call_flow/dial_queue/jobs/monitor.rb
@@ -5,11 +5,17 @@ module CallFlow::DialQueue::Jobs
     def self.perform
       active_campaigns = Campaign.where("id in (select distinct campaign_id from caller_sessions where on_call = 1)")
       active_campaigns.each do |campaign|
-        ImpactPlatform::Metrics.sample("dialer.dial_queue.numbers.available.count", campaign.dial_queue.available.size, "ac-#{campaign.account_id}.ca-#{campaign.id}.dm-#{campaign.type}")
-        ImpactPlatform::Metrics.sample("dialer.dial_queue.callers.available.count", campaign.caller_sessions.available.count, "ac-#{campaign.account_id}.ca-#{campaign.id}.dm-#{campaign.type}")
-        ImpactPlatform::Metrics.sample("dialer.dial_queue.callers.on_call.count", campaign.caller_sessions.on_call.count, "ac-#{campaign.account_id}.ca-#{campaign.id}.dm-#{campaign.type}")
-        ImpactPlatform::Metrics.sample("dialer.dial_queue.ringing.count", campaign.ringing_count, "ac-#{campaign.account_id}.ca-#{campaign.id}.dm-#{campaign.type}")
-        ImpactPlatform::Metrics.sample("dialer.dial_queue.presented.count", campaign.presented_count, "ac-#{campaign.account_id}.ca-#{campaign.id}.dm-#{campaign.type}")
+        source = "ac-#{campaign.account_id}.ca-#{campaign.id}.dm-#{campaign.type}"
+        prefix = "dialer.dial_queue"
+        ImpactPlatform::Metrics.sample("#{prefix}.numbers.available.count", campaign.dial_queue.available.size, source)
+        ImpactPlatform::Metrics.sample("#{prefix}.numbers.presented.count", campaign.dial_queue.available.all(:presented).size, source)
+        ImpactPlatform::Metrics.sample("#{prefix}.numbers.recycled.count", campaign.dial_queue.recycle_bin.size, source)
+
+        ImpactPlatform::Metrics.sample("#{prefix}.callers.available.count", campaign.caller_sessions.available.count, source)
+        ImpactPlatform::Metrics.sample("#{prefix}.callers.on_call.count", campaign.caller_sessions.on_call.count, source)
+
+        ImpactPlatform::Metrics.sample("#{prefix}.ringing.count", campaign.ringing_count, source)
+        ImpactPlatform::Metrics.sample("#{prefix}.presented.count", campaign.presented_count, source)
       end
     end
   end
