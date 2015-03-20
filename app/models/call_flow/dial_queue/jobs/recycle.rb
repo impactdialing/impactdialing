ac0a3470abe4b7791b5e1bcc4c903c53ed681cb3
Jeremiah Heller <jeremiah@impactdialing.com>
2015-03-20 16:05:45 -0700

    troubleshooting bugsnag
    see if error raised directly from job appears in dashboard

--- app/models/call_flow/dial_queue/jobs/recycle.rb
+++ app/models/call_flow/dial_queue/jobs/recycle.rb
@@ -13,6 +13,8 @@ module CallFlow::DialQueue::Jobs
         puts msg
       }
 
+      raise "Testing Bugsnag notifications"
+
       # 2 weeks is a long time but there are currently no restrictions on Campaign#recycle_rate
       # if the campaign is created via API or the form is hacked then recycle rate could be > 72 hours
       # 2014 (w/ mid-term election) saw total of 1103 different campaigns though so 
