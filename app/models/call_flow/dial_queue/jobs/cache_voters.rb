7fad1f6ca312abeb810a5d532d3a3ecb540bd91f
Jeremiah Heller <jeremiah@impactdialing.com>
2015-04-01 16:05:30 -0700

    requeue CallFlow::DialQueue::Jobs::CacheVoters on ResqueTerm
    [#91650680]

--- app/models/call_flow/dial_queue/jobs/cache_voters.rb
+++ app/models/call_flow/dial_queue/jobs/cache_voters.rb
@@ -8,21 +8,25 @@ module CallFlow::DialQueue::Jobs
     extend LibratoResque
 
     def self.perform(campaign_id, voter_ids, enabled)
-      metrics    = ImpactPlatform::Metrics::JobStatus.started(self.to_s.underscore.gsub('/','_'))
-      voters     = Voter.where(id: voter_ids).includes({
-        campaign: :account,
-        custom_voter_field_values: :custom_voter_field
-      }, :voter_list, :household)
-      campaign   = Campaign.find(campaign_id)
-      dial_queue = CallFlow::DialQueue.new(campaign)
+      begin
+        metrics    = ImpactPlatform::Metrics::JobStatus.started(self.to_s.underscore.gsub('/','_'))
+        voters     = Voter.where(id: voter_ids).includes({
+          campaign: :account,
+          custom_voter_field_values: :custom_voter_field
+        }, :voter_list, :household)
+        campaign   = Campaign.find(campaign_id)
+        dial_queue = CallFlow::DialQueue.new(campaign)
 
-      if enabled.to_i > 0
-        dial_queue.cache_all(voters)
-      else
-        dial_queue.remove_all(voters)
+        if enabled.to_i > 0
+          dial_queue.cache_all(voters)
+        else
+          dial_queue.remove_all(voters)
+        end
+
+        metrics.completed
+      rescue Resque::TermException => e
+        Resque.enqueue(self, campaign_id, voter_ids, enabled)
       end
-
-      metrics.completed
     end
   end
 end
