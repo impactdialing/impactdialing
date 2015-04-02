97e2ceeaf1d843af1359b3aecb5202515d0f67f9
Jeremiah Heller <jeremiah@impactdialing.com>
2015-04-02 16:15:35 -0700

    configure hirefire-resource macros for resque queues

---
+++ config/initializers/hirefire.rb
@@ -0,0 +1,12 @@
+HireFire::Resource.configure do |config|
+  auto_scaling_queues = [
+    :reports, :general, :dial_queue, :billing,
+    :dialer_worker, :simulator_worker
+  ]
+  
+  auto_scaling_queues.each do |queue_name|
+    config.dyno(queue_name) do
+      HireFire::Macro::Resque.queue(queue_name)
+    end
+  end
+end
