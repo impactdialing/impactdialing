4c55e496979cdd760038418475c2bdf7f76d55f7
Alex Kaufman <alexkaufman06@gmail.com>
2015-04-27 16:54:59 -0700

    Update specs and make them available to callers

--- app/helpers/campaign_helper.rb
+++ app/helpers/campaign_helper.rb
@@ -36,12 +36,11 @@ module CampaignHelper
   def missing_data_text(collection, collection_dependency, options, &block)
     add_dependency_msg = "In order to add a new " + options[:collection_type] + ", you must first "
     link = link_to("add a new " + options[:dependency_type], send("new_client_" + options[:dependency_type] + "_path"))
-    link_path = "new_client_" + options[:dependency_type] + "_path"
     no_collection_msg = "No " + options[:collection_type] + " entered."
 
-    if collection.empty?
+    if collection.zero?
       rendered_message = (content_tag(:p, no_collection_msg))
-      if collection_dependency.empty?
+      if collection_dependency.zero?
         rendered_message = content_tag(:div, class: ["callout", "alert", "clearfix"]) do
           (content_tag(:p, (add_dependency_msg + link).html_safe))
         end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+shared_examples 'collection and dependency issue' do
+  context 'when the collection is empty' do
+    context 'and when the collection dependency is empty' do
+      it 'displays the alert message to add the dependency' do
+        expect(helper.missing_data_text(collection, dependency, options)).to include 'In order to add a new'
+        expect(helper.missing_data_text(collection, dependency, options)).to_not include 'No #collection# entered'
+      end
+    end
+
+    context 'when the collection dependency is not empty' do
+      let(:collection) { 0 }
+      let(:dependency) { 1 }
+
+      it 'displays "no #collection# entered"' do
+        expect(helper.missing_data_text(collection, dependency, options)).to include 'entered'
+        expect(helper.missing_data_text(collection, dependency, options)).to_not include 'In order to add a new'
+      end
+    end
+  end
+
+  context 'when the collection is not empty' do
+    let(:collection) { 1 }
+    let(:dependency) { 1 }
+    let(:block) { "I AM BLOCK OF CODE" }
+
+    it 'displays neither "no #collection# entered" nor the alert message' do
+      # use yield_matcher to yield just the block
+      expect(helper.missing_data_text(collection, dependency, options) do block end).to_not include 'In order to add a new'
+      expect(helper.missing_data_text(collection, dependency, options) do block end).to_not include 'entered'
+    end
+  end
+end
+
+describe ApplicationHelper do
+  describe '#missing_data_text' do
+    let(:collection) { 0 }
+    let(:dependency) { 0 }
+
+    describe 'when collection is campaigns & dependency is scripts' do
+      let(:options) {{ collection_type: 'campaign', dependency_type: 'script' }}
+
+      it_behaves_like 'collection and dependency issue'
+    end
+
+    describe 'when collection is callers & dependency is campaigns' do
+      let(:options) {{ collection_type: 'caller', dependency_type: 'campaign' }}
+
+      it_behaves_like 'collection and dependency issue'
+    end
+  end
+end
