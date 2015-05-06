ab6e2393d850f9d2d77ca825f9ef5b6ef2a76760
Frank <fmeza07@gmail.com>
2015-05-06 14:17:09 -0700

    Refactor user contexts

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -3,73 +3,74 @@ require 'application_policy'
 
 
 shared_examples 'admin and supervisor authorizations' do
-  it 'allows admin access to index page' do
-    expect(policy_admin.index?).to eq true
-  end
+  context 'user is admin' do
+    let(:admin) { build(:user) }
 
-  it 'returns false if the user is a supervisor' do
-    expect(policy_supervisor.index?).to eq false
-  end
+    it 'allows access to index page' do
+      expect(policy_admin.index?).to eq true
+    end
 
-  it 'returns true if the user is an administrator' do
-    expect(policy_admin.create?).to eq true
-  end
+    it 'allows access to #create' do
+      expect(policy_admin.create?).to eq true
+    end
 
-  it 'returns false if the user is a supervisor trying to create' do
-    expect(policy_supervisor.create?).to eq false
-  end
+    it 'allows access to #new' do
+      expect(policy_admin.new?).to eq true
+    end
 
-  it 'returns true if the user is an administrator' do
-    expect(policy_admin.show?).to eq true
-  end
+    it 'allows access to show page' do
+      expect(policy_admin.show?).to eq true
+    end
 
-  it 'returns false if the user is a supervisor' do
-    expect(policy_supervisor.show?).to eq false
-  end
+    it 'allows access to #edit' do
+      expect(policy_admin.edit?).to eq true
+    end
 
-  it 'returns true if the user is an administrator' do
-    expect(policy_admin.new?).to eq true
-  end
+    it 'allows access to #update' do
+      expect(policy_admin.update?).to eq true
+    end
 
-  it 'returns false if the user is a supervisor' do
-    expect(policy_supervisor.new?).to eq false
+    it 'allows access to #destroy' do
+      expect(policy_admin.destroy?).to eq true
+    end
   end
+  
+  context 'user is supervisor' do
+    let(:supervisor) { build(:user, {role: 'supervisor'}) }
 
-  it 'returns true if the user is an administrator' do
-    expect(policy_admin.edit?).to eq true
+    it 'disallows access to index page' do
+      expect(policy_supervisor.index?).to eq false
+    end
+    it 'disallows access to #create' do
+      expect(policy_supervisor.create?).to eq false
+    end
+
+    it 'disallows access to #show' do
+      expect(policy_supervisor.show?).to eq false
+    end
+
+    it 'disallows access to #new' do
+      expect(policy_supervisor.new?).to eq false
+    end
+
+    it 'disallows access to #edit' do
+      expect(policy_supervisor.edit?).to eq false
+    end
+
+    it 'disallows access to #update' do
+      expect(policy_supervisor.update?).to eq false
+    end
+
+    it 'disallows access to #destroy' do
+      expect(policy_supervisor.destroy?).to eq false
+    end
   end
-
-  it 'returns false if the user is a supervisor' do
-    expect(policy_supervisor.edit?).to eq false
-  end
-
-  it 'returns true if the user is an administrator' do
-    expect(policy_admin.update?).to eq true
-  end
-
-  it 'returns false if the user is a supervisor' do
-    expect(policy_supervisor.update?).to eq false
-  end
-
-  it 'returns true if the user is an administrator' do
-    expect(policy_admin.destroy?).to eq true
-  end
-
-  it 'returns false if the user is a supervisor' do
-    expect(policy_supervisor.destroy?).to eq false
-  end
-end
-
-shared_context 'user authorization' do
-  let(:supervisor) { create(:user, {role: 'supervisor'}) }
-  let(:admin) { create(:user) }
 end
 
 describe ApplicationPolicy do
-  include_context 'user authorization'
 
   context 'the script class' do
-    let(:script) { create(:script) }
+    let(:script) { build(:script) }
     let(:policy_admin) { ScriptPolicy.new(admin, script) }
     let(:policy_supervisor) { ScriptPolicy.new(supervisor, script) }
 
@@ -80,7 +81,7 @@ describe ApplicationPolicy do
   end
 
   context 'the campaign class' do
-    let(:campaign) { create(:campaign) }
+    let(:campaign) { build(:campaign) }
     let(:policy_admin) { CampaignPolicy.new(admin, campaign) }
     let(:policy_supervisor) { CampaignPolicy.new(supervisor, campaign) }
 
@@ -90,7 +91,7 @@ describe ApplicationPolicy do
   end
 
   context 'the caller class' do
-    let(:caller) { create(:caller) }
+    let(:caller) { build(:caller) }
     let(:policy_admin) { CallerPolicy.new(admin, caller) }
     let(:policy_supervisor) { CallerPolicy.new(supervisor, caller) }
 
