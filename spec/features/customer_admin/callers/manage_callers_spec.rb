694141b8a5631d08fc485229bb1685c62872fa14
Monty Rohde <nathanarohde@gmail.com>
2015-05-11 10:10:41 -0700

    display an error message when username is blank.

--- spec/features/customer_admin/callers/manage_callers_spec.rb
+++ spec/features/customer_admin/callers/manage_callers_spec.rb
@@ -1,28 +1,45 @@
 require 'rails_helper'
 
-describe 'Caller Management', :type => :feature do
+describe 'add caller', :type => :feature do
   let(:admin){ create(:user) }
   let(:account){ admin.account }
   let(:campaign){ create(:power, account: account, active: true) }
 
-  context 'Basic subscriptions' do
-    before do
-      account.billing_subscription.update_attributes!(plan: 'basic')
-    end
+  before do
+    account.billing_subscription.update_attributes!(plan: 'basic')
+  end
 
-    it 'can create callers and assign them to a campaign' do
-      expect(account.campaigns).to include(campaign)
-      web_login_as(admin)
-      visit '/client/callers'
-      click_on 'Add new caller'
-      fill_in 'Username (no spaces)', with: 'someguy'
-      fill_in 'Password', with: 'secret'
-      select campaign.name, from: 'Campaign'
-      click_on 'Save'
+  it 'creates a valid caller and assigns them to a campaign' do
+    expect(account.campaigns).to include(campaign)
+    web_login_as(admin)
+    visit '/client/callers'
+    click_on 'Add new caller'
+    fill_in 'Username (no spaces)', with: 'someguy'
+    fill_in 'Password', with: 'secret'
+    select campaign.name, from: 'Campaign'
+    click_on 'Save'
 
-      expect(page).to have_content 'Caller saved'
-      expect(page).to have_content 'Displaying 1 Caller'
-      expect(page).to have_content 'someguy'
-    end
+    expect(page).to have_content 'Caller saved'
+    expect(page).to have_content 'Displaying 1 caller'
+    expect(page).to have_content 'someguy'
+  end
+
+  it 'generates correct error when Username field is blank' do
+    web_login_as(admin)
+    visit '/client/callers'
+    click_on 'Add new caller'
+    click_on 'Save'
+
+    expect(page).to have_content "Username can't be blank"
   end
 end
+
+describe 'edit caller', :type => :feature do
+  let(:admin){ create(:user) }
+  let(:account){ admin.account }
+  let(:campaign){ create(:power, account: account, active: true) }
+  let(:caller){ create(:caller, campaign: campaign, account: account) }
+
+  before do
+    account.billing_subscription.update_attributes!(plan: 'basic')
+  end
