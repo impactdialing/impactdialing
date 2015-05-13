384a3722f087e1ec7657170a88c60b841b7caafa
Monty Rohde <nathanarohde@gmail.com>
2015-05-13 14:41:49 -0700

    change tests to work with refactoring of en.yml and csv_validator.

--- spec/features/customer_admin/voter_lists/upload_error_spec.rb
+++ spec/features/customer_admin/voter_lists/upload_error_spec.rb
@@ -18,47 +18,48 @@ describe 'Upload', js: true, type: :feature do
     visit edit_client_campaign_path(campaign)
   end
 
-  context 'malformed csv file' do
-    it 'generates proper error message' do
-      upload_list('malformed.csv')
-      expect(page).to have_content(I18n.t('csv_validator.malformed'))
-    end
-  end
-  context 'voter list with' do
+  context 'voter list' do
 
-    context 'no content' do
-      it 'generates proper error message' do
+    context 'malformed csv file' do
+      it 'should display malformed csv error message' do
+        upload_list('malformed.csv')
+        expect(page).to have_content(I18n.t('csv_validator.malformed'))
+      end
+    end
+
+    context 'csv has no content' do
+      it 'should display no content error message' do
         upload_list('voter_list_empty.csv')
         expect(page).to have_content(I18n.t('csv_validator.missing_header_or_rows'))
       end
     end
 
-    context 'only headers' do
-      it 'generates proper error message' do
+    context 'csv only has headers' do
+      it 'should display missing header or rows error message' do
         upload_list('voter_list_only_headers.csv')
         expect(page).to have_content(I18n.t('csv_validator.missing_header_or_rows'))
       end
     end
 
-    context 'no headers' do
-      it 'generates proper error message' do
+    context 'csv has no headers' do
+      it 'should display missing header or rows error message' do
         upload_list('voters_with_no_header_info.csv')
         expect(page).to have_content(I18n.t('csv_validator.missing_header_or_rows'))
       end
     end
 
     context 'duplicate header names' do
-      it 'generates proper error message' do
+      it 'should display duplicate header error message' do
         upload_list('valid_voters_duplicate_phone_headers.csv')
         expect(page).to have_content(I18n.t('csv_validator.duplicate_headers', :duplicate_headers => "PHONENUMBER"))
       end
     end
 
     context 'only headers and duplicate header names' do
-      it 'generates proper error message' do
-        upload_list('valid_voters_duplicate_phone_headers.csv')
-        expect(page).to have_content(I18n.t('csv_validator.missing_header_or_rows'))
+      it 'should display both duplicate header and missing header or rows error messages' do
+        upload_list('voter_list_only_headers_with_duplicates.csv')
         expect(page).to have_content(I18n.t('csv_validator.duplicate_headers', :duplicate_headers => "PHONENUMBER"))
+        expect(page).to have_content(I18n.t('csv_validator.missing_header_or_rows'))
       end
     end
   end
