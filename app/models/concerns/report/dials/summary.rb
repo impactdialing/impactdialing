f0a6644dcc042095b94226d865b38b7bb4361de0
Jeremiah Heller <jeremiah@impactdialing.com>
2015-06-12 16:58:52 -0700

    fix percentages on dial summary

--- app/models/concerns/report/dials/summary.rb
+++ app/models/concerns/report/dials/summary.rb
@@ -17,47 +17,47 @@ private
       {
         status: 'Available to dial',
         number: :total_households_to_dial,
-        perc: :household_perc
+        perc:   :household_perc
       },
       {
         status: '» Not dialed',
         number: :households_not_dialed_count,
-        perc: :household_perc
+        perc:   :available_household_perc
       },
       {
         status: '» Retrying',
         number: :dialed_and_available_for_retry_count,
-        perc: :household_perc
+        perc:   :available_household_perc
       },
       {
         status: 'Not available to dial',
         number: :total_households_not_to_dial,
-        perc: :household_perc
+        perc:   :household_perc
       },
       {
         status: '» Completed',
         number: :households_completely_dispositioned,
-        perc: :household_perc
+        perc:   :not_available_household_perc
       },
       {
         status: '» Pending retry',
         number: :dialed_and_pending_retry,
-        perc: :household_perc
+        perc:   :not_available_household_perc
       },
       {
         status: '» DNC matches',
         number: :households_blocked_by_dnc,
-        perc: :household_perc
+        perc:   :not_available_household_perc
       },
       {
         status: '» Cell matches',
         number: :households_blocked_by_cell,
-        perc: :household_perc
+        perc:   :not_available_household_perc
       },
       {
         status: 'Total',
         number: :total_households,
-        perc: :household_perc
+        perc:   :household_perc
       }
     ]
   end
@@ -66,12 +66,11 @@ private
     headers = ['Status', 'Number', 'Percent']
   end
 
-  def total_households
-    stats.total_households
-  end
+  def perc(count=0, total)
+    return '0%' if total.zero?
 
-  def total_active_households
-    stats.total_active_households
+    quo = count / total.to_f
+    "#{(quo * 100).floor}%"
   end
 
 public
@@ -80,24 +79,16 @@ public
     @stats    = CallStats::Summary.new(campaign)
   end
 
-  def pending_recycle_rate_message
-    "» Dialed in last #{pluralize(campaign.recycle_rate, 'hour')}"
-  end
-
   def household_perc(n)
-    return '0%' if total_households.zero?
-
-    n ||= 0
-    quo = n / total_households.to_f
-    "#{(quo * 100).floor}%"
+    perc(n, stats.total_households)
   end
 
-  def active_household_perc(n)
-    return '0%' if total_active_households.zero?
+  def available_household_perc(n)
+    perc(n, stats.total_households_to_dial)
+  end
 
-    n ||= 0
-    quo = n / total_active_households.to_f
-    "#{(quo * 100).floor}%"
+  def not_available_household_perc(n)
+    perc(n, stats.total_households_not_to_dial)
   end
 
   def make
