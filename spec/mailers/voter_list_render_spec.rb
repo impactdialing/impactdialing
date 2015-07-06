9a4e758ee5d9a6267e422fa4224415eeb0e4f45c
Jeremiah Heller <jeremiah@impactdialing.com>
2015-07-06 16:24:06 -0700

    fix Of X numbers so X = total_numbers instead of saved_numbers
    because invalid_numbers are now included in list

--- spec/mailers/voter_list_render_spec.rb
+++ spec/mailers/voter_list_render_spec.rb
@@ -1,6 +1,25 @@
 require 'spec_helper'
 
-describe 'VoterListRender', type: :mailer do
+RSpec::Matchers.define :render_completed_email_content do |gather_options|
+  match do |actual|
+    actual =~ /#{stats[:saved_numbers]} of #{stats[:total_numbers]} unique phone numbers imported successfully./ &&
+    actual =~ /#{stats[:saved_leads]} of #{stats[:total_leads]} leads imported successfully./ &&
+    actual =~ /Of #{stats[:total_numbers]} numbers:/ &&
+    actual =~ /#{stats[:new_numbers]} are new numbers/ &&
+    actual =~ /#{stats[:pre_existing_numbers]} numbers had leads added or updated/ &&
+    actual =~ /#{stats[:dnc_numbers]} matched numbers in the DNC/ &&
+    actual =~ /#{stats[:cell_numbers]} were cell phone numbers/ &&
+    actual =~ /Of #{stats[:saved_leads]} leads:/ &&
+    actual =~ /#{stats[:new_leads]} are new leads/ &&
+    actual =~ /#{stats[:updated_leads]} leads were updated/
+  end
+
+  chain :with_nested_say do |say_text|
+    @say_texts = [*say_text]
+  end
+end
+
+describe 'VoterListRender (views/voter_list_mailer/completed)', type: :mailer do
   let(:stats) do
     {
       saved_numbers: 5,
@@ -19,18 +38,15 @@ describe 'VoterListRender', type: :mailer do
   let(:renderer){ VoterListRender.new }
   let(:template){ renderer.completed(:text, stats) }
 
-  it 'renders the text template to a string: views/voter_list_mailer/completed' do
-    t = template.to_s
+  context 'text' do
+    it 'renders the text template to a string' do
+      expect(template.to_s).to render_completed_email_content
+    end
+  end
 
-    expect(t).to match(/#{stats[:saved_numbers]} of #{stats[:total_numbers]} unique phone numbers imported successfully./)
-    expect(t).to match(/#{stats[:saved_leads]} of #{stats[:total_leads]} leads imported successfully./)
-    expect(t).to match(/Of #{stats[:saved_numbers]} numbers:/)
-    expect(t).to match(/- #{stats[:new_numbers]} are new numbers/)
-    expect(t).to match(/- #{stats[:pre_existing_numbers]} numbers had leads added or updated/)
-    expect(t).to match(/- #{stats[:dnc_numbers]} matched numbers in the DNC/)
-    expect(t).to match(/- #{stats[:cell_numbers]} were cell phone numbers/)
-    expect(t).to match(/Of #{stats[:saved_leads]} leads:/)
-    expect(t).to match(/- #{stats[:new_leads]} are new leads/)
-    expect(t).to match(/- #{stats[:updated_leads]} leads were updated/)
+  context 'html' do
+    it 'renders the html template to a string' do
+      expect(template.to_s).to render_completed_email_content
+    end
   end
 end
