5c1a7714aeef18a386b05643ded5d7258e220c10
Jeremiah Heller <jeremiah@impactdialing.com>
2015-07-20 17:09:50 -0700

    update DialQueue Recycle job to process campaigns updated in last 30 days
    support for WIP: when a list is enabled & incomplete
    leads are added to an active & otherwise completed
    household, then add numbers to recycle bin & remove from completed.

--- spec/models/concerns/call_flow/dial_queue/jobs/recycle_spec.rb
+++ spec/models/concerns/call_flow/dial_queue/jobs/recycle_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'CallFlow::DialQueue::Jobs::Recycle' do
+  subject{ CallFlow::DialQueue::Jobs::Recycle }
   let(:account){ create(:account) }
   let(:fake_dial_queue) do
     double('FakeDialQueue', {
@@ -11,23 +12,39 @@ describe 'CallFlow::DialQueue::Jobs::Recycle' do
       dialed: {}
     })
   end
+  let(:campaigns) do
+    create_list(:campaign, 10, account: account)
+  end
+  let(:caller_session_campaigns){ [] }
+
   before do
-    campaigns = create_list(:campaign, 10, account: account)
     5.times do
       campaign = campaigns.sample
+      caller_session_campaigns << campaign
       attrs = {campaign: campaign}
       create(:caller_session, attrs)
-      Timecop.travel(3.weeks.ago) do
+      Timecop.travel(1.weeks.ago) do
         create(:caller_session, attrs)
       end
     end
-
+    3.times do |i|
+      campaign = campaigns.select{|campaign| !caller_session_campaigns.include?(campaign) }[i]
+      campaign.update_column :updated_at, 31.days.ago
+    end
     allow(CallFlow::DialQueue).to receive(:new){ fake_dial_queue }
   end
 
   it 'loads distinct campaign ids for all caller sessions created in the last 2 weeks' do
-    expect(fake_dial_queue).to receive(:recycle!)
+    # 5 campaigns have recent caller sessions
+    expect(fake_dial_queue).to receive(:recycle!).at_least(5).times
+    subject.perform
+  end
 
-    CallFlow::DialQueue::Jobs::Recycle.perform
+  # if recycle is received more than 7 times then duplicate campaigns were loaded
+  it 'loads distinct campaign ids updated in the last 30 days that were not loaded via caller sessions' do
+    # 2 campaigns have been updated in last 30 days
+    expect(fake_dial_queue).to receive(:recycle!).exactly(7).times
+    subject.perform
   end
 end
+
