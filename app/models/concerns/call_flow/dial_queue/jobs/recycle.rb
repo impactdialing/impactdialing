11f55a405f004c7d5c82ae5722655a160a02a0f1
Jeremiah Heller <jeremiah@impactdialing.com>
2015-08-26 12:22:48 -0700

    Revert "rm CallFlow::DialQueue::Jobs::Recycle"
    This reverts commit b34c06156f5388d64302c4abacd302015a3582bc.

---
+++ app/models/concerns/call_flow/dial_queue/jobs/recycle.rb
@@ -0,0 +1,66 @@
+require 'librato_resque'
+
+module CallFlow::DialQueue::Jobs
+  class Recycle
+    @queue = :dial_queue
+    extend LibratoResque
+
+    def self.perform
+      processed = []
+      by_caller_session do |campaign|
+        dial_queue = CallFlow::DialQueue.new(campaign)
+        recycle(dial_queue)
+        processed << campaign.id
+      end
+
+      by_updated(processed) do |campaign|
+        dial_queue = CallFlow::DialQueue.new(campaign)
+        recycle(dial_queue)
+      end
+    end
+
+private
+    def self.recycle(dial_queue)
+      stale = dial_queue.available.presented_and_stale
+
+      stale.each do |scored_phone|
+        score     = scored_phone.last
+        phone     = scored_phone.first
+        household = campaign.households.find_by_phone(phone)
+        
+        if household.present?
+          household.update_attributes({
+            presented_at: Time.at(score)
+          })
+          dial_queue.dialed(household)
+        end
+      end
+
+      dial_queue.recycle!
+    end
+
+    def self.by_caller_session(&block)
+      # 2 weeks is a long time but there are currently no restrictions on Campaign#recycle_rate
+      # if the campaign is created via API or the form is hacked then recycle rate could be > 72 hours
+      # 2014 (w/ mid-term election) saw total of 1103 different campaigns though so 
+      # loading caller sessions from the last 2 weeks won't return many distinct campaigns
+      processed = []
+      CallerSession.where('created_at > ?', 2.weeks.ago).select('DISTINCT caller_sessions.campaign_id, caller_sessions.id').includes(:campaign).find_in_batches do |caller_sessions|
+        caller_sessions.each do |caller_session|
+          next if processed.include? caller_session.campaign.id
+          yield caller_session.campaign
+          processed << caller_session.campaign.id
+        end
+      end
+    end
+
+    def self.by_updated(processed, &block)
+      Campaign.where('id NOT IN (?)', processed).where('updated_at > ?', 30.days.ago).find_in_batches do |campaigns|
+        campaigns.each do |campaign|
+          yield campaign
+        end
+      end
+    end
+  end
+end
+
---
+++ spec/models/concerns/call_flow/dial_queue/jobs/recycle_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe 'CallFlow::DialQueue::Jobs::Recycle' do
+  subject{ CallFlow::DialQueue::Jobs::Recycle }
+  let(:account){ create(:account) }
+  let(:fake_dial_queue) do
+    double('FakeDialQueue', {
+      available: double('FakeAvailable', {
+        presented_and_stale: []
+      }),
+      recycle!: {},
+      dialed: {}
+    })
+  end
+  let(:campaigns) do
+    create_list(:campaign, 10, account: account)
+  end
+  let(:caller_session_campaigns){ [] }
+
+  before do
+    5.times do
+      campaign = campaigns.sample
+      caller_session_campaigns << campaign
+      attrs = {campaign: campaign}
+      create(:caller_session, attrs)
+      Timecop.travel(1.weeks.ago) do
+        create(:caller_session, attrs)
+      end
+    end
+    3.times do |i|
+      campaign = campaigns.select{|campaign| !caller_session_campaigns.include?(campaign) }[i]
+      campaign.update_column :updated_at, 31.days.ago
+    end
+    allow(CallFlow::DialQueue).to receive(:new){ fake_dial_queue }
+  end
+
+  it 'loads distinct campaign ids for all caller sessions created in the last 2 weeks' do
+    # 5 campaigns have recent caller sessions
+    expect(fake_dial_queue).to receive(:recycle!).at_least(5).times
+    subject.perform
+  end
+
+  # if recycle is received more than 7 times then duplicate campaigns were loaded
+  it 'loads distinct campaign ids updated in the last 30 days that were not loaded via caller sessions' do
+    # 2 campaigns have been updated in last 30 days
+    expect(fake_dial_queue).to receive(:recycle!).exactly(7).times
+    subject.perform
+  end
+end
+
