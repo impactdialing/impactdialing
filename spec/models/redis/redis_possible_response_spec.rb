05a99dbbff007246b85bdc613d4040ad35b884c6
Jeremiah Heller <jeremiah@impactdialing.com>
2015-08-31 17:23:09 -0700

    red -> green

--- spec/models/redis/redis_possible_response_spec.rb
+++ spec/models/redis/redis_possible_response_spec.rb
@@ -3,16 +3,22 @@ require 'rails_helper'
 describe RedisPossibleResponse, :type => :model do
   let(:possible_responses) do
     [
-      double('PossibleResponse', {id: 1, keypad: 1, value: 'test'}),
-      double('PossibleResponse', {id: 1, keypad: 2, value: 'test1'}),
-      double('PossibleResponse', {id: 1, keypad: 2, value: 'test2'})
+      double('PossibleResponse', {id: 3, keypad: 1, value: 'test'}),
+      double('PossibleResponse', {id: 4, keypad: 2, value: 'test1'}),
+      double('PossibleResponse', {id: 5, keypad: 3, value: 'test2'})
     ]
   end
   it "should give possible responses for a question" do
     possible_responses.each do |possible_response|
       RedisPossibleResponse.persist_possible_response(1, possible_response)
     end
+
+    expected = [
+      {"id"=>1, "possible_response_id" => 5, "keypad"=>3, "value"=>"test2"},
+      {"id"=>1, "possible_response_id" => 4, "keypad"=>2, "value"=>"test1"},
+      {"id"=>1, "possible_response_id" => 3, "keypad"=>1, "value"=>"test"}
+    ]
     
-    expect(RedisPossibleResponse.possible_responses(1)).to eq([{"id"=>1, "keypad"=>2, "value"=>"test2"}, {"id"=>1, "keypad"=>2, "value"=>"test1"}, {"id"=>1, "keypad"=>1, "value"=>"test"}])
+    expect(RedisPossibleResponse.possible_responses(1)).to eq(expected)
   end
 end
--- spec/requests/twiml/phones_only_start_calling_spec.rb
+++ spec/requests/twiml/phones_only_start_calling_spec.rb
@@ -14,13 +14,13 @@ describe 'Phone-only caller dials TwiML app number' do
 
   context 'Incorrect PIN entered' do
     it 'asks for PIN again' do
-      post identify_caller_path, {Digits: '1234', attempt: 1}
+      post identify_caller_path, {Digits: '1234', attempt: 1, AccountSid: 'account-sid', CallSid: 'call-sid'}
       expect(response.body).to include 'Incorrect pin.'
       expect(response.body).to include 'Please enter your pin and then press star.'
     end
 
     it 'hangs-up after 3 incorrect PIN messages' do
-      post identify_caller_path, {Digits: '1234', attempt: 3}
+      post identify_caller_path, {Digits: '1234', attempt: 3, AccountSid: 'account-sid', CallSid: 'call-sid'}
       expect(response.body).to include 'Incorrect pin.'
       expect(response.body).to include '<Hangup/>'
     end
@@ -29,11 +29,11 @@ describe 'Phone-only caller dials TwiML app number' do
   context 'Correct PIN entered' do
     it 'creates a new phones only caller session' do
       expect{
-        post identify_caller_path, {Digits: caller.pin, attempt: 1}
+        post identify_caller_path, {Digits: caller.pin, attempt: 1, AccountSid: 'account-sid', CallSid: 'call-sid'}
       }.to change{ PhonesOnlyCallerSession.count }.by(1)
     end
     it 'caches phones-only script questions' do
-      post identify_caller_path, {Digits: caller.pin, attempt: 2}
+      post identify_caller_path, {Digits: caller.pin, attempt: 2, AccountSid: 'account-sid', CallSid: 'call-sid'}
       expect(response).to be_success
       expect(resque_jobs(:dial_queue)).to include({
         'class' => 'CachePhonesOnlyScriptQuestions',
@@ -42,7 +42,7 @@ describe 'Phone-only caller dials TwiML app number' do
     end
 
     it 'prompts the caller to have instructions read or start calling' do
-      post identify_caller_path, {Digits: caller.pin, attempt: 3}
+      post identify_caller_path, {Digits: caller.pin, attempt: 3, AccountSid: 'account-sid', CallSid: 'call-sid'}
       expect(response.body).to include I18n.t(:caller_instruction_choice)
     end
   end
