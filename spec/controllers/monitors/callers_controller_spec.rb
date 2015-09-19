933736fe08557cc56b34d9a941ed9097124c96ba
Jeremiah Heller <jeremiah@impactdialing.com>
2015-09-19 15:32:38 -0700

    red -> green

--- spec/controllers/monitors/callers_controller_spec.rb
+++ spec/controllers/monitors/callers_controller_spec.rb
@@ -16,7 +16,12 @@ describe Monitors::CallersController, :type => :controller do
     let(:conference_sid){ 'CFww834eJSKDJFjs328JF92JSDFwe' }
     let(:call_sid){ caller_session.sid }
     let(:conference_name){ caller_session.session_key }
-    let(:moderator){ create(:moderator) }
+    let(:moderator) do
+      create(:moderator, {
+        call_sid: call_sid,
+        caller_session: caller_session
+      })
+    end
     let(:valid_params) do
       {
         session_id: caller_session.id,
@@ -60,16 +65,9 @@ describe Monitors::CallersController, :type => :controller do
 
     context 'a caller is on a call' do
       before do
-        call_attempt = create(:call_attempt, {
-          caller_session: caller_session,
-          status: 'Call in progress'
-        })
-        create(:voter, {
-          call_attempts: [call_attempt],
-          caller_session: caller_session
-        })
+        allow(caller_session).to receive(:connected_to_lead?){ true }
+        allow(CallerSession).to receive(:find).with(valid_params[:session_id].to_s){ caller_session }
       end
-
       it 'renders a message with monitoring type and caller identity info when caller is on a call' do
         put :switch_mode, valid_params
         expect(response.body).to eq "Status: Monitoring in eavesdrop mode on #{caller_session.caller.identity_name}."
