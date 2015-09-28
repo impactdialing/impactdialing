d7593a6856f8307d8bef77f9bfe6b3f7681cd5d4
Jeremiah Heller <jeremiah@impactdialing.com>
2015-09-28 11:15:08 -0700

    organize

--- spec/models/redis/redis_on_hold_caller_spec.rb
+++ spec/models/redis/redis_on_hold_caller_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 describe RedisOnHoldCaller, :type => :model do
+  let(:campaign_id){ 1 }
+  let(:caller_session_id){ 1 }
+  let(:caller_session_id_two){ 2 }
+
+  before do
+    RedisOnHoldCaller.add(campaign_id, caller_session_id)
+  end
   
   it "should add caller" do
-    RedisOnHoldCaller.add(1, 1, DataCentre::Code::TWILIO)
-    expect(RedisOnHoldCaller.longest_waiting_caller(1, DataCentre::Code::TWILIO)).to eq("1")
-    expect(RedisOnHoldCaller.longest_waiting_caller(1, DataCentre::Code::TWILIO)).to eq(nil)
+    expect(RedisOnHoldCaller.longest_waiting_caller(1)).to eq("1")
+    expect(RedisOnHoldCaller.longest_waiting_caller(1)).to eq(nil)
   end
   
   it "should remove caller" do
-    RedisOnHoldCaller.add(1, 1, DataCentre::Code::TWILIO)
-    RedisOnHoldCaller.remove_caller_session(1, 1, DataCentre::Code::TWILIO)
-    expect(RedisOnHoldCaller.longest_waiting_caller(1, DataCentre::Code::TWILIO)).to eq(nil)
+    RedisOnHoldCaller.remove_caller_session(campaign_id, caller_session_id)
+    expect(RedisOnHoldCaller.longest_waiting_caller(1)).to eq(nil)
   end
   
   it "should add to bottom of list" do
-    RedisOnHoldCaller.add(1, 1, DataCentre::Code::TWILIO)
-    RedisOnHoldCaller.add_to_bottom(1, 2, DataCentre::Code::TWILIO)
-    expect(RedisOnHoldCaller.longest_waiting_caller(1, DataCentre::Code::TWILIO)).to eq("2")
+    RedisOnHoldCaller.add_to_bottom(campaign_id, caller_session_id_two)
+    expect(RedisOnHoldCaller.longest_waiting_caller(campaign_id)).to eq(caller_session_id_two.to_s)
   end
 end
