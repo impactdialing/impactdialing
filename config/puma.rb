21740ae2224f1ca1080e9a7ced6deae6645a850d
Jeremiah Heller <jeremiah@impactdialing.com>
2015-09-29 14:24:43 -0700

    debug puma: disconnect on_restart
    first few requests to app after restart raise
    weird exceptions like ActiveModel::MissingAttribute

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+require File.join(File.dirname(File.absolute_path(__FILE__)), '..', 'lib', 'impact_platform', 'mysql')
+
 workers_count = Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 1)
 workers workers_count
@@ -12,16 +14,13 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Valid on Rails up to 4.1 the initializer method of setting `pool` size
   ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = workers_count * threads_count
-    ActiveRecord::Base.establish_connection(config)
-    ActiveRecord::Base.connection_proxy.instance_variable_get(:@shards).each do |k,v|
-      v.clear_reloadable_connections!
-    end
+    ImpactPlatform::MySQL.reconnect!(workers_count * threads_count)
   end
   if defined?(Resque)
      Resque.redis = ENV["REDIS_URL"] || "redis://127.0.0.1:6379"
   end
 end
 
+on_restart do
+  ImpactPlatform::MySQL.disconnect!
+end
