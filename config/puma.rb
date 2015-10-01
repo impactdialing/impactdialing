b19e9dd803d545baaff981e442c0a4d22cc57e25
Jeremiah Heller <jeremiah@impactdialing.com>
2015-10-01 08:07:15 -0700

    stop disconnecting redis on_restart
    - redis-rb 3.1 will reconnect automatically

--- config/puma.rb
+++ config/puma.rb
@@ -12,30 +12,21 @@ port        ENV['PORT']     || 5000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Valid on Rails up to 4.1 the initializer method of setting `pool` size
+  # Valid on Rails up to 4.1 
+  redis_url    = ENV['REDIS_URL']
+  Resque.redis = redis_url
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      :url => redis_url,
+      :namespace => 'resque'
+    }
+  end
+
   ActiveSupport.on_load(:active_record) do
     ImpactPlatform::MySQL.reconnect!(workers_count * threads_count)
-    redis_url = ENV['REDIS_URL']
-    Resque.redis = redis_url
-    Sidekiq.configure_client do |config|
-      config.redis = {
-        :url => redis_url,
-        :namespace => 'resque'
-      }
-    end
   end
 end
 
 on_restart do
-  $redis_call_flow_connection.disconnect
-  $redis_call_end_connection.disconnect
-  $redis_dialer_connection.disconnect
-  $redis_on_hold_connection.disconnect
-  $redis_question_pr_uri_connection.disconnect
-  $redis_phones_ans_uri_connection.disconnect
-  $redis_caller_session_uri_connection.disconnect
-  $redis_call_uri_connection.disconnect
-  Resque.redis.disconnect
-  Sidekiq.redis.disconnect
   ImpactPlatform::MySQL.disconnect!
 end
