97c9f38f56c3e54233faa572f3031aa47f6926d4
Jeremiah Heller <jeremiah@impactdialing.com>
2015-10-14 11:23:58 -0700

    twilio-ruby: refactor

--- app/models/concerns/twilio_logger.rb
+++ app/models/concerns/twilio_logger.rb
@@ -2,9 +2,13 @@ class TwilioLogger
   def self.error(response)
     exception = response['RestException'] || {}
     if exception['Message'] || exception['MoreInfo']
-      msg = "#{exception['Message']} (#{exception['Code']}) (#{exception['Status']}) (#{exception['MoreInfo']})"
-      Rails.logger.error("[TwilioRestException] #{msg}")
+      msg = "Code[#{exception['Code']}] Status[#{exception['Status']}] MoreInfo[#{exception['MoreInfo']}] #{exception['Message']}" 
+      log(msg)
     end
-    Rails.logger.error("[TwilioRestExceptionResponse] #{response}")
+    log("[Response] #{response}")
+  end
+
+  def self.log(message)
+    Rails.logger.error("[TwilioRestException] #{message}")
   end
 end
