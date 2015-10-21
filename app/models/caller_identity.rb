3cf93e2324ac0c14bd52008fdac451fa554f2bc9
Jeremiah Heller <jeremiah@impactdialing.com>
2015-10-20 21:04:02 -0700

    indent

--- app/models/caller_identity.rb
+++ app/models/caller_identity.rb
@@ -1,15 +1,14 @@
 class CallerIdentity < ActiveRecord::Base 
  belongs_to :caller  
  
-def self.create_uniq_pin
-   uniq_pin=nil
-   while !uniq_pin do
-     pins = (0...100).map { |_| rand.to_s[2..8] }.uniq
-     uniq_pin = (pins - (CallerIdentity.where(pin: pins).pluck(:pin) + Caller.where(pin: pins).pluck(:pin)).uniq).first
-   end
-   uniq_pin
- end
- 
+  def self.create_uniq_pin
+    uniq_pin=nil
+    while !uniq_pin do
+      pins = (0...100).map { |_| rand.to_s[2..8] }.uniq
+      uniq_pin = (pins - (CallerIdentity.where(pin: pins).pluck(:pin) + Caller.where(pin: pins).pluck(:pin)).uniq).first
+    end
+    uniq_pin
+  end
 end
 
 # ## Schema Information
