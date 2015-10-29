2f99454a46bf0244c0b297014ab7023f7e023e3c
Jeremiah Heller <jeremiah@impactdialing.com>
2015-10-29 10:15:53 -0700

    moves ui away from enable/disable & to list activity log
    [#63216832]

--- spec/features/customer_admin/voter_lists/add_to_campaign_spec.rb
+++ spec/features/customer_admin/voter_lists/add_to_campaign_spec.rb
@@ -9,7 +9,7 @@ describe 'Add entries to Campaign', js: true, type: :feature, file_uploads: true
   include_context 'voter csv import' do
     let(:csv_file_upload){ cp_tmp('valid_voters_list_redis.csv') }
 
-    def choose_and_upload_list
+    def choose_and_upload_list(option=nil)
       choose_list(csv_file_upload)
       fill_in 'List name', with: 'Munsters cast'
       select 'Phone', from: 'Phone'
@@ -17,8 +17,8 @@ describe 'Add entries to Campaign', js: true, type: :feature, file_uploads: true
       select 'LastName', from: 'LAST'
       select 'Email', from: 'Email'
       #save_and_open_page
-      choose upload_option
-      click_on 'Save & upload'
+      choose option || upload_option
+      click_on 'Upload'
 
       process_pending_import_jobs
       visit edit_client_campaign_path(campaign)
@@ -41,7 +41,15 @@ describe 'Add entries to Campaign', js: true, type: :feature, file_uploads: true
   context 'when "Add to call list" is selected' do
     let(:upload_option){ "Add to call list" }
     it 'adds uploaded entries to the Campaign call list' do
+      # hm, this will probably fail sporadically...
+      datetime = Time.now.in_time_zone(campaign.time_zone).strftime('%b %e, %Y at %l:%M%P')
       choose_and_upload_list
+
+      expect(page).to have_content 'Available to dial 2 100%'
+      expect(page).to have_content 'Not dialed 2 100%'
+      expect(page).to have_content "Munsters cast Added 2 Households and 3 Leads #{datetime}"
+    end
+  end
       expect(page).to have_content 'Available to dial 2 100%'
       expect(page).to have_content 'Not dialed 2 100%'
     end
