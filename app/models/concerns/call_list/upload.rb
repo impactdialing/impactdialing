5cc753a08d9b17725c8303f9c3cd1d776583398d
Jeremiah Heller <jeremiah@impactdialing.com>
2015-10-29 16:53:44 -0700

    adds CallList::Upload to centralize list upload handling
    [#63216832]

---
+++ app/models/concerns/call_list/upload.rb
@@ -0,0 +1,41 @@
+class CallList::Upload
+  attr_reader :parent_instance, :file, :child_instance, :namespace
+
+private
+  def save_to_s3
+    s3path = "#{s3root}#{s3filename}"
+    return s3path if file.nil?
+
+    clean_file = Windozer.to_unix( file.read )
+    AmazonS3.new.write(s3path, clean_file)
+
+    s3path
+  end
+
+  def s3root
+    "#{Rails.env}/uploads/#{namespace}/"
+  end
+
+public
+  def initialize(parent_instance, namespace, upload_params, child_instance_params)
+    new_child_method = "new_#{namespace}"
+    @namespace       = namespace
+    @parent_instance = parent_instance
+    @file            = upload_params[:datafile]
+    @child_instance  = parent_instance.send(new_child_method, child_instance_params)
+  end
+
+  def save
+    @child_instance.s3path             = save_to_s3
+    @child_instance.uploaded_file_name = file.try :original_filename
+  end
+
+  def s3filename
+    @s3filename ||= if child_instance.s3path.blank?
+                      "#{child_instance.name}_#{Time.now.to_i}_#{rand(999)}"
+                    else
+                      child_instance.s3path.gsub(base_path,'')
+                    end
+  end
+end
+
---
+++ spec/models/concerns/call_list/upload_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe CallList::Upload do
+  include_context 'voter csv import'
+  let(:csv_file_upload_data) do
+    File.new(csv_file_upload).read
+  end
+  let(:campaign){ create(:power) }
+  let(:params) do
+    {
+      upload: {
+        datafile: File.new(csv_file_upload)
+      },
+      voter_list: {
+        name: "Bob's your uncle"
+      }
+    }
+  end
+  let(:amazon_s3) do
+    instance_double('AmazonS3')
+  end
+  let(:s3filename){ 'pseudo-random-file-name' }
+  let(:s3path){ "#{Rails.env}/uploads/voter_list/#{s3filename}" }
+  describe '.new(campaign, params)' do
+    subject{ CallList::Upload.new(campaign, :voter_list, params) }
+
+    it 'sets @file = params[:upload][:datafile]' do
+      expect(subject.file).to eq params[:upload][:datafile]
+    end
+    it 'sets @parent_instance = campaign' do
+      expect(subject.parent_instance).to eq campaign
+    end
+    it 'sets @child_instance = parent_instance.send(new_child_method)' do
+      expect(subject.child_instance).to be_kind_of VoterList
+    end
+  end
+
+  describe '#save' do
+    subject{ CallList::Upload.new(campaign, :voter_list, params) }
+
+    before do
+      allow(amazon_s3).to receive(:write).with(s3path, csv_file_upload_data)
+      allow(subject).to receive(:s3filename){ s3filename }
+      allow(Windozer).to receive(:to_unix){ csv_file_upload_data }
+      allow(AmazonS3).to receive(:new){ amazon_s3 }
+    end
+
+    it 'uploads the file to S3' do
+      expect(amazon_s3).to receive(:write).with(s3path, csv_file_upload_data)
+      subject.save
+    end
+    it 'sets s3path on child_instance' do
+      subject.save
+      expect(subject.child_instance.s3path).to eq s3path
+    end
+    it 'sets uploaded_file_name on child_instance' do
+      filename = File.basename csv_file_upload
+      allow(subject.file).to receive(:original_filename){ filename }
+      subject.save
+      expect(subject.child_instance.uploaded_file_name).to eq filename
+    end
+  end
+end
+
