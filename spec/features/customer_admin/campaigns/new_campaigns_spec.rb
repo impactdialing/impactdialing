d4df057051c9c4885ba8e39a3cb50793528f9e16
Jeremiah Heller <jeremiah@impactdialing.com>
2015-11-13 14:15:48 -0800

    save campaign Settings & Messages with same click
    - wraps both tab panels in one form

---
+++ spec/features/customer_admin/campaigns/new_campaigns_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe 'creating new campaigns', js: true do
+  include FakeCallData
+
+  let(:admin){ create(:user) }
+  let(:account){ admin.account }
+  let(:script) do
+    create_campaign_with_script(:preview, account).first
+  end
+  before do
+    create(:recording, account: account)
+    script
+    web_login_as(admin)
+    visit new_client_campaign_path
+  end
+  context 'Power' do
+    it 'accepts General and Message settings in the first save' do
+      fill_in 'Name', with: 'ui-awesome'
+      fill_in 'Caller ID', with: '5555551234'
+      select script.name, from: 'Script'
+      select 'Power', from: 'Dialing mode'
+      select '(GMT-07:00) Arizona', from: 'Time zone'
+      fill_in 'Hours between redials', with: '12'
+
+      click_link 'Messages'
+      check 'Auto-detect whether human or machine answers a call'
+      check 'Callers can click to drop recorded message after a call is answered'
+
+      click_button 'Save'
+
+      expect(page).to have_content 'Campaign saved'
+      visit edit_client_campaign_path(Campaign.last)
+
+      expect(page).to have_selector "input[value='ui-awesome']"
+      expect(page).to have_selector "input[value='5555551234']"
+
+      click_link 'Messages'
+      within('label[for="campaign_answering_machine_detect"]') do
+        expect(page).to have_selector 'input[checked="checked"]'
+      end
+      within('label[for="campaign_caller_can_drop_message_manually"]') do
+        expect(page).to have_selector 'input[checked="checked"]'
+      end
+    end
+  end
+end
