88f25e599753ea625834d26085a617f4f8daff28
Jeremiah Heller <jeremiah@impactdialing.com>
2015-11-25 08:22:38 -0800

    Windows, readyfiles & parallel oh my

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -12,14 +12,14 @@ Sauce.config do |config|
   config[:start_tunnel]            = ENV['USE_SAUCE']
   config[:start_local_application] = ENV['USE_SAUCE']
   config[:browsers]                = [
-    ['Windows 8','Internet Explorer','10'],
-    ['Windows 8.1','Internet Explorer','11'],
-    ['Windows 7','Internet Explorer','9'],
+    #['Windows 8','Internet Explorer','10'],
+    #['Windows 8.1','Internet Explorer','11'],
+    #['Windows 7','Internet Explorer','9'],
     #['Windows 7','Internet Explorer','10'],
     #['Windows 7','Internet Explorer','11'],
-    ['Windows 10','Internet Explorer','11'],
+    #['Windows 10','Internet Explorer','11'],
     #['Windows 8','Firefox','40'],
-    #['Windows 8','Chrome','46'],
+    ['Windows 8','Chrome','46'],
     #['Linux','Chrome','46'],
     #['Linux','Firefox','40'],
     #['OS X 10.10','Firefox','40'],
