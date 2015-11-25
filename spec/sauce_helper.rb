dced4af20afa4167476ed8716076a0a3943d9ed9
Jeremiah Heller <jeremiah@impactdialing.com>
2015-11-24 16:41:17 -0800

    update sauce config

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -12,21 +12,21 @@ Sauce.config do |config|
   config[:start_tunnel]            = ENV['USE_SAUCE']
   config[:start_local_application] = ENV['USE_SAUCE']
   config[:browsers]                = [
-    #['Windows 8','InternetExplorer','10.0'],
-    #['Windows 8.1','InternetExplorer','11.0'],
-    #['Windows 7','InternetExplorer','9.0'],
-    #['Windows 7','InternetExplorer','10.0'],
-    #['Windows 7','InternetExplorer','11.0'],
-    #['Windows 10','InternetExplorer','11.0'],
+    ['Windows 8','Internet Explorer','10'],
+    ['Windows 8.1','Internet Explorer','11'],
+    ['Windows 7','Internet Explorer','9'],
+    #['Windows 7','Internet Explorer','10'],
+    #['Windows 7','Internet Explorer','11'],
+    ['Windows 10','Internet Explorer','11'],
     #['Windows 8','Firefox','40'],
-    ['Windows 8','Chrome','46'],
+    #['Windows 8','Chrome','46'],
     #['Linux','Chrome','46'],
     #['Linux','Firefox','40'],
     #['OS X 10.10','Firefox','40'],
     #['OS X 10.10','Chrome','46'],
-    #['OS X 10.9','Safari','7.0'],
-    #['OS X 10.10','Safari','8.0'],
-    #['OS X 10.11','Safari','9.0']
+    #['OS X 10.9','Safari','7'],
+    #['OS X 10.10','Safari','8'],
+    #['OS X 10.11','Safari','9']
   ]
 end
 
