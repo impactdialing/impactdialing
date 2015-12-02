f24fe2a5b78e2da9c36698956da8b59195b2e75e
Lisa Folliard <lisaf37@gmail.com>
2015-12-01 17:38:06 -0800

    whitespace

--- config/initializers/notifications/call_flow/phones_only/scripts.rb
+++ config/initializers/notifications/call_flow/phones_only/scripts.rb
@@ -1,8 +1,8 @@
-ActiveSupport::Notifications.subscribe('scripts.saved') do |name, start, finish, id, payload|  
-  script         = payload[:script]
+ActiveSupport::Notifications.subscribe('scripts.saved') do |name, start, finish, id, payload|
+  script = payload[:script]
 
   if script.created_at != script.updated_at
     # created_at == updated_at => script was just created
     CachePhonesOnlyScriptQuestions.add_to_queue(script.id, 'update')
   end
-end 
+end
