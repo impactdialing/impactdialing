cf145f7856337ae6a92032156cf609f848036556
Lisa Folliard <lisaf37@gmail.com>
2015-12-02 17:41:03 -0800

    add UUID support to TokenGenerator

--- app/models/concerns/token_generator.rb
+++ app/models/concerns/token_generator.rb
@@ -2,4 +2,9 @@ module TokenGenerator
   def self.sha_hexdigest(*args)
     return Digest::SHA1.hexdigest(args.flatten.join('--'))
   end
+
+  def self.uuid
+    uuid = UUID.new
+    uuid.generate
+  end
 end
