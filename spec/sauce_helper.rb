7fc995b06d66ce1da25f40447d22d44e420567f1
Jeremiah Heller <jeremiah@impactdialing.com>
2016-01-04 08:47:24 -0800

    support configuring browser under test via USE_SAUCE env var

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -8,14 +8,14 @@ Sauce.config do |config|
   config[:start_tunnel]               = (not ENV['CIRCLE_SHA1'])
   config[:sauce_connect_4_executable] = "#{ENV['HOME']}/sc-bin/bin/sc"
 
-  config[:browsers]                = [
+  #config[:browsers]                = [
     #['Windows 8','Internet Explorer','10'],
-    ['Windows 8.1','Internet Explorer','11'],
-    ['Windows 7','Internet Explorer','9'],
+    #['Windows 8.1','Internet Explorer','11'],
+    #['Windows 7','Internet Explorer','9'],
     #['Windows 7','Internet Explorer','10'],
     #['Windows 7','Internet Explorer','11'],
-    ['Windows 10','Internet Explorer','11'],
-    ['Windows 8','Firefox','40'],
+    #['Windows 10','Internet Explorer','11'],
+    #['Windows 8','Firefox','40'],
     #['Windows 8','Chrome','46'],
     #['Linux','Chrome','46'],
     #['Linux','Firefox','40'],
@@ -24,7 +24,20 @@ Sauce.config do |config|
     #['OS X 10.9','Safari','7'],
     #['OS X 10.10','Safari','8'],
     #['OS X 10.11','Safari','9']
-  ]
+  #]
+  browsers = {
+    'ie11' => ['Windows 8.1', 'Internet Explorer', '11'],
+    'ie10' => ['Windows 7', 'Internet Explorer', '10'],
+    'ie9' => ['Windows 7', 'Internet Explorer', '9'],
+    'ch' => ['Windows 7', 'Chrome', nil],
+    'ff' => ['Windows 10', 'Firefox', 40],
+    'sa7' => ['OS X 10.9', 'Safari', '7'],
+    'sa9' => ['OS X 10.10', 'Safari', '9'],
+    'lch' => ['Linux', 'Chrome', nil],
+    'lff' => ['Linux', 'Firefox', 40]
+  }
+  browser = browsers[ENV['USE_SAUCE']]
+  raise "Unknown browser: #{ENV['USE_SAUCE']}, test run aborted." if browser.nil?
 end
 
 Capybara.default_driver = :sauce
