59f64a03ba32321c89f23340633a49890b2e8dee
Jeremiah Heller <jeremiah@impactdialing.com>
2016-01-12 18:11:14 -0800

    set secret_key_base to enable rails cookie encrypt
    [completes #88720572]

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ImpactDialing::Application.config.secret_token = 'c2d0cbae6d52b383b414231e2904fc33e38e45e28ade747443efd307fae235368bb45365fb1fbd8530bf47a798b325d3dd61537ed811911106a1701828aff7cf'
+ImpactDialing::Application.config.secret_key_base = '9aa4a3cb8405829c88725bed32214586744f1e874ea661c80420b6ea798d6bb35dd8cd026a13aa33cc024a810d49261a55f78f6ec94c3fa9a863a12d28cbdb47'
