aad070de6e4568e551cad4ed84ccb6fbcf0be599
Jeremiah Heller <jeremiah@impactdialing.com>
2016-02-05 13:37:58 -0800

    test Billing::SubscriptionManager callbacks

--- spec/models/billing/subscription_manager_spec.rb
+++ spec/models/billing/subscription_manager_spec.rb
@@ -5,6 +5,10 @@ describe Billing::SubscriptionManager, :type => :model do
     Billing::SubscriptionManager.new(customer_id, subscription, quota)
   end
 
+  let(:provider_object) do
+    double('StripeObject')
+  end
+
   describe '#prorate?(new_plan, callers_allowed)' do
     let(:customer_id){ 'cus_243fij0wije' }
     let(:old_plan){ 'trial' }
@@ -30,9 +34,9 @@ describe Billing::SubscriptionManager, :type => :model do
     end
     let(:payment_gateway) do
       double('PaymentGatewayFake', {
-        update_subscription: double('StripeSubscription'),
-        create_charge: double('StripeCharge'),
-        create_and_pay_invoice: double('StripeInvoice'),
+        update_subscription: provider_object,
+        create_charge: provider_object,
+        create_and_pay_invoice: provider_object,
         cancel_subscription: nil
       })
     end
@@ -102,6 +106,7 @@ describe Billing::SubscriptionManager, :type => :model do
     let(:old_plan){ 'trial' }
     let(:new_plan){ 'basic' }
     let(:callers_allowed){ 1 }
+    let(:prorate){ false }
     let(:opts) do
       {
         callers_allowed: callers_allowed
@@ -122,16 +127,14 @@ describe Billing::SubscriptionManager, :type => :model do
     end
     let(:payment_gateway) do
       double('PaymentGatewayFake', {
-        update_subscription: double('StripeSubscription'),
-        create_charge: double('StripeCharge'),
-        create_and_pay_invoice: double('StripeInvoice'),
+        update_subscription: provider_object,
+        create_charge: provider_object,
+        create_and_pay_invoice: provider_object,
         cancel_subscription: nil
       })
     end
     let(:plans) do
-      double('Billing::Plans', {
-        validate_transition!: true
-      })
+      Billing::Plans.new
     end
     let(:manager) do
       Billing::SubscriptionManager.new(customer_id, subscription, quota)
@@ -145,6 +148,29 @@ describe Billing::SubscriptionManager, :type => :model do
       allow(Billing::Plans).to receive(:new){ plans }
     end
 
+    shared_examples_for 'all update!s resulting in provider_object' do
+      let(:expected_opts) do
+        opts.merge({
+          prorate: prorate,
+          old_plan_id: subscription.plan
+        })
+      end
+      before do
+        allow(payment_gateway).to receive(:update_recurring_subscription!){ nil }
+        allow(payment_gateway).to receive(:create_charge){ provider_object }
+      end
+      it 'yields provider_obj to callbacks' do
+        expect{ |b|
+          manager.update!('per_minute', opts, &b)
+        }.to yield_with_args(provider_object, Hash)
+      end
+      it 'yields updated options hash to callbacks' do
+        expect{ |b|
+          manager.update!('per_minute', opts, &b)
+        }.to yield_with_args(anything, expected_opts)
+      end
+    end
+
     it 'tells `plans` to validate the transition' do
       expect(plans).to receive(:validate_transition!).with(old_plan, new_plan, quota.minutes_available?, opts)
       manager.update!(new_plan, opts)
@@ -155,6 +181,7 @@ describe Billing::SubscriptionManager, :type => :model do
         manager.update!(new_plan, opts)
       end
       context 'old_plan is not trial AND' do
+        let(:prorate){ true }
         context 'this is an upgrade in plans OR' do
           let(:new_plan){ 'pro' }
           before do
@@ -164,13 +191,14 @@ describe Billing::SubscriptionManager, :type => :model do
             allow(subscription).to receive(:plan){ 'basic' }
           end
           it 'tells `payment_gateway` to prorate upgrades' do
-            expect(payment_gateway).to receive(:update_subscription).with(new_plan, callers_allowed, true)
+            expect(payment_gateway).to receive(:update_subscription).with(new_plan, callers_allowed, prorate)
             manager.update!(new_plan, opts)
           end
           it 'tells `payment_gateway` to create and pay an invoice' do
             expect(payment_gateway).to receive(:create_and_pay_invoice)
             manager.update!(new_plan, opts)
           end
+          it_behaves_like 'all update!s resulting in provider_object'
         end
 
         context 'plan is the same w/ addition of callers' do
@@ -185,16 +213,18 @@ describe Billing::SubscriptionManager, :type => :model do
             allow(subscription).to receive(:plan){ 'pro' }
           end
           it 'tells `payment_gateway` to prorate increase in callers' do
-            expect(payment_gateway).to receive(:update_subscription).with('pro', callers_allowed + 1, true)
+            expect(payment_gateway).to receive(:update_subscription).with('pro', callers_allowed + 1, prorate)
             manager.update!('pro', opts)
           end
           it 'tells `payment_gateway` to create and pay an invoice' do
             expect(payment_gateway).to receive(:create_and_pay_invoice)
             manager.update!('pro', opts)
           end
+          it_behaves_like 'all update!s resulting in provider_object'
         end
       end
       context 'old_plan is trial OR this is a downgrade in plans' do
+        let(:prorate){ false }
         before do
           allow(plans).to receive(:is_trial?){ true }
           allow(plans).to receive(:is_upgrade?){ false }
@@ -202,7 +232,7 @@ describe Billing::SubscriptionManager, :type => :model do
           allow(subscription).to receive(:plan){ 'trial' }
         end
         it 'tells `payment_gateway` to NOT prorate the subscription change (old_plan is a trial)' do
-          expect(payment_gateway).to receive(:update_subscription).with(new_plan, callers_allowed, false)
+          expect(payment_gateway).to receive(:update_subscription).with(new_plan, callers_allowed, prorate)
           manager.update!(new_plan, opts)
         end
 
@@ -221,19 +251,28 @@ describe Billing::SubscriptionManager, :type => :model do
     end
     context 'plans.recurring? => false' do
       let(:valid_amount_paid){ 12 }
+      let(:opts) do
+        {
+          amount_paid: 20,
+          autorecharge: 0,
+        }
+      end
+      let(:prorate){ false }
       before do
         allow(plans).to receive(:recurring?){ false }
+        allow(subscription).to receive(:plan){ 'trial' }
       end
       it 'tells `payment_gateway` to create a charge' do
-        allow(subscription).to receive(:plan){ 'trial' }
         expect(payment_gateway).to receive(:create_charge).with(valid_amount_paid, nil)
         manager.update!('per_minute', {amount_paid: valid_amount_paid})
       end
       it 'passes an autorecharge boolean flag as 2nd arg to `payment_gateway.create_charge`' do
-        allow(subscription).to receive(:plan){ 'trial' }
         expect(payment_gateway).to receive(:create_charge).with(valid_amount_paid, 1)
         opts = {amount_paid: valid_amount_paid, autorecharge: 1}
         manager.update!('per_minute', opts)
+      end
+      context 'provider_object is present' do
+        it_behaves_like 'all update!s resulting in provider_object'
       end
     end
   end
