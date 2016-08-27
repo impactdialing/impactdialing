260dbfa2144cf2746b3159d8a9f758d3a700255a
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2016-08-26 17:19:27 -0700

    run calculate dials job every 2 secs instead of 3

--- lib/scheduler/predictive.rb
+++ lib/scheduler/predictive.rb
@@ -18,7 +18,7 @@ class Scheduler::Predictive < Celluloid::SupervisionGroup
   autoload :Simulator, 'scheduler/predictive/simulator'
 
   supervise Scheduler::Predictive::Simulator, as: :simulator, args: [30]
-  supervise Scheduler::Predictive::Dialer, as: :dialer, args: [3]
+  supervise Scheduler::Predictive::Dialer, as: :dialer, args: [2]
 
   def self.run!
     Logger.info "Scheduler::Predictive booting..."
