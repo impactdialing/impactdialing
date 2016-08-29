528954c4951bf48225e6f3950f07eb47ac8a7d6a
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2016-08-29 10:30:08 -0700

    add pusher host to config for global deployment

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,9 +3,11 @@ require "pusher"
 PUSHER_APP_ID = ENV['PUSHER_APP_ID']
 PUSHER_KEY    = ENV['PUSHER_KEY']
 PUSHER_SECRET = ENV['PUSHER_SECRET']
+PUSHER_HOST   = ENV['PUSHER_HOST'] || 'api-mt1.pusher.com' # mt1 is US East
 Pusher.app_id = PUSHER_APP_ID
 Pusher.key    = PUSHER_KEY
 Pusher.secret = PUSHER_SECRET
+Pusher.host   = PUSHER_HOST
 
 if Rails.env == 'test'
   module Pusher
