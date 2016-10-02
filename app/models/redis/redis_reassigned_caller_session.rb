d0ab0d6fbdcaee8b304765cf337d8f6050ed40cb
Nikhil <fernandes.nikhil@gmail.com>
2016-10-02 20:37:17 +0530

    Redis connection pool: Change direct use of variable

--- app/models/redis/redis_reassigned_caller_session.rb
+++ app/models/redis/redis_reassigned_caller_session.rb
@@ -1,15 +1,22 @@
 class RedisReassignedCallerSession
 
+  def redis_connection_pool
+    $redis_caller_session_uri_connection
+  end
+
   def self.set_campaign_id(caller_session_id, campaign_id)
-    $redis_caller_session_uri_connection.set "reassigned_:#{caller_session_id}", campaign_id
+    redis_connection_pool.with{|conn| conn.set "reassigned_:#{caller_session_id}", campaign_id}
+    # $redis_caller_session_uri_connection.set "reassigned_:#{caller_session_id}", campaign_id
   end
 
   def self.campaign_id(caller_session_id)
-    $redis_caller_session_uri_connection.get "reassigned_:#{caller_session_id}"
+    redis_connection_pool.with{|conn| conn.get "reassigned_:#{caller_session_id}"}
+    # $redis_caller_session_uri_connection.get "reassigned_:#{caller_session_id}"
   end
 
   def self.delete(caller_session_id)
-    $redis_caller_session_uri_connection.del "reassigned_:#{caller_session_id}"
+    redis_connection_pool.with{|conn| conn.del "reassigned_:#{caller_session_id}"}
+    # $redis_caller_session_uri_connection.del "reassigned_:#{caller_session_id}"
   end
 
 end
