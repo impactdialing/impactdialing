fe094cfee903ca7292ff7f42bb131664ca341817
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2016-10-03 16:18:38 -0700

    get specs green (or mostly green)

--- spec/support/time_helpers.rb
+++ spec/support/time_helpers.rb
@@ -1,11 +1,8 @@
 module TimeHelpers
   def make_it_outside_calling_hours(campaign)
-    Timecop.travel(Time.new(2015, 1, 1, 9).utc) do
-      campaign.update_attributes({
-        start_time: Time.now.utc - 2.hour,
-        end_time: Time.now.utc - 1.hour
-      })
-    end
-    return campaign
+    campaign.update({
+      start_time: Time.now.utc - 2.hour,
+      end_time: Time.now.utc - 1.hour
+    })
   end
 end
