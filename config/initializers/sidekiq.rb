3274b4ec6abf2b520cb97d90000e74ae0124fe3a
Nikhil <fernandes.nikhil@gmail.com>
2016-10-07 14:49:43 +0530

    Redis connection pool: move pool size to env variables

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,11 +5,13 @@ STDOUT.puts "Sidekiq is connecting to #{url}. REDIS_URL is #{ENV['REDIS_URL']}"
 Rails.application.config.after_initialize do
   ActiveSupport.on_load(:active_record) do
     require 'librato_sidekiq/server'
+    sidekq_server_pool_size = Integer(ENV['SIDEKIQ_SERVER_REDIS_POOL_SIZE'] || 30)
+    sidekq_client_pool_size = Integer(ENV['SIDEKIQ_CLIENT_REDIS_POOL_SIZE'] || 10)
 
     redis_conn = proc { Redis.new(network_timeout: 3, namespace:"resque", url: url)}
 
     Sidekiq.configure_server do |config|
-      config.redis = ConnectionPool.new(size: 30, &redis_conn)
+      config.redis = ConnectionPool.new(size: sidekq_server_pool_size, &redis_conn)
 
       require 'impact_platform/mysql'
       min_pool_size = Sidekiq.options[:concurrency]
@@ -22,7 +24,7 @@ Rails.application.config.after_initialize do
     end
 
     Sidekiq.configure_client do |config|
-      config.redis = ConnectionPool.new(size: 5, &redis_conn)
+      config.redis = ConnectionPool.new(size: sidekq_client_pool_size, &redis_conn)
     end
   end
 end
