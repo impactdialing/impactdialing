5094cbf1abf0b094051dd8a25e947bbfb3e93a1c
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2016-10-07 12:27:18 -0700

    run phantom caller job every 1 min

--- jobs/phantom_caller_job.rb
+++ jobs/phantom_caller_job.rb
@@ -26,7 +26,7 @@ class PhantomCallerJob
 
   def self.perform
     t = TwilioLib.new(TWILIO_ACCOUNT,TWILIO_AUTH)
-    CallerSession.on_call.where("updated_at < ? ", 5.minutes.ago).each do |cs|
+    CallerSession.on_call.where("updated_at < ? ", 1.minute.ago).each do |cs|
       if cs.sid.starts_with?("CA")
         call_response = Hash.from_xml(t.call("GET", "Calls/" + cs.sid, {}))['TwilioResponse']['Call']
         cs.end_running_call if call_response.try(:[],"Status") == 'completed'
@@ -37,6 +37,6 @@ class PhantomCallerJob
       caller_session.end_running_call
       RedisCallerSession.remove_phantom_caller(cs)
     end
-    CallerSession.on_call.where("updated_at < ? and endtime is not null", 5.minutes.ago).each {|x| x.end_running_call}
+    CallerSession.on_call.where("updated_at < ? and endtime is not null", 1.minute.ago).each {|x| x.end_running_call}
   end
 end
