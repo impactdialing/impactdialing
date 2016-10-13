99f32406ba9a82e831f61cd20f6c687ed1645b90
Nikhil <fernandes.nikhil@gmail.com>
2016-10-13 16:12:48 +0530

    Reports issue: not dialed calls in reports download

--- app/models/caller_campaign_report_strategy.rb
+++ app/models/caller_campaign_report_strategy.rb
@@ -258,7 +258,7 @@ public
   end
 
   def household_query
-    Household.where(campaign_id: @campaign.id).includes(:call_attempts, :voters)
+    Household.where(campaign_id: @campaign.id).where("status NOT IN (?)", [CallAttempt::Status::RINGING,CallAttempt::Status::READY,"not called"]).includes(:call_attempts, :voters)
   end
 
   def download_all_voters_lead
