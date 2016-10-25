64aa35304b34ca4991af618d9e320e9a8b6072ca
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2016-10-24 23:57:12 -0700

    stop restarting on app health warning (#75)
    under the hypothesis that this the cause of exploding db connections over the last 2 days

--- lib/app_health/monitor/predictive_dial_rate.rb
+++ lib/app_health/monitor/predictive_dial_rate.rb
@@ -54,12 +54,6 @@ module AppHealth
       def alert_if_not_ok
         unless ok?
           AppHealth::Alarm.trigger!(alarm_key, alarm_description, alarm_details)
-
-          #fixme most of the time, when the predictive dialer stops, restarting the PS app fixes it.
-          # no idea why, but it seems to work, so might as well automate it.
-          heroku = PlatformAPI.connect_oauth(ENV['HEROKU_AUTOSCALE_OAUTH_TOKEN']) #fixme so lazy, re-using the autoscale token
-          heroku.dyno.restart_all(ENV['HEROKU_APP_NAME'])
-
           return false
         end
         return true
