01b43898602c5af60a0661d4993c47c558fadc01
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2016-11-16 11:10:17 -0500

    add import queue to hirefire

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,9 +1,9 @@
 HireFire::Resource.configure do |config|
   auto_scaling_queues = [
     :reports, :general, :dial_queue, :billing,
-    :dialer_worker, :simulator_worker
+    :dialer_worker, :simulator_worker, :import
   ]
-  
+
   auto_scaling_queues.each do |queue_name|
     config.dyno(queue_name) do
       HireFire::Macro::Resque.queue(queue_name)
